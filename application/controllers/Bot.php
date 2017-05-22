b54c8949b2f4be1b3650901cd250f28425254565
shervne <shervin@lazymeal.com>
2017-05-22 13:15:43 -0700

    Updated api.ai webhook

--- application/controllers/Bot.php
+++ application/controllers/Bot.php
@@ -10,30 +10,27 @@ class Bot extends CI_Controller {
 		
 	}
 	
+	function fetch_intent($apiai_id){
+		print_r($this->Apiai_model->fetch_intent($apiai_id));
+	}
+	
+	
 	function apiai_webhook(){
 		//This function receives incoming requests from api.ai, logs and processes them:
 		$new = $this->Engagement_model->log_engagement(array(
 				'us_id' => 651, //api.ai API User
 				'node_type_id' => 653, //Bot API Webhook Log
-				'blob' => json_encode($_POST), //Dump all incoming variables
+				'blob' => json_encode(objectToArray($_POST)), //Dump all incoming variables
 		));
 		
 		//Return success message to bot:
 		header('Content-Type: application/json');
-		header("HTTP/1.1 ".( $new['id'] ? "200 OK" : "400" ));
 		echo json_encode(array(
-				'status' 	=> ( $new['id'] ? 1 : 0 ),
-				'message' 	=> ( $new['id'] ? 'Webhook #'.$new['id'].' logged.' : 'Error while logging webhook.'),
+				'status' => array(
+						'code' => ( $new['id'] ? 200 : 206 ),
+						'errorType' => ( $new['id'] ? 'success' : 'unknown_error'),
+						'errorDetails' => ( $new['id'] ? 'Webhook #'.$new['id'].' logged.' : 'Error while logging webhook.'),
+				)
 		));
 	}
-
-	function test($node_id){
-		print_r($this->Apiai_model->sync_entity($node_id));
-	}
-	
-	function in($node_id){
-		print_r($this->Apiai_model->fetch_intent($node_id));
-	}
-	
-
 }
