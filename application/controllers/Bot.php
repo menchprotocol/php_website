36d93894b36aae234cc42e4b558640603e8c5787
shervne <shervin@lazymeal.com>
2017-07-13 18:39:54 -0700

    Fix

--- application/controllers/Bot.php
+++ application/controllers/Bot.php
@@ -330,9 +330,10 @@ class Bot extends CI_Controller {
 					//Test logging 2:
 					$errr = $this->db->_error_message();
 					$last_q = $this->db->last_query();
+					
 					$this->Us_model->log_engagement(array(
 							'action_pid' => 777, //New Optin
-							'json_blob' => 'Last Query: '.$last_q.' ||| ERROR: '.print_r($errr,true).' ||| '.json_encode($eng_data),
+							'json_blob' => 'Last Query: '.$last_q.' ||| ERROR: '.$errr.' ||| '.json_encode($eng_data),
 							'us_id' => 777,
 							'platform_pid' => 777, //The facebook page
 					));
