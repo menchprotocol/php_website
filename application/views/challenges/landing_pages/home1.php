4115420c0da30ff519ba84eb0c431323084b1e75
shervne <shervin@lazymeal.com>
2017-08-09 17:27:10 -0700

    Home page mobile tweaks

--- application/views/challenges/landing_pages/home1.php
+++ application/views/challenges/landing_pages/home1.php
@@ -1,12 +1,13 @@
 	<div class="page-header header-filter" data-parallax="true" style="background-image: url('/img/bg.jpg');">
         <div class="container">
             <div class="row">
-				<div class="col-md-6">
+				<div class="col-sm-9">
 					<h1>Make it Happen. Now.</h1>
                     <h4>Mench is a marketplace for 2-12 week result-driven challenges lead by experienced entrepreneurs.</h4>
                     <br />
                     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg"><i class="fa fa-search"></i> &nbsp;&nbsp; Browse Challenges</a>
 				</div>
+				<div class="col-sm-3">&nbsp;</div>
             </div>
         </div>
     </div>
