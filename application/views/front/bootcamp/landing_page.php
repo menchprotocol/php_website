76f06e44c12f896a58f163ddf47734afebdbf9d9
sherv <sherv@DESKTOP-93NCHP5>
2017-10-03 23:00:15 -0700

    Fixes

--- application/views/front/bootcamp/landing_page.php
+++ application/views/front/bootcamp/landing_page.php
@@ -82,8 +82,10 @@ if(!isset($bootcamp['c__cohorts'][0])){
                 foreach($sprints as $sprint){
                     echo '<div>';
                         echo '<h4>Week #'.$sprint['cr_outbound_rank'].': '.$sprint['c_objective'].'</h4>';
-                        echo '<p class="curriculum_p">'.nl2br(trim($sprint['c_todo_overview'])).'</p>';
+                        echo '<p id="week_'.$sprint['cr_outbound_rank'].'">'.$sprint['c_todo_overview'].'</p>';
                     echo '</div>';
+                    //JS Showdown update:
+                    echo '<script> update_showdown($("#week_'.$sprint['cr_outbound_rank'].'"),$("#week_'.$sprint['cr_outbound_rank'].'").html()); </script>';
                 }
                 ?>
               </div>
@@ -217,7 +219,7 @@ if(!isset($bootcamp['c__cohorts'][0])){
 
 <script>
 $( document ).ready(function() {
-    var showdowns = ["#c_additional_goals","#c_prerequisites","#c_todo_overview","#u_tangible_experience","#u_bio",".curriculum_p"];
+    var showdowns = ["#c_additional_goals","#c_prerequisites","#c_todo_overview","#u_tangible_experience","#u_bio"];
     var arrayLength = showdowns.length;
     for (var i = 0; i < arrayLength; i++) {
     	update_showdown($(showdowns[i]),$(showdowns[i]).html());
