1aeeddbd97fae39096b30f48dc079ca7095e26c1
sherv <sherv@DESKTOP-93NCHP5>
2017-10-18 23:42:13 -0700

    NEw test

--- application/views/front/student/assignments.php
+++ application/views/front/student/assignments.php
@@ -1,51 +1,24 @@
 assignments
 
+
 <script>
-
-$( document ).ready(function() {
-	$('#me2').html('Check');
-
-
-	//Facebook Kit
-	(function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	     /*
-	     MessengerExtensions.getUserID(function success(uids) {
-       	// User ID was successfully obtained. 
-           	var psid = uids.psid;
-       		$('#me').html(psid);
-         }, function error(err, errorMessage) {      
-       	// Error handling code
-         	$('#me').html(err+':'+errorMessage);
-         });
-		*/
-         
-	}(document, 'script', 'facebook-jssdk'));
-
-	$('#me2').html('success1');
-
-	
-	
-    MessengerExtensions.getContext('1782431902047009', 
-	  function success(result){
-   	 $('#me2').html('success2');
-   	 $('#me').html(result.psid);
-	  },
-	  function error(result){
-		  $('#me2').html('fail1');
-		  $('#me').html(result);
-	  }
-	);
-    	
-});
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+</script>      
 
 
-    
+<script>
+  window.extAsyncInit = function() {
+    // the Messenger Extensions JS SDK is done loading 
+	    alert('done loading');
+  };
 </script>
+
 
 This is it:
 <div id="me"></div>
