157406dc18e59ee7c67bb3e53adc56f9a1d2c1e6
sherv <sherv@DESKTOP-93NCHP5>
2017-10-18 23:23:37 -0700

    Facebook User ID

--- application/views/cockpit/engagements.php
+++ application/views/cockpit/engagements.php
@@ -1,3 +1,9 @@
+<script>
+function show_log(e_json,e_id){
+	console.log(e_json);
+	$('#icon_'+e_id).css('color','#00CC00');
+}
+</script>
 
 <style>
 table, tr, td, th { text-align:left !important; font-size:14px; cursor:default !important; line-height:120% !important; }
@@ -14,6 +20,7 @@ td { padding:5px 0 !important; }
 		<!-- <th style="width:50px;">Data</th>  -->
 		<th style="width:110px;">Applied To</th>
 		<th style="width:140px;">Bootcamp <i class="fa fa-info-circle" aria-hidden="true" data-toggle="tooltip" data-placement="left" title="If Set, this means that the bootcamp team can also view this engagement in their Activity Timeline"></i></th>
+		<th style="width:30px; text-align:center !important;">&nbsp;</th>
 	</tr>
 </thead>
 <tbody>
@@ -34,7 +41,8 @@ foreach($engagements as $e){
             }
         echo '</td>';
         echo '<td>'.( $e['e_b_id']>0 ? '<a href="/console/'.$e['b_id'].'">'.$e['c_objective'].'</a>' : '' ).'</td>';
-    echo '</tr>';
+        echo '<td style="text-align:center !important;">'.( strlen($e['e_json'])>0 ? '<script> e_json_'.$e['e_id'].' = '.$e['e_json'].'; </script><a href="javascript:show_log(e_json_'.$e['e_id'].','.$e['e_id'].');" data-toggle="tooltip" title="Load JSON data in the browser console to analyze further" aria-hidden="true" data-placement="left"><i class="fa fa-search-plus" id="icon_'.$e['e_id'].'" aria-hidden="true"></i></a>' : '' ).'</td>';
+        echo '</tr>';
 }
 ?>
 </tbody>
--- application/views/front/student/assignments.php
+++ application/views/front/student/assignments.php
@@ -1,1 +1,13 @@
 assignments
+
+<script>
+    MessengerExtensions.getUserID(function success(uids) {
+  	// User ID was successfully obtained. 
+      	var psid = uids.psid;
+  		$('#me').html(psid);
+    }, function error(err, errorMessage) {      
+  	// Error handling code
+    });    
+</script>
+
+<div id="me"></div>
