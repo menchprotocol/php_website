62f0282a5f3070f71e410bc53a9e9ba647a00587
sherv <sherv@DESKTOP-93NCHP5>
2017-10-18 23:30:23 -0700

    Fixes

--- application/views/front/student/assignments.php
+++ application/views/front/student/assignments.php
@@ -1,15 +1,40 @@
 assignments
 
 <script>
-    MessengerExtensions.getUserID(function success(uids) {
-  	// User ID was successfully obtained. 
-      	var psid = uids.psid;
-  		$('#me').html(psid);
-    }, function error(err, errorMessage) {      
-  	// Error handling code
-    	$('#me').html(err+':'+errorMessage);
-    });    
+
+$( document ).ready(function() {
+	$('#me2').html('Check');
+
+
+	//Facebook Kit
+	(function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+
+
+	     MessengerExtensions.getUserID(function success(uids) {
+       	// User ID was successfully obtained. 
+           	var psid = uids.psid;
+       		$('#me').html(psid);
+         }, function error(err, errorMessage) {      
+       	// Error handling code
+         	$('#me').html(err+':'+errorMessage);
+         });
+
+         
+	}(document, 'script', 'facebook-jssdk'));
+
+	
+    	
+});
+
+
+    
 </script>
 
 This is it:
 <div id="me"></div>
+<div id="me2"></div>
