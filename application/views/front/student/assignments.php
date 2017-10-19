59e043434addae04bd5247642575c1050f362e35
sherv <sherv@DESKTOP-93NCHP5>
2017-10-19 00:03:02 -0700

    More updates

--- application/views/front/student/assignments.php
+++ application/views/front/student/assignments.php
@@ -6,47 +6,29 @@
   js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'Messenger'));
-</script>      
 
+//the Messenger Extensions JS SDK is done loading:
+window.extAsyncInit = function() {
+	//Get User ID:
+    MessengerExtensions.getUserID(function success(uids) {
+    	// User ID was successfully obtained.
+      	var psid = uids.psid;
+      	$('#"page_content"').html(psid);
+    }, function error(err, errorMessage) {
+    	$('#"page_content"').html('Unknown error while loading content.');
+    });
 
-<script>
-  window.extAsyncInit = function() {
+};
 
-	  MessengerExtensions.getContext('1782431902047009', 
-    	  function success(result){
-      		  $('#me3').html(result.psid);
-    	  },
-    	  function error(result){
-    		  $('#me3').html('Fail3');
-    	  }
-    	);
-
-
-		
-    // the Messenger Extensions JS SDK is done loading 
-	  MessengerExtensions.getUserID(function success(uids) {
-	  	// User ID was successfully obtained. 
-	      	var psid = uids.psid;
-	      	$('#me2').html(psid);
-	    }, function error(err, errorMessage) {      
-	  	// Error handling code
-	    	$('#me2').html('Fail');
-	    });    
-  };
-</script>
-
-
-<script>
-
+//Optionally you can close webview like this:
 function close_webview(){
 	window.location = 'https://www.messenger.com/closeWindow/?display_text=Closing....';
 }
 </script>
 
-<a href="javascript:close_webview();">close_webview</a>
-<br /><br />
 
-This is it:
-<div id="me"></div>
-<div id="me2"></div>
-<div id="me3"></div>
+<h1>Assignments</h1>
+<h2>Assignments</h2>
+<h3>Assignments</h3>
+
+<div id="page_content"></div>
