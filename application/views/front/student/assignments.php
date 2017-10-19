e99a64fe232a3734ade3ffa6c861d7fd72c4357b
sherv <sherv@DESKTOP-93NCHP5>
2017-10-18 23:45:05 -0700

    Closer

--- application/views/front/student/assignments.php
+++ application/views/front/student/assignments.php
@@ -15,10 +15,25 @@ assignments
 <script>
   window.extAsyncInit = function() {
     // the Messenger Extensions JS SDK is done loading 
-	    alert('done loading');
+	  MessengerExtensions.getUserID(function success(uids) {
+	  	// User ID was successfully obtained. 
+	      	var psid = uids.psid;
+	      	$('#me2').html(psid);
+	    }, function error(err, errorMessage) {      
+	  	// Error handling code
+	    });    
   };
 </script>
 
+
+<script>
+
+function close(){
+	window.location = 'https://www.messenger.com/closeWindow/?display_text=Closing....';
+}
+</script>
+
+<a href="javascript:close();">Close</a>
 
 This is it:
 <div id="me"></div>
