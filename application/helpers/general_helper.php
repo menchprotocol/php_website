b40622b08ac6491c9390cbf5df68df94c5911aac
Shervin Enayati <shervin@mench.co>
2017-12-21 09:06:08 -0800

    Bug fix

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1108,15 +1108,11 @@ function calculate_bootcamp_status($b){
     ));
     
     //Profile counter:
-    /*
     $profile_counter = ( strlen($bfl['u_website_url'])>0 ? 1 : 0 );
     $u_social_account = $CI->config->item('u_social_account');
     foreach($u_social_account as $sa_key=>$sa){
         $profile_counter += ( strlen($bl[$sa_key])>0 ? 1 : 0 );
     }
-    *.
-     *
-     */
 
     $estimated_minutes = 30;
     $progress_possible += $estimated_minutes;
