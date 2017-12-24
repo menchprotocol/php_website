9d3248f271ade34e2d9d714dfee6750c0c835ac7
Shervin Enayati <shervin@mench.co>
2017-12-24 18:47:55 +0000

    Big Fix

--- application/controllers/Bot.php
+++ application/controllers/Bot.php
@@ -18,6 +18,7 @@ class Bot extends CI_Controller {
         $i_messages = $this->Db_model->i_fetch(array(
             'i_fb_att_id' => 0,
             'i_media_type !=' => 'text',
+            'i_media_type !=' => 'video',
             'i_status >=' => 0, //Published in any form. This may need more logic
         ),$limit);
 
