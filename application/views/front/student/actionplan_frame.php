502e35aa09fc61b25b5b0da05dec6ef991cbf1c9
Shervin Enayati <shervin@mench.co>
2017-12-30 07:20:16 +0330

    Action Plan now working!!!

--- application/views/front/student/actionplan_frame.php
+++ application/views/front/student/actionplan_frame.php
@@ -32,7 +32,7 @@
             var psid = thread_context.psid;
             var signed_request = thread_context.signed_request;
             //Fetch Page:
-            $.post("/my/display_actionplan/"+psid+"/<?= intval($b_id) ?>/<?= intval($c_id) ?>?sr="+signed_request, {}, function(data) {
+            $.post("/my/display_actionplan/"+psid+"/<?= (isset($b_id) ? intval($b_id) : 0) ?>/<?= ( isset($c_id) ? intval($c_id) : 0) ?>?sr="+signed_request, {}, function(data) {
                 //Update UI to confirm with user:
                 $( "#page_content").html(data);
             });
@@ -45,6 +45,7 @@
     };
 
 <?php } ?>
+
 
 //Optionally you can close webview like this:
 function close_webview(){
