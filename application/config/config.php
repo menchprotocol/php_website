2c00f228e2ce1cab3b41d447d9a163284c216e0f
Shervin Enayati <shervin@mench.co>
2018-02-15 16:32:47 -0800

    Font Tweaks

--- application/config/config.php
+++ application/config/config.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Los_Angeles');
 
 //Primary website variables:
 $config['website'] = array(
-    'version' => 2.14,
+    'version' => 2.15,
     'name' => 'Mench',
     'legaL_name' => 'Mench Media Inc.',
     'url' => 'https://mench.co/', //Important to end with "/" as other links depend on this.
