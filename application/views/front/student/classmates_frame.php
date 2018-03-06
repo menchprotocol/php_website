c1fd6c75cd19aff6a54c9305d25638841124a27d
Shervin Enayati <shervin@mench.co>
2018-03-05 18:35:29 -0800

    Bug Fix

--- application/views/front/student/classmates_frame.php
+++ application/views/front/student/classmates_frame.php
@@ -48,7 +48,7 @@ if(isset($uadmission) && count($uadmission)>0) {
             function error(err){
 
                 //Give them instructions on how to access via mench.co:
-                $("#page_content").html('<div class="alert alert-info" role="alert" style="line-height:110%;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> <a href="https://mench.co/login" target="_blank" style="font-weight:bold;">Login to Mench</a> to access your Action Plan. Use [Forgot Password] if you never logged in before.</div>');
+                $("#page_content").html('<div class="alert alert-info" role="alert" style="line-height:110%;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> To access your Action Plan you need to <a href="https://mench.co/login" target="_blank" style="font-weight:bold;">Login</a>. Use [Forgot Password] if you never logged in before.</div>');
 
             }
         );
