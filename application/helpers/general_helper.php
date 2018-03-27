2a47a5c1fed48b68002837936f894fc7a6392318
Shervin Enayati <shervin@mench.co>
2018-03-26 18:14:30 -0700

    Tweak

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1626,7 +1626,7 @@ function echo_r($b,$class,$append_class=null){
 
     echo '<li class="list-group-item '.$append_class.'">';
 
-    echo '<span class="pull-right">';
+    echo '<span class="pull-right" title="Class ID '.$class['r_id'].'">';
     if($class['r__current_admissions']>0){
 
         //How many students, if any, are enrolled in support packages?
