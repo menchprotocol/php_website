a995262de749cce325cdbf7dfa0a74dc597d3d35
Shervin Enayati <shervin@mench.co>
2018-04-13 18:39:21 -0700

    v2.99 Algolia search is up in console

--- application/views/console/shared/d_header.php
+++ application/views/console/shared/d_header.php
@@ -60,7 +60,7 @@ $uri_segment_2 = $this->uri->segment(2);
                         <table style="width: 100%; border:0; padding:0; margin:0;">
                             <tr>
                                 <td style="width:40px;"><a href="/console"><img src="/img/bp_128.png" /></a></td>
-                                <td><?php if($udata['u_id']==1){ ?> <input type="text" id="console_search" placeholder="Search Bootcamps..."> <?php } ?></td>
+                                <td><?php if($udata['u_status']==3){ ?> <input type="text" id="console_search" placeholder="Search Bootcamps..."> <?php } ?></td>
                             </tr>
                         </table>
 					</span>
