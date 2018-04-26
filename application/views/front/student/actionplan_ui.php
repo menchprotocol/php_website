9a2c65c3a56898093400235b88719b5e3caf323e
Shervin Enayati <shervin@mench.co>
2018-04-26 13:17:26 -0700

    Tweaks

--- application/views/front/student/actionplan_ui.php
+++ application/views/front/student/actionplan_ui.php
@@ -219,16 +219,16 @@ if($level==2){
                 $textarea_note = 'Include optional feedback for your instructor';
             }
 
-            //What instructions do we need to give?
-            if($red_note) {
-                echo '<div style="color:#FF0000;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Marking as complete requires ' . $red_note . '</div>';
-            }
             echo '<div>Estimated time to complete: '.echo_time($intent['c_time_estimate'],1).'</div>';
             echo '<div class="mark_done" id="initiate_done"><a href="javascript:start_report();" class="btn btn-tight btn-black" style="padding-left:8px; padding-right:8px;"><i class="fa fa-check-circle initial"></i>Mark as Complete</a></div>';
 
 
             //Submission button visible after first button was clicked:
             echo '<div class="mark_done" style="display:none; margin-top:10px;">';
+            //What instructions do we need to give?
+            if($red_note) {
+                echo '<div style="color:#FF0000;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Marking as complete requires ' . $red_note . '</div>';
+            }
             echo '<textarea id="us_notes" class="form-control maxout" placeholder="'.$textarea_note.'"></textarea>';
             echo '<a href="javascript:mark_done();" class="btn btn-tight btn-black"><i class="fa fa-check-circle" aria-hidden="true"></i>Submit</a>';
             echo '</div>';
