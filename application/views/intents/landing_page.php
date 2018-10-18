3923d89bd65d60aec613cbdba4829f04561a1069
Shervin Enayati <shervin@mench.co>
2018-10-17 21:00:23 -0700

    landing page tweaks

--- application/views/intents/landing_page.php
+++ application/views/intents/landing_page.php
@@ -1,6 +1,6 @@
 <style>
     .body-container .msg, .body-container li, p, .body-container a { font-size:1.1em !important; }
-    .msg { margin-top:10px !important; font-weight:300 !important; line-height: 120%; }
+    .msg { margin-top:10px !important; font-weight:300 !important; line-height: 120% !important; }
     .msg a { max-width: none; }
 </style>
 
