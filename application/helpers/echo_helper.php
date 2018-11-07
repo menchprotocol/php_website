c9e8197582e2bed256d0e2f105de1df3d3d46664
Shervin Enayati <shervin@mench.com>
2018-11-07 05:55:23 -0800

    Chatbot logic

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -297,7 +297,12 @@ function echo_i($i,$u_full_name=null,$fb_format=false){
             if($fb_format){
 
                 //Show an option to open action plan:
-                $i['i_message'] = str_replace('@'.$i['i_outbound_u_id'], $us[0]['u_full_name'], $i['i_message']);
+                $i['i_message'] = str_replace('@'.$i['i_outbound_u_id'], $us[0]['u_full_name'].' /open_actionplan', $i['i_message']);
+
+                if(substr_count($i['i_message'],'/slice')>0){
+                    $time_range = explode(':', one_two_explode('/slice:',' ',$i['i_message']) ,2);
+                    $i['i_message'] = str_replace('/slice:'.$time_range[0].':'.$time_range[1], '', $i['i_message']);
+                }
 
             } else {
 
@@ -368,6 +373,7 @@ function echo_i($i,$u_full_name=null,$fb_format=false){
                 }
 
             }
+
         }
     }
 
