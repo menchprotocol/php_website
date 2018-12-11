6c441907d90f0a6411fe4dbe13bd726d5585d556
Shervin Enayati <shervin@mench.co>
2018-12-11 13:48:35 -0800

    Disabled Algolia

--- application/models/Db_model.php
+++ application/models/Db_model.php
@@ -2644,6 +2644,8 @@ class Db_model extends CI_Model {
 
     function algolia_sync($obj,$obj_id=0){
 
+        return false; //Disabled Algolia
+
 	    //Define the support objects indexed on algolia:
         $obj_id = intval($obj_id);
 
