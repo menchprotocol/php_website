9c7deb6ca28c0b0f9587855ac3c54502eba45eb5
Shervin Enayati <shervin@mench.co>
2018-12-12 11:02:15 -0800

    Upgrading Communication Model...

--- application/views/shared/public_footer.php
+++ application/views/shared/public_footer.php
@@ -8,31 +8,31 @@
                 <li class="social-li">
                     <a href="https://m.me/askmench?ref=ACTIONPLANADD10_<?= $this->config->item('in_primary_id') ?>"
                        class="social-link" data-toggle="tooltip"
-                       title="Get started with Mench Personal Assistant on Facebook Messenger" data-placement="top"><i
+                       title="Connect to your Mench Personal Assistant on Messenger" data-placement="top"><i
                                 class="fab fa-facebook-messenger"></i></a>
                     <a href="https://github.com/askmench" target="_blank"
-                       class="social-link" data-toggle="tooltip" title="Help develop Mench on GitHub"
+                       class="social-link" data-toggle="tooltip" title="Develop Mench on GitHub"
                        data-placement="top"><i class="fab fa-github"></i></a>
                     <a href="https://www.youtube.com/channel/UCOH64HiAIfJlz73tTSI8n-g" target="_blank"
-                       class="social-link" data-toggle="tooltip" title="Subscribe to our YouTube Channel"
+                       class="social-link" data-toggle="tooltip" title="Subscribe to Mench on YouTube"
                        data-placement="top"><i class="fab fa-youtube"></i></a>
                     <a href="https://www.facebook.com/askmench" target="_blank" class="social-link"
-                       data-toggle="tooltip" title="Like us on Facebook" data-placement="top"><i
+                       data-toggle="tooltip" title="Like Mench on Facebook" data-placement="top"><i
                                 class="fab fa-facebook"></i></a>
                     <a href="https://www.linkedin.com/company/askmench/" target="_blank" class="social-link"
-                       data-toggle="tooltip" title="Follow us on LinkedIn" data-placement="top"><i
+                       data-toggle="tooltip" title="Follow Mench on LinkedIn" data-placement="top"><i
                                 class="fab fa-linkedin"></i></a>
                     <a href="https://twitter.com/askmench" target="_blank" class="social-link" data-toggle="tooltip"
-                       title="Follow us on Twitter" data-placement="top"><i class="fab fa-twitter"></i></a>
+                       title="Follow Mench on Twitter" data-placement="top"><i class="fab fa-twitter"></i></a>
                     <a href="https://www.instagram.com/askmench/" target="_blank" class="social-link"
-                       data-toggle="tooltip" title="Follow us on Instagram" data-placement="top"><i
+                       data-toggle="tooltip" title="Follow Mench on Instagram" data-placement="top"><i
                                 class="fab fa-instagram"></i></a>
                     <a href="mailto:support@mench.com" class="social-link"><i class="fas fa-envelope"
                                                                               data-toggle="tooltip"
                                                                               title="Send us an email"
                                                                               data-placement="top"></i></a>
                     <a href="/terms" class="social-link"><i class="fas fa-balance-scale" data-toggle="tooltip"
-                                                            title="Review our terms of service and privacy policy"
+                                                            title="Read our terms of service and privacy policy"
                                                             data-placement="top"></i></a>
                 </li>
                 <li class="legal-name"><i>v<?= $this->config->item('app_version') ?></i></li>
