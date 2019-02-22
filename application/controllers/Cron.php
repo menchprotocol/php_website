f3042fe6cebbb26e25bcda780d5fb9621dae6bbf
Shervin Enayati <shervin@mench.co>
2019-02-22 11:15:08 -0800

    URL Policy Tweaking

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -27,7 +27,7 @@ class Cron extends CI_Controller
 
             $data = fn___curl_call(urldecode($_GET['url']));
             //$data = $this->Matrix_model->fn___digest_url(urldecode($_GET['url']));
-            echo htmlentities($data['body_html']);
+            echo htmlentities($data['response']);
 
         } else {
             echo '<form action="" method="GET">';
