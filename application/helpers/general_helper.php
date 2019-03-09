932a85866035b999b13344d8d67ce5d5c8430949
Shervin Enayati <shervin@mench.com>
2019-03-08 21:41:35 -0800

    Input validation

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -274,7 +274,7 @@ function is_valid_icon($string, $only_return_requirements = false){
     if ($is_img) {
         //Image URLs are valid:
         return true;
-    } elseif(substr($string, 0, 12) == '<i class="fa' && substr_count($string , ' fa-')==1 && substr($string, -6) == '"></i>'){
+    } elseif(substr($string, 0, 12) == '<i class="fa' && substr_count($string , ' fa-')>=1 && substr_count($string , ' fa-')>=2 && substr($string, -6) == '"></i>'){
         //FontAwesome icons are supported https://fontawesome.com/icons
         return true;
     } elseif(is_emojis_only($string)){
