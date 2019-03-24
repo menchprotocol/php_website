120ec72f5e4dbfce27eb33de9bfaf15aa5b7848d
Shervin Enayati <shervin@mench.com>
2019-03-23 21:31:55 -0700

    Updated Facebook Chat Plugin

--- application/views/view_shared/messenger_web_chat.php
+++ application/views/view_shared/messenger_web_chat.php
@@ -1,33 +1,38 @@
 <?php
-$session_en = $this->session->userdata('user');
+
+//Fetch some needed variables:
 $fb_settings = $this->config->item('fb_settings');
 $url_part_1 = $this->uri->segment(1);
+
 ?>
+
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
 <script>
-    //Facebook SDK for JavaScript:
-    window.fbAsyncInit = function () {
+    window.fbAsyncInit = function() {
         FB.init({
-            appId: '<?= $fb_settings['app_id'] ?>',
-            autoLogAppEvents: true,
-            xfbml: true,
-            version: '<?= $fb_settings['default_graph_version'] ?>'
+            xfbml            : true,
+            version          : '<?= $fb_settings['default_graph_version'] ?>'
         });
     };
 
-    (function (d, s, id) {
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    }(document, 'script', 'facebook-jssdk'));</script>
 
-</script>
+<div class="fb-customerchat"
+     attribution=setup_tool
+     minimized="true"
+     page_id="<?= $fb_settings['page_id'] ?>"
+     theme_color="#2f2739"
+     logged_in_greeting="Hi! How can I help you land your dream job?"
+     logged_out_greeting="Hi! How can I help you land your dream job?">
+     <?= ( is_numeric($url_part_1) ? ' ref="'.$url_part_1.'" ' : '' ) ?>
+</div>
 
-<div class="fb-customerchat" minimized="true"
-     ref="<?= ( is_numeric($url_part_1) ? $url_part_1 : 0 ) ?>" <?= ($session_en ? 'logged_in_greeting="' . fn___one_two_explode('', ' ', $session_en['en_name']) . ', how can I supercharge your tech career?"' : '') ?>
-     logged_out_greeting="Hi 👋 How can we help you?" greeting_dialog_display="hide" theme_color="#2f2739"
-     page_id="<?= $fb_settings['page_id'] ?>"></div>
 
 <div class="app-version hide-mini <?= fn___echo_advance() ?>">v<?= $this->config->item('app_version') ?></div>
