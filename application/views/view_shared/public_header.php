0b5db8d971db8d07a1f8052ffa73d8ed41803422
Shervin Enayati <shervin@mench.com>
2019-04-30 16:29:30 -0700

    Landing Page tweaks

--- application/views/view_shared/public_header.php
+++ application/views/view_shared/public_header.php
@@ -30,12 +30,6 @@ $url_part_1 = $this->uri->segment(1);
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
             <a class="navbar-brand" href="/"><img src="/img/bp_128.png"/><span
                         style="text-transform: lowercase; color: #2f2739;">Mench</span></a>
         </div>
