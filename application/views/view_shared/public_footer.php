894add416bf1aec298fbebc5566d372f438a01b0
Shervin Enayati <shervin@mench.com>
2019-04-30 16:39:19 -0700

    Landing Page tweaks

--- application/views/view_shared/public_footer.php
+++ application/views/view_shared/public_footer.php
@@ -8,25 +8,28 @@
 
                 <li class="social-li">
 
-                    <a href="/7436"
-                       class="social-link" data-toggle="tooltip" title="Learn more about Mench"
-                       data-placement="top"><i class="fal fa-info-circle"></i></a>
+                    <a href="https://angel.co/askmench/jobs" target="_blank"
+                       class="social-link" data-toggle="tooltip" title="See openings on Angel List"
+                       data-placement="top"><i class="fab fa-angellist"></i></a>
 
                     <a href="https://github.com/askmench" target="_blank"
                        class="social-link" data-toggle="tooltip" title="Mench is open-source. contribute on GitHub"
                        data-placement="top"><i class="fab fa-github"></i></a>
 
-                    <a href="https://angel.co/askmench/jobs" target="_blank"
-                       class="social-link" data-toggle="tooltip" title="See openings on Angel List"
-                       data-placement="top"><i class="fab fa-angellist"></i></a>
-
                     <a href="https://askmench.slack.com" target="_blank"
-                       class="social-link" data-toggle="tooltip" title="Chat with us on Slack"
+                       class="social-link" data-toggle="tooltip" title="Join the Conversation on Slack"
                        data-placement="top"><i class="fab fa-slack"></i></a>
 
                     <a href="https://www.youtube.com/channel/UCOH64HiAIfJlz73tTSI8n-g" target="_blank"
                        class="social-link" data-toggle="tooltip" title="Subscribe on YouTube"
                        data-placement="top"><i class="fab fa-youtube"></i></a>
+
+                    <a href="https://twitter.com/askmench" target="_blank" class="social-link" data-toggle="tooltip"
+                       title="Follow Mench on Twitter" data-placement="top"><i class="fab fa-twitter"></i></a>
+
+                    <a href="https://www.instagram.com/askmench/" target="_blank" class="social-link"
+                       data-toggle="tooltip" title="Follow on Instagram" data-placement="top"><i
+                                class="fab fa-instagram"></i></a>
 
                     <a href="https://www.facebook.com/askmench" target="_blank" class="social-link"
                        data-toggle="tooltip" title="Follow on Facebook" data-placement="top"><i
@@ -36,16 +39,13 @@
                        data-toggle="tooltip" title="Follow on LinkedIn" data-placement="top"><i
                                 class="fab fa-linkedin"></i></a>
 
-                    <a href="https://twitter.com/askmench" target="_blank" class="social-link" data-toggle="tooltip"
-                       title="Follow Mench on Twitter" data-placement="top"><i class="fab fa-twitter"></i></a>
-
-                    <a href="https://www.instagram.com/askmench/" target="_blank" class="social-link"
-                       data-toggle="tooltip" title="Follow on Instagram" data-placement="top"><i
-                                class="fab fa-instagram"></i></a>
-
                     <a href="/platform"
                        class="social-link" data-toggle="tooltip" title="Platform Stats"
                        data-placement="top"><i class="fas fa-analytics"></i></a>
+
+                    <a href="/login"
+                       class="social-link" data-toggle="tooltip" title="Sign In"
+                       data-placement="top"><i class="fas fa-sign-in"></i></a>
 
                     <a href="/8263?expand_mode=1&hide_subscribe=1" class="social-link" data-toggle="tooltip"
                        title="Read terms of service and privacy policy" data-placement="top"><i
@@ -54,6 +54,10 @@
                 </li>
             </ul>
         </nav>
+
+        <div class="pfooter" style="font-size:0.8em;"><img src="/img/bp_128.png">Mench v<?= $this->config->item('app_version') ?></div>
+
+
     </div>
 </footer>
 
