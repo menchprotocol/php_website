a35ab3aef18a31da4227cc9fbf7a1e8953f0c314
Shervin Enayati <shervin@mench.co>
2019-05-08 01:45:04 -0700

    Conditional Milestone Enhancements...

--- application/views/view_shared/messenger_nav.php
+++ application/views/view_shared/messenger_nav.php
@@ -4,17 +4,25 @@
         $en_all_2738 = $this->config->item('en_all_2738');
         $navigation = array(
             array(
-                'my_url' => 'actionplan',
+                'my_url' => '/messenger/actionplan',
                 'anchor' => $en_all_2738[6138]['m_icon'].' '.$en_all_2738[6138]['m_name'],
             ),
             array(
-                'my_url' => 'myaccount',
+                'my_url' => '/messenger/myaccount',
                 'anchor' => $en_all_2738[6137]['m_icon'].' '.$en_all_2738[6137]['m_name'],
             ),
         );
 
+        //If miner give access back to platform:
+        if(en_auth(array(1308))){
+            array_push($navigation, array(
+                'my_url' => '/platform',
+                'anchor' => '<span class="micro-image">'.$en_all_2738[4488]['m_icon'].'</span> '.$en_all_2738[4488]['m_name'].' &nbsp;<i class="fas fa-long-arrow-right"></i>',
+            ));
+        }
+
         foreach ($navigation as $nav_item) {
-            echo '<li><a href="/messenger/' . $nav_item['my_url'] . '" ' . (isset($current) && $current == $nav_item['my_url'] ? ' style="color:#FFF; background-color:#2f2739 !important;"' : '') . '>' . $nav_item['anchor'] . '</a></li>';
+            echo '<li><a href="' . $nav_item['my_url'] . '" ' . (isset($current) && $current == $nav_item['my_url'] ? ' style="color:#FFF; background-color:#2f2739 !important;"' : '') . '>' . $nav_item['anchor'] . '</a></li>';
         }
 
         //Student Logout button:
