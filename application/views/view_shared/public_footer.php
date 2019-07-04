28d691b9aafa9ba65e26b11c7e14cd1c206a8916
Shervin Enayati <shervin@mench.com>
2019-07-04 08:46:13 -0700

    UI Tweak

--- application/views/view_shared/public_footer.php
+++ application/views/view_shared/public_footer.php
@@ -60,7 +60,7 @@
             </ul>
         </nav>
 
-        <div class="pfooter" style="font-size:0.8em;"><?= '<img src="/img/mench_white.png" />Mench is open-source (v'. $this->config->item('app_version') ?>)</div>
+        <div class="pfooter" style="font-size:0.8em;"><?= '<img src="/img/mench_white.png" />Mench v'. $this->config->item('app_version') ?></div>
 
     </div>
 </footer>
