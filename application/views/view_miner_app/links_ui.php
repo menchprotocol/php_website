216a0a90452b105b94e8af66a129d2e61d3b4b23
Shervin Enayati <shervin@mench.co>
2019-08-20 19:30:21 -0700

    Re-added link type counter for user view

--- application/views/view_miner_app/links_ui.php
+++ application/views/view_miner_app/links_ui.php
@@ -244,6 +244,7 @@ if(isset($_GET['ln_type_entity_id'])){
 
 }
 
+
 $has_filters = ( count($_GET) > 0 );
 
 ?>
@@ -386,14 +387,36 @@ echo '</div></td>';
 
         } else {
 
-            //Show all link types:
             echo '<select class="form-control border" name="ln_type_entity_id" id="ln_type_entity_id" class="border" style="width: 100% !important;">';
-            echo '<option value="0">All Link Types</option>';
-            foreach($this->config->item('en_all_4593') /* Link Types */ as $en_id => $m){
-                //Echo drop down:
-                echo '<option value="' . $en_id . '" ' . ((isset($_GET['ln_type_entity_id']) && $_GET['ln_type_entity_id'] == $en_id) ? 'selected="selected"' : '') . '>' . $m['m_name'] . '</option>';
+
+            if(isset($_GET['ln_creator_entity_id'])) {
+
+                //Fetch details for this user:
+                $all_link_count = 0;
+                $select_ui = '';
+                foreach ($this->Links_model->ln_fetch($ini_filter, array('en_type'), 0, 0, array('en_name' => 'ASC'), 'COUNT(ln_type_entity_id) as trs_count, SUM(ln_credits) as credits_sum, en_name, ln_type_entity_id', 'ln_type_entity_id, en_name') as $ln) {
+                    //Echo drop down:
+                    $select_ui .= '<option value="' . $ln['ln_type_entity_id'] . '" ' . ((isset($_GET['ln_type_entity_id']) && $_GET['ln_type_entity_id'] == $ln['ln_type_entity_id']) ? 'selected="selected"' : '') . '>' . $ln['en_name'] . ' ('  . number_format($ln['trs_count'], 0) . ')</option>';
+                    $all_link_count += $ln['trs_count'];
+                }
+
+                //Now that we know the total show:
+                echo '<option value="0">All ('  . number_format($all_link_count, 0) . ')</option>';
+                echo $select_ui;
+
+            } else {
+
+                //Load all fast:
+                echo '<option value="0">All Link Types</option>';
+                foreach($this->config->item('en_all_4593') /* Link Types */ as $en_id => $m){
+                    //Echo drop down:
+                    echo '<option value="' . $en_id . '" ' . ((isset($_GET['ln_type_entity_id']) && $_GET['ln_type_entity_id'] == $en_id) ? 'selected="selected"' : '') . '>' . $m['m_name'] . '</option>';
+                }
+
             }
+
             echo '</select>';
+
 
         }
 
