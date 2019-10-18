9da69839cdb06163ab2749739b7d68f596e9e015
Shervin Enayati <shervin@mench.co>
2019-10-18 12:35:48 -0700

    Home Page land

--- application/views/mench_launching.php
+++ application/views/mench_launching.php
@@ -9,59 +9,20 @@
     -->
 
     <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="/img/mench-v2-16.png">
+    <link rel="icon" type="image/png" href="/img/mench.gif">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>MENCH</title>
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:800|Open+Sans:300,400,600|Nanum+Gothic+Coding&display=swap" rel="stylesheet">
 
-    <link href="/css/custom/mench.css?v=v1.07" rel="stylesheet"/>
+</head>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/fbf7f3ae67.js" crossorigin="anonymous"></script>
-    <script src="/js/lib/jquery.textcomplete.min.js"></script>
-    <script src="/js/lib/autocomplete.jquery.min.js"></script>
-    <script src="/js/lib/algoliasearch.min.js"></script>
-    <script src="/js/lib/sortable.min.js" type="text/javascript"></script>
-    <script src="/js/custom/global-js.js?v=v1.07" type="text/javascript"></script>
-    <script src="/js/custom/platform-js.js?v=v1.07" type="text/javascript"></script>
+<body style="background-color: #fbfbfb;">
 
-    <script type="text/javascript"> var js_active_superpowers = [];  var js_pl_id = 1;  var algolia_filter = '';  var js_en_all_4737 = {"6184":{"m_icon":"<i class=\"fas fa-globe yellow \"><\/i>","m_name":"PUBLISH","m_desc":"newly added by miner but not yet checked by moderator","m_parents":[7774,7355,7356,4737]},"6183":{"m_icon":"<i class=\"far fa-spinner fa-spin \"><\/i>","m_name":"DRAFT","m_desc":"newly added, pending review","m_parents":[7774,7356,4737]},"6182":{"m_icon":"<i class=\"far fa-trash-alt \"><\/i>","m_name":"ARCHIVE","m_desc":"archived","m_parents":[10671,4737]}};  var js_en_all_6404 = {"11062":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"ALGOLIA ENABLED","m_desc":"1","m_parents":[6404]},"11071":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"BLOG TITLE MAX LENGTH","m_desc":"100","m_parents":[6404]},"11069":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"BYTES PER WORD","m_desc":"30000","m_parents":[6404]},"11079":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"DEFAULT PLATFORM TIMEZONE","m_desc":"America\/Los_Angeles","m_parents":[6404]},"11076":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"FACEBOOK APP ID","m_desc":"1782431902047009","m_parents":[6404]},"11077":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"FACEBOOK DEFAULT GRAPH VERSION","m_desc":"v3.2","m_parents":[4506,6404]},"11074":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"FACEBOOK MESSENGER MAX MESSAGE LENGTH","m_desc":"2000","m_parents":[6404]},"11075":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"FACEBOOK PAGE ID","m_desc":"381488558920384","m_parents":[6404]},"11064":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"ITEMS PER PAGE","m_desc":"100","m_parents":[6404]},"11065":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"MAGIC LINK VALID SECONDS","m_desc":"3600","m_parents":[6404]},"11063":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"MAX FILE SIZE [MB]","m_desc":"25","m_parents":[6404]},"11060":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"MENCH PLATFORM VERSION","m_desc":"1.07","m_parents":[6404]},"11067":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"NODS PER WORD","m_desc":"50","m_parents":[6404]},"11066":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"PASSWORD MIN CHARACTERS","m_desc":"6","m_parents":[6404]},"11072":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"PLAYER NAME MAX LENGTH","m_desc":"140","m_parents":[6404]},"11073":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"READABLE MESSAGE MAX LENGTH","m_desc":"280","m_parents":[6404]},"11057":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"READ MARKS MAX","m_desc":"89","m_parents":[6404,4358]},"11056":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"READ MARKS MIN","m_desc":"-89","m_parents":[6404,4358]},"11061":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"READ MONTHLY FREE WORDS","m_desc":"2000","m_parents":[6404]},"11070":{"m_icon":"<i class=\"far fa-lambda\"><\/i>","m_name":"UNKNOWN FILE WORDS","m_desc":"3","m_parents":[6404]}};  var js_en_all_6177 = {"6181":{"m_icon":"<i class=\"fas fa-globe blue\"><\/i>","m_name":"PUBLISH","m_desc":"live and ready to be shared with users","m_parents":[7774,7358,7357,6177]},"6180":{"m_icon":"<i class=\"far fa-spinner fa-spin\"><\/i>","m_name":"DRAFT","m_desc":"being mined, to be published soon","m_parents":[7774,7358,6177]},"6178":{"m_icon":"<i class=\"far fa-trash-alt isred\"><\/i>","m_name":"ARCHIVE","m_desc":"archived","m_parents":[10672,6177]}};  var js_en_all_6186 = {"6176":{"m_icon":"<i class=\"fas fa-globe ispink\"><\/i>","m_name":"PUBLISH","m_desc":"live and ready to be shared with users","m_parents":[7774,7360,7359,6186]},"6175":{"m_icon":"<i class=\"far fa-spinner fa-spin\"><\/i>","m_name":"DRAFT","m_desc":"being mined, to be published soon","m_parents":[7774,7364,7360,6186]},"6173":{"m_icon":"<i class=\"far fa-trash-alt isred\"><\/i>","m_name":"ARCHIVE","m_desc":"archived","m_parents":[10686,10678,10673,6186]}};  var random_loading_message = ["Are you having a good day today?","Be gentle with yourself today.","Congratulate yourself for the great job you're doing","Crunching the latest data, just for you. Hang tight...","Have a glass of water nearby? Time for a sip!","Offer hugs. Someone probably needs them.","You are unique!","Get a drink of water. Stay hydrated!","Have you danced today?","Have you listened to your favourite song recently? \ud83c\udfb5","Have you stretched recently?","Have you recently told someone you're proud of them?","Help is out there. Don't be afraid to ask.","Hey! Life is tough, but so are you! \ud83d\udcaa","Hey, jump up for a sec and stretch, yeah? \ud83d\udc50","I know it's cheesey but I hope you have a grate day!","Identity is fluid! People can change - be accepting.","Is there a window you can look through? The world is beautiful. \ud83c\udf06","Is your seat comfortable? Can you adjust your chair properly?","It can be hard to get started, can't it? That's okay, you got this.","It's so great to have you here today","Keep growing, keep learning, keep moving forward!","Learning new things is important - open your eyes to the world around you!","Making things awesome...","Novel, new, silly, & unusual activities can help lift your mood.","Play for a few minutes. Doodle, learn solitaire, fold a paper airplane, do something fun.","Don't take yourself for granted. You're important.","Rest your eyes for a moment. Look at something in the distance and count to five! \ud83c\udf33","Self care is important, look after and love yourself, you're amazing!","Set aside time for a hobby. Gardening, drone building, knitting, do something for the pure pleasure of it.","So often our power lies not in ourselves, but in how we help others find their own strength","Sometimes doing something nice for somebody else is the best way to feel good about yourself! \ud83d\udc6d","Stop. Breathe. Be here now.","Stop. Take three slow deep breaths.","Take 5 minutes to straighten the space around you. Set a timer.","Take a break before you need it. It will make it easier to prevent burnout.","Take a moment to send a message to someone you love \ud83d\ude3b","Take care of yourself. We need you.","Technology is a tool. Use it wisely.","The impact you leave on the universe can never be erased.","There are no impostors here","There's someone who is so so grateful that you exist together.","Today is a great day to let a friend know how much you appreciate them.","Water is good for you year round. If you're thirsty, you're dehydrated.","We all have superpowers. You included. I hope you are using yours to make your life a joyful one.","When's the last time you treated yourself?","With the dawning of a new day comes a clean slate and lots of opportunity.","You are fantastic","You are loved. <3","You are so very important \ud83d\udc9b\ud83d\udc9b\ud83d\udc95","You can do this!","You cannot compare your successes to the apparent achievements of others. \ud83c\udf04","You deserve to be safe and to have nice things happen to you.","You have the power to change the world.","You're allowed to start small. \ud83d\udc1e","have you hugged anyone lately?","it's time to check your thirst level, human.","\ud83d\udc97: don't forget to take a little bit of time to say hi to a friend","\ud83c\udf38: remember to let your eyes rest, maybe by looking at a plant...","\ud83d\ude4c: take a second to adjust your posture","\ud83d\ude0e\ud83c\udf08\ud83d\udc95"];  var random_saving_message = ["Learning everyday \ud83d\ude0e","Growing with you \ud83c\udf38","Getting smarter ^~^"]; </script></head>
-
-<body>
-
-
-
-
-<!-- FOOTER -->
-<div class="container fixed-bottom">
-    <div class="row">
-        <table class="footer-bottom">
-            <tr>
-                <td>
-                    <div class="supwerpower_view">
-                        <img src="/img/mench-v2-128.png" class="search-toggle footer-logo mench-spin" /><span class="mench-logo montserrat search-toggle">MENCH</span>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
+<div style="text-align: center;">
+    <img src="/img/mench.gif" width="200" style="margin: 30px 0;" />
+    <div style="font-family:'Montserrat'; font-size: 1.8em;">READ. PLAY. BLOG.</div>
+    <br />
+    <div style="font-family:'Montserrat'">NOV 2019</div>
 </div>
 
-
-<div class="container">
-
-    <div class="row">
-        <div class="col-lg">
-            <div class="montserrat">READ. PLAY. BLOG.</div>
-            <div class="montserrat">LAUNCHING NOV 2019</div>
-        </div>
-    </div>
-
-
-</div>
 </body>
