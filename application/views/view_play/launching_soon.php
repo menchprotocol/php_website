83e3b29176cf10f24943e1c0134e99e7b4ddf7ee
Shervin Enayati <shervin@mench.co>
2019-10-26 18:59:56 -0700

    Content improvements

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -21,7 +21,7 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
             <ul class="intructions-list double-line-list">
                 <li>An interactive publishing platform for sharing stories & ideas that matter</li>
                 <li>A conversational reading experience offered over the web or Messenger</li>
-                <li>A learning game that players <b class="montserrat play">PLAY</b> every time they <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> or <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li>A learning game that rewards players every time they read and blog</li>
                 <li class="learn_more hidden">An open-source protocol for building & sharing consensus</li>
                 <li class="learn_more hidden">A non-profit organization on a mission to expand human potential</li>
             </ul>
