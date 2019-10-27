9d4fbe438cee417bbb81b88b29de117c18a68d0a
Shervin Enayati <shervin@mench.co>
2019-10-26 19:01:15 -0700

    Content improvements

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -9,6 +9,7 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
         <div class="col-lg-2" style="text-align:left;">
             <img src="/img/mench-v2-128.png" class="mench-spin" />
+            <b class="montserrat read">READ</b> <b class="montserrat play">PLAY</b> <b class="montserrat blog">BLOG</b>
         </div>
 
         <div class="col-lg-6">
