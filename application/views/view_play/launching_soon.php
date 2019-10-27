877e14e4e8baac62acb5d4a51d6220380d445e64
Shervin Enayati <shervin@mench.co>
2019-10-26 17:56:37 -0700

    Content improvements

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -19,8 +19,8 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
             <h2>ABOUT MENCH</h2>
             <ul class="intructions-list double-line-list">
-                <li>A learning game where players earn <b class="montserrat">MENCH COINS</b> as they <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> or <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
-                <li>A publishing platform for interactive blogs read over the web or Messenger</li>
+                <li>An interactive blogging platform available on the web or Messenger</li>
+                <li>A learning game incentivizing players to <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> and <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
                 <li>An open-source protocol for building and sharing consensus</li>
                 <li>A non-profit organization on a mission to expand human potential</li>
                 <li>A community of creators who share stories and ideas that matter</li>
@@ -29,14 +29,14 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
             <h2>PLAYING MENCH</h2>
             <ul class="intructions-list">
-                <li>Earn 1x <?= $en_all_2738[6205]['m_icon'] ?> <b class="montserrat">COIN</b> per each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
-                <li>Earn 1x <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COIN</b> per each word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li>Earn 1x <?= $en_all_2738[6205]['m_icon'] ?> coin per each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
+                <li>Earn 1x <?= $en_all_2738[4535]['m_icon'] ?> coin per each word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
                 <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COINS</b></li>
             </ul>
 
-            <p>Estimated release date: <b class="montserrat">Early 2020</b></p>
+            <p>Release Date is ~ <b class="montserrat">Early 2020</b></p>
 
             <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-blog montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a></p>
 
