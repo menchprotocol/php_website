8d25d197ab2e2e61b84e1769fba5e4ef1429558f
Shervin Enayati <shervin@mench.co>
2019-10-26 17:33:07 -0700

    Home Page intro

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -1,17 +1,48 @@
 <div class="container">
 
-    <div class="row" style="padding-top: 50px;">
+    <div class="row" style="padding-top: 30px;">
+
         <div class="col-lg-3">&nbsp;</div>
+
         <div class="col-lg-2" style="text-align:left;">
             <img src="/img/mench-v2-128.png" class="mench-spin" />
         </div>
+
         <div class="col-lg-5">
+
+
             <h1>BLOGGING. REINVENTED.</h1>
-            <p>MENCH is an interactive publishing platform set to launch on December 25th 2019.</p>
-            <p>Apply for an early-access publisher account <a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" style="font-weight: bold; text-decoration: underline;">here</a>.</p>
-            <p>Login with your publisher account <a href="/play/signin" style="font-weight: bold; text-decoration: underline;">here</a>.</p>
+
+
+            <h2>ABOUT MENCH</h2>
+            <ul class="intructions-list double-line-list">
+                <li>A learning game where players earn <b class="montserrat">MENCH COINS</b> as they <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> or <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li>A publishing platform for interactive blogs read over the web or Messenger</li>
+                <li>An open-source protocol for building and sharing consensus</li>
+                <li>A non-profit organization on a mission to expand human potential</li>
+                <li>A community of creators who share stories and ideas that matter</li>
+            </ul>
+
+
+            <h2>PLAYING MENCH</h2>
+            <ul class="intructions-list">
+                <li>Earn 1x <?= $en_all_2738[6205]['m_icon'] ?> <b class="montserrat">COIN</b> per each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
+                <li>Earn 1x <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COIN</b> per each word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
+                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
+                <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COINS</b></li>
+            </ul>
+
+            <p>Estimated release date: <b class="montserrat">Early 2020</b></p>
+
+            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-blog montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a></p>
+
+            <p><b class="montserrat">P.S.</b> as a blogger you can join the wait list to get an early-access publisher account; or you can <a href="/play/signin" style="font-weight: bold; text-decoration: underline;">login</a> if you already have one.</p>
+
         </div>
+
         <div class="col-lg-2">&nbsp;</div>
     </div>
 
 </div>
+
--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -11,22 +11,22 @@ $en_all_10591 = $this->config->item('en_all_10591'); //PLAYER PLAYS
 
             <h1>MENCH IS</h1>
             <ul class="intructions-list double-line-list">
-                <li>A game of words where players earn coins as they <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> and <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
-                <li>A publishing platform for creating and sharing interactive content</li>
+                <li>A learning game where players earn <b class="montserrat">MENCH COINS</b> as they <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> and <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li>A publishing platform to collaboratively create interactive content and share with your audience</li>
                 <li>A personalized reading experience delivered over the web or Messenger</li>
-                <li class="learn_more hidden">A non-profit and open-source project for building and sharing consensus</li>
-                <li class="learn_more hidden">A community of thinkers and doers who share stories and ideas that matter</li>
+                <li class="learn_more_start"><a href="javascript:void(0);" onclick="$('.learn_more_start').remove();$('.learn_more').toggleClass('hidden');">LEARN MORE</a></li>
+                <li class="learn_more hidden">A non-profit organization on a mission to expand your potential</li>
+                <li class="learn_more hidden">An open-source protocol for building and sharing consensus</li>
+                <li class="learn_more hidden">A community of creators who share stories and ideas that matter</li>
             </ul>
-            <div class="learn_more"><span class="parent-icon icon-block-sm"><i class="fas fa-search-plus"></i></span><a href="javascript:void(0);" onclick="$('.learn_more').toggleClass('hidden')">LEARN MORE</a></div>
-
 
             <h1 style="margin-top:21px;">HOW TO PLAY</h1>
             <ul class="intructions-list">
-                <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> coin for each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
-                <li>Earn a <?= $en_all_2738[4535]['m_icon'] ?> coin for each word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
-                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month FREE</li>
-                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
-                <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> coins</li>
+                <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> <b class="montserrat">COIN</b> for each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
+                <li>Earn a <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COIN</b> for each word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
+                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for <b class="montserrat">$<?= config_var(11162) ?>/month</b></li>
+                <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COINS</b></li>
             </ul>
 
             <?php
