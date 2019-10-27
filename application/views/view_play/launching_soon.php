e08b59b501118a5253a4bb26601d50283b6a1fb2
Shervin Enayati <shervin@mench.co>
2019-10-26 18:49:59 -0700

    Content improvements

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -26,19 +26,22 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
                 <li>A non-profit organization on a mission to expand human potential</li>
             </ul>
 
+            <div class="learn_more hidden">
+                <h2>HOW TO <b class="play">PLAY</b></h2>
+                <ul class="intructions-list">
+                    <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> coin for every word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
+                    <li>Earn a <?= $en_all_2738[4535]['m_icon'] ?> coin for every word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                    <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
+                    <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
+                    <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> coins</li>
+                </ul>
 
-            <h2>HOW TO <b class="play">PLAY</b></h2>
-            <ul class="intructions-list">
-                <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> coin for every word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
-                <li>Earn a <?= $en_all_2738[4535]['m_icon'] ?> coin for every word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
-                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
-                <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
-                <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> coins</li>
-            </ul>
+                <p>Release Date is ~ <b class="montserrat">Early 2020</b></p>
 
-            <p>Release Date is ~ <b class="montserrat">Early 2020</b></p>
+            </div>
 
-            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-play montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a></p>
+            <a href="javascript:void(0);" onclick="$('.learn_more').toggleClass('hidden');" class="btn btn-read montserrat learn_more">LEARN MORE <i class="fas fa-search-plus"></i></a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-play montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a>
 
             <p>Or <a href="/play/signin" style="font-weight: bold; text-decoration: underline;">login</a> if you already have an account.</p>
 
