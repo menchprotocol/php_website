41b61a494acc411f3b7b89473f28e7b706db1f1d
Shervin Enayati <shervin@mench.co>
2019-10-26 18:11:28 -0700

    Content improvements

--- application/views/view_play/launching_soon.php
+++ application/views/view_play/launching_soon.php
@@ -29,8 +29,8 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
             <h2>PLAYING MENCH</h2>
             <ul class="intructions-list">
-                <li>Earn 1x <?= $en_all_2738[6205]['m_icon'] ?> coin for every word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
-                <li>Earn 1x <?= $en_all_2738[4535]['m_icon'] ?> coin for every word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
+                <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> coin for every word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
+                <li>Earn a <?= $en_all_2738[4535]['m_icon'] ?> coin for every word you <b class="montserrat blog"><?= $en_all_2738[4535]['m_name'] ?></b></li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b> unlimited words for $<?= config_var(11162) ?>/month</li>
                 <li>Earn monthly cash with your <?= $en_all_2738[4535]['m_icon'] ?> <b class="montserrat">COINS</b></li>
@@ -38,7 +38,7 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
             <p>Release Date is ~ <b class="montserrat">Early 2020</b></p>
 
-            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-blog montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a></p>
+            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform" class="btn btn-play montserrat">JOIN WAIT LIST <i class="fas fa-arrow-right"></i></a></p>
 
             <p><b class="montserrat">P.S.</b> as a blogger you can join the wait list to get an early-access publisher account; or you can <a href="/play/signin" style="font-weight: bold; text-decoration: underline;">login</a> if you already have one.</p>
 
