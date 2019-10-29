018e538042114a3d46ab3fb510776e689543d133
Shervin Enayati <shervin@mench.co>
2019-10-29 13:31:14 -0700

    Home Page Tweaks

--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -7,20 +7,21 @@ $en_all_10591 = $this->config->item('en_all_10591'); //PLAYER PLAYS
 <div class="container">
 
     <div class="row">
-        <div class="col-lg-12">
+
+        <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+
             <?php
             $en_all_4527 = $this->config->item('en_all_4527'); //Platform Cache
             echo '<h1>'.$en_all_4527[11968]['m_name'].'</h1>';
             echo '<ul class="none-list">';
             foreach($this->config->item('en_all_11968') as $en_id => $m){
-                echo '<li class="'.( in_array(11982 , $m['m_parents']) ? '' : ' learn_more hidden ' ).'"><span class="icon-block-sm">'.$m['m_icon'].'</span> <b class="montserrat">'.$m['m_name'].'</b> '.$m['m_desc'].'</li>';
+                echo '<li class="'.( in_array(11982 , $m['m_parents']) && !$session_en ? '' : ' learn_more hidden ' ).'"><span class="icon-block-sm">'.$m['m_icon'].'</span> <b class="montserrat">'.$m['m_name'].'</b> '.$m['m_desc'].'</li>';
             }
             echo '<li class="learn_more"><span class="icon-block-sm"><i class="fas fa-search-plus"></i></span> <a href="javascript:void(0);" onclick="$(\'.learn_more\').toggleClass(\'hidden\')" style="text-decoration: underline;">LEARN MORE</a></li>';
             echo '</ul>';
             ?>
-        </div>
-        <div class="col-lg-12 hidden learn_more">
 
+            <div class="learn_more hiddden">
             <h1 style="margin-bottom: 0;">HOW TO PLAY</h1>
             <ul class="decimal-list">
                 <li>Earn a <?= $en_all_2738[6205]['m_icon'] ?> coin for each word you <b class="montserrat read"><?= $en_all_2738[6205]['m_name'] ?></b></li>
@@ -44,7 +45,10 @@ $en_all_10591 = $this->config->item('en_all_10591'); //PLAYER PLAYS
             echo '<tbody><tr><td colspan="3"><span class="icon-block"><i class="far fa-yin-yang fa-spin"></i></span></td></tr></tbody>';
             echo '</table>';
             ?>
+            </div>
+
         </div>
+
     </div>
 
     <script>
