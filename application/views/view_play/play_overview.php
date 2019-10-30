d0a0bf481a908883f84d1ff27f8795e74a26cca4
Shervin Enayati <shervin@mench.co>
2019-10-29 18:38:21 -0700

    Home Page Tweaks

--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -24,7 +24,7 @@ $en_all_10591 = $this->config->item('en_all_10591'); //PLAYER PLAYS
         <div class="learn_more hidden">
             <h1>HOW TO PLAY</h1>
             <ul class="none-list">
-                <li><b class="montserrat play"><?= $en_all_2738[4536]['m_icon'] .' '. $en_all_2738[4536]['m_name'] ?></b> awarded for a new account</li>
+                <li><b class="montserrat play"><?= $en_all_2738[4536]['m_icon'] .' '. $en_all_2738[4536]['m_name'] ?></b> awarded for a new player</li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_icon'] .' '. $en_all_2738[6205]['m_name'] ?></b> awarded for each word read</li>
                 <li><b class="montserrat blog"><?= $en_all_2738[4535]['m_icon'] .' '. $en_all_2738[4535]['m_name'] ?></b> awarded for each word blogged</li>
                 <li><b class="montserrat read"><?= $en_all_2738[6205]['m_icon'] .' '.$en_all_2738[6205]['m_name'] ?></b> up to <?= config_var(11061) ?> words/month <b class="montserrat">FREE</b></li>
