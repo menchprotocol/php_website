d1f3f614d52d16cede8dcc0b88a8c92aa33c74a8
Shervin Enayati <shervin@mench.co>
2019-10-29 20:55:09 -0700

    Home Page Tweaks

--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -17,7 +17,7 @@ $en_all_10591 = $this->config->item('en_all_10591'); //PLAYER PLAYS
         foreach($this->config->item('en_all_11968') as $en_id => $m){
             echo '<li class="'.( in_array(11982 , $m['m_parents']) && !$session_en ? '' : ' learn_more hidden ' ).'"><span class="icon-block-sm">'.$m['m_icon'].'</span> <b class="montserrat">'.$m['m_name'].'</b> '.$m['m_desc'].'</li>';
         }
-        echo '<li class="learn_more"><span class="icon-block-sm"><i class="fas fa-search-plus"></i></span> <a href="javascript:void(0);" onclick="$(\'.learn_more\').toggleClass(\'hidden\');update_basic_stats(0);" style="text-decoration: underline;">MORE</a></li>';
+        echo '<li class="learn_more"><span class="icon-block-sm"><i class="fas fa-search-plus"></i></span> <a href="javascript:void(0);" onclick="$(\'.learn_more\').toggleClass(\'hidden\');update_basic_stats(0);" style="text-decoration: underline;">LEARN MORE</a></li>';
         echo '</ul>';
         ?>
 
