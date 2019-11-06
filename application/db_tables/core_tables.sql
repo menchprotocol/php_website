f41dbe3a71d83d5dc9ea5122653468c7c795f43f
Shervin Enayati <shervin@mench.co>
2019-11-06 09:16:22 -0800

    New singular ledger

--- application/db_tables/core_tables.sql
+++ application/db_tables/core_tables.sql
@@ -1,131 +1,32 @@
 
 --
--- Last Updated: Aug 20 2019
---
--- Mench's database consistent of 3 tables:
---
--- 1] Intents
--- 2] Entities
--- 3] Links
+-- The single table that runs the MENCH platform
 --
 
-
-create table table_intents
+create table mench_ledger
 (
-  in_id                           serial       not null constraint table_intents_pkey primary key,
-  in_status_entity_id             integer      not null,
-  in_outcome                      varchar(255) not null,
-  in_completion_method_entity_id            integer      not null,
-  in_completion_seconds           smallint     not null,
-  in_metadata                     text
+
+  -- CORE VARIABLES:
+  transaction_id                           bigserial primary key, -- Unique, system-generated
+  transaction_timestamp                    timestamp,             -- PST -8 Timezone
+  transaction_content                      text,                  -- The core content being added to the ledger
+  transaction_coins                        double precision,      -- Positive value when blogging and negative value when reading
+  transaction_order                        smallint,              -- Order relative to its siblings
+  transaction_metadata                     text,                  -- System-generated variables for faster performance
+
+  -- PLAYER REFERENCES:
+  transaction_player_type_id               integer,               -- References one of these players: https://mench.com/play/4593
+  transaction_player_status_id             integer,               -- References one of these players: https://mench.com/play/6186
+  transaction_player_miner_id              integer,               -- References one of these players: https://mench.com/play/4430
+  transaction_player_parent_id             integer,               -- References a player as parent
+  transaction_player_child_id              integer,               -- References a player as child
+
+  -- BLOG REFERENCES:
+  transaction_blog_parent_id               integer,               -- References a blog as parent
+  transaction_blog_child_id                integer,               -- References a blog as child
+
+  -- OTHER REFERENCES:
+  transaction_transaction_id               bigint,                -- References another ledger transaction
+  transaction_external_id                  bigint                 -- References an ID in an external platform
+
 );
-
-
-create table table_entities
-(
-  en_id                           serial       not null constraint table_entities_pkey primary key,
-  en_status_entity_id             integer      not null,
-  en_name                         varchar(255) not null,
-  en_trust_score                  integer      not null,
-  en_icon                         text,
-  en_metadata                     text
-);
-
-
-create table table_links
-(
-  ln_id                           bigserial    not null constraint table_links_pkey primary key,
-  ln_status_entity_id             integer      not null,
-  ln_type_entity_id               integer      not null,
-  ln_creator_entity_id            integer      not null,
-  ln_parent_entity_id             integer      not null,
-  ln_child_entity_id              integer      not null,
-  ln_parent_intent_id             integer      not null,
-  ln_child_intent_id              integer      not null,
-  ln_parent_link_id               bigint       not null,
-  ln_external_id                  bigint       not null,
-  ln_order                        smallint     not null,
-  ln_timestamp                    timestamp(4) not null,
-  ln_words                        double precision not null,
-  ln_content                      text,
-  ln_metadata                     text
-);
-
-
-
-
-
-
--- Intents
-alter table table_intents
-  owner to us;
-
-create unique index table_intents_in_id_uindex
-  on table_intents (in_id);
-
-create index table_intents_in_completion_method_entity_id_index
-  on table_intents (in_completion_method_entity_id);
-
-create index table_intents_in_completion_seconds_index
-  on table_intents (in_completion_seconds);
-
-create index table_intents_in_status_entity_id_index
-  on table_intents (in_status_entity_id);
-
-
--- Entities
-alter table table_entities
-  owner to us;
-
-create unique index table_entities_en_id_uindex
-  on table_entities (en_id);
-
-create index table_entities_en_trust_score_index
-  on table_entities (en_trust_score);
-
-create index table_entities_en_status_entity_id_index
-  on table_entities (en_status_entity_id);
-
-
-
--- Links
-alter table table_links
-  owner to us;
-
-create unique index table_ledger_tr_id_uindex
-  on table_links (ln_id);
-
-create index table_ledger_tr_timestamp_index
-  on table_links (ln_timestamp);
-
-create index table_ledger_tr_en_creator_id_index
-  on table_links (ln_creator_entity_id);
-
-create index table_ledger_tr_en_type_id_index
-  on table_links (ln_type_entity_id);
-
-create index table_ledger_tr_en_parent_id_index
-  on table_links (ln_parent_entity_id);
-
-create index table_ledger_tr_en_child_id_index
-  on table_links (ln_child_entity_id);
-
-create index table_ledger_tr_in_parent_id_index
-  on table_links (ln_parent_intent_id);
-
-create index table_ledger_tr_in_child_id_index
-  on table_links (ln_child_intent_id);
-
-create index table_ledger_tr_tr_parent_id_index
-  on table_links (ln_parent_link_id);
-
-create index table_ledger_tr_order_index
-  on table_links (ln_order);
-
-create index table_links_ln_status_entity_id_index
-  on table_links (ln_status_entity_id);
-
-create index table_links_ln_index_index
-  on table_links (ln_external_id);
-
-
