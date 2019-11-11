0cd905a4515907d8bf47cbc5471ab1588fa4c362
Shervin Enayati <shervin@mench.com>
2019-11-11 03:31:40 -0800

    The magic 12

--- application/db_tables/core_tables.sql
+++ application/db_tables/core_tables.sql
@@ -1,32 +1,25 @@
 
 --
--- The single table that runs the MENCH platform
+-- The single table that runs MENCH
 --
 
 create table mench_ledger
 (
 
-  -- CORE VARIABLES:
-  transaction_id                bigserial primary key, -- Unique, system-generated
-  transaction_timestamp         timestamp,             -- GMT -8 Los Angeles Time
-  transaction_content           text,                  -- The core content being added to the ledger
-  transaction_coins             double precision,      -- Positive value when blogging and negative value when reading
-  transaction_order             smallint,              -- Order relative to its siblings
-  transaction_metadata          text,                  -- System-generated variables for faster performance
-
-  -- ENTITY REFERENCES:
-  transaction_entity_type_id    integer,               -- References one of these players: https://mench.com/play/4593
-  transaction_entity_status_id  integer,               -- References one of these players: https://mench.com/play/6186
-  transaction_entity_miner_id   integer,               -- References one of these players: https://mench.com/play/4430
-  transaction_entity_parent_id  integer,               -- References a player as parent
-  transaction_entity_child_id   integer,               -- References a player as child
-
-  -- INTENT REFERENCES:
-  transaction_intent_parent_id  integer,               -- References a blog as parent
-  transaction_intent_child_id   integer,               -- References a blog as child
-
-  -- OTHER REFERENCES:
-  transaction_transaction_id    bigint,                -- References another ledger transaction
-  transaction_external_id       bigint                 -- References an ID in an external platform
+  -- CONTENT:
+  transaction_id                bigserial primary key, -- Unique/system-generated ID
+  transaction_timestamp         timestamp,             -- Los Angeles time zone
+  transaction_coins             double precision,      -- Positive when input/blog and negative value output/read
+  transaction_correlation       double precision,      -- a number between -1 and 1 indicating relative weight
+  transaction_data              text,                  -- [ Optional ] The knowledge added to the ledger
+  transaction_metadata          text,                  -- [ Optional ] Cache variables generated using cron-jobs
+  
+  -- CONNECTIONS:
+  transaction_type_id           bigint,                -- References one of these players: https://mench.com/play/4593
+  transaction_status_id         bigint,                -- References one of these players: https://mench.com/play/6186
+  transaction_miner_id          bigint,                -- [ Optional ] References a player: https://mench.com/play/4430
+  transaction_parent_id         bigint,                -- [ Optional ] References a parent player/blog
+  transaction_child_id          bigint,                -- [ Optional ] References a child player/blog
+  transaction_reference_id      bigint                 -- [ Optional ] References an internal/external player/blog/object
 
 );
