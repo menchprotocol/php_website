4d0e7977981e379c0fe86205a31fe389b1818621
Shervin Enayati <shervin@mench.co>
2019-11-20 08:03:33 -0800

    Coming Soon Home

--- application/views/view_play/coming_soon.php
+++ application/views/view_play/coming_soon.php
@@ -1,14 +1,14 @@
 <div class="container">
     <div class="row">
-        <div class="col-lg">
+        <div class="col-lg" style="padding-top: 30px;">
 
             <p><b class="montserrat">MENCH</b> is an interactive microblogging platform on blockchain.</p>
 
-            <p><b class="montserrat">MENCH</b> blogs are unique as they focus on a single idea, can link to each other and be up-voted by citing references such as books or articles. Bloggers use a simple web app to store, organize and publish ideas that matter. Readers experience a hyper-engaged and personalized reading experience over the web or Messenger. <b class="montserrat">MENCH</b> is on a mission to build an open idea index that makes relevant information more accessible.</p>
+            <p><b class="montserrat">MENCH</b> blogs are unique as they focus on a single idea, can link to each other and be up-voted by citing references such as books or articles. Bloggers use a simple web app to store, organize and publish ideas that matter. Readers experience a hyper-engaged and personalized reading experience over the web or Messenger.</p>
+
+            <p><b class="montserrat">MENCH</b> is on a mission to build an open idea index that makes relevant information more accessible.</p>
 
             <p>Public Beta launching in early 2020. <a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform">Join our waitlist</a> for an early-access blogger account and more updates.</p>
-
-            <p>Team <b class="montserrat">MENCH</b></p>
 
         </div>
     </div>
