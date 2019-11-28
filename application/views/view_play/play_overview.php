2ccc0cb7798306389a2a34b5497a8031e141319d
Shervin Enayati <shervin@mench.co>
2019-11-28 12:30:16 -0800

    READ PLAY BLOG Intro tweak

--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -6,7 +6,7 @@
             <?php
             echo '<br />';
             foreach($this->config->item('en_all_2738') as $en_id => $m){
-                echo '<h1 class="inline montserrat color'.$en_id.'"><span class="icon-block-lg en-icon">'.echo_en_icon($m['m_icon']).'</span> '.$m['m_name'].'</h1>';
+                echo '<h2 class="inline montserrat color'.$en_id.'"><span class="icon-block-lg en-icon">'.echo_en_icon($m['m_icon']).'</span> '.$m['m_name'].'</h2>';
                 echo '<p class="inline"> '.$m['m_desc'].'</p>';
                 echo '<br />';
             }
@@ -18,7 +18,7 @@
         <div class="col-lg-12">
             <?php
             echo '<br />';
-            echo '<h1 class="inline montserrat blue"><span class="icon-block-lg en-icon"><i class="far fa-users blue"></i></span> PLAYERS</h1>';
+            echo '<h2 class="inline montserrat blue"><span class="icon-block-lg en-icon"><i class="far fa-users blue"></i></span> PLAYERS</h2>';
             echo '<table id="leaderboard" class="table table-sm table-striped">';
             echo '<tbody><tr><td colspan="3"><span class="icon-block"><i class="far fa-yin-yang fa-spin"></i></span></td></tr></tbody>';
             echo '</table>';
