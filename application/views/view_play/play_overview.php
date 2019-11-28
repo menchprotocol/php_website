24c155ba3f2dc08222ae61d60aad586071b2886e
Shervin Enayati <shervin@mench.co>
2019-11-28 13:43:35 -0800

    Call to action tweak

--- application/views/view_play/play_overview.php
+++ application/views/view_play/play_overview.php
@@ -5,14 +5,14 @@
         <div class="col-lg-12">
             <?php
             $navigation = array(
-                4536 => '/play/signin', //PLAY
-                6205 => '/read', //READ
-                4535 => '/blog', //BLOG
+                4536 => '<a href="/play/signin" class="montserrat">START PLAYING</a>', //PLAY
+                6205 => '<a href="/read" class="montserrat">START READING</a>', //READ
+                4535 => '<a href="/blog" class="montserrat">START BLOGGING</a>', //BLOG
             );
             echo '<br />';
             foreach($this->config->item('en_all_2738') as $en_id => $m){
                 echo '<h2 class="inline montserrat color'.$en_id.'"><span class="icon-block-lg en-icon">'.echo_en_icon($m['m_icon']).'</span> '.$m['m_name'].'</h2>';
-                echo '<p class="inline"> '.$m['m_desc'].' <span class="inline-block"><a href="'.$navigation[$en_id].'" class="montserrat color'.$en_id.'">'.$m['m_name'].'</a> <i class="fas fa-arrow-right color'.$en_id.'"></i></span></p>';
+                echo '<p class="inline"> '.$m['m_desc'].' <span class="inline-block">'.$navigation[$en_id].' <i class="fas fa-arrow-right"></i></span></p>';
                 echo '<br />';
             }
             ?>
@@ -23,7 +23,7 @@
         <div class="col-lg-12">
             <?php
             echo '<br />';
-            echo '<h2 class="inline montserrat blue"><span class="icon-block-lg en-icon"><i class="far fa-users blue"></i></span> '.echo_number(unique_players()).' PLAYERS</h2>';
+            echo '<h2 class="inline montserrat blue"><span class="icon-block-lg en-icon"><i class="far fa-users blue"></i></span> PLAYERS</h2>';
             echo '<table id="leaderboard" class="table table-sm table-striped">';
             echo '<tbody><tr><td colspan="3"><span class="icon-block"><i class="far fa-yin-yang fa-spin"></i></span></td></tr></tbody>';
             echo '</table>';
