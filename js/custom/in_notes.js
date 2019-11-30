23c645d3b27364988c99979fd4da6016e121b5e8
Shervin Enayati <shervin@mench.co>
2019-11-29 16:23:26 -0800

    Blog Note Input Iterations...

--- js/custom/in_notes.js
+++ js/custom/in_notes.js
@@ -125,7 +125,9 @@ $(document).ready(function () {
         $(this).focus(function() {
             $( '#notes_control_'+focus_ln_type_entity_id ).removeClass('hidden');
         }).focusout(function() {
-            $( '#notes_control_'+focus_ln_type_entity_id ).addClass('hidden');
+            if(!$('#ln_content').val().length){
+                $( '#notes_control_'+focus_ln_type_entity_id ).addClass('hidden');
+            }
         })
 
         autosize($(this));
