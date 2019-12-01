72e5741f296a81b02feb2cb10dcdccaf20334380
Shervin Enayati <shervin@mench.co>
2019-12-01 15:22:31 -0800

    Show MENCH

--- application/views/view_play/coming_soon.php
+++ application/views/view_play/coming_soon.php
@@ -4,7 +4,7 @@
 
             <p><b class="montserrat">MENCH</b> is an open-source protocol to organize and share ideas across expert sources. Creators use a simple web app to index and collectively organize ideas based on topics. Readers get to easily discover top ideas and keep up-to-date over the web or Messenger. In the last 30 days alone, there has been 10,000+ marketing books published on amazon, not to mention all the videos, articles, courses and other digital content that hold valuable ideas hostage.</p>
 
-            <p><b class="montserrat">MENCH</b> is on a mission to democratize key ideas published anytime, anywhere.</p>
+            <p><b class="montserrat">MENCH</b> aims to democratize key ideas published anytime, anywhere.</p>
 
             <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform">Request a demo</a> or <a href="/play/signin">login</a> to get started.</p>
 
