b53e19d579c9084c3af4803a2abb83a95f228dae
Shervin Enayati <shervin@mench.co>
2019-12-01 17:52:55 -0800

    Description Update

--- application/views/view_play/coming_soon.php
+++ application/views/view_play/coming_soon.php
@@ -2,12 +2,14 @@
     <div class="row">
         <div class="col-lg">
 
-            <p><b class="montserrat">MENCH</b> is an open-source protocol to organize and share ideas across expert sources. Creators use a simple web app to index and collectively organize ideas based on topics. Readers easily discover top ideas and keep up-to-date over the web or Messenger. In the last 30 days, 10,000+ books have been published under "marketing" on amazon.com alone, not to mention all the videos, articles, online courses and other digital content that share valuable ideas. How is someone into marketing supposed to keep up?</p>
+            <p><b class="montserrat">MENCH</b> is an open-source ledger to organize and share ideas that matter. In the last 30 days, 10,000+ books have been published under the topic "marketing" on amazon.com alone. How can anyone keep up? In exchange for cryptocoins, Miners index key ideas from expert authors, group them based on topics and identify repeats across different sources. Readers use the web or Messenger to read, listen and watch videos on topics or ideas they care about.</p>
 
-            <p><b class="montserrat">MENCH</b> aims to democratize key ideas published anytime, anywhere.</p>
-
-            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform">Request a demo</a> or <a href="/play/signin">login</a> to get started.</p>
+            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSca_W0_pP1anDmJ9_iJx82icCXTjKMjblXCx9hIHrlScwUoGg/viewform">Register</a> or <a href="/play/signin">login</a> to get started.</p>
 
         </div>
     </div>
 </div>
+
+MENCH
+
+Visit mench.com to mine or .
