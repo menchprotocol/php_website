98d23708247965b2f136cfa8b029a40a33dc0818
Shervin Enayati <shervin@mench.co>
2019-12-05 10:33:55 -0800

    Intro simplification

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 *
 */
 
-//Generated 2019-12-05 10:25:59 PST
+//Generated 2019-12-05 10:33:38 PST
 
 //MENCH GLOSSARY:
 $config['en_ids_4463'] = array(4535,4485,3084,2738,4536,4430,6199,3000,4755,6205);
@@ -37,7 +37,7 @@ $config['en_all_4463'] = array(
     2738 => array(
         'm_icon' => '<img src="/img/mench-v2-128.png" class="mench-logo mench-spin no-radius">',
         'm_name' => 'MENCH',
-        'm_desc' => 'is an interactive publishing platform that allows anyone to share ideas that matter. Play to earn crypto-coins for every word you read or write. Write microblogs that focus on a key idea but can be linked together to form bigger ideas. Read interactively on the web or Messenger, set your avatar & unlock superpowers as you grow.',
+        'm_desc' => 'is an interactive publishing platform that allows anyone to share ideas that matter. Play by earning crypto coins for every word you read or write. Write microblogs that each focus on a key idea but can be linked to form bigger ideas. Read interactively on the web or Messenger, set your avatar & unlock superpowers as you grow.',
         'm_parents' => array(4463,12041,2792,3303,7524,3325,3326,3324,4527,1,7312,2750),
     ),
     4536 => array(
