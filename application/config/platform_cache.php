58118903369e611e557df1c08fafc9fd6aae8c83
Shervin Enayati <shervin@mench.co>
2019-12-05 15:05:11 -0800

    Bug Fixed

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -2521,7 +2521,7 @@ $config['en_all_6404'] = array(
     11060 => array(
         'm_icon' => '',
         'm_name' => 'MENCH PLATFORM VERSION',
-        'm_desc' => '1.0146',
+        'm_desc' => '1.01493',
         'm_parents' => array(6404),
     ),
     11067 => array(
