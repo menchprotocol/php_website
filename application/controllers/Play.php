9ada32f2bfcf03cb2dd0f72295b0c1f03224e352
Shervin Enayati <shervin@mench.co>
2019-12-05 10:28:45 -0800

    PLAYERS READ WRITE

--- application/controllers/Play.php
+++ application/controllers/Play.php
@@ -729,9 +729,9 @@ fragment PostListingItemSidebar_post on Post {
         echo '<table id="leaderboard" class="table table-sm table-striped">';
 
         echo '<tr>';
-        echo '<td><span class="parent-icon icon-block"><i class="fas fa-circle blue"></i></span><b class="montserrat blue">PLAYER</b></td>';
+        echo '<td><span class="parent-icon icon-block"><i class="fas fa-circle blue"></i></span><b class="montserrat blue">PLAYERS</b></td>';
         echo '<td><span class="parent-icon icon-block"><i class="fas fa-circle ispink"></i></span><b class="montserrat ispink">READ</b></td>';
-        echo '<td><span class="parent-icon icon-block"><i class="fas fa-circle yellow"></i></span><b class="montserrat yellow">BLOG</b></td>';
+        echo '<td><span class="parent-icon icon-block"><i class="fas fa-circle yellow"></i></span><b class="montserrat yellow">WRITE</b></td>';
         echo '</tr>';
 
         //Did we find anyone?
