d431ce7d72eb6a859df4fa5142892b8ef351db75
Shervin Enayati <shervin@mench.co>
2019-12-04 22:18:16 -0800

    MENCH Intro

--- application/views/view_read/read_overview.php
+++ application/views/view_read/read_overview.php
@@ -3,7 +3,6 @@
 
     <?php $en_all_4463 = $this->config->item('en_all_4463') ?>
 
-    <div class="read-topic"><span class="icon-block"><i class="fas fa-lightbulb-on"></i></span>INTRODUCTION</div>
     <div class="alert alert-info">
         <div><b class="montserrat"><?= $en_all_4463[2738]['m_name'] ?></b> <?= $en_all_4463[2738]['m_desc'] ?></div>
         <div style="margin-top:10px;"><i class="fas fa-pen-square"></i> <a href="/blog">Start Blogging</a><span class="top-players"> &nbsp; <i class="fas fa-medal"></i> <a href="javascript:void(0);" onclick="load_leaderboard()">Top Players</a></span></div>
