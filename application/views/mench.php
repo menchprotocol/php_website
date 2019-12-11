06bfb36c4eb9cafaf7fd627cc542bc35e7663adb
Shervin Enayati <shervin@mench.co>
2019-12-10 20:35:43 -0800

    The story

--- application/views/mench.php
+++ application/views/mench.php
@@ -45,26 +45,28 @@
         new TypeIt('.read_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('LEARN').pause(tempo(0)).go().destroy();
 
         new TypeIt('.blog_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('on the web').pause(tempo(1)).go().destroy();
-        new TypeIt('.blog_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('for Messenger').pause(tempo(0)).go().destroy();
+        new TypeIt('.blog_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('for chat apps').pause(tempo(0)).go().destroy();
 
-        new TypeIt('.read_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('on Messenger').pause(tempo(1)).go().destroy();
+        new TypeIt('.read_content', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('on Messenger').pause(tempo(1)).go().destroy();
+        new TypeIt('.read_content', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('on WhatsApp').pause(tempo(0)).go().destroy();
+        new TypeIt('.read_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('READ').pause(tempo(0)).go().destroy();
+        new TypeIt('.read_content', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('on Slack').pause(tempo(0)).go().destroy();
 
         new TypeIt('.play_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('a learning game').pause(tempo(1)).go().destroy();
+        new TypeIt('.play_content', { speed:speed(), startDelay:tl+=tempo(0) }).delete().type('SUPERPOWERS').pause(tempo(1)).go();
         new TypeIt('.play_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('EARN').pause(tempo(0)).go().destroy();
-        new TypeIt('.play_content', { speed:speed(), startDelay:tl+=tempo(0) }).delete().type('SUPERPOWERS').pause(tempo(1)).go().destroy();
         new TypeIt('.play_content', { speed:speed(), startDelay:tl+=tempo(0) }).delete().type('crypto-coins').pause(tempo(1)).go().destroy();
 
 
         new TypeIt('.blog_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('BLOG').pause(tempo(1)).go().destroy();
         new TypeIt('.blog_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('collaboratively').pause(tempo(1)).go().destroy();
 
-
-        new TypeIt('.read_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('READ').pause(tempo(0)).go().destroy();
-        new TypeIt('.blog_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('interactively').pause(tempo(1)).go().destroy();
-
+        new TypeIt('.read_content', { speed:speed(), startDelay:tl+=tempo(2) }).delete().type('interactively').pause(tempo(1)).go().destroy();
 
         new TypeIt('.play_title', { speed:speed(), startDelay:tl+=tempo(1) }).delete().type('PLAY').pause(tempo(0)).go().destroy();
         new TypeIt('.play_content', { speed:speed(), startDelay:tl+=tempo(2) }).type('for the fun of it').pause(tempo(1)).go().destroy();
+
+        console.log(tl + ' seconds runtime');
 
     }
 
