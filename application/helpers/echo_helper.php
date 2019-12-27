3804e313cbed158c64cb394275bf137604f5aa0a
Shervin Enayati <shervin@mench.co>
2019-12-26 17:20:37 -0800

    SELECT ONE/MORE

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1553,7 +1553,7 @@ function echo_in_read($in, $footnotes = null, $common_prefix = null, $extra_clas
     $ui .= '<td>';
     $ui .= '<b class="montserrat blog-url inline-block">'.echo_in_title($in['in_title'], false, $common_prefix).'</b>';
     if($footnotes){
-        $ui .= '<span class="'.$extra_class.'"><span class="montserrat blog-info doupper inline-block>'.$footnotes.'</span></span>';
+        $ui .= '<span class="'.$extra_class.'"><span class="montserrat blog-info doupper inline-block">'.$footnotes.'</span></span>';
     }
 
     //Now do measurements:
