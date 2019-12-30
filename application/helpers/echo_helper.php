1b0157d3ae7a98288c575fe8ae15df66ae7c8d06
Shervin Enayati <shervin@mench.co>
2019-12-29 19:51:13 -0800

    Featured visibility

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1548,7 +1548,7 @@ function echo_in_blog($in)
             //It has been featured, list topics:
             $ui .= '<div class="montserrat blog-info doupper">FEATURED IN';
             foreach($featured_topics as $topic){
-                $ui .= ' <span class="icon-block">'.$topic['en_icon'].'</span>'.$topic['en_name'];
+                $ui .= '<span class="icon-block-sm">'.$topic['en_icon'].'</span>'.$topic['en_name'];
             }
             $ui .= '</div>';
         } else {
