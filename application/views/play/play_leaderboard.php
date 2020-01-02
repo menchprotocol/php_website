0b4cd7ecf4331ecb0732cb79291ff6e1fd4cc97e
Shervin Enayati <shervin@mench.co>
2020-01-02 15:19:56 -0800

    fad support

--- application/views/play/play_leaderboard.php
+++ application/views/play/play_leaderboard.php
@@ -1,7 +1,10 @@
 
+<?php
+$en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
+?>
 
 <div class="container">
-    <h1 class="montserrat blue"><span class="icon-block-xlg icon_photo"><i class="fas fa-users-crown"></i></span>LEADERBOARD</h1>
+    <h1 class="montserrat blue"><span class="icon-block-xlg icon_photo"><?= $en_all_11035[11087]['m_icon'] ?></span><?= $en_all_11035[11087]['m_name'] ?></h1>
     <div id="load_top_players"></div>
 </div>
 
