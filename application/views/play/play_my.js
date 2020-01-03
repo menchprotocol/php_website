8070911d2fa51f70475a092e139c1213ff50dbc9
Shervin Enayati <shervin@mench.co>
2020-01-03 11:12:04 -0800

    iterations

--- application/views/play/play_my.js
+++ application/views/play/play_my.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     //Setup auto focus:
     $('#openEn6197').on('show.bs.collapse', function () {
         //call a service here
+        alert('nice');
         $('#en_name').focus();
     });
 
