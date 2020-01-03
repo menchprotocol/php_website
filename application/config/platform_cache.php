f5a709a0e4c968107849e7d54e76860dab64e7ba
Shervin Enayati <shervin@mench.co>
2020-01-02 17:41:11 -0800

    iterations

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 *
 */
 
-//Generated 2020-01-02 17:38:32 PST
+//Generated 2020-01-02 17:40:54 PST
 
 //PLAYER NOTIFICATIONS CHANNEL:
 $config['en_ids_12220'] = array(12221,12222);
@@ -238,7 +238,7 @@ $config['en_all_3289'] = array(
     ),
 );
 
-//GENDER:
+//PLAYER GENDER:
 $config['en_ids_3290'] = array(3292,3291,6121);
 $config['en_all_3290'] = array(
     3292 => array(
@@ -1525,7 +1525,7 @@ $config['en_all_11007'] = array(
     ),
     11012 => array(
         'm_icon' => '<i class="fas fa-usd-circle yellow" aria-hidden="true"></i>',
-        'm_name' => 'UNLIMITED FOR $50/YEAR = $0.96/WEEK',
+        'm_name' => 'UNLIMITED FOR $50/YEAR = $0.96/WEEK [SAVE 20%]',
         'm_desc' => '',
         'm_parents' => array(11163,11007),
     ),
@@ -2693,7 +2693,7 @@ $config['en_all_10692'] = array(
 );
 
 //PLATFORM MEMORY:
-$config['en_ids_4527'] = array(12066,10725,7758,10809,12145,10990,11018,11047,4983,12079,10589,11968,4229,4486,4485,12012,7302,4737,7356,12138,7355,11021,6201,12112,7585,10602,7309,7712,7751,6150,12201,10712,10719,10627,10716,3290,10746,7364,10717,10721,10720,11080,2738,12105,4463,11035,7555,11059,11058,6225,6404,4527,11054,6232,11039,6194,10956,6827,4426,4997,12220,4454,4986,7551,11028,4537,6177,11007,10957,6206,3289,4592,11089,11033,3000,7303,11088,4755,10718,6287,10571,7357,6192,11081,6193,6146,6345,5967,4280,10570,4277,6102,7704,7494,10590,7347,6103,10692,6255,7304,6186,7360,7359,4341,4593,10593,10591,12141,10658,10711,11031,11084,6204,10710,6805,7358,10869);
+$config['en_ids_4527'] = array(12066,10725,7758,10809,12145,10990,11018,11047,4983,12079,10589,11968,4229,4486,4485,12012,7302,4737,7356,12138,7355,11021,6201,12112,7585,10602,7309,7712,7751,6150,12201,10712,10719,10627,10716,10746,7364,10717,10721,10720,11080,2738,12105,4463,11035,7555,11059,11058,6225,6404,4527,11054,6232,11039,6194,10956,3290,6827,4426,4997,12220,4454,4986,7551,11028,4537,6177,11007,10957,6206,3289,4592,11089,11033,3000,7303,11088,4755,10718,6287,10571,7357,6192,11081,6193,6146,6345,5967,4280,10570,4277,6102,7704,7494,10590,7347,6103,10692,6255,7304,6186,7360,7359,4341,4593,10593,10591,12141,10658,10711,11031,11084,6204,10710,6805,7358,10869);
 $config['en_all_4527'] = array(
     12066 => array(
         'm_icon' => '<i class="far fa-info-circle" aria-hidden="true"></i>',
@@ -2905,12 +2905,6 @@ $config['en_all_4527'] = array(
         'm_desc' => '',
         'm_parents' => array(3311,11097,4527,10869),
     ),
-    3290 => array(
-        'm_icon' => '<i class="far fa-transgender blue"></i>',
-        'm_name' => 'GENDER',
-        'm_desc' => '',
-        'm_parents' => array(4527,6204),
-    ),
     10746 => array(
         'm_icon' => '<i class="fas fa-industry"></i>',
         'm_name' => 'INDUSTRY',
@@ -3036,6 +3030,12 @@ $config['en_all_4527'] = array(
         'm_name' => 'PLAYER AVATAR',
         'm_desc' => '',
         'm_parents' => array(6225,6204,11008,4527),
+    ),
+    3290 => array(
+        'm_icon' => '<i class="far fa-transgender blue" aria-hidden="true"></i>',
+        'm_name' => 'PLAYER GENDER',
+        'm_desc' => '',
+        'm_parents' => array(4527,6204),
     ),
     6827 => array(
         'm_icon' => '<i class="far fa-users-crown"></i>',
@@ -4984,7 +4984,7 @@ $config['en_all_6201'] = array(
 );
 
 //SINGLE SELECTABLE:
-$config['en_ids_6204'] = array(4737,7585,10602,3290,10956,12220,4454,6177,11007,3289,6186,4593,10591);
+$config['en_ids_6204'] = array(4737,7585,10602,10956,3290,12220,4454,6177,11007,3289,6186,4593,10591);
 $config['en_all_6204'] = array(
     4737 => array(
         'm_icon' => '<i class="fas fa-sliders-h yellow" aria-hidden="true"></i>',
@@ -5004,17 +5004,17 @@ $config['en_all_6204'] = array(
         'm_desc' => '',
         'm_parents' => array(10893,6204,7302,4527),
     ),
-    3290 => array(
-        'm_icon' => '<i class="far fa-transgender blue"></i>',
-        'm_name' => 'GENDER',
-        'm_desc' => '',
-        'm_parents' => array(4527,6204),
-    ),
     10956 => array(
         'm_icon' => '<i class="fas fa-paw blue" aria-hidden="true"></i>',
         'm_name' => 'PLAYER AVATAR',
         'm_desc' => '',
         'm_parents' => array(6225,6204,11008,4527),
+    ),
+    3290 => array(
+        'm_icon' => '<i class="far fa-transgender blue" aria-hidden="true"></i>',
+        'm_name' => 'PLAYER GENDER',
+        'm_desc' => '',
+        'm_parents' => array(4527,6204),
     ),
     12220 => array(
         'm_icon' => '<i class="fas fa-flag blue" aria-hidden="true"></i>',
