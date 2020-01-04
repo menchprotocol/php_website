aee840fa60afd0af28930fdde0c79c8dbe0efa49
Shervin Enayati <shervin@mench.co>
2020-01-04 14:00:08 -0800

    iterations

--- application/views/blog/blog_modify.php
+++ application/views/blog/blog_modify.php
@@ -227,8 +227,8 @@ foreach ($this->config->item('en_all_11021') as $en_id => $m){
             }
 
             //ADD NEW NOTE:
-            $this_tab .= '<div class="list-group-item itemblog add_note_' . $en_id2 . ( $is_author && $is_active ? '' : ' hidden ' ).'">';
             $this_tab .= '<div class="'.superpower_active(10939).'">';
+            $this_tab .= '<div class="list-group-item itemblog add_note_' . $en_id2 . ( $is_author && $is_active ? '' : ' hidden ' ).'">';
             $this_tab .= '<form class="box box' . $en_id2 . '" method="post" enctype="multipart/form-data">'; //Used for dropping files
 
 
