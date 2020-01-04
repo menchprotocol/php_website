b856cc261cd04db70ab5ad0eba6c459b55e058b7
Shervin Enayati <shervin@mench.co>
2020-01-03 19:16:25 -0800

    iterations

--- application/views/play/play_modify.php
+++ application/views/play/play_modify.php
@@ -35,11 +35,6 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
     echo '<span class="icon-block en_status_play_id_' . $player['en_id'] . ( $is_published ? ' hidden ' : '' ).'"><span data-toggle="tooltip" data-placement="bottom" title="'.$en_all_6177[$player['en_status_play_id']]['m_name'].': '.$en_all_6177[$player['en_status_play_id']]['m_desc'].'">' . $en_all_6177[$player['en_status_play_id']]['m_icon'] . '</span></span>';
 
 
-    //MY ACCOUNT
-    if(isset($session_en['en_id']) && $session_en['en_id']==$player['en_id']){
-        echo '<a href="/play" class="btn btn-play btn-five inline-block" style="padding-top:10px;" data-toggle="tooltip" data-placement="bottom" title="'.$en_all_11035[4536]['m_desc'].'">'.$en_all_11035[4536]['m_icon'].' '.$en_all_11035[4536]['m_name'].'</a>';
-    }
-
     //MODIFY
     echo '<a href="javascript:void(0);" onclick="en_modify_load(' . $player['en_id'] . ',0)" class="btn btn-play btn-five inline-block '. superpower_active(10983) .'"><i class="fas fa-cog"></i></a>';
 
