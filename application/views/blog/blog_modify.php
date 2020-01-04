04e082a479f208e315a5c4ef9c389944a54cf6d1
Shervin Enayati <shervin@mench.co>
2020-01-04 15:56:47 -0800

    iterations

--- application/views/blog/blog_modify.php
+++ application/views/blog/blog_modify.php
@@ -46,7 +46,7 @@ foreach ($this->config->item('en_all_11021') as $en_id => $m){
         echo '<div style="margin-bottom: 5px;">';
 
             //Blog Status:
-            echo '<div class="inline-block">'.echo_in_dropdown(4737, $in['in_status_play_id'], 'btn-blog', $is_author && $is_active).'</div>';
+            echo '<div class="inline-block side-margin">'.echo_in_dropdown(4737, $in['in_status_play_id'], 'btn-blog', $is_author && $is_active).'</div>';
 
             if(in_array($in['in_status_play_id'], $this->config->item('en_ids_12138') /* Blog Statuses Featured */)){
                 //Blog Featured Icons:
@@ -95,7 +95,7 @@ foreach ($this->config->item('en_all_11021') as $en_id => $m){
     } else {
 
         echo '<div class="center-right">';
-            echo '<div class="inline-block" style="margin:0 0 5px 0;">'.echo_in_dropdown(7585, $in['in_type_play_id'], 'btn-blog', $is_author && $is_active).'</div>';
+            echo '<div class="inline-block side-margin">'.echo_in_dropdown(7585, $in['in_type_play_id'], 'btn-blog', $is_author && $is_active).'</div>';
             echo '<div class="inline-block '.superpower_active(10984).'" style="width:89px; margin:0 0 5px 5px;">'.echo_in_text(4362, $in['in_read_time'], $in['in_id'], $is_author && $is_active, 0).'</div>';
         echo '</div>';
 
