581f243fd1bba1b61f647a3848a9cf4b12c7792b
Shervin Enayati <shervin@mench.co>
2020-01-06 15:30:43 -0800

    iterations

--- application/views/read/read_ledger.php
+++ application/views/read/read_ledger.php
@@ -225,9 +225,9 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
 
 echo '<div class="container">';
 
-    echo '<h1 class="'.extract_icon_color($en_all_11035[11999]['m_icon']).'"><span class="icon-block-xlg icon_photo">'.$en_all_11035[11999]['m_icon'].'</span>'.$en_all_11035[11999]['m_name'].'</h1>';
+    echo '<h1 class="'.extract_icon_color($en_all_11035[11999]['m_icon']).' inline-block"><span class="icon-block-xlg icon_photo">'.$en_all_11035[11999]['m_icon'].'</span>'.$en_all_11035[11999]['m_name'].'</h1>';
 
-    echo '<div class="inline-block '.superpower_active(10964).'" style="padding-left:7px;"><i class="far fa-filter"></i><a href="javascript:void();" onclick="$(\'.show-filter\').toggleClass(\'hidden\');">FILTER</a></div>';
+    echo '<div class="inline-block '.superpower_active(10964).'" style="padding-left:7px;"><i class="far fa-filter"></i><a href="javascript:void();" onclick="$(\'.show-filter\').toggleClass(\'hidden\');" class="montserrat">FILTER</a></div>';
 
 
     echo '<div class="inline-box show-filter '.( $has_filters && 0 ? '' : 'hidden' ).'">';
