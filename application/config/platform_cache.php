a4faab3a00a9e27166360de62c0c49bc36039d87
Shervin Enayati <shervin@mench.co>
2020-01-06 14:01:20 -0800

    iterations

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -2313,7 +2313,7 @@ $config['en_all_6404'] = array(
     11060 => array(
         'm_icon' => '',
         'm_name' => 'MENCH PLATFORM VERSION',
-        'm_desc' => '1.2111',
+        'm_desc' => '1.2112',
         'm_parents' => array(6404),
     ),
     11079 => array(
