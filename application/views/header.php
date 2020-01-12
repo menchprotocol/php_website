65421043275fd5c7078a4654654f4307312ec005
Shervin Enayati <shervin@mench.co>
2020-01-11 17:17:58 -0800

    iterations

--- application/views/header.php
+++ application/views/header.php
@@ -90,14 +90,9 @@ if(!isset($hide_header) || !$hide_header){
 
                             if($identifier=='play'){
 
-                                //Player Coins:
-                                echo '<span class="parent-icon icon-block icon_en_'.$session_en['en_id'].'">'.$session_en['en_icon'].'</span>';
-                                echo '<span class="montserrat en_name_first_'.$session_en['en_id'].'">'.one_two_explode('',' ',$session_en['en_name']).'</span>';
-
-                                echo '<span class="'.superpower_active(10967).' inline-block show-max">';
                                 echo '<span class="parent-icon icon-block">'.$m['m_icon'].'</span>';
-                                echo '<span class="current_count montserrat"><i class="far fa-yin-yang fa-spin"></i></span>';
-                                echo '</span>';
+                                echo '<span class="current_count montserrat '.superpower_active(10967).'"><i class="far fa-yin-yang fa-spin"></i></span>';
+                                echo '<span class="montserrat '.$identifier.'_name">' . $m['m_name'] . '</span>';
 
                             } elseif($identifier=='read'){
 
