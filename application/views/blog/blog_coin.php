df3b8776d5e9a218aa07429a51833b328c9cd295
Shervin Enayati <shervin@mench.co>
2020-01-15 23:40:22 -0800

    iterations

--- application/views/blog/blog_coin.php
+++ application/views/blog/blog_coin.php
@@ -212,9 +212,8 @@ foreach ($this->config->item('en_all_11021') as $en_id => $m){
             }
 
             //ADD NEW NOTE:
-            $this_tab .= '<div class="'.superpower_active(10939).'">';
             $this_tab .= '<div class="list-group-item itemblog add_note_' . $en_id2 . ( $is_author && $is_active ? '' : ' hidden ' ).'">';
-            $this_tab .= '<form class="box box' . $en_id2 . '" method="post" enctype="multipart/form-data">'; //Used for dropping files
+            $this_tab .= '<form class="box box' . $en_id2 . '" method="post" enctype="multipart/form-data" class="'.superpower_active(10939).'">'; //Used for dropping files
 
 
 
@@ -254,7 +253,6 @@ foreach ($this->config->item('en_all_11021') as $en_id => $m){
 
 
             $this_tab .= '</form>';
-            $this_tab .= '</div>';
             $this_tab .= '</div>';
 
             $this_tab .= '</div>';
