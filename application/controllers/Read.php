63235544e527b6f3c4b86c662e00bffc350986cf
Shervin Enayati <shervin@mench.co>
2020-01-17 20:41:59 -0800

    Iterations

--- application/controllers/Read.php
+++ application/controllers/Read.php
@@ -104,7 +104,7 @@ class Read extends CI_Controller
         }
 
         $this->load->view('header', array(
-            'title' => 'PLAY READ IDEA',
+            'title' => '🔵PLAY 🔴READ 🟡IDEA',
         ));
         $this->load->view('read/read_home');
         $this->load->view('footer');
