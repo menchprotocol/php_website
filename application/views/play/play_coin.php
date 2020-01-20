c7efb8b002ca88c567c367dc1696f8caca0cf1c1
Shervin Enayati <shervin@mench.co>
2020-01-19 18:44:38 -0800

    Iterations

--- application/views/play/play_coin.php
+++ application/views/play/play_coin.php
@@ -398,7 +398,7 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
 
                 } elseif($default_active){
 
-                    $this_tab .= '<div class="alert alert-warning">No '.$m2['m_name'].' added yet.</div>';
+                    $this_tab .= '<div class="alert alert-warning">No ideas added yet.</div>';
 
                 }
 
