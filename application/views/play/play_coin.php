937ad71dc75d53958e1312684d412cbabde39888
Shervin Enayati <shervin@mench.co>
2020-01-21 07:42:20 -0800

    Iterations

--- application/views/play/play_coin.php
+++ application/views/play/play_coin.php
@@ -378,14 +378,13 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
 
                 //IDEA NOTES
                 $idea_note_filters = array(
-                    'in_status_play_id IN (' . join(',', $this->config->item('en_ids_12138')) . ')' => null, //Idea Statuses Featured
                     'ln_status_play_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //Link Statuses Public
                     'ln_type_play_id' => $en_id2,
                     '(ln_owner_play_id='.$player['en_id'].' OR ln_child_play_id='.$player['en_id'].' OR ln_parent_play_id='.$player['en_id'].')' => null,
                 );
 
                 //COUNT ONLY
-                $item_counters = $this->READ_model->ln_fetch($idea_note_filters, array('in_child'), 0, 0, array(), 'COUNT(ln_id) as totals');
+                $item_counters = $this->READ_model->ln_fetch($idea_note_filters, array(), 0, 0, array(), 'COUNT(ln_id) as totals');
                 $counter = $item_counters[0]['totals'];
                 $default_active = ($en_id2==4983 && ($counter>0 || !count($this->READ_model->ln_fetch(array(
                             'ln_type_play_id IN (' . join(',', $this->config->item('en_ids_4592')) . ')' => null, //Player-to-Player Links
