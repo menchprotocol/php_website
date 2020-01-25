18be1e06ddc7f741123d9cdf12d851e69821ef27
Shervin Enayati <shervin@mench.co>
2020-01-24 17:02:56 -0800

    Iterations

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -2872,7 +2872,7 @@ function echo_in_text($cache_en_id, $current_value, $in_ln__id, $is_author, $tab
     $attributes = ( $is_author ? '' : 'disabled' ).' tabindex="'.$tabindex.'" old-value="'.$current_value.'" class="form-control montserrat inline-block in_update_text text__'.$cache_en_id.'_'.$in_ln__id.' in_ln__id_'.$in_ln__id.' texttype_'.$cache_en_id.($is_idea_title_lg?'_lg':'_sm').'" cache_en_id="'.$cache_en_id.'" in_ln__id="'.$in_ln__id.'" ';
 
 
-    $tooltip_span_start = '<span class="'.( !$is_author ? 'edit-locked' : '' ).'" data-toggle="tooltip" data-placement="bottom" title="'.$en_all_12112[$cache_en_id]['m_name'].'">';
+    $tooltip_span_start = '<span class="'.( !$is_author ? 'edit-locked no-padding' : '' ).'" data-toggle="tooltip" data-placement="bottom" title="'.$en_all_12112[$cache_en_id]['m_name'].'">';
     $tooltip_span_end = '</span>';
 
 
