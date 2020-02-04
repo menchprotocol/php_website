550ff14d7bac4169810cca66949693e2db4750b2
Shervin Enayati <shervin@mench.co>
2020-02-04 13:37:30 -0800

    v1.24 Fix

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1534,7 +1534,7 @@ function echo_in_stat_play($in_id){
         $play_coins = $CI->READ_model->ln_fetch(array(
             'ln_status_play_id IN (' . join(',', $CI->config->item('en_ids_7359')) . ')' => null, //Link Statuses Public
             'ln_type_play_id' => 4983,
-            'ln_child_idea_id' => ,
+            'ln_child_idea_id' => $in_id,
         ), array(), 0, 0, array(), 'COUNT(ln_id) as total_coins');
         if($play_coins[0]['total_coins'] > 0){
             return '<span class="icon-block '.superpower_active(10984).'">'.$en_all_2738[6205]['m_icon'].'</span>'.echo_number($play_coins[0]['total_coins']);
