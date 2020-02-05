7ee6658cefb94ebbb433e51c07a07382a3d7cffe
Shervin Enayati <shervin@mench.co>
2020-02-04 19:18:28 -0800

    Iterations

--- application/views/idea/idea_home.php
+++ application/views/idea/idea_home.php
@@ -8,6 +8,7 @@
 
     $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
     $en_all_6201 = $this->config->item('en_all_6201'); //IDEA TABLE
+    $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
     if(!$session_en){
 
@@ -15,59 +16,41 @@
 
     } else {
 
+        //Add New Idea:
+        $superpower = 10939; //IDEA PEN TO START
 
-
-        echo '<div class="add-idea-toggle">';
-
-        //List current ideas:
-        $player_ideas = $this->READ_model->ln_fetch(array(
-            'in_status_play_id IN (' . join(',', $this->config->item('en_ids_7356')) . ')' => null, //Idea Statuses Active
-            'ln_status_play_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //Link Statuses Public
-            'ln_type_play_id' => 10573, //Idea Note Bookmarks
-            'ln_parent_play_id' => $session_en['en_id'], //For this trainer
-        ), array('in_child'), 0, 0, array('in_title' => 'ASC'));
-        if(count($player_ideas)){
+        if(superpower_assigned($superpower)) {
 
             echo '<div class="list-group">';
-            foreach($player_ideas as $bookmark_in){
+
+            //List current ideas:
+            foreach($this->READ_model->ln_fetch(array(
+                'in_status_play_id IN (' . join(',', $this->config->item('en_ids_7356')) . ')' => null, //Idea Statuses Active
+                'ln_status_play_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //Link Statuses Public
+                'ln_type_play_id' => 10573, //Idea Note Bookmarks
+                'ln_parent_play_id' => $session_en['en_id'], //For this trainer
+            ), array('in_child'), 0, 0, array('in_title' => 'ASC')) as $bookmark_in){
                 echo echo_in_idea($bookmark_in);
             }
+
+            echo '<div class="ideaCreateStatusUpdate montserrat" style="padding-bottom: 20px;"></div>';
+
+            echo '<div class="list-group-item itemidea '.superpower_active($superpower).'" style="padding:5px 0;">
+                <div class="input-group border">
+                    <span class="input-group-addon addon-lean" style="margin-top: 6px;"><span class="icon-block">'.$en_all_2738[4535]['m_icon'].'</span></span>
+                    <input type="text"
+                           class="form-control form-control-thick algolia_search dotransparent"
+                           maxlength="' . config_var(11071) . '"
+                           id="newIdeaTitle"
+                           style="margin-bottom: 0; padding: 5px 0;"
+                           placeholder="'.$en_all_6201[4736]['m_name'].'">
+                </div><div class="algolia_pad_search hidden in_pad_bottom"></div></div>';
+
             echo '</div>';
 
         } else {
 
-            //No bookmarks yet:
-            echo '<div class="alert alert-warning">No ideas created yet. Tap'.$add_idea_btn.' to add your first idea.</div>';
-
-        }
-
-
-        //Add New Idea:
-        echo '<a href="javascript:void(0);" onclick="idea_create_initiate()" class="btn btn-idea btn-five icon-block-lg" style="padding-top:10px;">'.$en_all_11035[12214]['m_icon'].' '.$en_all_11035[12214]['m_name'].'</a>';
-
-        echo '</div>';
-
-
-
-
-
-        //Add Idea Title:
-        echo '<div class="add-idea-toggle hidden">';
-
-        echo '<h1 class="idea"><span class="icon-block">' . $en_all_11035[12214]['m_icon'] . '</span>'.$en_all_11035[12214]['m_name'].'</h1>';
-
-        if(superpower_assigned(10939)) {
-
-            echo '<textarea id="newIdeaTitle" class="form-control algolia_search" placeholder="'.$en_all_6201[4736]['m_name'].'"></textarea>';
-
-            echo '<div class="ideaCreateStatusUpdate montserrat" style="padding-bottom: 20px;"></div>';
-
-            echo '<div class="ideaCreationController"><a href="javascript:void(0);" onclick="idea_create()" class="btn btn-idea btn-five icon-block-lg">SAVE</a> or <a href="javascript:void(0);" onclick="$(\'.add-idea-toggle\').toggleClass(\'hidden\');"><u>Cancel</u></a></div>';
-
-        } else {
-
             //Introduce Super Power:
-            $superpower = 10939; //IDEA PEN TO START
             $en_all_10957 = $this->config->item('en_all_10957'); //PLAY SUPERPOWERS
             echo '<div style="padding:10px 0;"><p>Unlock the superpowers of '.$en_all_10957[$superpower]['m_icon'].' '.$en_all_10957[$superpower]['m_name'].' to '.$en_all_10957[$superpower]['m_desc'].'</p></div>';
 
@@ -76,10 +59,6 @@
             echo '<div style="padding:10px 0;"><a href="'.$en_all_10876[$superpower]['m_desc'].'" class="btn btn-idea montserrat">GET STARTED <i class="fad fa-step-forward"></i></a></div>';
 
         }
-
-        echo '</div>';
-
-
     }
     ?>
 </div>
