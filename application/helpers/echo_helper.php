4d905c57b89fe3019fc80afa0fc9711868f2cdd7
Shervin Enayati <shervin@mench.co>
2020-02-10 11:51:25 -0800

    Iterations

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -518,7 +518,7 @@ function echo_ln($ln, $is_inner = false)
     if(!isset($en_all_4593[$ln['ln_type_play_id']])){
         //We've probably have not yet updated php cache, set error:
         $en_all_4593[$ln['ln_type_play_id']] = array(
-            'm_icon' => '<i class="fad fa-exclamation-triangle redalert"></i>',
+            'm_icon' => '<i class="fad fa-exclamation-triangle"></i>',
             'm_name' => 'Link Type Not Synced in PHP Cache',
             'm_desc' => '',
             'm_parents' => array(),
