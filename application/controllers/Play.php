3cda38a6dde6b9bdaf8fae30090d89f9f7ae2e64
Shervin Enayati <shervin@mench.co>
2020-03-18 12:16:00 -0700

    Iterations towards v1.31

--- application/controllers/Play.php
+++ application/controllers/Play.php
@@ -1746,7 +1746,7 @@ fragment PostListingItemSidebar_post on Post {
                 'en_status_play_id IN (' . join(',', $this->config->item('en_ids_7358')) . ')' => null, //Player Statuses Active
             ), array('en_child'), 0) as $en) {
                 if(!strlen($en['en_icon']) || $en['en_icon']!==$m['m_icon']){
-                    echo '['.htmlentities($en['en_icon']).'] Updated to ['.htmlentities($m['m_icon']).']<hr />';
+                    echo '@'.$en['en_id'].' ['.htmlentities($en['en_icon']).'] Updated to ['.htmlentities($m['m_icon']).']<hr />';
                     $updated++;
                     $this->PLAY_model->en_update($en['en_id'], array(
                         'en_icon' => $m['m_icon'],
