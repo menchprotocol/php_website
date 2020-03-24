6df76d0de71f133efa86015bcb45842964f18d22
Shervin Enayati <shervin@mench.co>
2020-03-24 10:27:42 -0700

    Iterations towards v1.31

--- application/views/play/play_home.php
+++ application/views/play/play_home.php
@@ -34,9 +34,6 @@ $play_coins = $this->READ_model->ln_fetch(array(
     echo '<table class="table table-sm table-striped dotransparent tablepadded">';
 
     echo '<tr>';
-
-
-    echo '<tr>';
     echo '<td class="play fixedColumns MENCHcolumn1"><span class="play"><span class="icon-block">' . $en_all_2738[4536]['m_icon'] . '</span><span class="montserrat" title="'.number_format($play_coins[0]['total_coins'], 0).'">'.echo_number($play_coins[0]['total_coins']).'</span><b class="block montserrat"><span class="icon-block show-max">&nbsp;</span>'.$en_all_2738[4536]['m_name'].'S</b></span></td>';
     echo '<td class="blog fixedColumns MENCHcolumn3"><span class="blog"><span class="icon-block">' . $en_all_2738[4535]['m_icon'] . '</span><span class="montserrat" title="'.number_format($blog_coins[0]['total_coins'], 0).'">'.echo_number($blog_coins[0]['total_coins']).'</span><b class="block montserrat"><span class="icon-block show-max">&nbsp;</span>'.$en_all_2738[4535]['m_name'].'S</b></span></td>';
     echo '<td class="read fixedColumns MENCHcolumn2"><span class="read"><span class="icon-block">' . $en_all_2738[6205]['m_icon'] . '</span><span class="montserrat" title="'.number_format($read_coins[0]['total_coins'], 0).'">'.echo_number($read_coins[0]['total_coins']).'</span><b class="block montserrat"><span class="icon-block show-max">&nbsp;</span>'.$en_all_2738[6205]['m_name'].'S</b></span></td>';
