5fb6f368be86d8d391626af4bffc43c0ce9251d0
Shervin Enayati <shervin@mench.co>
2020-03-24 19:37:47 -0700

    v1.31 Iterations towards v1.32

--- application/views/play/play_coin.php
+++ application/views/play/play_coin.php
@@ -386,8 +386,8 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH PLAYER NAVIGATION
 
                 //BLOG NOTES
                 $blog_note_filters = array(
-                    'in_status_play_id IN (' . join(',', $this->config->item('en_ids_7355')) . ')' => null, //Blog Statuses Public
-                    'ln_status_play_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //Link Statuses Public
+                    'ln_status_play_id IN (' . join(',', $this->config->item('en_ids_7360')) . ')' => null, //Link Statuses Active
+                    'in_status_play_id IN (' . join(',', $this->config->item('en_ids_7356')) . ')' => null, //Blog Statuses Active
                     'ln_type_play_id' => $en_id2,
                     '(ln_owner_play_id='.$player['en_id'].' OR ln_child_play_id='.$player['en_id'].' OR ln_parent_play_id='.$player['en_id'].')' => null,
                 );
