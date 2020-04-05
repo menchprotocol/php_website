b54a6ae9b2c86192e652bac96057a1dd327367ee
Shervin Enayati <shervin@mench.co>
2020-04-05 14:26:29 -0700

    Iterations towards v1.33

--- application/models/READ_model.php
+++ application/models/READ_model.php
@@ -1560,19 +1560,26 @@ class READ_model extends CI_Model
                     );
                 }
 
-                echo '<div id="readScroll">&nbsp;</div>';
+                $is_home = ($ins[0]['in_id']==config_var(12156));
 
-                //Redirect to login page:
-                echo '<div class="inline-block margin-top-down read-add"><a class="btn btn-read" href="/read/'.$ins[0]['in_id'].'">START HERE <i class="fad fa-step-forward"></i></a></div>';
+                if($is_home){
+                    echo '<div id="readScroll">&nbsp;</div>';
+
+                    //Redirect to login page:
+                    echo '<div class="inline-block margin-top-down read-add"><a class="btn btn-read" href="/read/'.$ins[0]['in_id'].'">START HERE <i class="fad fa-step-forward"></i></a></div>';
+
+                }
 
                 //Any Sub Topics?
                 if(count($in__children) > 0){
 
-                    //Give option to review:
-                    echo '<div class="inline-block margin-top-down read-add">&nbsp;or&nbsp;<a class="btn btn-read" href="javascript:void();" onclick="toggle_read()"><i class="fad fa-search-plus read_topics"></i><i class="fad fa-search-minus read_topics hidden"></i> LIST '.count($in__children).' READ'.echo__s(count($in__children)).'</a></div>';
+                    if($is_home){
+                        //Give option to review:
+                        echo '<div class="inline-block margin-top-down read-add">&nbsp;or&nbsp;<a class="btn btn-read" href="javascript:void();" onclick="toggle_read()"><i class="fad fa-search-plus read_topics"></i><i class="fad fa-search-minus read_topics hidden"></i> LIST '.count($in__children).' READ'.echo__s(count($in__children)).'</a></div>';
+                    }
 
                     //List Children:
-                    echo '<div class="list-group read_topics hidden">';
+                    echo '<div class="list-group '.( $is_home ? '' : 'read_topics hidden' ).'">';
                     foreach($in__children as $key => $child_in){
                         echo echo_in_read($child_in, in_array($ins[0]['in_type_source_id'], $this->config->item('en_ids_6193')));
                     }
