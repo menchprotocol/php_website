0d2f1042e1cc45d28a6be11e2d63367e4f267496
Shervin Enayati <shervin@mench.co>
2020-04-07 20:24:45 -0700

    Iterations towards v1.33

--- application/views/read/read_coin.php
+++ application/views/read/read_coin.php
@@ -8,12 +8,5 @@
 <div class="container container-wide">
     <?php
     $this->READ_model->read_echo($in['in_id'], superpower_assigned());
-
-    //Home Page Stats?
-    if($in['in_id']==config_var(12156)){
-
-        echo '<div style="padding:34px 0;"><span class="icon-block"><i class="fad fa-info-circle grey"></i></span><div class="title-block">As of <span data-toggle="tooltip" data-placement="top" title="'.date("Y-m-d H:i:s", $this->config->item('ps_timestamp')).' PST" class="montserrat">'.date("F jS", $this->config->item('ps_timestamp')).'</span>, <b class="montserrat">MENCH</b> has indexed <span class="montserrat source inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_source_count'), 0).' SOURCES (People & Content)">'.echo_number($this->config->item('ps_source_count')).'</span> <i class="fas fa-circle source"></i> SOURCE COINS</span> that earned <span class="montserrat blog inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_blog_count'), 0).' BLOGS">'.echo_number($this->config->item('ps_blog_count')).'</span> <i class="fas fa-circle blog"></i> BLOG COINS</span> that generated <span class="montserrat read inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_read_count'), 0).'">'.echo_number($this->config->item('ps_read_count')).'</span> <i class="fas fa-circle read"></i> READ COINS</span></div></div>';
-
-    }
     ?>
 </div>
--- application/views/source/source_home.php
+++ application/views/source/source_home.php
@@ -14,5 +14,8 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH NAVIGATION
     //Top Players
     echo '<div class="read-topic"><span class="icon-block">'.$en_all_11035[12437]['m_icon'].'</span>'.$en_all_11035[12437]['m_name'].'</div>';
     echo '<div id="load_leaderboard"></div>';
+
+    //Total Stats:
+    echo '<div style="padding:21px 0;"><span class="icon-block"><i class="fad fa-info-circle grey"></i></span><div class="title-block">As of <span data-toggle="tooltip" data-placement="top" title="'.date("Y-m-d H:i:s", $this->config->item('ps_timestamp')).' PST" class="montserrat">'.date("F jS", $this->config->item('ps_timestamp')).'</span>, <b class="montserrat">MENCH</b> has indexed <span class="montserrat source inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_source_count'), 0).' SOURCES (People & Content)">'.echo_number($this->config->item('ps_source_count')).'</span> <i class="fas fa-circle source"></i> SOURCE COINS</span> that earned <span class="montserrat blog inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_blog_count'), 0).' BLOGS">'.echo_number($this->config->item('ps_blog_count')).'</span> <i class="fas fa-circle blog"></i> BLOG COINS</span> that generated <span class="montserrat read inline-block"><span data-toggle="tooltip" data-placement="top" title="'.number_format($this->config->item('ps_read_count'), 0).'">'.echo_number($this->config->item('ps_read_count')).'</span> <i class="fas fa-circle read"></i> READ COINS</span></div></div>';
     ?>
 </div>
