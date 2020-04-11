7923ad3dab6240a79eaa8496280fc4bf43b4b5ab
Shervin Enayati <shervin@mench.co>
2020-04-10 17:47:29 -0700

    Iterations towards v1.33

--- application/views/source/source_auth0.php
+++ application/views/source/source_auth0.php
@@ -3,8 +3,9 @@
 //This page is loaded after user successfully authenticates via Auth0
 
 require 'vendor/autoload.php';
+use Auth0\SDK\Auth0;
 
-$auth0 = new Auth0\SDK\Auth0([
+$auth0 = new Auth0([
     'domain' => 'mench.auth0.com',
     'client_id' => 'ExW9bFiMnJX21vogqcbKCLn08djYWnsi',
     'client_secret' => $this->config->item('cred_auth0_client_secret'),
