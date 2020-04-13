df3ada1ea55e3f57493a582ab436be344b3ef423
Shervin Enayati <shervin@mench.co>
2020-04-13 10:08:32 -0700

    Iterations towards v1.33 - Navigation Updates

--- application/views/source/source_coin.php
+++ application/views/source/source_coin.php
@@ -340,7 +340,7 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH  NAVIGATION
                         if($is_unlocked || $is_available){
 
                             //Superpower Available
-                            $this_tab .= '<a class="list-group-item itemsetting btn-superpower superpower-frame-'.$superpower_en_id.' '.( in_array($superpower_en_id, $this->session->userdata('session_superpowers_activated')) ? 'active' : '' ).'" '.($should_unlock ? 'href="'.$has_training_url.'"' : 'href="javascript:void();" onclick="toggle_superpower('.$superpower_en_id.')" title="'.$m3['m_name'].' '.$m3['m_desc'].' @'.$superpower_en_id.'"').'><span class="icon-block '.$extract_icon_color.'">'.$m3['m_icon'].'</span><b class="montserrat '.$extract_icon_color.'">'.$m3['m_name'].'</b> '.$m3['m_desc'].( $should_unlock ? '<span class="icon-block"><i class="fas fa-unlock-alt"></i></span> UNLOCK NOW' : '' ).'</a>';
+                            $this_tab .= '<a class="list-group-item itemsetting btn-superpower superpower-frame-'.$superpower_en_id.' '.( in_array($superpower_en_id, $this->session->userdata('session_superpowers_activated')) ? 'active' : '' ).'" '.($should_unlock ? 'href="'.$has_training_url.'"' : 'href="javascript:void();" onclick="toggle_superpower('.$superpower_en_id.')" title="'.$m3['m_name'].' '.$m3['m_desc'].' @'.$superpower_en_id.'"').'><span class="icon-block '.$extract_icon_color.'">'.$m3['m_icon'].'</span><b class="montserrat '.$extract_icon_color.'">'.$m3['m_name'].'</b> '.$m3['m_desc'].( $should_unlock ? '<span class="icon-block"><i class="fas fa-lock black"></i></span>' : '' ).'</a>';
 
                         } else {
 
