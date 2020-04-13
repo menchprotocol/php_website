be014b944308cfb44923146b3ee8f5359fe071b3
Shervin Enayati <shervin@mench.co>
2020-04-12 21:45:26 -0700

    Iterations towards v1.33 - Navigation Updates

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -3919,7 +3919,7 @@ $config['en_all_6404'] = array(
     11060 => array(
         'm_icon' => '',
         'm_name' => 'MENCH PLATFORM VERSION',
-        'm_desc' => '1.322',
+        'm_desc' => '1.3223',
         'm_parents' => array(6404),
     ),
     12363 => array(
