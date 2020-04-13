786d2722cd5c6a2d0c7d486d1b15de0e9fcfc700
Shervin Enayati <shervin@mench.co>
2020-04-12 21:22:10 -0700

    Iterations towards v1.33 - Navigation Updates

--- application/views/source/source_leaderboard.php
+++ application/views/source/source_leaderboard.php
@@ -12,7 +12,6 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH NAVIGATION
 <div class="container">
     <?php
     //Top Players
-    echo '<div class="read-topic"><span class="icon-block">'.$en_all_11035[12437]['m_icon'].'</span>'.$en_all_11035[12437]['m_name'].'</div>';
     echo '<div id="load_leaderboard"></div>';
     ?>
 </div>
