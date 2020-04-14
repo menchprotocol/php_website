7650cfd5dacebc2a9d65b77766886bf97e9e6478
Shervin Enayati <shervin@mench.co>
2020-04-14 14:21:06 -0700

    Iterations towards v1.35

--- application/models/READ_model.php
+++ application/models/READ_model.php
@@ -2088,30 +2088,15 @@ class READ_model extends CI_Model
 
                 //TEXT RESPONSE
 
-                echo '<div class="previous_reads">';
-
-                    echo '<div class="edit-text '.($previous_answers ? '' : ' hidden ').'">';
-                        echo '<div class="read-topic"><span class="icon-block-sm"><i class="fas fa-history"></i></span>YOUR PREVIOUS ANSWER:</div>';
-                        echo $previous_answers;
-                        echo '<div><a href="javascript:void(0)" onclick="$(\'.edit-text\').toggleClass(\'hidden\');$(\'#read_text_answer\').focus();"><span class="icon-block-sm"><i class="fas fa-pen-square"></i></span>UPDATE</a></div>';
-                    echo '</div>';
-
-
-                    echo '<div class="edit-text '.($previous_answers ? ' hidden ' : '').'"><textarea class="border i_content padded read_input" placeholder="Your Answer Here..." id="read_text_answer">'.( $previous_answers ? $read_completes[0]['ln_content'] : '' ).'</textarea></div>';
-
-                echo '</div>';
+                echo '<div class="previous_reads"><textarea class="border i_content padded read_input" placeholder="Your Answer Here..." id="read_text_answer">'.( $previous_answers ? $read_completes[0]['ln_content'] : '' ).'</textarea></div>';
 
                 echo '<div class="text_saving_result margin-top-down previous_reads"></div>';
-
 
                 //Show Previous Button:
                 echo echo_in_read_previous($ins[0]['in_id'], $recipient_en);
 
                 //Save/Upload & Next:
-                echo '<div class="margin-top-down inline-block previous_reads"><a class="btn btn-read" href="javascript:void(0);" onclick="read_text_answer()">'.( $previous_answers ? 'UPDATE' : 'ANSWER' ).' & NEXT <i class="fad fa-step-forward"></i></a>&nbsp;&nbsp;</div>';
-
-                //List Children:
-                echo_in_list($ins[0], $in__children, $recipient_en, $push_message, null, true, false);
+                echo '<div class="margin-top-down inline-block previous_reads"><a class="btn btn-read" href="javascript:void(0);" onclick="read_text_answer()">SAVE & NEXT <i class="fad fa-step-forward"></i></a>&nbsp;&nbsp;</div>';
 
                 echo '<script> $(document).ready(function () { autosize($(\'#read_text_answer\')); $(\'#read_text_answer\').focus(); }); </script>';
 
