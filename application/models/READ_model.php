9accd2ed614a1c172acab2ac2f5d9a9ea24cc297
Shervin Enayati <shervin@mench.co>
2020-04-14 12:24:07 -0700

    Iterations towards v1.35

--- application/models/READ_model.php
+++ application/models/READ_model.php
@@ -2138,11 +2138,11 @@ class READ_model extends CI_Model
 
                 } else {
 
-                    //File Replace:
-                    echo '<label class="btn btn-read inline-block" for="fileType'.$ins[0]['in_type_source_id'].'" data-toggle="tooltip" style="margin-right:10px;" title="Upload files up to ' . config_var(11063) . ' MB" data-placement="top"><i class="fad fa-cloud-upload-alt"></i> REPLACE</label>';
-
                     //Next Reads:
                     echo_in_list($ins[0], $in__children, $recipient_en, $push_message, null, true);
+
+                    //File Replace:
+                    echo '<label class="btn btn-read inline-block" for="fileType'.$ins[0]['in_type_source_id'].'" data-toggle="tooltip" title="Upload files up to ' . config_var(11063) . ' MB" data-placement="top"><i class="fad fa-cloud-upload-alt"></i> REPLACE</label>';
 
                 }
 
