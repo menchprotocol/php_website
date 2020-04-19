1af0fd217613206b6b19d15533a8c1489bcad30e
Shervin Enayati <shervin@mench.co>
2020-04-18 18:28:04 -0700

    Iterations towards v1.35 - Plugins V1

--- application/views/source/source_plugin_load.php
+++ application/views/source/source_plugin_load.php
@@ -7,7 +7,7 @@ $en_all_11035 = $this->config->item('en_all_11035'); //MENCH NAVIGATION
 echo '<div class="container">';
 
     //Plugins, Header & Source Link:
-    echo '<h1 style="padding-top:5px;"><a href="/plugin"><span class="icon-block">'.echo_en_icon($en_all_11035[6287]['m_icon']).'</span></a> &raquo; <a href="/source/'.$plugin_en_id.'"><span class="icon-block">'.echo_en_icon($en_all_6287[$plugin_en_id]['m_icon']).'</span>'.$en_all_6287[$plugin_en_id]['m_name'].'</a></h1>';
+    echo '<h1 style="padding-top:5px;"><a href="/plugin"><span class="icon-block">'.echo_en_icon($en_all_11035[6287]['m_icon']).'</span></a><a href="/source/'.$plugin_en_id.'"><span class="icon-block">'.echo_en_icon($en_all_6287[$plugin_en_id]['m_icon']).'</span>'.$en_all_6287[$plugin_en_id]['m_name'].'</a></h1>';
 
 
     //Optional Description:
