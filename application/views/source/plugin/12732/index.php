2717d4d6127b975568e2e4f66e0e3f6fd09a264b
Shervin Enayati <shervin@mench.co>
2020-04-20 09:06:57 -0700

    Iterations towards v1.35

--- application/views/source/plugin/12732/index.php
+++ application/views/source/plugin/12732/index.php
@@ -6,9 +6,9 @@ $in_query = ( isset($_GET['in_id']) && intval($_GET['in_id']) ? array('in_id' =>
 $en_query = ( isset($_GET['en_id']) && intval($_GET['en_id']) ? array('en_id' => $_GET['en_id']) : array() );
 
 if(!count($en_query)){
-    echo 'IDEA: '.nl2br(print_r($this->IDEA_model->in_match_ln_status($session_en['en_id']), $in_query)).'<hr />';
+    echo 'IDEA: '.nl2br(print_r($this->IDEA_model->in_match_ln_status($session_en['en_id'], $in_query), true)).'<hr />';
 }
 
 if(!count($in_query)){
-    echo 'SOURCE: '.nl2br(print_r($this->SOURCE_model->en_match_ln_status($session_en['en_id']), $en_query)).'<hr />';
+    echo 'SOURCE: '.nl2br(print_r($this->SOURCE_model->en_match_ln_status($session_en['en_id'], $en_query), true)).'<hr />';
 }
