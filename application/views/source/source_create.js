30e1a3804731079e552fc5a1811b1145116683e8
Shervin Enayati <shervin@mench.co>
2020-04-21 15:21:30 -0700

    Iterations towards v1.36

--- application/views/source/source_create.js
+++ application/views/source/source_create.js
@@ -7,7 +7,6 @@ function create_process(){
         in_loaded_id:in_loaded_id,
         source_name: $('#source_name').val(),
         source_url: $('#source_url').val(),
-        source_en_12769: parseInt($('.dropi_12769_0_0.active').attr('new-en-id')),
         source_en_3000: parseInt($('.dropi_3000_0_0.active').attr('new-en-id')),
     });
 
@@ -40,16 +39,5 @@ function preview_update_dropdown(element_id, new_en_id){
     $('.dropd_'+element_id+'_0_0 .dropi_' + element_id +'_0_0').removeClass('active');
     $('.dropd_'+element_id+'_0_0 .optiond_' + new_en_id+'_0_0').addClass('active');
     $('.dropd_'+element_id+'_0_0').attr('selected-val' , new_en_id);
-
-    //Changing Idea Status?
-    if(element_id==12769){
-        if(new_en_id==3000){
-            //Show Content Details:
-            $('.content_type_only').removeClass('hidden');
-        } else {
-            //Hide Content Details:
-            $('.content_type_only').addClass('hidden');
-        }
-    }
 
 }
--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -30,31 +30,24 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
     echo '<h1 class="'.extract_icon_color($en_all_11035[12762]['m_icon']).'" style="padding-top:5px;"><span class="icon-block">'.echo_en_icon($en_all_11035[12762]['m_icon']).'</span>'.$en_all_11035[12762]['m_name'].'</h1>';
 
 
-
     //IDEA REFERENCE
     echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_2738[4535]['m_icon'].'</span><a href="/idea/'.$ins[0]['in_id'].'">'.echo_in_title($ins[0]).'</a></h2>';
 
 
-    //Full Name
+    //Source Name
     echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[6197]['m_icon'].'</span>'.$en_all_12762[6197]['m_name'].'</h2>';
-    echo '<div class="form-group is-empty"><input type="text" id="source_name" '.( isset($_GET['source_name']) ? ' value="'.$_GET['source_name'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[6197]['m_desc'].'"></div>';
-
-
-    //URL
-    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span>'.$en_all_12762[12763]['m_name'].'</h2>';
-    echo '<div class="form-group is-empty"><input type="url" id="source_url" '.( isset($_GET['source_url']) ? ' value="'.$_GET['source_url'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_desc'].'"></div>';
+    echo '<div class="form-group is-empty"><input type="text" id="source_name" '.( isset($_GET['source_name']) ? ' value="'.$_GET['source_name'].'" ' : '' ).' class="form-control border montserrat doupper" placeholder="'.$en_all_12762[6197]['m_desc'].'"></div>';
 
 
     //Source Type
     echo '<h2><span class="icon-block">'.$en_all_12762[12769]['m_icon'].'</span>'.$en_all_12762[12769]['m_name'].'</h2>';
-    echo echo_in_dropdown(12769, 3084, 'btn-source');
-
-
-
-    //Content Type
-    echo '<div class="content_type_only hidden">';
-
     echo echo_in_dropdown(3000, 3005, 'btn-source');
+
+
+    //Source URL
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span>'.$en_all_12762[12763]['m_name'].'</h2>';
+    echo '<div class="form-group is-empty"><input type="url" id="source_url" '.( isset($_GET['source_url']) ? ' value="'.$_GET['source_url'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_desc'].'"></div>';
+
 
     //Author(s)
     echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[12764]['m_icon'].'</span>'.$en_all_12762[12764]['m_name'].'</h2>';
@@ -65,15 +58,13 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
                            class="form-control source form-control-thick montserrat doupper algolia_search dotransparent add-input"
                            maxlength="' . config_var(11072) . '"
                            id="authorName"
-                           placeholder="PERSON NAME">
+                           placeholder="AUTHOR FULL NAME">
                 </div><div class="algolia_pad_search hidden pad_expand"></div></div>';
 
 
-    echo '</div>';
-
 
     //CREATE BUTTON:
-    echo '<div style="margin-top: 21px;"><a href="javascript:void();" onclick="create_process()" class="btn btn-source">'.$en_all_12762[12771]['m_name'].'</a></div>';
+    echo '<div style="margin-top:34px;"><a href="javascript:void();" onclick="create_process()" class="btn btn-source">'.$en_all_12762[12771]['m_name'].'</a></div>';
 
 
     ?>
