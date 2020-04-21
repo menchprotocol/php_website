1627d97d442640345e9d0ec96dc9287d42060467
Shervin Enayati <shervin@mench.co>
2020-04-21 14:20:21 -0700

    v1.35 - Iterations towards v1.36

--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -14,31 +14,34 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
     echo '<h1 class="'.extract_icon_color($en_all_11035[12762]['m_icon']).'" style="padding-top:5px;"><span class="icon-block">'.echo_en_icon($en_all_11035[12762]['m_icon']).'</span>'.$en_all_11035[12762]['m_name'].'</h1>';
 
 
-    //Full Name
-    echo '<h2><span class="icon-block">'.$en_all_12762[6197]['m_icon'].'</span>'.$en_all_12762[6197]['m_name'].'</h2>';
-    echo '<div class="form-group is-empty"><input type="text" id="source_name" '.( isset($_GET['source_name']) ? ' value="'.$_GET['source_name'].'" ' : '' ).' class="form-control border"></div>';
-
-
-    //URL
-    echo '<h2><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span>'.$en_all_12762[12763]['m_name'].'</h2>';
-    echo '<div class="form-group is-empty"><input type="url" id="source_url" '.( isset($_GET['source_url']) ? ' value="'.$_GET['source_url'].'" ' : '' ).' class="form-control border"></div>';
-
-
     //Source Type
     echo '<h2><span class="icon-block">'.$en_all_12762[12769]['m_icon'].'</span>'.$en_all_12762[12769]['m_name'].'</h2>';
     echo echo_in_dropdown(12769, 3084, 'btn-source');
 
 
+    //Full Name
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[6197]['m_icon'].'</span>'.$en_all_12762[6197]['m_name'].'</h2>';
+    echo '<div class="form-group is-empty"><input type="text" id="source_name" '.( isset($_GET['source_name']) ? ' value="'.$_GET['source_name'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[6197]['m_desc'].'"></div>';
+
+
+    //URL
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span>'.$en_all_12762[12763]['m_name'].'</h2>';
+    echo '<div class="form-group is-empty"><input type="url" id="source_url" '.( isset($_GET['source_url']) ? ' value="'.$_GET['source_url'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_desc'].'"></div>';
+
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_11035[3000]['m_icon'].'</span>'.$en_all_11035[3000]['m_name'].'</h2>';
+
+
     //Content Type
-    echo '<h2><span class="icon-block">'.$en_all_11035[3000]['m_icon'].'</span>'.$en_all_11035[3000]['m_name'].'</h2>';
+    echo '<div class="content_type_only">';
+
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_11035[3000]['m_icon'].'</span>'.$en_all_11035[3000]['m_name'].'</h2>';
     echo echo_in_dropdown(3000, 3005, 'btn-source');
 
-
     //Author(s)
-    echo '<h2><span class="icon-block">'.$en_all_12762[12764]['m_icon'].'</span>'.$en_all_12762[12764]['m_name'].'</h2>';
+    echo '<h2 style="margin-top: 21px;"><span class="icon-block">'.$en_all_12762[12764]['m_icon'].'</span>'.$en_all_12762[12764]['m_name'].'</h2>';
     echo '<div id="new-children" class="list-group-item list-adder itemsource no-side-padding">
                 <div class="input-group border">
-                    <span class="input-group-addon addon-lean icon-adder"><span class="icon-block">'.$en_all_2738[4536]['m_icon'].'</span></span>
+                    <span class="input-group-addon addon-lean icon-adder"><span class="icon-block">'.$en_all_12762[12764]['m_icon'].'</span></span>
                     <input type="text"
                            class="form-control source form-control-thick montserrat doupper algolia_search dotransparent add-input"
                            maxlength="' . config_var(11072) . '"
@@ -46,6 +49,8 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
                            placeholder="AUTHOR NAME">
                 </div><div class="algolia_pad_search hidden pad_expand"></div></div>';
 
+
+    echo '</div>';
 
     ?>
 </div>
