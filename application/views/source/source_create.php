525f7b0ffeb06899dcdf9353f6e98ab90ed52f6f
Shervin Enayati <shervin@mench.co>
2020-04-21 16:16:43 -0700

    Iterations towards v1.36

--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -19,22 +19,22 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
 
     //Content Title
-    echo '<h2 style="margin-top:34px;">'.$en_all_12762[12772]['m_name'].'</h2>';
+    echo '<h2 style="margin-top:34px;" class="space-left">'.$en_all_12762[12772]['m_name'].'</h2>';
     echo '<div><span class="icon-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper" placeholder="'.$en_all_12762[12772]['m_desc'].'"></div></div>';
 
 
     //Content Type
-    echo '<h2>'.$en_all_12762[3000]['m_name'].'</h2>';
+    echo '<h2 class="space-left">'.$en_all_12762[3000]['m_name'].'</h2>';
     echo '<div>'.echo_in_dropdown(3000, ( isset($_GET['content_type']) ? $_GET['content_type'] : 3005 /* Books */ ), 'btn-source').'</div>';
 
 
     //Content URL
-    echo '<h2 style="margin-top: 21px;">'.$en_all_12762[12763]['m_name'].'</h2>';
+    echo '<h2 style="margin-top: 21px;" class="space-left">'.$en_all_12762[12763]['m_name'].'</h2>';
     echo '<div><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty inline-block"><input type="url" id="content_url" '.( isset($_GET['content_url']) ? ' value="'.$_GET['content_url'].'" ' : '' ).' class="form-control border" placeholder="'.str_replace(' ','',$en_all_12762[12763]['m_desc']).'"></div></div>';
 
 
     //Industry Experts
-    echo '<h2 style="margin-top: 21px;"><span class="icon-block">&nbsp;</span>'.$en_all_11035[3084]['m_name'].'</h2>';
+    echo '<h2 style="margin-top: 21px;" class="space-left">'.$en_all_11035[3084]['m_name'].'</h2>';
     echo '<div id="new-children" class="list-group-item list-adder itemsource no-side-padding">
                 <div class="input-group border">
                     <span class="input-group-addon addon-lean icon-adder"><span class="icon-block">'.$en_all_11035[3084]['m_icon'].'</span></span>
