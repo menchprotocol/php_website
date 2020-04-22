f2f569be550456f19be36e10918f9ff7e8707c8b
Shervin Enayati <shervin@mench.co>
2020-04-21 21:59:25 -0700

    Iterations towards v1.36

--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -19,7 +19,7 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
 
     //Content Title
-    echo '<div><span class="icon-block icon-white-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" style="padding-left: 0;" placeholder="'.$en_all_12762[12772]['m_name'].'"></div></div>';
+    echo '<div><span class="icon-block icon-white-block" style="padding-top: 7px;">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" style="padding-left: 0;" placeholder="'.$en_all_12762[12772]['m_name'].'"></div></div>';
 
     //Content URL
     echo '<div><span class="icon-block icon-white-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="url" id="ln_content" '.( isset($_GET['ln_content']) ? ' value="'.$_GET['ln_content'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_name'].'" style="padding-left: 0;"></div></div>';
