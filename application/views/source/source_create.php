81ea8395b6ad80fbb83e13f7a636140d8c750f53
Shervin Enayati <shervin@mench.co>
2020-04-21 21:42:48 -0700

    Iterations towards v1.36

--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -19,11 +19,11 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
 
     //Content Title
-    echo '<div><span class="icon-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" placeholder="'.$en_all_12762[12772]['m_desc'].'"></div></div>';
+    echo '<div><span class="icon-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" placeholder="'.$en_all_12762[12772]['m_name'].'"></div></div>';
 
 
     //Content URL
-    echo '<div><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="url" id="ln_content" '.( isset($_GET['ln_content']) ? ' value="'.$_GET['ln_content'].'" ' : '' ).' class="form-control border" placeholder="'.str_replace(' ','',$en_all_12762[12763]['m_desc']).'"></div></div>';
+    echo '<div><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="url" id="ln_content" '.( isset($_GET['ln_content']) ? ' value="'.$_GET['ln_content'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_name'].'"></div></div>';
     //echo '<span id="en_type_link_id" class="space-left"></span>';
     //echo '<p id="en_link_preview" class="hideIfEmpty space-left"></p>';
 
@@ -36,12 +36,12 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
     echo '<div class="new_experts hideIfEmpty"></div>';
     echo '<div id="addContent" class="list-group-item list-adder itemsource no-side-padding">
                 <div class="input-group border">
-                    <span class="input-group-addon addon-lean icon-adder"><span class="icon-block">'.$en_all_11035[3084]['m_icon'].'</span></span>
+                    <span class="input-group-addon addon-lean icon-adder"><span class="icon-block">'.$en_all_12762[12764]['m_icon'].'</span></span>
                     <input type="text"
                            class="form-control source form-control-thick montserrat doupper algolia_search dotransparent add-input"
                            maxlength="' . config_var(11072) . '"
                            id="authorName"
-                           placeholder="AUTHOR FULL NAME">
+                           placeholder="'.$en_all_12762[12764]['m_name'].'">
                 </div><div class="algolia_pad_search hidden pad_expand"></div></div>';
 
 
