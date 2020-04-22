b1b09200888d4158e9b5ee863955db953b5f9525
Shervin Enayati <shervin@mench.co>
2020-04-21 21:48:10 -0700

    Iterations towards v1.36

--- application/views/source/source_create.php
+++ application/views/source/source_create.php
@@ -19,17 +19,15 @@ $en_all_2738 = $this->config->item('en_all_2738'); //MENCH
 
 
     //Content Title
-    echo '<div><span class="icon-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" placeholder="'.$en_all_12762[12772]['m_name'].'"></div></div>';
-
-
-    //Content URL
-    echo '<div><span class="icon-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="url" id="ln_content" '.( isset($_GET['ln_content']) ? ' value="'.$_GET['ln_content'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_name'].'"></div></div>';
-    //echo '<span id="en_type_link_id" class="space-left"></span>';
-    //echo '<p id="en_link_preview" class="hideIfEmpty space-left"></p>';
-
+    echo '<div><span class="icon-block icon-white-block">'.$en_all_12762[12772]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="text" id="content_title" '.( isset($_GET['content_title']) ? ' value="'.$_GET['content_title'].'" ' : '' ).' class="form-control border montserrat doupper source" placeholder="'.$en_all_12762[12772]['m_name'].'"></div></div>';
 
     //Content Type
-    echo '<div class="space-left" style="padding-bottom: 10px;">'.echo_in_dropdown(3000, ( isset($_GET['content_type']) ? $_GET['content_type'] : 3005 /* Books */ ), 'btn-source').'</div>';
+    echo '<div class="space-left" style="padding-bottom: 15px;">'.echo_in_dropdown(3000, ( isset($_GET['content_type']) ? $_GET['content_type'] : 3005 /* Books */ ), 'btn-source').'</div>';
+
+    //Content URL
+    echo '<div><span class="icon-block icon-white-block">'.$en_all_12762[12763]['m_icon'].'</span><div class="form-group is-empty block-one inline-block"><input type="url" id="ln_content" '.( isset($_GET['ln_content']) ? ' value="'.$_GET['ln_content'].'" ' : '' ).' class="form-control border" placeholder="'.$en_all_12762[12763]['m_name'].'"></div></div>';
+    //echo '<span id="en_type_link_id" class="space-left"></span>';
+    //echo '<p id="en_link_preview" class="hideIfEmpty space-left"></p>';
 
 
     //Industry Experts
