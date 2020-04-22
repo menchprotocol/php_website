4506e227daaf43b8c9cd860de02e4f91ad423b19
Shervin Enayati <shervin@mench.co>
2020-04-22 15:01:24 -0700

    Iterations towards v1.36

--- application/models/DISCOVER_model.php
+++ application/models/DISCOVER_model.php
@@ -889,7 +889,7 @@ class DISCOVER_model extends CI_Model
             } else {
 
                 //IDEA TITLE
-                echo '<h1 style="padding-top:5px;" class="block-one"><span class="icon-block top-icon"><i class="fas fa-circle discover"></i></span>' . echo_in_title($ins[0]) . '</h1>';
+                echo '<h1 style="padding-top:5px;" class="block-one"><span class="icon-block top-icon"><i class="fas fa-circle discover"></i></span><span class="title-block-lg">' . echo_in_title($ins[0]) . '</span></h1>';
 
 
                 foreach ($in__messages as $message_ln) {
@@ -1092,7 +1092,7 @@ class DISCOVER_model extends CI_Model
                 }
 
                 //DISCOVER TITLE
-                echo '<h1 style="padding-top:1px;" class="block-one"><span class="icon-block top-icon"><i class="fas fa-circle discover" aria-hidden="true"></i></span>' . echo_in_title($ins[0]) . '</h1>';
+                echo '<h1 style="padding-top:1px;" class="block-one"><span class="icon-block top-icon"><i class="fas fa-circle discover" aria-hidden="true"></i></span><span class="title-block-lg">' . echo_in_title($ins[0]) . '</span></h1>';
 
             } else {
 
