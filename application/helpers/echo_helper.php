011fe94a1eadde94c460c31a2963d3fa49aed0f4
Shervin Enayati <shervin@mench.co>
2020-04-24 11:50:44 -0700

    Iterations towards v1.36

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -77,7 +77,7 @@ function echo_url_types($url, $en_type_link_id)
 
     if ($en_type_link_id == 4256 /* Generic URL */) {
 
-        return '<a href="' . $url . '" target="_blank"><span class="url_truncate"><span class="icon-block-xs"><i class="far fa-external-link"></i></span>' . echo_url_clean($url) . '</span></a>';
+        return '<div class="paddingup block"><a href="' . $url . '" target="_blank"><span class="url_truncate"><span class="icon-block-xs"><i class="far fa-external-link"></i></span>' . echo_url_clean($url) . '</span></a></div>';
 
     } elseif ($en_type_link_id == 4257 /* Embed Widget URL? */) {
 
