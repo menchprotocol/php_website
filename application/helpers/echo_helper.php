ef041a99d3815c04658dba1b7ba7e0ef83a338cb
Shervin Enayati <shervin@mench.co>
2020-04-26 16:55:44 -0700

    Iterations towards v1.36

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1044,7 +1044,7 @@ function echo_in_discover($in, $parent_is_or = false, $common_prefix = null, $ex
 
 
     //DISCOVER ICON
-    $ui .= '<span class="icon-block">'.( $can_click ? '<i class="fas fa-circle discover"></i>' : '<i class="far fa-lock discover"></i>' ).'</span>';
+    $ui .= '<span class="icon-block">'.( $can_click ? '<i class="fas fa-circle discover"></i>' : '<i class="fad fa-eye-slash discover"></i>' ).'</span>';
     $ui .= '<b class="montserrat idea-url title-block" style="padding-right:23px;">'.echo_in_title($in, false, $common_prefix).'</b>';
 
 
