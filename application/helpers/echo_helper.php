faf864db50a685aceeaa70225d5ff8bd6917152a
Shervin Enayati <shervin@mench.co>
2020-04-26 17:24:27 -0700

    Iterations towards v1.36

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1064,7 +1064,13 @@ function echo_in_discover($in, $parent_is_or = false, $common_prefix = null, $ex
 
     }
 
+    if($can_click && $completion_rate['completion_percentage']>0){
+        $ui .= '<div class="high5x">&nbsp;</div>';
+    }
+
     $ui .= ( $can_click ? '</a>' : '' );
+
+
     $ui .= '</div>';
 
     return $ui;
