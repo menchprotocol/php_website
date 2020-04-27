520430bf32ed47670602b8a010cc47b2d1d833c4
Shervin Enayati <shervin@mench.co>
2020-04-27 07:32:35 -0700

    Iterations towards v1.36

--- application/models/DISCOVER_model.php
+++ application/models/DISCOVER_model.php
@@ -1120,7 +1120,7 @@ class DISCOVER_model extends CI_Model
             } else {
 
                 //JS Redirect asap:
-                echo "<script> $(document).ready(function () { window.location = '/discover/next/' + in_loaded_id + '?came_from=".$in_id."'; }); </script>";
+                echo "<script> $(document).ready(function () { window.location = '/discover/next/' + in_loaded_id + '".( isset($_GET['came_from']) && $_GET['came_from']>0 ? '?came_from='.$_GET['came_from'] : '' )."'; }); </script>";
 
             }
         }
