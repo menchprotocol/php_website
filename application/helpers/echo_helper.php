421464e7416d79b9839bb6460abfafdda44df7f9
Shervin Enayati <shervin@mench.co>
2020-04-29 17:14:43 -0700

    Iterations towards v1.37

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1437,7 +1437,10 @@ function echo_en_basic($en)
 {
     $ui = '<a href="/source/'.$en['en_id'] . '" class="list-group-item no-side-padding itemsource en-item object_highlight">';
     $ui .= '<span class="icon-block">' . echo_en_icon($en['en_icon']) . '</span>';
-    $ui .= '<span class="title-block title-no-right montserrat '.extract_icon_color($en['en_icon']).'">'.$en['en_name'].'</span>';
+    $ui .= '<span class="title-block title-no-right montserrat">'.$en['en_name'].'</span>';
+    if(strlen($en['ln_content'])){
+        $ui .= '<span>&nbsp;'.echo_ln_urls($en['ln_content'] , $en['ln_type_source_id']).'</span>';
+    }
     $ui .= '</a>';
     return $ui;
 }
