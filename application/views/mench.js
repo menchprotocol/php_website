d6b3b054de66101b174c7c790cbdafe2844028bd
Shervin Enayati <shervin@mench.co>
2020-04-30 15:20:35 -0700

    Iterations towards v1.37

--- application/views/mench.js
+++ application/views/mench.js
@@ -150,7 +150,7 @@ function echo_search_result(alg_obj){
     var obj_icon = ( is_idea ? '<i class="fas fa-circle '+( js_session_superpowers_assigned.includes(10939) ? 'idea' : 'discover' )+'"></i>' : alg_obj.alg_obj_icon );
     var obj_full_name = ( alg_obj._highlightResult && alg_obj._highlightResult.alg_obj_name.value ? alg_obj._highlightResult.alg_obj_name.value : alg_obj.alg_obj_name );
 
-    return '<span class="icon-block-sm">'+ obj_icon +'</span>' + ( is_public ? '' : '<span class="icon-block-sm"><i class="far fa-spinner fa-spin"></i></span>' ) + '<span class="'+ ( !is_idea ? js_extract_icon_color(obj_icon) : '' ) +'">' + htmlentitiesjs(obj_full_name) + '</span>';
+    return '<span class="icon-block-sm">'+ obj_icon +'</span>' + ( is_public ? '' : '<span class="icon-block-sm"><i class="far fa-spinner fa-spin"></i></span>' ) + '<span class="'+ ( !is_idea ? js_extract_icon_color(obj_icon) : '' ) +'">' + htmlentitiesjs(obj_full_name+'aa') + '</span>';
 
 }
 
