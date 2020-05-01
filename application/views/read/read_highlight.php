2f2c9c07674485fce7264662c63d89a9d9d0397e
Shervin Enayati <shervin@mench.co>
2020-04-30 17:54:13 -0700

    Iterations towards v1.37

--- application/views/read/read_highlight.php
+++ application/views/read/read_highlight.php
@@ -4,20 +4,30 @@
 <div class="container">
     <?php
 
-    //DISPLAY HIGHLIGHTS
-    $en_all_11035 = $this->config->item('en_all_11035'); //SOURCE
-    echo '<div class="read-topic"><span class="icon-block">'.$en_all_11035[12896]['m_icon'].'</span>'.$en_all_11035[12896]['m_name'].'</div>';
-
-    echo '<div id="actionplan_steps" class="list-group no-side-padding">';
-    foreach($this->LEDGER_model->ln_fetch(array(
+    $player_highlights = $this->LEDGER_model->ln_fetch(array(
         'ln_creator_source_id' => $session_en['en_id'],
         'ln_type_source_id' => 12896, //HIGHLIGHTS
         'in_status_source_id IN (' . join(',', $this->config->item('en_ids_7355')) . ')' => null, //PUBLIC
         'ln_status_source_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //PUBLIC
-    ), array('in_previous'), 0, 0, array('ln_id' => 'DESC')) as $priority => $in) {
-        echo echo_in_read($in, false, null, null, true);
+    ), array('in_previous'), 0, 0, array('ln_id' => 'DESC'));
+
+    //DISPLAY HIGHLIGHTS
+    $en_all_11035 = $this->config->item('en_all_11035'); //SOURCE
+    echo '<div class="read-topic"><span class="icon-block">'.$en_all_11035[12896]['m_icon'].'</span>'.$en_all_11035[12896]['m_name'].'</div>';
+
+    if(!count($player_highlights)){
+
+        echo '<div class="alert alert-danger"><span class="icon-block"><i class="fas fa-exclamation-circle read"></i></span>No highlights yet. You can highlight an idea using the '.$en_all_11035[12896]['m_icon'].' icon while reading.</div>';
+
+    } else {
+
+        echo '<div id="actionplan_steps" class="list-group no-side-padding">';
+        foreach($player_highlights as $priority => $in) {
+            echo echo_in_read($in, false, null, null, true);
+        }
+        echo '</div>';
+
     }
-    echo '</div>';
 
     ?>
 </div>
--- application/views/read/read_home.php
+++ application/views/read/read_home.php
@@ -31,44 +31,48 @@ $en_all_11035 = $this->config->item('en_all_11035'); //SOURCE
 echo '<div class="read-topic"><span class="icon-block">'.$en_all_11035[7347]['m_icon'].'</span>'.$en_all_11035[7347]['m_name'].'</div>';
 
 
-echo '<div class="clear-reads-list">';
+
 
 //List Reads:
-$all_completed = true;
-echo '<div id="actionplan_steps" class="list-group no-side-padding">';
-foreach($player_reads as $priority => $in) {
-    $completion_rate = $this->READ_model->read_completion_progress($session_en['en_id'], $in);
-    echo echo_in_read($in, false, null, null, true, $completion_rate);
-    if($completion_rate['completion_percentage']!=100 && $all_completed){
-        $all_completed = false;
+if(!count($player_reads)){
+
+    echo '<div class="alert alert-danger"><span class="icon-block"><i class="fas fa-exclamation-circle read"></i></span>No Reads yet. Visit <a href="/" class="underline">Home</a> to start reading.</div>';
+
+} else {
+
+    echo '<div class="clear-reads-list">';
+
+    $all_completed = true;
+    echo '<div id="actionplan_steps" class="list-group no-side-padding">';
+    foreach($player_reads as $priority => $in) {
+        $completion_rate = $this->READ_model->read_completion_progress($session_en['en_id'], $in);
+        echo echo_in_read($in, false, null, null, true, $completion_rate);
+        if($completion_rate['completion_percentage']!=100 && $all_completed){
+            $all_completed = false;
+        }
     }
+    echo '</div>';
+
+    //NEXT
+    if(!$all_completed){
+        echo '<div class="inline-block margin-top-down pull-right"><a href="/read/next" class="btn btn-read btn-circle">'.$en_all_11035[12211]['m_icon'].'</a></div>';
+    }
+
+
+    echo '</div>';
+
+
+    //READ DELETE ALL (ACCESSIBLE VIA MAIN MENU)
+    echo '<div class="clear-reads-list hidden margin-top-down">';
+    echo '<div class="alert alert-danger" role="alert">';
+    echo '<span class="icon-block"><i class="fas fa-exclamation-circle read"></i></span><b class="read montserrat">DELETE ALL READ COINS?</b>';
+    echo '<br /><span class="icon-block">&nbsp;</span>Action cannot be undone.';
+    echo '</div>';
+    echo '<p style="margin-top:20px;"><a href="javascript:void(0);" onclick="read_clear_all()" class="btn btn-read"><i class="far fa-trash-alt"></i> DELETE ALL</a> or <a href="javascript:void(0)" onclick="$(\'.clear-reads-list\').toggleClass(\'hidden\')" style="text-decoration: underline;">Cancel</a></p>';
+    echo '</div>';
+
 }
-echo '</div>';
 
-
-//HOME
-echo '<div class="inline-block margin-top-down pull-left"><a href="/" class="btn btn-read btn-circle">'.$en_all_11035[12581]['m_icon'].'</a></div>';
-
-
-//NEXT
-if(!$all_completed){
-    echo '<div class="inline-block margin-top-down pull-right"><a href="/read/next" class="btn btn-read btn-circle">'.$en_all_11035[12211]['m_icon'].'</a></div>';
-}
-
-echo '</div>';
-
-
-
-
-
-//READ DELETE ALL (ACCESSIBLE VIA MAIN MENU)
-echo '<div class="clear-reads-list hidden margin-top-down">';
-echo '<div class="alert alert-danger" role="alert">';
-echo '<span class="icon-block"><i class="fas fa-exclamation-circle read"></i></span><b class="read montserrat">DELETE ALL READ COINS?</b>';
-echo '<br /><span class="icon-block">&nbsp;</span>Action cannot be undone.';
-echo '</div>';
-echo '<p style="margin-top:20px;"><a href="javascript:void(0);" onclick="read_clear_all()" class="btn btn-read"><i class="far fa-trash-alt"></i> DELETE ALL</a> or <a href="javascript:void(0)" onclick="$(\'.clear-reads-list\').toggleClass(\'hidden\')" style="text-decoration: underline;">Cancel</a></p>';
-echo '</div>';
 
 
 
