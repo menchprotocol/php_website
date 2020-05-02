8ffb9d675d61cb12248768cf775931c1e4683227
Shervin Enayati <shervin@mench.co>
2020-05-01 19:39:31 -0700

    Iterations towards v1.37

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -668,10 +668,11 @@ function echo_in_read($in, $parent_is_or = false, $common_prefix = null, $extra_
     $is_highlight = ( isset($in['ln_type_source_id']) && $in['ln_type_source_id']==12896 );
     $metadata = unserialize($in['in_metadata']);
     $has_time_estimate = ( isset($metadata['in__metadata_max_seconds']) && $metadata['in__metadata_max_seconds']>0 );
-    $completion_rate['completion_percentage'] = 0;
 
     if(!$completion_rate && !$is_highlight && $recipient_en){
         $completion_rate = $CI->READ_model->read_completion_progress($recipient_en['en_id'], $in);
+    } else {
+        $completion_rate['completion_percentage'] = 0;
     }
 
     $can_click = ( ( $parent_is_or && in_array($in['in_status_source_id'], $CI->config->item('en_ids_12138')) ) || $completion_rate['completion_percentage']>0 || $show_editor ); //|| $recipient_en
