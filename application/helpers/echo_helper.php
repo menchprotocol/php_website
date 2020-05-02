d3438acba7695f3c6583473f2d83037dc31d5528
Shervin Enayati <shervin@mench.co>
2020-05-02 14:11:31 -0700

    Iterations towards v1.37

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -674,11 +674,11 @@ function echo_in_read($in, $parent_is_or = false, $common_prefix = null, $extra_
 
     $ui  = '<div id="ap_in_'.$in['in_id'].'" '.( isset($in['ln_id']) ? ' sort-link-id="'.$in['ln_id'].'" ' : '' ).' class="list-group-item no-side-padding '.( $show_editor ? 'bookshelf_sort' : '' ).' itemread '.$extra_class.'">';
     $ui .= ( $can_click ? '<a href="/'.$in['in_id'] . '" class="itemread">' : '' );
-    if($can_click && $completion_rate['completion_percentage']>0){
+    if($can_click && $completion_rate['completion_percentage']>0 && $completion_rate['completion_percentage']<100){
         $ui .= '<div class="progress-bg-list" title="Read '.$completion_rate['steps_completed'].'/'.$completion_rate['steps_total'].' Ideas ('.$completion_rate['completion_percentage'].'%)"><div class="progress-done" style="width:'.$completion_rate['completion_percentage'].'%"></div></div>';
     }
 
-    $ui .= '<span class="icon-block">'.( $can_click ? '<i class="fas fa-circle read"></i>' : '<i class="fas fa-circle idea"></i>' ).'</span>';
+    $ui .= '<span class="icon-block">'.( $can_click && $completion_rate['completion_percentage']==100 ? '<i class="fas fa-circle read"></i>' : '<i class="fas fa-circle idea"></i>' ).'</span>';
     $ui .= '<b class="montserrat idea-url title-block">'.echo_in_title($in, $common_prefix).'</b>';
 
     //Search for Idea Image:
