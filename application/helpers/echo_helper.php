4e460fc7bb14efe2a26acf99edd2332bc53d6ab6
Shervin Enayati <shervin@mench.co>
2020-05-01 19:08:20 -0700

    Iterations towards v1.37

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -1161,12 +1161,12 @@ function echo_in_previous_read($in_id, $recipient_en){
 
 
         //Check Highlight status
-        $is_highlighted = count($this->LEDGER_model->ln_fetch(array(
+        $is_highlighted = count($CI->LEDGER_model->ln_fetch(array(
             'ln_profile_source_id' => $recipient_en['en_id'],
             'ln_next_idea_id' => $in_id,
             'ln_type_source_id' => 12896, //HIGHLIGHTS
-            'in_status_source_id IN (' . join(',', $this->config->item('en_ids_7355')) . ')' => null, //PUBLIC
-            'ln_status_source_id IN (' . join(',', $this->config->item('en_ids_7359')) . ')' => null, //PUBLIC
+            'in_status_source_id IN (' . join(',', $CI->config->item('en_ids_7355')) . ')' => null, //PUBLIC
+            'ln_status_source_id IN (' . join(',', $CI->config->item('en_ids_7359')) . ')' => null, //PUBLIC
         )));
 
         $ui .= '<div class="inline-block margin-top-down pull-left"><a class="btn btn-read btn-circle" href="javascript:void(0);" onclick="in_bookmark('.$in_id.', '.( $is_highlighted ? 0 : 1 ).')">'.( $is_highlighted ? '<i class="fas fa-bookmark"></i>' : '<i class="far fa-bookmark"></i>' ).'</a></div>';
