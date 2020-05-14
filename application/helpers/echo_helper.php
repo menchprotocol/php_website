6530912fb8aa8c869f094b6e569e277378344d14
Shervin Enayati <shervin@mench.co>
2020-05-14 15:53:06 -0700

    Iterations towards v1.39

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -661,7 +661,7 @@ function echo_in_read($in, $common_prefix = null, $show_editor = false, $complet
 
     //Right Stats:
     if($has_time_estimate || $idea_count){
-        $ui .= '<div class="pull-right montserrat" style="width:136px;"><span style="width:53px; display: inline-block;">'.( $idea_count ? '<i class="fas fa-circle idea"></i><span style="padding-left:3px;" class="idea">'.$idea_count.'</span>' : '' ).'</span>'.( $has_time_estimate ? '<span class="grey">'.echo_time_range($metadata).'</span>': '' ).'</div>';
+        $ui .= '<div class="pull-right montserrat" style="'.( $show_editor ? 'width:155px;' : 'width:136px;' ).'"><span style="width:53px; display: inline-block;">'.( $idea_count ? '<i class="fas fa-circle idea"></i><span style="padding-left:3px;" class="idea">'.$idea_count.'</span>' : '' ).'</span>'.( $has_time_estimate ? '<span class="grey">'.echo_time_range($metadata).'</span>': '' ).'</div>';
     }
 
 
