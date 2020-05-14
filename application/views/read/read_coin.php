321449e3e5de726341d3241bf00eae4bd5577f70
Shervin Enayati <shervin@mench.co>
2020-05-14 11:32:40 -0700

    Iterations towards v1.39

--- application/views/read/read_coin.php
+++ application/views/read/read_coin.php
@@ -412,8 +412,8 @@ if(!$read_in_home){
                 echo '<table class="table table-sm" style="background-color: transparent !important; margin-bottom: 0;"><tr>';
                 echo '<td class="icon-block check-icon" style="padding: 0 !important;"><i class="'.( $previously_selected ? 'fas' : 'far' ).' fa-circle read"></i></td>';
 
-                echo '<td style="width: auto; padding: 0 !important;">';
-                echo '<b class="montserrat idea-url inline-block" style="margin-left:0; width:100%;">'.echo_in_title($child_in, $common_prefix).'</b>';
+                echo '<td style="width:calc(100% - 193px); padding: 0 !important;">';
+                echo '<b class="montserrat idea-url" style="margin-left:0;">'.echo_in_title($child_in, $common_prefix).'</b>';
                 echo '</td>';
 
                 //Right Stats:
