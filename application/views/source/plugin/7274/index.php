cf3ac44531e424b34461873fc750058ed6e469b7
Shervin Enayati <shervin@mench.co>
2020-05-16 15:09:40 -0700

    Iterations towards v1.39

--- application/views/source/plugin/7274/index.php
+++ application/views/source/plugin/7274/index.php
@@ -18,9 +18,8 @@ foreach($cron_jobs as $cron_job){
     }
 }
 
-echo '<div style="margin-bottom: 34px;">Copy/Paste the following code in crontab -e</div>';
-
-echo '<textarea style="background-color:#FFFFFF; padding:20px; font-family: monospace; font-size:0.8em; height: 500px; width: 100%; border-radius: 10px;">';
+echo '<div style="margin-bottom:13px;">Copy/Paste the following code in crontab -e</div>';
+echo '<textarea style="background-color:#FFFFFF; padding:20px; font-family: monospace; font-size:0.8em; height:377px; width: 100%; border-radius: 10px;">';
 echo '# PLUGINS WITH CRON JOBS:'."\n"."\n";
 foreach($cron_jobs as $cron_job){
     echo str_pad($cron_job['ln_content'], $longest_time, " ", STR_PAD_RIGHT).' '.config_var(7274).' '.str_pad($cron_job['en_id'], $longest_id, " ", STR_PAD_RIGHT).' #'.$cron_job['en_name']."\n";
