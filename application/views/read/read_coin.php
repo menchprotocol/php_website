ba4703836712b30391b5737d2788499bd66b0b12
Shervin Enayati <shervin@mench.co>
2020-05-18 22:08:52 -0700

    Iterations towards v1.40

--- application/views/read/read_coin.php
+++ application/views/read/read_coin.php
@@ -1,8 +1,10 @@
 <script>
     var in_loaded_id = <?= $in['in_id'] ?>;
 </script>
+<script async src="https://platform-api.sharethis.com/js/sharethis.js#property=5ec369bdaa9dfe001ab3f797&product=sticky-share-buttons"></script>
 <script src="/application/views/read/read_coin.js?v=<?= config_var(11060) ?>"
         type="text/javascript"></script>
+
 
 <div class="container">
 <?php
