4c00b51e6e1a83667654f18d24f86df503b03133
Shervin Enayati <shervin@mench.co>
2020-05-22 11:13:22 -0700

    Iterations towards v1.41

--- application/views/source/plugin/7260/index.php
+++ application/views/source/plugin/7260/index.php
@@ -1,8 +1,5 @@
 <?php
 
-$sources__4737 = $this->config->item('sources__4737'); // Idea Status
-
-
 $orphan_ideas = $this->IDEA_model->fetch(array(
     ' NOT EXISTS (SELECT 1 FROM mench_interactions WHERE idea__id=read__right AND read__type IN (' . join(',', $this->config->item('sources_id_4486')) . ') AND read__status IN ('.join(',', $this->config->item('sources_id_7360')) /* ACTIVE */.')) ' => null,
     'idea__status IN (' . join(',', $this->config->item('sources_id_7356')) . ')' => null, //ACTIVE
@@ -12,33 +9,16 @@ $orphan_ideas = $this->IDEA_model->fetch(array(
 if(count($orphan_ideas) > 0){
 
     //List orphans:
-    foreach($orphan_ideas as $count => $orphan_in) {
-
-        //Show idea:
-        echo '<div>'.($count+1).') <span data-toggle="tooltip" data-placement="right" title="'.$sources__4737[$orphan_in['idea__status']]['m_name'].': '.$sources__4737[$orphan_in['idea__status']]['m_desc'].'">' . $sources__4737[$orphan_in['idea__status']]['m_icon'] . '</span> <a href="/g'.$orphan_in['idea__id'].'"><b>'.$orphan_in['idea__title'].'</b></a>';
-
-        //Do we need to delete?
-        if(isset($_GET['take_action']) && $_GET['take_action']=='delete_all'){
-
-            //Delete idea links:
-            $links_deleted = $this->IDEA_model->unlink($orphan_in['idea__id'] , $session_source['source__id']);
-
-            //Delete idea:
-            $this->IDEA_model->update($orphan_in['idea__id'], array(
-                'idea__status' => 6182, /* Idea Deleted */
-            ), true, $session_source['source__id']);
-
-            //Show confirmation:
-            echo ' [Idea + '.$links_deleted.' links Deleted]';
-
-        }
-
-        //Done showing the idea:
-        echo '</div>';
+    echo '<div class="list-group">';
+    foreach($orphan_ideas as $idea) {
+        echo view_idea($idea, 0, false, true);
     }
+    echo '</div>';
+
 
     //Show option to delete all:
     if(!isset($_GET['take_action']) || $_GET['take_action']!='delete_all'){
+        echo '<br />';
         echo '<br />';
         echo '<a class="delete-all" href="javascript:void(0);" onclick="$(\'.delete-all\').toggleClass(\'hidden\')">Delete All</a>';
         echo '<div class="delete-all hidden maxout"><b class="read">WARNING</b>: All ideas and all their links will be deleted. ONLY do this after reviewing all orphans one-by-one and making sure they cannot become a child of an existing idea.<br /><br /></div>';
--- application/views/source/plugin/7269/index.php
+++ application/views/source/plugin/7269/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$sources__6177 = $this->config->item('sources__6177'); //Source Status
-
 $source_orphans = $this->SOURCE_model->fetch(array(
     ' NOT EXISTS (SELECT 1 FROM mench_interactions WHERE source__id=read__down AND read__type IN (' . join(',', $this->config->item('sources_id_4592')) . ') AND read__status IN ('.join(',', $this->config->item('sources_id_7360')) /* ACTIVE */.')) ' => null,
     'source__status IN (' . join(',', $this->config->item('sources_id_7358')) . ')' => null, //ACTIVE
@@ -9,34 +7,16 @@ $source_orphans = $this->SOURCE_model->fetch(array(
 
 if(count($source_orphans) > 0){
 
-    //List orphans:
-    foreach($source_orphans  as $count => $source_orphan) {
-
-        //Show source:
-        echo '<div>'.($count+1).') <span data-toggle="tooltip" data-placement="right" title="'.$sources__6177[$source_orphan['source__status']]['m_name'].': '.$sources__6177[$source_orphan['source__status']]['m_desc'].'">' . $sources__6177[$source_orphan['source__status']]['m_icon'] . '</span> <a href="/@'.$source_orphan['source__id'].'"><b>'.$source_orphan['source__title'].'</b></a>';
-
-        //Do we need to delete?
-        if(isset($_GET['take_action']) && $_GET['take_action']=='delete_all'){
-
-            //Delete links:
-            $links_deleted = $this->SOURCE_model->unlink($source_orphan['source__id'], $session_source['source__id']);
-
-            //Delete source:
-            $this->SOURCE_model->update($source_orphan['source__id'], array(
-                'source__status' => 6178, /* Player Deleted */
-            ), true, $session_source['source__id']);
-
-            //Show confirmation:
-            echo ' [Source + '.$links_deleted.' links Deleted]';
-
-        }
-
-        echo '</div>';
-
+    echo '<div class="list-group">';
+    foreach($source_orphans as $source) {
+        echo view_source($source, false, null, true, true);
     }
+    echo '</div>';
+
 
     //Show option to delete all:
     if(!isset($_GET['take_action']) || $_GET['take_action']!='delete_all'){
+        echo '<br />';
         echo '<br />';
         echo '<a class="delete-all" href="javascript:void(0);" onclick="$(\'.delete-all\').toggleClass(\'hidden\')">Delete All</a>';
         echo '<div class="delete-all hidden maxout"><b class="read">WARNING</b>: All sources and all their links will be deleted. ONLY do this after reviewing all orphans one-by-one and making sure they cannot become a child of an existing source.<br /><br /></div>';
