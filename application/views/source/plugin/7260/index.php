c3a9b3098031a35648a9243643e2ce65fd0bb264
Shervin Enayati <shervin@mench.co>
2020-05-22 11:59:29 -0700

    Iterations towards v1.41

--- application/views/source/plugin/7260/index.php
+++ application/views/source/plugin/7260/index.php
@@ -15,16 +15,6 @@ if(count($orphan_ideas) > 0){
     }
     echo '</div>';
 
-
-    //Show option to delete all:
-    if(!isset($_GET['take_action']) || $_GET['take_action']!='delete_all'){
-        echo '<br />';
-        echo '<br />';
-        echo '<a class="delete-all" href="javascript:void(0);" onclick="$(\'.delete-all\').toggleClass(\'hidden\')">Delete All</a>';
-        echo '<div class="delete-all hidden maxout"><b class="read">WARNING</b>: All ideas and all their links will be deleted. ONLY do this after reviewing all orphans one-by-one and making sure they cannot become a child of an existing idea.<br /><br /></div>';
-        echo '<a class="delete-all hidden maxout" href="?take_action=delete_all" onclick="">Confirm: <b>Delete All</b> &raquo;</a>';
-    }
-
 } else {
     echo '<span class="icon-block"><i class="fas fa-check-circle"></i></span>No orphans found!';
 }
--- application/views/source/plugin/7269/index.php
+++ application/views/source/plugin/7269/index.php
@@ -13,16 +13,6 @@ if(count($source_orphans) > 0){
     }
     echo '</div>';
 
-
-    //Show option to delete all:
-    if(!isset($_GET['take_action']) || $_GET['take_action']!='delete_all'){
-        echo '<br />';
-        echo '<br />';
-        echo '<a class="delete-all" href="javascript:void(0);" onclick="$(\'.delete-all\').toggleClass(\'hidden\')">Delete All</a>';
-        echo '<div class="delete-all hidden maxout"><b class="read">WARNING</b>: All sources and all their links will be deleted. ONLY do this after reviewing all orphans one-by-one and making sure they cannot become a child of an existing source.<br /><br /></div>';
-        echo '<a class="delete-all hidden maxout" href="?take_action=delete_all" onclick="">Confirm: <b>Delete All</b> &raquo;</a>';
-    }
-
 } else {
     echo '<span class="icon-block"><i class="fas fa-check-circle"></i></span>No orphans found!';
 }
