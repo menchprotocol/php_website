6c87959b3ed8ce572e407471764acab3e0d04e81
Shervin Enayati <shervin@mench.co>
2020-05-25 15:43:29 -0700

    Iterations towards v1.41

--- application/views/source/plugin/12138/index.php
+++ application/views/source/plugin/12138/index.php
@@ -1,8 +1,10 @@
 <?php
 
 //List all Feature Ideas
+echo '<div class="list-group">';
 foreach($this->IDEA_model->fetch(array(
     'idea__status IN (' . join(',', $this->config->item('sources_id_12138')) . ')' => null, //FEATURED
 ), 0, 0, array('idea__weight' => 'DESC')) as $idea){
     echo view_idea($idea);
 }
+echo '</div>';
