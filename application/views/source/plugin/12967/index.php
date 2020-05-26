9db409a722c10bd6800c4968690d4bd308538bc0
Shervin Enayati <shervin@mench.co>
2020-05-25 20:25:25 -0700

    Iterations towards v1.41

--- application/views/source/plugin/12967/index.php
+++ application/views/source/plugin/12967/index.php
@@ -39,7 +39,7 @@ foreach($this->config->item('sources__12968') as $source__id => $m) {
         'read__type IN (' . join(',', $this->config->item('sources_id_4592')) . ')' => null, //SOURCE LINKS
         'read__status IN (' . join(',', $this->config->item('sources_id_7360')) . ')' => null, //ACTIVE
         'source__status IN (' . join(',', $this->config->item('sources_id_7358')) . ')' => null, //ACTIVE
-        'source__icon !=' => $m['m_icon'], //Different Icon
+        '(LENGTH(source__icon) < 1 OR source__icon IS NULL OR source__icon != \''.$m['m_icon'].'\')' => null, //Missing Icon
     ), array('read__down'), 0) as $en) {
         $updated++;
         $this->SOURCE_model->update($en['source__id'], array(
