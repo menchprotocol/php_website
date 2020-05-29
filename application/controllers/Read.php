cd219a491d0af39a67506788f702971c2ebc1c1b
Shervin Enayati <shervin@mench.co>
2020-05-28 21:41:11 -0700

    Iterations towards v1.41

--- application/controllers/Read.php
+++ application/controllers/Read.php
@@ -466,8 +466,10 @@ class Read extends CI_Controller
 
         if(!$read_idea_home){
             $idea__id_added = $this->READ_model->start($session_source['source__id'], $idea__id);
-            if($idea__id_added && $idea__id_added == $idea__id){
-                $success_message = '<div class="alert alert-info" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Successfully added to your Reads. You can now continue below.</div>';
+            if($idea__id_added){
+                if($idea__id_added == $idea__id){
+                    $success_message = '<div class="alert alert-info" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Successfully added to your Reads. You can now continue below.</div>';
+                }
             } else {
                 //Failed to add to Reads:
                 return redirect_message('/r', '<div class="alert alert-danger" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle read"></i></span>Failed to add idea to your Reads.</div>');
