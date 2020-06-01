f2f83786be100f853a3676e07091c22c17b7b98e
Shervin Enayati <shervin@mench.co>
2020-06-01 16:55:13 -0700

    Iterations towards v1.42

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1059,7 +1059,7 @@ function view_idea_list($idea, $ideas_next, $recipient_source, $prefix_statement
         $common_prefix = idea_calc_common_prefix($ideas_next, 'idea__title');
         $prefix_statement = ( $prefix_statement ? $prefix_statement : $common_prefix );
 
-        echo '<div class="read-topic">'.(strlen($prefix_statement) ? trim($prefix_statement).':' : '<span class="icon-block">&nbsp;</span>UP NEXT:').'</div>';
+        echo '<div class="read-topic">'.(strlen($prefix_statement) ? trim($prefix_statement) : '<span class="icon-block">&nbsp;</span>UP NEXT:').'</div>';
         echo '<div class="list-group">';
         foreach($ideas_next as $key => $next_idea){
             echo view_idea_read($next_idea, $common_prefix);
