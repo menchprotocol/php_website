fc791d1d9fa681d100c65b48bc54a0448b4f1509
Shervin Enayati <shervin@mench.co>
2020-06-15 13:47:07 -0700

    Iterations towards v1.43 - Working On #2864 & #2865

--- application/models/READ_model.php
+++ application/models/READ_model.php
@@ -1044,6 +1044,8 @@ class READ_model extends CI_Model
     {
 
         //CHECK DOWN/NEXT
+        $completed_first_levels = array();
+        $first_incomplete = null;
         $found_trigger = false;
         foreach ($this->READ_model->fetch(array(
             'read__left' => $idea['idea__id'],
@@ -1088,15 +1090,20 @@ class READ_model extends CI_Model
 
             } elseif ($is_complete) {
 
-                //This is complete, but maybe there is a child that's not:
-                $found_next = $this->READ_model->find_next($source__id, $next_idea, 0, false);
-                if ($found_next) {
-                    return $found_next;
-                }
+                array_push($completed_first_levels, $next_idea);
 
             }
         }
 
+
+        //Still here? All first levels are completed:
+        foreach ($completed_first_levels as $next_idea) {
+            //This is complete, but maybe there is a child that's not:
+            $found_next = $this->READ_model->find_next($source__id, $next_idea, 0, false);
+            if ($found_next) {
+                return $found_next;
+            }
+        }
 
 
         if ($search_up) {
