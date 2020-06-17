b26bd569ebc91ecc0144e1b07e3560cdee739ee1
Shervin Enayati <shervin@mench.co>
2020-06-16 21:34:07 -0700

    Iterations towards v1.44

--- application/views/header.php
+++ application/views/header.php
@@ -264,7 +264,7 @@ if(!isset($hide_header)){
                         //echo '<td class="block-link"><a class="icon_12899" href="javascript:void(0);" title="'.$sources__11035[12899]['m_name'].'">'.$sources__11035[12899]['m_icon'].'</a></td>';
 
                         //Sign In/Up
-                        echo '<td class="block-link" style="width: auto;"><a href="@s" class="montserrat">'.$sources__11035[4269]['m_name'].' '.$sources__11035[4269]['m_icon'].'</a></td>';
+                        echo '<td class="block-link block-sign-link"><a href="@s" class="montserrat"><span class="show-max">'.$sources__11035[4269]['m_name'].'&nbsp;</span>'.$sources__11035[4269]['m_icon'].'</a></td>';
 
                     }
 
