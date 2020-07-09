3332915da4642a68ed87dba5461dbe9a784fc24d
Shervin Enayati <shervin@mench.co>
2020-07-08 20:29:14 -0700

    Iterations towards v1.46

--- application/views/e/plugin/4527/index.php
+++ application/views/e/plugin/4527/index.php
@@ -31,7 +31,7 @@ echo '//Generated '.date("Y-m-d H:i:s").' PST<br />';
 
 
 //MEMORY STAT COUNTS
-foreach($CI->config->item('e___13478') as $x__type => $m) {
+foreach($this->config->item('e___13478') as $x__type => $m) {
     echo '$config[\'s___'.$x__type.'\'] = '.x_stats_count($x__type).'; //'.$m['m_name'].'<br />';
 }
 
