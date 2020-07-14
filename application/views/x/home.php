d6e6c0a775f7a0bd663da2c83462bf9a563f0c6c
Shervin Enayati <shervin@mench.co>
2020-07-14 12:24:19 -0700

    Iterations towards v1.47

--- application/views/x/home.php
+++ application/views/x/home.php
@@ -68,7 +68,7 @@
             echo '</div>';
         } else {
             //None for now:
-            echo '<div class="alert alert-info no-margin"><span class="icon-block">&nbsp;</span>You have no '.$e___11035[13510]['m_icon'].' '.$e___11035[13510]['m_name'].' yet.</div>';
+            echo '<div class="alert alert-info no-margin"><span class="icon-block">'.$e___11035[13510]['m_icon'].'</span>You have no '.$e___11035[13510]['m_name'].' just yet.</div>';
         }
         echo '<div class="doclear">&nbsp;</div>';
 
@@ -116,7 +116,7 @@
     }
     echo '</div>';
     echo '<div class="doclear">&nbsp;</div>';
-    echo '<div class="alert alert-info no-margin margin-top-down"><span class="icon-block">'.$e___11035[7256]['m_icon'].'</span>Tip: '.$e___11035[7256]['m_name'].' to find '.$e___12467[12274]['m_name'].' and '.$e___12467[12273]['m_name'].'.</div>';
+    echo '<div class="alert alert-info no-margin margin-top-down"><span class="icon-block">'.$e___11035[7256]['m_icon'].'</span>Tip: You can '.$e___11035[7256]['m_name'].' to find featured '.$e___12467[12274]['m_name'].' & '.$e___12467[12273]['m_name'].'.</div>';
 
 
     ?>
