f1168c9b9b4a2850e0a64682bf52d4a4c2e2bc5e
Shervin Enayati <shervin@mench.co>
2020-07-15 12:03:46 -0700

    Iterations towards v1.47

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1916,6 +1916,7 @@ function view_e($e, $is_parent = false, $extra_class = null, $control_enabled = 
     $is_public = in_array($e['e__status'], $CI->config->item('n___7357'));
     $is_x_published = ( !$x__id || in_array($e['x__status'], $CI->config->item('n___7359')));
     $is_hidden = (!isset($session_e['en_id']) || $e['e__id']!=$session_e['en_id']) && (filter_array($e__profiles, 'e__id', '4755') || in_array($e['e__id'], $CI->config->item('n___4755')));
+    $is_hidden = false;
 
     if(!$session_e && (!$is_public || !$is_x_published)){
         //Not logged in, so should only see published:
