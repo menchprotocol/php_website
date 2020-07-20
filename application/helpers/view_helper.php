25ca6768823fbf175d428b72c4dac1237d361b76
Shervin Enayati <shervin@mench.co>
2020-07-20 12:45:31 -0700

    Iterations towards v1.47

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -778,7 +778,7 @@ function view_i_x($i, $common_prefix = null, $show_editor = false, $completion_r
             //MENCH COINS
             $ui .= '<div class="row">';
                 $ui .= '<div class="col-4">'.view_coins_i(12274, $i).'</div>';
-                $ui .= '<div class="col-8"><span class="inline-block" style="width: 89px;">'.view_coins_i(12273, $i).'</span>'.($i_stats['i___13292'] ? '<span class="mono-space inline-block">'.view_time_hours($i_stats['i___13292']).'</span>' : '' ).'</div>';
+                $ui .= '<div class="col-8"><span class="inline-block" style="width:64px;">'.view_coins_i(12273, $i).'</span>'.($i_stats['i___13292'] ? '<span class="mono-space inline-block">'.view_time_hours($i_stats['i___13292']).'</span>' : '' ).'</div>';
             $ui .= '</div>';
 
         $ui .= '</div>';
