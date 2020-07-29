f8e050c333676e916c97968457f1dab2cc323cb7
Shervin Enayati <shervin@mench.co>
2020-07-29 13:16:52 -0700

    Iterations towards v1.47

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -1002,14 +1002,14 @@ class X_model extends CI_Model
                 } else {
 
                     //TEXT ONLY
-                    $output_body_message = str_replace($identifier_string, '<span '.$text_tooltip.'>' . $es[0]['e__title'] . '</span>'.' ', $output_body_message);
+                    $output_body_message = str_replace($identifier_string, '<span '.$text_tooltip.'>' . ( !substr_count($es[0]['e__title'], ' ') ? '<span class="icon-block-xs e__icon_'.$es[0]['e__id'].'">'.view_e__icon($es[0]['e__icon']).'</span>' : '' ) . '<span class="text__6197_'.$es[0]['e__id'].'">' . $es[0]['e__title'] . '</span></span>'.' ', $output_body_message);
 
                 }
 
             } else {
 
                 //FULL SOURCE LINK
-                $output_body_message = str_replace($identifier_string, '<span '.$text_tooltip.'><a class="montserrat" href="/@' . $es[0]['e__id'] . '">'.( !in_array($es[0]['e__status'], $this->config->item('n___7357')) ? '<span class="img-block icon-block-xs">'.$e___6177[$es[0]['e__status']]['m_icon'].'</span> ' : '' ).'<span class="img-block icon-block-xs">'.view_e__icon($es[0]['e__icon']).'</span>' . $es[0]['e__title'] . '</a></span>'.' ', $output_body_message);
+                $output_body_message = str_replace($identifier_string, '<span '.$text_tooltip.'><a class="montserrat" href="/@' . $es[0]['e__id'] . '">'.( !in_array($es[0]['e__status'], $this->config->item('n___7357')) ? '<span class="icon-block-xs">'.$e___6177[$es[0]['e__status']]['m_icon'].'</span> ' : '' ).'<span class="icon-block-xs e__icon_'.$es[0]['e__id'].'">'.view_e__icon($es[0]['e__icon']).'</span><span class="text__6197_'.$es[0]['e__id'].'">' . $es[0]['e__title'] . '</span></a></span>'.' ', $output_body_message);
 
             }
 
