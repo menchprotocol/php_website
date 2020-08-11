f674027c2f0e9ef55ecc81433c57275b52bbcc7a
Shervin Enayati <shervin@mench.co>
2020-08-11 11:16:06 -0700

    Iterations towards v1.51

--- application/views/e/layout.js
+++ application/views/e/layout.js
@@ -462,8 +462,7 @@ function e_load_search(element_focus, is_e_parent, shortcut) {
                 header: function (data) {
                     if (!data.isEmpty) {
 
-                        var return_string = '<b class="source montserrat">' + data.query.toUpperCase() + '</b>';
-                        return (js_session_superpowers_assigned.includes(13422) ? '<a href="javascript:e__add(0,'+is_e_parent+')" class="suggestion">' + '<span class="icon-block"><i class="far fa-plus-circle add-plus source"></i></span>' + return_string + '</a>' : '') + '<a href="javascript:load_13428(0, \''+data.query+'\')" class="suggestion">' + '<span class="icon-block">'+js_e___11035[13428]['m_icon']+'</span><b class="source montserrat">' + js_e___11035[13428]['m_title'] + ':</b> ' + return_string + '</a>';
+                        return (js_session_superpowers_assigned.includes(13422) ? '<a href="javascript:e__add(0,'+is_e_parent+')" class="suggestion">' + '<span class="icon-block"><i class="far fa-plus-circle add-plus source"></i></span>' + '<b class="source montserrat">' + data.query.toUpperCase() + '</b>' + '</a>' : '') + '<a href="javascript:load_13428(0, \''+data.query+'\')" class="suggestion">' + '<span class="icon-block">'+js_e___11035[13428]['m_icon']+'</span><b class="source montserrat">LAUNCH ' + js_e___11035[13428]['m_title'] + ':</b></a>';
 
                     }
                 },
