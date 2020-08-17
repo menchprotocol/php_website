e9ccbab2ea52db0fa8935be359027064ee99cdad
Shervin Enayati <shervin@mench.co>
2020-08-16 21:45:17 -0700

    Iterations towards v1.51

--- application/views/e/plugin/13602/index.php
+++ application/views/e/plugin/13602/index.php
@@ -18,7 +18,7 @@ echo '</tr>';
 
 foreach($this->config->item('e___12467') as $x__type => $m) {
     echo '<tr>';
-    echo '<td class="montserrat doupper">'.$m['m_icon'].' '.$m['m_name'].'</td>';
+    echo '<td class="montserrat doupper">'.$m['m_icon'].' '.$m['m_title'].'</td>';
     for($i=0;$i<$total_months;$i++){
         echo '<td>'.rand(0,2000).'</td>';
     }
