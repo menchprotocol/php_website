e0b3440edebe4c84bb727052c4c7c10df184f609
Shervin Enayati <shervin@mench.co>
2020-08-16 21:49:03 -0700

    Iterations towards v1.51

--- application/views/e/plugin/13602/index.php
+++ application/views/e/plugin/13602/index.php
@@ -9,9 +9,9 @@ echo '<table>';
 
 
 echo '<tr>';
-echo '<td width="289px">Name</td>';
+echo '<td width="289px">&nbsp;</td>';
 for($i=0;$i<$total_months;$i++){
-    echo '<td width="144px" style="font-size: 0.8em;">'.date("ym", mktime(0, 0, 0, $start_month+$i, date("j"), $start_year)).'</td>';
+    echo '<td width="144" style="font-size: 0.8em;"><b>'.date("ym", mktime(0, 0, 0, $start_month+$i, date("j"), $start_year)).'</b></td>';
 }
 echo '</tr>';
 
@@ -20,7 +20,7 @@ foreach($this->config->item('e___12467') as $x__type => $m) {
     echo '<tr>';
     echo '<td width="289px" class="montserrat doupper">'.$m['m_icon'].' '.$m['m_title'].'</td>';
     for($i=0;$i<$total_months;$i++){
-        echo '<td width="144px" style="font-size: 0.8em;">'.rand(0,2000).'</td>';
+        echo '<td width="144" style="font-size: 0.8em;">'.rand(0,2000).'</td>';
     }
     echo '</tr>';
 }
