a6fe0e23beb44967dca6c8f58434bede155dccb6
Shervin Enayati <shervin@mench.co>
2020-08-16 21:43:43 -0700

    Iterations towards v1.51

---
+++ application/views/e/plugin/13602/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$start_year = 2017;
+$start_month = 01;
+$total_months = 48;
+
+
+echo '<table>';
+
+
+echo '<tr>';
+echo '<td>Name</td>';
+for($i=0;$i<$total_months;$i++){
+    echo '<td>'.date("Y-m-d H:i:s", mktime(0, 0, 0, $start_month+$i, date("j"), $start_year)).'</td>';
+}
+echo '</tr>';
+
+
+foreach($this->config->item('e___12467') as $x__type => $m) {
+    echo '<tr>';
+    echo '<td class="montserrat doupper">'.$m['m_icon'].' '.$m['m_name'].'</td>';
+    for($i=0;$i<$total_months;$i++){
+        echo '<td>'.rand(0,2000).'</td>';
+    }
+    echo '</tr>';
+}
+
+
+echo '</table>';
