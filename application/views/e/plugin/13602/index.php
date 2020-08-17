193f05b615f0f78ee29a392a0b450f4e5f97242f
Shervin Enayati <shervin@mench.co>
2020-08-16 21:53:14 -0700

    Iterations towards v1.51

--- application/views/e/plugin/13602/index.php
+++ application/views/e/plugin/13602/index.php
@@ -8,19 +8,20 @@ $total_months = 48;
 echo '<table>';
 
 
+
 echo '<tr>';
-echo '<td><div style="width: 400px;">&nbsp;</div></td>';
+echo '<td><div class="col_name" style="width: 400px;">&nbsp;</div></td>';
 for($i=0;$i<$total_months;$i++){
-    echo '<td style="font-size: 0.8em;"><div style="width: 120px;"><b>'.date("ym", mktime(0, 0, 0, $start_month+$i, date("j"), $start_year)).'</b></div></td>';
+    echo '<td style="font-size: 0.8em;"><div class="col_stat"><b>'.date("ym", mktime(0, 0, 0, $start_month+$i, date("j"), $start_year)).'</b></div></td>';
 }
 echo '</tr>';
 
 
 foreach($this->config->item('e___12467') as $x__type => $m) {
     echo '<tr>';
-    echo '<td class="montserrat doupper"><div style="width: 400px;">'.$m['m_icon'].' '.$m['m_title'].'</div></td>';
+    echo '<td class="montserrat doupper"><div class="col_name" style="width: 400px;">'.$m['m_icon'].' '.$m['m_title'].'</div></td>';
     for($i=0;$i<$total_months;$i++){
-        echo '<td style="font-size: 0.8em;"><div style="width: 120px;">'.rand(0,2000).'</div></td>';
+        echo '<td style="font-size: 0.8em;"><div class="col_stat">'.rand(0,2000).'</div></td>';
     }
     echo '</tr>';
 }
