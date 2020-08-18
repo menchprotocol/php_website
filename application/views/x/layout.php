aa6ac0afc38491146ad5ed91436c4fa3e9dd5bfb
Shervin Enayati <shervin@mench.co>
2020-08-17 23:49:16 -0700

    Iterations towards v1.51 - Remove Source Wizard Close #2929

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -150,22 +150,6 @@ echo $main_title;
 
 
 
-//MESSAGES
-$counter = null; //Hide message count
-echo '<div style="margin-bottom:33px;">';
-foreach($messages as $message_x) {
-    echo $this->X_model->message_send(
-        $message_x['x__message'],
-        $user_e
-    );
-}
-echo '</div>';
-
-if($in_my_x && !count($x_completes) && in_array($i_focus['i__type'], $this->config->item('n___12211'))){
-    //Give option to mark as read:
-    //echo '<div class="margin-top-down"><span class="icon-block">&nbsp;</span><a class="btn btn-x" href="javascript:void(0);" onclick="go_12211()">'.$e___11035[12211]['m_title'].' '.$e___11035[12211]['m_icon'].'</a></div>';
-}
-
 
 
 
@@ -335,6 +319,32 @@ if($tab_pill_count > 1){
 
 //Show All Tab Content:
 echo $tab_content;
+
+
+
+
+
+
+
+
+//MESSAGES
+$counter = null; //Hide message count
+echo '<div style="margin-bottom:33px;">';
+foreach($messages as $message_x) {
+    echo $this->X_model->message_send(
+        $message_x['x__message'],
+        $user_e
+    );
+}
+echo '</div>';
+
+if($in_my_x && !count($x_completes) && in_array($i_focus['i__type'], $this->config->item('n___12211'))){
+    //Give option to mark as read:
+    //echo '<div class="margin-top-down"><span class="icon-block">&nbsp;</span><a class="btn btn-x" href="javascript:void(0);" onclick="go_12211()">'.$e___11035[12211]['m_title'].' '.$e___11035[12211]['m_icon'].'</a></div>';
+}
+
+
+
 
 
 
