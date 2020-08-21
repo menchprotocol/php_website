b61ab6c0ee916344b0c1b6ec2bdd2da73379989a
Shervin Enayati <shervin@mench.co>
2020-08-21 15:40:53 -0700

    Iterations towards v1.53

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -230,7 +230,7 @@ foreach($this->config->item('e___'.$tab_group) as $x__type => $m){
         if(!$in_my_x){
 
             //NEXT IDEAS
-            $focus_tab .= view_i_list($i_focus, $is_next, $user_e, view_i_tree_stats($i_stats));
+            $focus_tab .= view_i_list($i_focus, $is_next, $user_e, view_i_tree_stats($i_stats, true));
 
             //IDEA PREVIOUS
             $focus_tab .= view_i_list($i_focus, $this->X_model->fetch(array(
