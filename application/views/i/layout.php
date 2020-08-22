fc8242b702ebae55a69abba14f50a9ccbe241b9c
Shervin Enayati <shervin@mench.co>
2020-08-22 11:15:53 -0700

    Iterations towards v1.53

--- application/views/i/layout.php
+++ application/views/i/layout.php
@@ -186,7 +186,7 @@ foreach($this->config->item('e___'.$tab_group) as $x__type => $m){
     } elseif($x__type==12273){
 
         //IDEA TREE STATS
-        $focus_tab .= '<div style="padding:5px 0 13px;" class="grey">'.view_i_tree_stats($i_stats).'</div>';
+        $focus_tab .= '<div style="margin:5px 0 13px;" class="grey">'.view_i_tree_stats($i_stats).'</div>';
 
         //IDEAS
         $is_next = $this->X_model->fetch(array(
