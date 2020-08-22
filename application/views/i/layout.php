13dea523588fa8548e42aefdbf1e1bd97ed338d3
Shervin Enayati <shervin@mench.co>
2020-08-22 10:45:16 -0700

    Iterations towards v1.53

--- application/views/i/layout.php
+++ application/views/i/layout.php
@@ -75,7 +75,7 @@ echo '</div>';
 
 //IDEA TITLE
 echo '<div class="itemidea" style="padding:5px 0;">';
-echo view_input_text(4736, $i_focus['i__title'], $i_focus['i__id'], ($e_of_i && $is_active), 0, true, view_cache(4737 /* Idea Status */, $i_focus['i__status'], true, 'top', $i_focus['i__id']));
+echo view_input_text(4736, $i_focus['i__title'], $i_focus['i__id'], ($e_of_i && $is_active), 0, true, view_i_icon($i_focus));
 echo '</div>';
 
 
