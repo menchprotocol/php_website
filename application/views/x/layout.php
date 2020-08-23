557e696e4c5952accc37ac22306482b6065e965d
Shervin Enayati <shervin@mench.co>
2020-08-23 13:21:30 -0700

    Iterations towards v1.53

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -717,10 +717,6 @@ if($in_my_x){
     echo '<div class="row">';
     echo '<div class="discover-controller">';
 
-    if(!count($sitemap_items)){
-        echo '<div class="montserrat">GO NEXT:</div>';
-    }
-
     foreach($this->config->item('e___13289') as $e__id => $m) {
 
         $url = '';
@@ -768,9 +764,7 @@ if($in_my_x){
 
         }
 
-        if(count($sitemap_items) || $url){
-            echo '<div>'.( $url ? $url : '&nbsp;' ).'</div>';
-        }
+        echo '<div>'.( $url ? $url : '&nbsp;' ).'</div>';
 
     }
 
