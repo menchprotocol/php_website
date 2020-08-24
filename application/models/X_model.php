90c27c0dc69681f00e01bb5989dd5786bff11b2e
Shervin Enayati <shervin@mench.co>
2020-08-23 22:23:37 -0700

    Iterations towards v1.53

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -966,7 +966,7 @@ class X_model extends CI_Model
                     } elseif($e_profile['x__type'] == 4256 /* URL */) {
 
                         array_push($e_urls, $e_profile['x__message']);
-                        $e_appendix .= '<div class="e-appendix paddingup"><a href="'.$e_profile['x__message'].'" target="_blank" class="montserrat '.extract_icon_color($e_profile['e__icon']).'"><span class="icon-block-xs">'.view_e__icon($e_profile['e__icon']).'</span>' . $e_profile['e__title'] . '<span class="icon-block-xs"><i class="fas fa-external-link"></i></span></a></div>';
+                        $e_appendix .= '<div class="e-appendix paddingup"><a href="'.$e_profile['x__message'].'" target="_blank"><span class="icon-block-xs">'.view_e__icon($e_profile['e__icon']).'</span>' . $e_profile['e__title'] . '<span class="icon-block-xs"><i class="fas fa-external-link"></i></span></a></div>';
                         //$e_appendix .= '<div class="e-appendix paddingup">' . view_x__message($e_profile['x__message'], $e_profile['x__type'], $message_input) . '</div>';
 
                     } else {
