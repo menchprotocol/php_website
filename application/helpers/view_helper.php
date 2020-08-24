983edd65804c72091f82d9b4650685bf4ea190ae
Shervin Enayati <shervin@mench.co>
2020-08-24 13:18:12 -0700

    Iterations towards v1.54

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -164,7 +164,7 @@ function view_url_embed($url, $full_message = null, $return_array = false)
 
                 //Header For Time
                 if($end_time){
-                    $embed_html_code .= '<div class="headline" style="padding-bottom: 0;"><span class="icon-block-xs"><i class="fas fa-cut"></i></span>'.round_minutes($end_time-$start_time).' MIN | '.view_time_hours($start_time, true).'<span class="mid-range">-</span>'.view_time_hours($end_time, true).'</div>';
+                    $embed_html_code .= '<div class="headline" style="padding-bottom: 0;"><span class="icon-block-xs"><i class="fas fa-cut"></i></span>'.view_time_hours($start_time, true).'<span class="mid-range">-</span>'.view_time_hours($end_time, true).' = '.round_minutes($end_time-$start_time).' MIN</div>';
                 }
 
                 $embed_html_code .= '<div class="media-content"><div class="yt-container video-sorting" style="margin-top:5px;"><iframe src="//www.youtube.com/embed/' . $video_id . '?wmode=opaque&theme=light&color=white&keyboard=1&autohide=2&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&start=' . $start_time . ($end_time ? '&end=' . $end_time : '') . '" frameborder="0" allowfullscreen class="yt-video"></iframe></div></div>';
