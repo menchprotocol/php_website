07133e2c161d71413d64dc7157df56ec55356a92
Shervin Enayati <shervin@mench.co>
2020-08-24 22:54:27 -0700

    Iterations towards v1.54

--- application/config/platform_cache.php
+++ application/config/platform_cache.php
@@ -3803,7 +3803,7 @@ $config['e___6404'] = array(
     ),
     11060 => array(
         'm_title' => 'MENCH VERSION',
-        'm_message' => 'v1.531',
+        'm_message' => 'v1.5311',
         'm_icon' => '<i class="fad fa-code"></i>',
         'm_profile' => array(6404),
     ),
