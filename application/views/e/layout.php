0e50e333a0e7dd50fb6f2b8eb9fa1d77c95d9620
Shervin Enayati <shervin@mench.co>
2020-08-24 21:17:38 -0700

    Iterations towards v1.54

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -664,7 +664,7 @@ $superpower_any = ( $user_e ? count($this->session->userdata('session_superpower
 
         }
 
-        if(!$counter && !$superpower_13422 && !in_array($x__type, $this->config->item('n___12574'))){
+        if(!$counter && !$superpower_13422 && !($x__type==6255 && $source_is_e) && !in_array($x__type, $this->config->item('n___12574'))){
             //Hide since Zero without exception @12574:
             continue;
         }
