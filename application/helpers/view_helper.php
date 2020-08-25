b925f01d851c325238bfe1787d3a615b1b754d66
Shervin Enayati <shervin@mench.co>
2020-08-24 22:32:32 -0700

    Iterations towards v1.54

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1172,9 +1172,8 @@ function view_i_list($i, $is_next, $user_e, $prefix_statement = null){
 
     $ui = '<div class="doclear">&nbsp;</div>';
 
-    if(strlen($prefix_statement)){
-        $ui .= '<div class="pull-left headline"><span class="icon-block">'.$e___12467[12273]['m_icon'].'</span>'.$prefix_statement.( $common_prefix ? ' '.$common_prefix : '' ).'</div>';
-    }
+
+    $ui .= '<div class="pull-left headline"><span class="icon-block">'.$e___12467[12273]['m_icon'].'</span>'.( strlen($prefix_statement) ? $prefix_statement : 'UP NEXT:' ).( $common_prefix ? ' '.$common_prefix : '' ).'</div>';
 
     $ui .= '<a href="javascript:void(0);" onclick="$(\'.i_x_stats\').toggleClass(\'hidden\')" class="pull-right grey right-adj" title="'.$e___11035[13689]['m_title'].'">'.$e___11035[13689]['m_icon'].'</a>';
 
