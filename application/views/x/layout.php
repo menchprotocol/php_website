0bd8a040741039e6b203c78203ef9a7498c08990
Shervin Enayati <shervin@mench.co>
2020-08-31 01:17:29 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -723,7 +723,7 @@ if(!count($sitemap_items) && $in_my_x){
     if($i_completion_percentage<100){
 
         //Not Yet Completed
-        echo '<div class="msg alert no-margin"><span class="icon-block"><i class="fas fa-lock"></i></span><span class="title-block-md">Navigation unlocked once completed. You\'re '.$i_completion_percentage.'% complete. Tap <span class="icon-block-xs"><i class="fas fa-step-forward black"></i></span> to continue.</span></div>';
+        echo '<div class="alert no-margin"><span class="icon-block"><i class="fas fa-lock"></i></span><span class="title-block-md">Navigation unlocked once completed. You\'re '.$i_completion_percentage.'% complete. Tap <span class="icon-block-xs"><i class="fas fa-step-forward black"></i></span> to continue.</span></div>';
 
     } else {
 
