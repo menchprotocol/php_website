70c5d2969f13c7aa9a3834f57e2c6268e4fe00e8
Shervin Enayati <shervin@mench.co>
2020-08-31 22:54:07 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -303,7 +303,7 @@ if($in_my_x && count($this->X_model->fetch(array(
     )))){
 
     //Recommended to Save This Idea:
-    echo '<div class="msg alert no-margin space-left">Save idea in your profile? Tap <i class="far fa-bookmark black"></i></div>';
+    echo '<div class="msg alert no-margin space-left">Save idea for quicker access? Tap <i class="far fa-bookmark black"></i></div>';
 
 }
 
