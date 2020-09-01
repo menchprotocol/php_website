cbd2e1c736fb75c9035370c2076b4b815164e382
Shervin Enayati <shervin@mench.co>
2020-08-31 20:06:56 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -748,7 +748,7 @@ echo $tab_content;
 //Home Navigation Notice
 if(!count($sitemap_items) && $in_my_x && $i_completion_percentage<100){
     //Not Yet Completed
-    echo '<div class="alert no-margin"><span class="icon-block"><i class="fas fa-lock"></i></span><span class="title-block">Navigation will unlock once you are 100% complete. Tap <i class="fas fa-step-forward black"></i> (below) to continue.</span></div>';
+    echo '<div class="alert no-margin"><span class="icon-block"><i class="fas fa-lock"></i></span><span class="title-block">Navigation unlock once 100% complete. Tap <i class="fas fa-step-forward black"></i> to continue.</span></div>';
 }
 
 
