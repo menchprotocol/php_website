bb6848e08e775b1cbf940d6f467dd8073a3b6c65
Shervin Enayati <shervin@mench.co>
2020-09-11 16:30:03 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -830,32 +830,32 @@ if($in_my_x){
                 'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
             )));
 
-            $control_btn = '<a class="grey-bg" href="javascript:void(0);" onclick="x_save('.$i_focus['i__id'].')"><span class="controller-nav toggle_saved '.( $is_saved ? '' : 'hidden' ).'">'.$e___11035[12896]['m_icon'].'</span><span class="controller-nav toggle_saved '.( $is_saved ? 'hidden' : '' ).'">'.$e___11035[12906]['m_icon'].'</span></a><span class="nav-title">'.$m['m_title'].'</span>';
+            $control_btn = '<a class="round-btn" href="javascript:void(0);" onclick="x_save('.$i_focus['i__id'].')"><span class="controller-nav toggle_saved '.( $is_saved ? '' : 'hidden' ).'">'.$e___11035[12896]['m_icon'].'</span><span class="controller-nav toggle_saved '.( $is_saved ? 'hidden' : '' ).'">'.$e___11035[12906]['m_icon'].'</span></a><span class="nav-title">'.$m['m_title'].'</span>';
 
         } elseif($e__id==12991 && count($sitemap_items)){
 
             //GO BACK
-            $control_btn = '<a class="controller-nav grey-bg" href="'.( isset($_GET['previous_x']) && $_GET['previous_x']>0 ? '/'.$_GET['previous_x'] : ( $previous_level_id > 0 ? '/x/x_previous/'.$previous_level_id.'/'.$i_focus['i__id'] : home_url() ) ).'">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
+            $control_btn = '<a class="controller-nav round-btn" href="'.( isset($_GET['previous_x']) && $_GET['previous_x']>0 ? '/'.$_GET['previous_x'] : ( $previous_level_id > 0 ? '/x/x_previous/'.$previous_level_id.'/'.$i_focus['i__id'] : home_url() ) ).'">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
 
         } elseif($e__id==13563){
 
             //EDIT
-            $control_btn = '<a class="controller-nav grey-bg" href="/~'.$i_focus['i__id'].'">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
+            $control_btn = '<a class="controller-nav round-btn" href="/~'.$i_focus['i__id'].'">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
 
         } elseif($e__id==12211){
 
             if($i_completed){
                 //GO NEXT IN TREE
-                $control_btn = '<a class="controller-nav grey-bg" href="javascript:void(0);" onclick="go_12211(\'/x/i_next/\')">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
+                $control_btn = '<a class="controller-nav round-btn" href="javascript:void(0);" onclick="go_12211(\'/x/i_next/\')">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
             } else {
                 //GO NEXT INCOMPLETE
-                $control_btn = '<a class="controller-nav grey-bg" href="javascript:void(0);" onclick="go_12211(\'/x/x_next/\')">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
+                $control_btn = '<a class="controller-nav round-btn" href="javascript:void(0);" onclick="go_12211(\'/x/x_next/\')">'.$m['m_icon'].'</a><span class="nav-title">'.$m['m_title'].'</span>';
             }
 
         } elseif($e__id==13871){
 
             //ADD COMMENT
-            $control_btn = '<a class="controller-nav new-13871 grey-bg" href="javascript:void(0);">'.$m['m_icon'].'</a><span class="nav-title new-13871">'.$m['m_title'].'</span>';
+            $control_btn = '<a class="controller-nav new-13871 round-btn" href="javascript:void(0);">'.$m['m_icon'].'</a><span class="nav-title new-13871">'.$m['m_title'].'</span>';
 
         }
 
