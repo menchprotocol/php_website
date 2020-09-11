1ec44cf1936e8b66eac993274d1221623f246d3a
Shervin Enayati <shervin@mench.co>
2020-09-11 16:19:36 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -789,7 +789,7 @@ echo $tab_content;
 
 
 //COMMENTS
-echo '<div style="padding:55px 0;">';
+echo '<div style="padding:55px 0 21px;">';
 echo view_i_note_mix(12419, $this->X_model->fetch(array(
     'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
     'x__type' => 12419, //COMMENTS
