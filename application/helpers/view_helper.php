8443e64cf79e5fad11fdec7b4d44e45dcf05c9ac
Shervin Enayati <shervin@mench.co>
2020-09-11 18:42:47 -0700

    Iterations towards v1.54

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1403,7 +1403,7 @@ function view_x_progress($completion_rate, $i, $show_micro){
 
     } else {
 
-        $ui .= '<div class="progress-bg-list"><div class="progress-done" style="width:'.$completion_rate['completion_percentage'].'%"></div></div>';
+        $ui .= '<div class="progress-bg-list '.( $show_micro ? ' progress-micro ' : '' ).'"><div class="progress-done" style="width:'.$completion_rate['completion_percentage'].'%"></div></div>';
 
     }
 
