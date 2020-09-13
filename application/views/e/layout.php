eac6cbf7f6cc2b8538b25d64a65c89241b4549a6
Shervin Enayati <shervin@mench.co>
2020-09-12 17:33:54 -0700

    Iterations towards v1.54

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -439,7 +439,7 @@ $superpower_any = ( $user_e ? count($this->session->userdata('session_superpower
             } elseif($user_e && $user_e['e__id']>0 && !count($list_i)) {
 
                 //Give option to get started:
-                $focus_tab .= '<div class="msg alert alert-warning montserrat" role="alert" style="text-decoration: none;"><span class="icon-block">'.$e___11035[10939]['m_icon'].'</span><a href="'.config_var(10939).'">'.$e___11035[10939]['m_title'].'<span class="icon-block"><i class="fas fa-arrow-right"></i></span></a></div>';
+                $focus_tab .= '<div class="msg alert alert-warning" role="alert" style="text-decoration: none;"><span class="icon-block">'.$e___11035[10939]['m_icon'].'</span><a href="'.config_var(10939).'">'.$e___11035[10939]['m_title'].'<span class="icon-block"><i class="fas fa-arrow-right"></i></span></a></div>';
 
             }
 
