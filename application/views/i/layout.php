86b1b43e0606a9af8f472f99300ed4f0a16a095f
Shervin Enayati <shervin@mench.co>
2020-09-13 00:46:46 -0700

    Iterations towards v1.57

--- application/views/i/layout.php
+++ application/views/i/layout.php
@@ -264,7 +264,7 @@ foreach($this->config->item('e___'.$tab_group) as $x__type => $m){
 
             //No Results:
             $e___12467 = $this->config->item('e___12467'); //MENCH COINS
-            $focus_tab .= '<div class="msg alert alert-danger" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span> No '.$e___12467[6255]['m_title'].' yet</div>';
+            //$focus_tab .= '<div class="msg alert alert-danger" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span> No '.$e___12467[6255]['m_title'].' yet</div>';
 
         }
 
