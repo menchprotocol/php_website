10a639e09c9a0bdf231005a2aeb8dee01082d137
Shervin Enayati <shervin@mench.co>
2020-09-12 17:39:56 -0700

    Iterations towards v1.54

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -14,7 +14,7 @@
 
 <?php
 
-echo '<div class="container wrap-card hideIfEmpty">';
+echo '<div class="containeroutside-card hideIfEmpty">';
 
 $e___11035 = $this->config->item('e___11035'); //MENCH NAVIGATION
 $e___13291 = $this->config->item('e___13291'); //DISCOVER TABS
