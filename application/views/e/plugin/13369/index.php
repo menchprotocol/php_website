c26ec04cd2fc5588bbe7d90c64c4de5c4d59304e
Shervin Enayati <shervin@mench.co>
2020-09-12 18:01:46 -0700

    Iterations towards v1.54

--- application/views/e/plugin/13369/index.php
+++ application/views/e/plugin/13369/index.php
@@ -1,5 +1,5 @@
 
 
-<div style="background-color: #222222; width: 600px; height: 871px; text-align: center;">
+<div style="background-color: #111111; width: 600px; height: 871px; text-align: center;">
     <i class="fas fa-circle idea" style="font-size: 25em; padding-top: 220px;"></i>
 </div>
