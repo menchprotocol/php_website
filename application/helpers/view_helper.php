8e4e2bb8d835bd1b729e48f4180f4179eb7f09dc
Shervin Enayati <shervin@mench.co>
2020-09-17 15:37:51 -0700

    Iterations towards v1.57

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -665,7 +665,7 @@ function view_coins_e($x__type, $e__id, $page_num = 0, $append_coin_icon = true)
         $count_query = $CI->X_model->fetch($query_filters, $join_objects, 1, 0, array(), 'COUNT(x__id) as totals');
         if($append_coin_icon){
             $e___12467 = $CI->config->item('e___12467'); //MENCH COINS
-            return ( $count_query[0]['totals'] > 0 ? '<span class="montserrat '.extract_icon_color($e___12467[$x__type]['m_icon']).'" title="'.$e___12467[$x__type]['m_title'].'">'.$e___12467[$x__type]['m_icon'].'&nbsp;'.view_number($count_query[0]['totals']).'</span>' : null);
+            return ( $count_query[0]['totals'] > 0 ? '<span class="montserrat '.extract_icon_color($e___12467[$x__type]['m_icon']).'" title="'.number_format($count_query[0]['totals'], 0).' '.$e___12467[$x__type]['m_title'].'" data-toggle="tooltip" data-placement="top">'.$e___12467[$x__type]['m_icon'].'&nbsp;'.view_number($count_query[0]['totals']).'</span>' : null);
         } else {
             return intval($count_query[0]['totals']);
         }
