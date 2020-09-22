518c586608cfd759253fa9e4b2963c07b273539e
Shervin Enayati <shervin@mench.co>
2020-09-22 14:42:29 -0700

    Iterations towards v1.57

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1243,14 +1243,15 @@ function view_i_note_list($x__type, $i_notes, $e_of_i, $show_empty_error = false
     $ui = '';
 
 
+    $ui .= '<div class="headline">'.$e___4485[$x__type]['m_title'].'</div>';
+
     if($show_empty_error && !count($i_notes) && $e_of_i){
         $ui .= '<div class="no_notes_' . $x__type .'" style="margin-bottom:13px;">';
-        $ui .= '<div class="msg alert alert-danger" role="alert"><span class="icon-block">&nbsp;</span>No '.ucwords(strtolower($e___4485[$x__type]['m_title'])).' yet. Be the first to post one</div>';
+        $ui .= '<div class="msg alert alert-danger" role="alert"><span class="icon-block">&nbsp;</span>No '.ucwords(strtolower($e___4485[$x__type]['m_title'])).' yet</div>';
         $ui .= '</div>';
     }
 
     //Show no-Message notifications for each message type:
-    $ui .= '<div class="headline">'.$e___4485[$x__type]['m_title'].'</div>';
     $ui .= '<div id="i_notes_list_'.$x__type.'" class="list-group">';
 
     //List current notes:
