cce430d9408e1a4b211650149e185bf29c8b7011
Shervin Enayati <shervin@mench.co>
2020-09-23 13:53:53 -0700

    Iterations towards v1.57

--- application/views/i/layout.php
+++ application/views/i/layout.php
@@ -78,32 +78,29 @@ echo view_input_text(4736, $i_focus['i__title'], $i_focus['i__id'], ($e_of_i && 
 echo '</div>';
 
 
-
-
-
-//IDEA TIME
-echo '<div class="inline-block both-margin left-margin '.superpower_active(10986).'">'.view_input_text(4356, $i_focus['i__duration'], $i_focus['i__id'], $e_of_i && $is_active, 0).'</div>';
-echo '<div class="doclear">&nbsp;</div>';
-
-//IDEA STATUS
-echo '<div class="pull-left both-margin inline-block left-margin">'.view_input_dropdown(4737, $i_focus['i__status'], 'btn-idea', $e_of_i, true, $i_focus['i__id']).'</div>';
-
-//IDEA TYPE
-echo '<div class="pull-left both-margin inline-block left-half-margin">'.view_input_dropdown(7585, $i_focus['i__type'], 'btn-idea', $e_of_i && $is_active, true, $i_focus['i__id']).'</div>';
-
-//PREVIEW DISCOVERY
-echo '<div class="inline-block pull-right both-margin" style="margin-right:33px;"><a class="btn btn-discover" href="/'.$i_focus['i__id'].'" style="font-size: 1em !important;"><span class="show-max">'.$e___11035[13562]['m_title'].' </span>'.$e___11035[13562]['m_icon'].'</a></div>';
-
-echo '<div class="doclear">&nbsp;</div>';
-
-
-
 //IDEA MESSAGES:
 echo view_i_note_list(4231, $this->X_model->fetch(array(
     'x__status IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
     'x__type' => 4231,
     'x__right' => $i_focus['i__id'],
 ), array('x__source'), 0, 0, array('x__sort' => 'ASC')), $e_of_i);
+
+
+//IDEA TIME
+echo '<div class="inline-block both-margin left-margin '.superpower_active(10986).'">'.view_input_text(4356, $i_focus['i__duration'], $i_focus['i__id'], $e_of_i && $is_active, 0).'</div>';
+echo '<div class="doclear">&nbsp;</div>';
+
+
+//IDEA TYPE
+echo '<div class="pull-left both-margin left-margin">'.view_input_dropdown(7585, $i_focus['i__type'], 'btn-idea', $e_of_i && $is_active, true, $i_focus['i__id']).'</div>';
+
+//IDEA STATUS
+echo '<div class="inline-block pull-left both-margin left-half-margin">'.view_input_dropdown(4737, $i_focus['i__status'], 'btn-idea', $e_of_i, true, $i_focus['i__id']).'</div>';
+
+//PREVIEW DISCOVERY
+echo '<div class="inline-block pull-right both-margin" style="margin-right:33px;"><a class="btn btn-discover" href="/'.$i_focus['i__id'].'" style="font-size: 1em !important;"><span class="show-max">'.$e___11035[13562]['m_title'].' </span>'.$e___11035[13562]['m_icon'].'</a></div>';
+
+echo '<div class="doclear">&nbsp;</div>';
 
 
 
