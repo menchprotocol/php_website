2f6ff7683e9976e54eda0f1e2db6ca3f2577f21a
Shervin Enayati <shervin@mench.co>
2020-09-22 22:20:06 -0700

    Iterations towards v1.57

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1242,9 +1242,6 @@ function view_i_note_list($x__type, $i_notes, $e_of_i, $show_empty_error = false
     $user_e = superpower_assigned();
     $ui = '';
 
-
-    $ui .= '<div class="headline">'.$e___4485[$x__type]['m_title'].'</div>';
-
     if($show_empty_error && !count($i_notes) && $e_of_i){
         $ui .= '<div class="no_notes_' . $x__type .'" style="margin-bottom:13px;">';
         $ui .= '<div class="msg alert alert-danger" role="alert"><span class="icon-block">&nbsp;</span>No '.ucwords(strtolower($e___4485[$x__type]['m_title'])).' yet</div>';
