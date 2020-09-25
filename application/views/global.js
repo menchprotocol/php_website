77f2383d296258b7a4490f1d57181c111c0f4d1b
Shervin Enayati <shervin@mench.co>
2020-09-25 13:22:33 -0700

    Iterations towards v1.57

--- application/views/global.js
+++ application/views/global.js
@@ -813,7 +813,7 @@ function i_note_activate(){
 }
 
 function i_note_counter(note_type_id, adjustment_count){
-    var current_count = parseInt($('.en-type-counter-'+note_type_id).text());
+    var current_count = parseInt( $('.en-type-counter-'+note_type_id).text().length ? $('.en-type-counter-'+note_type_id).text() : 0 );
     var new_count = current_count + adjustment_count;
     $('.en-type-counter-'+note_type_id).text(new_count);
 }
