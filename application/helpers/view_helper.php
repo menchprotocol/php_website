eb87e78c9de4d7db95c65b3bf40044d7f27a149e
Shervin Enayati <shervin@mench.co>
2020-09-27 14:29:59 -0700

    Iterations towards v1.59

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -29,10 +29,15 @@ function view_i_tree_stats($i_stats, $noshow_idea, $hide_time, $hide_idea = true
     //Variable time range:
     $ui = null;
 
+    //IDEAS
+    if(!$noshow_idea && $i_stats['i___6169'] > 0){
+        $ui .= '<span class="'.( $hide_idea ? ' i_x_stats hidden ' : '' ).' inline-block '.extract_icon_color($e___13544[13629]['m_icon']).'" '.( !$cover_show ? 'style="min-width:89px;"' : '' ).'>'.( $has_idea ? ( $cover_show ? '&nbsp;&nbsp;'.$e___13544[13629]['m_icon'].'&nbsp;' : '<span class="icon-block">'.$e___13544[13629]['m_icon'].'</span>' ) : '' ).( $has_idea ? view_number($i_stats['i___6169']).( $is_interactive ? '<span class="mid-range">-</span>'.view_number($i_stats['i___6170']) : '' ) : '&nbsp;' ).'</span>';
+    }
+
 
     //TIME STATS
     if($i_stats['i___6161'] > 0){
-        $ui .= '<span class="inline-block grey '.( $hide_time ? ' i_x_stats hidden ' : '' ).'" '.( !$cover_show ? 'style="min-width:120px;"' : '' ).'>';
+        $ui .= '<span class="inline-block grey '.( $hide_time ? ' i_x_stats hidden ' : '' ).'">';
         $ui .= ( $cover_show ? $e___13544[13292]['m_icon'].' ' : '<span class="icon-block">'.$e___13544[13292]['m_icon'].'</span>' );
 
         if($i_stats['i___6161']<30 && $i_stats['i___6162']<30){
@@ -43,11 +48,6 @@ function view_i_tree_stats($i_stats, $noshow_idea, $hide_time, $hide_idea = true
             $ui .= '<span class="inline-block">'.round_minutes($i_stats['i___6161']).( round_minutes($i_stats['i___6161']) != round_minutes($i_stats['i___6162']) ? '<span class="mid-range">-</span>'.round_minutes($i_stats['i___6162']) : '' ).( !$cover_show ? '&nbsp;MIN' : '\'' ).'</span>';
         }
         $ui .= '</span>';
-    }
-
-    //IDEAS
-    if(!$noshow_idea && $i_stats['i___6169'] > 0){
-        $ui .= '<span class="'.( $hide_idea ? ' i_x_stats hidden ' : '' ).' inline-block '.extract_icon_color($e___13544[13629]['m_icon']).'">'.( $has_idea ? ( $cover_show ? '&nbsp;&nbsp;'.$e___13544[13629]['m_icon'].'&nbsp;' : '<span class="icon-block">'.$e___13544[13629]['m_icon'].'</span>' ) : '' ).( $has_idea ? view_number($i_stats['i___6169']).( $is_interactive ? '<span class="mid-range">-</span>'.view_number($i_stats['i___6170']) : '' ) : '&nbsp;' ).'</span>';
     }
 
 
