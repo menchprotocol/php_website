84176c2bfcb4b520dee2a17e1c05acba2e00fc74
Shervin Enayati <shervin@mench.co>
2020-10-07 14:17:59 -0700

    Iterations Towards v1.60

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,30 +1,3 @@
 
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v8.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-     attribution=setup_tool
-     page_id="381488558920384"
-     greeting_dialog_display="hide"
-     ref=""
-     theme_color="#FC1B44">
-</div>
-
 
 </body>
