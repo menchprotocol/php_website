aba9f35796de2a578531b141001078c8171c0ee9
Shervin Enayati <shervin@mench.co>
2020-10-08 11:05:41 -0700

    Iterations Towards v1.60

--- application/views/e/app/13369/index.php
+++ application/views/e/app/13369/index.php
@@ -1,7 +1,9 @@
 
 <script type="text/javascript">
     function update_icon(){
-        $('#cover_frame').attr("class",$('#cover_input').val());
+        var new_icon = $('#cover_input').val();
+        alert(new_icon);
+        $('#cover_frame').attr("class",new_icon);
     }
     update_icon();
     $("#cover_input").change(function () {
@@ -9,7 +11,7 @@
     });
 </script>
 
-<input id="cover_input" type="text" value="fas fa-circle idea" class="form-control white-border">
+<input id="cover_input" type="text" value="fas fa-circle idea" class="form-control white-border" />
 
 <div style="background-color: #000000; width: 400px; height: 600px; text-align: center; border:3px solid #FC1B44; margin:21px 0;">
     <i id="cover_frame" class="" style="font-size: 21em; padding-top: 126px;"></i>
