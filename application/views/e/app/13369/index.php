89cd9c9054f3795feca78d595461dd78a88aab30
Shervin Enayati <shervin@mench.co>
2020-10-08 11:11:27 -0700

    Iterations Towards v1.60

--- application/views/e/app/13369/index.php
+++ application/views/e/app/13369/index.php
@@ -21,5 +21,5 @@
 <input id="cover_input" type="text" value="fas fa-circle idea" class="form-control white-border" />
 
 <div style="background-color: #000000; width: 400px; height: 600px; text-align: center; border:3px solid #FC1B44; margin:21px 0;">
-    <i id="cover_frame" style="font-size: 21em; padding-top: 126px;"></i>
+    <i id="cover_frame" style="font-size: 18em; padding-top: 144px;"></i>
 </div>
