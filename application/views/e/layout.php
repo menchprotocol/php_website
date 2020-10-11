cbb8d67fc7d0c5ab0da02bd15088ecceecff8633
Shervin Enayati <shervin@mench.co>
2020-10-11 15:22:25 -0700

    Iterations towards v161

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -640,7 +640,7 @@ $superpower_12701 = superpower_active(12701, true); //SUPERPOWER OF DISCOVERY GL
 
                 <!-- Source Status -->
                 <div class="headline no-left-padding"><?= '<span class="icon-block">'.$e___13571[6177]['m__icon'].'</span>'.$e___13571[6177]['m__title'] ?></div>
-                <select class="form-control border" id="e__type">
+                <select class="form-control border" id="e__type" name="e__type">
                     <?php
                     foreach($this->config->item('e___6177') /* Source Status */ as $x__type => $m){
                         echo '<option value="' . $x__type . '" title="' . $m['m__message'] . '">' . $m['m__title'] . '</option>';
@@ -658,7 +658,7 @@ $superpower_12701 = superpower_active(12701, true); //SUPERPOWER OF DISCOVERY GL
 
                 <!-- Source Title -->
                 <div class="headline no-left-padding"><?= '<span class="icon-block">'.$e___13571[6197]['m__icon'].'</span>'.$e___13571[6197]['m__title'] ?> [<span style="margin:0 0 10px 0;"><span id="charEnNum">0</span>/<?= view_memory(6404,6197) ?></span>]</div>
-                <textarea class="form-control text-edit border montserrat doupper" id="e__title" onkeyup="e__title_word_count()" data-lpignore="true"></textarea>
+                <textarea class="form-control text-edit border montserrat doupper" id="e__title" name="e__title" onkeyup="e__title_word_count()" data-lpignore="true"></textarea>
 
 
 
@@ -674,7 +674,7 @@ $superpower_12701 = superpower_active(12701, true); //SUPERPOWER OF DISCOVERY GL
                 </div>
                 <div class="form-group" style="margin:0 0 13px; border-radius: 10px;">
                     <div class="input-group border">
-                        <input type="text" id="e__icon" value="" data-lpignore="true" placeholder="" class="form-control" style="margin-bottom: 0;">
+                        <input type="text" id="e__icon" name="e__icon" value="" data-lpignore="true" placeholder="" class="form-control" style="margin-bottom: 0;">
                         <span class="input-group-addon addon-lean addon-grey icon-demo icon-block" style="padding-top:8px;"></span>
                     </div>
                 </div>
@@ -683,7 +683,7 @@ $superpower_12701 = superpower_active(12701, true); //SUPERPOWER OF DISCOVERY GL
                 <div class="e_has_link">
 
                     <div class="headline no-left-padding"><?= '<span class="icon-block">'.$e___13571[6186]['m__icon'].'</span>'.$e___13571[6186]['m__title'] ?></div>
-                    <select class="form-control border" id="x__status">
+                    <select class="form-control border" id="x__status" name="x__status">
                         <?php
                         foreach($this->config->item('e___6186') /* Transaction Status */ as $x__type => $m){
                             echo '<option value="' . $x__type . '" title="' . $m['m__message'] . '">' . $m['m__title'] . '</option>';
@@ -701,7 +701,7 @@ $superpower_12701 = superpower_active(12701, true); //SUPERPOWER OF DISCOVERY GL
                     <div class="headline no-left-padding" style="margin-top: 20px;"><?= '<span class="icon-block">'.$e___13571[4372]['m__icon'].'</span>'.$e___13571[4372]['m__title'] ?></div>
                     <form class="drag-box" method="post" enctype="multipart/form-data">
 
-                        <textarea class="form-control text-edit border" id="x__message" data-lpignore="true" placeholder="<?= $e___13571[4372]['m__message'] ?>"></textarea>
+                        <textarea class="form-control text-edit border" id="x__message" name="x__message" data-lpignore="true" placeholder="<?= $e___13571[4372]['m__message'] ?>"></textarea>
 
                         <div class="pull-left">
                             <div id="x__type_preview" class="hideIfEmpty"></div>
