4560939e0b8a6bb9f59c7ff95adaba7b71862882
Shervin Enayati <shervin@mench.co>
2020-10-15 13:39:36 -0700

    Iterations towards v161

--- application/views/e/account.php
+++ application/views/e/account.php
@@ -9,7 +9,7 @@ $e___11035 = $this->config->item('e___11035'); //MENCH NAVIGATION
 $e___14010 = $this->config->item('e___14010');
 
 //ACCOUNT SETTING
-echo '<h1 class="big-frame"><a href="/@'.$user_e['e__id'].'" style="text-decoration:none;" class="'.extract_icon_color($user_e['e__icon']).'">' . $user_e['e__title'] . '</a></h1>';
+echo '<h1 class="big-frame '.extract_icon_color($user_e['e__icon']).'">' . $user_e['e__title'] . '</h1>';
 
 echo '<div class="headline"><span class="icon-block">'.$e___11035[6225]['m__icon'].'</span>'.$e___11035[6225]['m__title'].'</div>';
 echo '<div class="accordion" id="MyAccountAccordion">';
