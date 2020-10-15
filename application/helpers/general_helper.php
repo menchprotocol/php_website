27040eaa97bb1b86577735be94b25d7cb7ee1a5f
Shervin Enayati <shervin@mench.co>
2020-10-14 19:04:48 -0700

    Iterations towards v161

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -690,7 +690,7 @@ function check_cookie()
     if(/*!superpower_assigned() &&*/ isset($_COOKIE['mench_keep_login'])) {
         echo '/signin?url=' . urlencode($_SERVER['PHP_SELF']);
         return false;
-        header("Location: " . '/signin?url=' . urlencode($_SERVER['PHP_SELF']), true, 307);
+        header("Location: " . '/signin?url=' . urlencode($_SERVER['REQUEST_URI']), true, 307);
         exit;
     }
 
