6a1688c1c58afe1c01b9b8e16d0ed806174f06b7
Shervin Enayati <shervin@mench.co>
2020-10-18 16:43:36 -0700

    Iterations towards v161

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -802,7 +802,7 @@ function view_i_x($i, $can_click, $common_prefix = null, $show_editor = false, $
             $ui .= '<b class="'.( $can_click ? ' montserrat ' : '' ).' i-url title-block">'.view_i_title($i, $common_prefix).'</b>';
 
         $ui .= '</div>';
-        $ui .= '<div class="col-md-2 show-max">';
+        $ui .= '<div class="col-md-2">';
 
             if($i_stats['i___6169'] > 0){
                 $ui .= '<span class="'.extract_icon_color($e___12467[12273]['m__icon']).' montserrat" title="'.$e___12467[12273]['m__title'].'" data-toggle="tooltip" data-placement="top"><span class="icon-block">'.$e___12467[12273]['m__icon'].'</span>'.view_number($i_stats['i___6170']).'</span>';
