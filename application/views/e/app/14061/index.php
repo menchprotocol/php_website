88d83e05e2267bb7e26bdc64c64008f7a9b417de
Shervin Enayati <shervin@mench.co>
2020-10-19 17:08:02 -0700

    Iterations Towards v162

--- application/views/e/app/14061/index.php
+++ application/views/e/app/14061/index.php
@@ -13,11 +13,11 @@ foreach($custom_query as $x){
     if(count($parts)==3 && second_calc($parts[1])>0 && second_calc($parts[2])>0){
         if($parts[1]>=60){
             echo '<div class="discover montserrat">1 NEED ADJUSTMENT!!!!!!!!</div>';
-            $x['x__message'] = str_replace(':'. $parts[1],':'.floor($parts[1]/60) .'.'. fmod($parts[1]/60),$x['x__message']);
+            $x['x__message'] = str_replace(':'. $parts[1],':'.floor($parts[1]/60) .'.'. fmod($parts[1],60),$x['x__message']);
         }
         if($parts[2]>=60){
             echo '<div class="discover montserrat">2 NEED ADJUSTMENT!!!!!!!!</div>';
-            $x['x__message'] = str_replace(':'. $parts[2],':'.floor($parts[2]/60) .'.'. fmod($parts[2]/60),$x['x__message']);
+            $x['x__message'] = str_replace(':'. $parts[2],':'.floor($parts[2]/60) .'.'. fmod($parts[2],60),$x['x__message']);
         }
         $new_message = str_replace('.',':',str_replace(':','|',$x['x__message']));
         echo '<div class="idea montserrat">REPLACE WITH: '.$new_message.'</div>';
