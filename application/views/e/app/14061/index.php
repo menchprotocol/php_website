1cffffdda1eba7cef6799624d32976bbf5d1ab30
Shervin Enayati <shervin@mench.co>
2020-10-19 17:24:35 -0700

    Iterations Towards v162

--- application/views/e/app/14061/index.php
+++ application/views/e/app/14061/index.php
@@ -1,5 +1,7 @@
 <?php
 
+die('retired for now'); //No longer needed
+
 $custom_query = $this->X_model->fetch(array(
     "x__type IN (4983,7545,13865,4231,13970,13971,10573,4601,12419,12896) AND x__status=6176 AND x__message LIKE '%:%' AND x__message LIKE '%@%'" => null,
 ), array(), 0);
