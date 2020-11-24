3357bbe81e50864e4b4756fef96805011f3f0976
Shervin Enayati <shervin@mench.co>
2020-11-24 11:10:14 -0700

    Iterations Towards v162

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1333,7 +1333,7 @@ function view_i_cover($x__type, $i, $show_editor, $message_input = null, $focus_
 
 
     $ui  = '<div '.( isset($i['x__id']) ? ' x__id="'.$i['x__id'].'" ' : '' ).' class="col-md-4 col-sm-6 i_class_'.$x__type.'_'.$i['i__id'].' no-padding '.( $show_editor ? ' cover_sort ' : '' ).'">';
-    $ui .= '<div class="cover-wrapper '.( $discovery_mode ? ( $completion_rate['completion_percentage']<100 ? 'wrap-discover' : '' /* grey */ ) : 'wrap-idea' ).'">';
+    $ui .= '<div class="cover-wrapper">';
     $ui .= ( $can_click ? '<a href="'.$href.'"' : '<div' ).' class="cover-link" style="background-image:url(\''.i_fetch_cover($i['i__id']).'\');">';
 
     if($completion_rate['completion_percentage']>0){
