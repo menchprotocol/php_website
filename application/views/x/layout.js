994a828b60f4606d86be11fd852a9b59b06643d7
Shervin Enayati <shervin@mench.co>
2020-12-01 10:27:51 -0700

    Iterations Towards v162

--- application/views/x/layout.js
+++ application/views/x/layout.js
@@ -54,6 +54,7 @@ function go_previous(href_url) {
     if(i_drip_mode_js && i_drip_pointer>1){
         i_drip_pointer--;
         //Simply go to the next drip:
+        history.pushState(null,null,'#drip'+i_drip_pointer);
         $('.drip_msg').addClass('hidden');
         $('.drip_msg_'+i_drip_pointer).removeClass('hidden');
         $('.final_drip').addClass('hidden');
@@ -69,6 +70,7 @@ function go_next(go_next_url){
 
         i_drip_pointer++;
         //Simply go to the next drip:
+        history.pushState(null,null,'#drip'+i_drip_pointer);
         $('.drip_msg').addClass('hidden');
         $('.drip_msg_'+i_drip_pointer).removeClass('hidden');
 
