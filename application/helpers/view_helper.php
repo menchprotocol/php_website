49467397d6bc3aa460d26338399b7f7aa4c7b263
Shervin Enayati <shervin@mench.co>
2020-12-02 14:54:26 -0700

    Iterations Towards v162

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1162,15 +1162,16 @@ function view_info_box($e__id){
     $CI =& get_instance();
     $e___14339 = $CI->config->item('e___14339');
     $ui = '';
-    $ui .= '<div class="row">';
     $ui .= '<h2 class="info_box_header montserrat">' . $e___14339[$e__id]['m__title'] . '</h2>';
+    $ui .= '<div class="row">';
     foreach($CI->config->item('e___'.$e__id) as $m) {
-        $ui .= '<div class="col-12 col-sm-4 info_box">';
-        $ui .= '<div class="info_box_cover">'.$m['m__icon'].'</div>';
-        $ui .= '<div class="info_box_title montserrat">'.$m['m__title'].'</div>';
-        $ui .= '<div class="info_box_message">'.$m['m__message'].'</div>';
+        $ui .= '<div class="col-12 col-sm-4">';
+            $ui .= '<div class="info_box">';
+                $ui .= '<div class="info_box_cover">'.$m['m__icon'].'</div>';
+                $ui .= '<div class="info_box_title montserrat">'.$m['m__title'].'</div>';
+                $ui .= '<div class="info_box_message">'.$m['m__message'].'</div>';
+            $ui .= '</div>';
         $ui .= '</div>';
-
     }
     $ui .= '</div>';
     return $ui;
