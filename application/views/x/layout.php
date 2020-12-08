053584f214f1ca9845d25d518e84e4fb7758325e
Shervin Enayati <shervin@mench.co>
2020-12-07 17:43:10 -0800

    Iterations Towards v162

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -380,7 +380,7 @@ if(count($fetch_13865)){
 //DISCOVER LAYOUT
 $i_stats = i_stats($i_focus['i__metadata']);
 $tab_group = 13291;
-$tab_pills = '<div class="header-drop hidden"><ul class="nav nav-tabs nav-sm '.superpower_active(10939).'">';
+$tab_pills = '<div class=""><ul class="nav nav-tabs nav-sm '.superpower_active(10939).'">'; //header-drop hidden
 $tab_content = '';
 $tab_pill_count = 0;
 
