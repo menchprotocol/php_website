804a53feed122b3b6f629821e40bfef1d879f092
Shervin Enayati <shervin@mench.co>
2020-12-08 16:02:25 -0800

    Iterations Towards v162

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -375,7 +375,7 @@ $superpower_12703 = superpower_active(12703, true); //SUPERPOWER OF CHAIN LINK
                                maxlength="' . view_memory(6404,4736) . '"
                                id="newIdeaTitle"
                                placeholder="'.$e___11035[14015]['m__title'].'">
-                    </div><div class="add_e_idea algolia_pad_search hidden">'.view_memory(6404,13914).'</div></div></div>';
+                    </div></div></div>';
 
 
                     $i_bookmarks = $this->X_model->fetch(array(
