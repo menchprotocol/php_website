59076968fc87b418dbe197705bc204d27ba60596
Shervin Enayati <shervin@mench.co>
2020-12-09 12:47:51 -0800

    Iterations Towards v162

--- application/views/i/layout.php
+++ application/views/i/layout.php
@@ -267,7 +267,7 @@ foreach($this->config->item('e___'.$tab_group) as $x__type => $m){
 
 
         //$ui .= '<div class="headline"><span class="icon-block">'.$e___11035[13542]['m__icon'].'</span>'.$e___11035[13542]['m__title'].'</div>';
-        $ui .= '<div id="list-in-' . $i_focus['i__id'] . '-0" class="row top-margin next_i">';
+        $ui .= '<div id="list-in-' . $i_focus['i__id'] . '-0" class="row next_i">';
         foreach($is_next as $next_i) {
             $ui .= view_i(13542, $next_i);
         }
