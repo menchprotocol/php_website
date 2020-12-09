7c92c615d16d228e0d3f88411196e5490b0d0313
Shervin Enayati <shervin@mench.co>
2020-12-09 10:59:43 -0800

    Iterations Towards v162

--- application/views/global.js
+++ application/views/global.js
@@ -1304,10 +1304,9 @@ function i_note_add_file(droppedFiles, uploadType, note_type_id) {
         success: function (data) {
 
             if(js_n___14311.includes(note_type_id)){
-                alert('power edit');
                 //Power Editor:
                 var current_value = $('.input_note_' + note_type_id).val();
-                $('.input_note_' + note_type_id).val(( current_value.length ? current_value+"\n\n" : '' ) + data.new_source);
+                $('.input_note_' + note_type_id).val(( current_value.length ? current_value+" " : '' ) + data.new_source);
                 i_note_poweredit_save(note_type_id);
             } else {
                 //Regular Editor:
