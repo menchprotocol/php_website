a8feb526626b97db72224cfc32f010b0c003f8ec
Shervin Enayati <shervin@mench.co>
2020-12-12 13:49:31 -0800

    Iterations Towards v162

--- application/views/x/layout.php
+++ application/views/x/layout.php
@@ -302,12 +302,6 @@ if($in_my_x && count($x_completes)){
     echo '<div class="headline"><span class="icon-block">'.$e___11035[14457]['m__icon'].'</span>'.str_replace('%S',view_time_difference(strtotime($x_completes[0]['x__time'])), $e___11035[14457]['m__title']).'</div>';
 }
 
-if(){
-    //HEADER
-    echo '<h1>' . view_i_title($i_focus) . '</h1>';
-}
-
-
 
 //MESSAGES
 foreach($messages as $message_x) {
