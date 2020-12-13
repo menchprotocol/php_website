dcc9474d8577763c36031908cea1969695704402
Shervin Enayati <shervin@mench.co>
2020-12-13 13:31:45 -0800

    Iterations Towards v162

--- application/views/global.js
+++ application/views/global.js
@@ -250,12 +250,8 @@ $(document).ready(function () {
     });
 
 
-    $('.editor_preview_4231').click(function (e) {
-        if (!(this).hasClass('ignore-click')) {
-            loadtab(14418, 14468);//Load Write Tab
-        } else{
-            alert('ignored');
-        }
+    $('.editor_preview_4231 *:not(.ignore-click)').click(function (e) {
+        loadtab(14418, 14468);//Load Write Tab
     });
 
 
