2d780bfdce914670a8c1285bd748f259362b1f9b
Shervin Enayati <shervin@mench.co>
2020-12-15 13:44:50 -0800

    Iterations Towards v170

--- application/views/header.php
+++ application/views/header.php
@@ -183,15 +183,15 @@ if(!isset($hide_header)){
                     echo '<div class="left_nav mench_nav">';
                     echo '<span class="inline-block pull-left">';
 
-                    echo '<a href="'.home_url($current_mench['x_id']).'">'.( !$user_e ? '<img src="/img/mench.png" class="mench-logo mench-spin" />' : '<span class="mench-circle"><i class="fas fa-circle '.$current_mench['x_name'].'"></i></span>' ).'<b class="montserrat text-logo">MENCH</b></a>';
+                    echo '<a href="'.home_url($current_mench['x_id']).'">'.( !$user_e ? '<img src="/img/mench.png" class="mench-logo mench-spin" />' : '<span class="mench-circle"><i class="fas fa-circle '.$current_mench['x_name'].'" title="'.strtoupper($current_mench['x_name']).'"></i></span>' ).'<b class="montserrat text-logo">MENCH</b></a>';
 
                     //EDIT IDEA
                     if($i__id && is_numeric($first_segment) && superpower_active(10939, true)){
                         //PUBLISH
-                        echo '<span class="mench-circle"><a href="/~'.$i__id.'" style="margin-left: 0;" title="'.$e___11035[13563]['m__title'].'" data-toggle="tooltip" data-placement="bottom">'.$e___11035[13563]['m__icon'].'</a></span>';
+                        echo '<span class="mench-circle"><a href="/~'.$i__id.'" style="margin-left: 0;" title="'.$e___11035[13563]['m__title'].'" data-toggle="tooltip" data-placement="right">'.$e___11035[13563]['m__icon'].'</a></span>';
                     } elseif($i__id && substr($first_segment, 0, 1)=='~'){
                         //DISCOVER
-                        echo '<span class="mench-circle"><a href="/'.$i__id.'" style="margin-left: 0;" title="'.$e___11035[13562]['m__title'].'" data-toggle="tooltip" data-placement="bottom">'.$e___11035[13562]['m__icon'].'</a></span>';
+                        echo '<span class="mench-circle"><a href="/'.$i__id.'" style="margin-left: 0;" title="'.$e___11035[13562]['m__title'].'" data-toggle="tooltip" data-placement="right">'.$e___11035[13562]['m__icon'].'</a></span>';
                     }
 
                     echo '</span>';
