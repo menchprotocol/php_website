d1787c8fd677f3104e859b1b4d6a5a1c23b7dae3
Shervin Enayati <shervin@mench.co>
2020-12-16 16:35:36 -0800

    Iterations Towards v170

--- application/views/e/get_started.php
+++ application/views/e/get_started.php
@@ -4,7 +4,7 @@ $e___11035 = $this->config->item('e___11035');
 
 <!-- HEADLINE -->
 <div class="container fixed-top" style="padding-bottom: 0 !important;">
-    <?= '<a href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'" style="margin-right: 8px;" class="pull-right"><b class="montserrat text-logo text__6197_14521">'.$e___11035[14521]['m__title'].' '.$e___11035[14521]['m__icon'].'</b></a>' ?>
+    <?= '<a href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'" style="margin-right: 8px;" class="pull-right inline-block"><b class="montserrat text-logo text__6197_14521">'.$e___11035[14521]['m__title'].'</b>'.$e___11035[14521]['m__icon'].'</a>' ?>
 </div>
 
 <div class="container">
@@ -13,7 +13,7 @@ $e___11035 = $this->config->item('e___11035');
     if(strlen($e___11035[14517]['m__message']) > 0){
         echo '<div class="msg top-margin">' . $e___11035[14517]['m__message'] . '</div>';
     } else {
-        echo '<h1 class="'.extract_icon_color($e___11035[14517]['m__icon']).'">' . $e___11035[14517]['m__title'] . '</h1>';
+        echo '<h1 class="top-margin '.extract_icon_color($e___11035[14517]['m__icon']).'">' . $e___11035[14517]['m__title'] . '</h1>';
     }
 
     echo view_e_settings(14517, false);
