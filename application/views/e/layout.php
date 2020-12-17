ccde51a0eac62e4cf0eb52e8e999b5fdb2b577ed
Shervin Enayati <shervin@mench.co>
2020-12-17 12:46:57 -0800

    Iterations Towards v170

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -353,10 +353,10 @@ $profiles = $this->X_model->fetch(array(
                 //MY IDEAS?
                 if($source_is_e){
 
-                    $ui .= '<div class="headline top-margin"><span class="icon-block">' . $e___11035[10573]['m__icon'] . '</span>' . $e___11035[10573]['m__title'] . '</div>';
+                    //$ui .= '<div class="headline top-margin"><span class="icon-block">' . $e___11035[10573]['m__icon'] . '</span>' . $e___11035[10573]['m__title'] . '</div>';
 
                     //Give Option to Add New Idea:
-                    $ui .= '<div class="list-group add_e_idea"><div class="list-group-item list-adder">
+                    $ui .= '<div class="list-group add_e_idea top-margin"><div class="list-group-item list-adder">
                     <div class="input-group border">
                         <a class="input-group-addon addon-lean icon-adder" href="javascript:void(0);" onclick="$(\'#newIdeaTitle\').focus();"><span class="icon-block">'.$e___11035[14015]['m__icon'].'</span></a>
                         <input type="text"
