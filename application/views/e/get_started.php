43c2677a5895debc980c06ccebb264cc01692b38
Shervin Enayati <shervin@mench.co>
2020-12-18 14:08:39 -0800

    Iterations Towards v170

--- application/views/e/get_started.php
+++ application/views/e/get_started.php
@@ -1,25 +1,30 @@
 <?php
 $e___11035 = $this->config->item('e___11035');
-$next_link = '<a href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'" style="margin-right: 8px;" class="pull-right inline-block"><b class="css__title special-text text__6197_14521 '.extract_icon_color($e___11035[14521]['m__icon']).'">'.$e___11035[14521]['m__title'].'</b>'.$e___11035[14521]['m__icon'].'</a>';
+
+
+
+//MENCH SETUP
+echo '<div class="container">';
+echo '<h1 class="top-margin '.extract_icon_color($e___11035[14517]['m__icon']).'">' . $e___11035[14517]['m__title'] . '</h1>';
+if(strlen($e___11035[14517]['m__message']) > 0){
+    echo '<div class="msg">' . $e___11035[14517]['m__message'] . '</div>';
+}
+
+echo view_e_settings(14517, false);
+echo '</div>';
+
+
+
+//CONTINUE:
+echo '<div class="container fixed-bottom light-bg">';
+echo '<div class="discover-controller">';
+echo '<div><a class="controller-nav btn btn-lrg btn-discover go-next" href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'">'.$e___11035[14521]['m__icon'].' '.$e___11035[14521]['m__title'].'</a></div>';
+echo '</div>';
+echo '</div>';
+
+
+
+
 ?>
 
-<!-- HEADLINE -->
-<div class="container fixed-top hidden" style="padding: 10px 0 !important;">
-    <?php echo $next_link; ?>
-</div>
-
-<div class="container">
-    <?php
-
-    echo '<h1 class="top-margin '.extract_icon_color($e___11035[14517]['m__icon']).'">' . $e___11035[14517]['m__title'] . '</h1>';
-    if(strlen($e___11035[14517]['m__message']) > 0){
-        echo '<div class="msg">' . $e___11035[14517]['m__message'] . '</div>';
-    }
-
-    echo view_e_settings(14517, false);
-
-    echo '<div style="padding: 34px 0; text-align: center;">'.$next_link.'</div>';
-
-    ?>
-</div>
 
