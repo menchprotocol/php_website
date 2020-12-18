860b0a49a6f9f0d1f71aee1a22a7f17e74c6d8da
Shervin Enayati <shervin@mench.co>
2020-12-18 14:09:35 -0800

    Iterations Towards v170

--- application/views/e/get_started.php
+++ application/views/e/get_started.php
@@ -18,7 +18,7 @@ echo '</div>';
 //CONTINUE:
 echo '<div class="container fixed-bottom light-bg">';
 echo '<div class="discover-controller">';
-echo '<div><a class="controller-nav btn btn-lrg btn-discover go-next" href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'">'.$e___11035[14521]['m__icon'].' '.$e___11035[14521]['m__title'].'</a></div>';
+echo '<div><a class="controller-nav btn btn-lrg btn-discover go-next" href="'.( $i__id > 0 ? '/x/x_start/'.$i__id : home_url() ).'">'.$e___11035[14521]['m__title'].' '.$e___11035[14521]['m__icon'].'</a></div>';
 echo '</div>';
 echo '</div>';
 
