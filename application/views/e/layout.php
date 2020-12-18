8514a0b66e94c168490fc57b270dc77e7197912c
Shervin Enayati <shervin@mench.co>
2020-12-17 19:57:07 -0800

    Iterations Towards v170

--- application/views/e/layout.php
+++ application/views/e/layout.php
@@ -406,7 +406,7 @@ $profiles = $this->X_model->fetch(array(
                     <div class="input-group border">
                         <a class="input-group-addon addon-lean icon-adder" href="javascript:void(0);" onclick="$(\'#newIdeaTitle\').focus();"><span class="icon-block">'.$e___11035[14016]['m__icon'].'</span></a>
                         <input type="text"
-                               class="form-control form-control-thick algolia_search dotransparent add_e_idea add-input"
+                               class="form-control form-control-thick css__title algolia_search dotransparent add_e_idea add-input"
                                maxlength="' . view_memory(6404,4736) . '"
                                id="newIdeaTitle"
                                placeholder="'.$e___11035[14016]['m__title'].'">
