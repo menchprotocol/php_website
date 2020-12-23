9b06d0b3a345a162e2bb26cc87d067a56f4a64f1
Shervin Enayati <shervin@mench.co>
2020-12-23 11:13:41 -0600

    Iterations Towards v170 - Reorganize Mench Apps #3047

--- application/views/app/14565.php
+++ application/views/app/14565.php
@@ -1,14 +1,18 @@
 <?php
 
+$is = $this->I_model->fetch(array(
+    'i__id' => view_memory(6404,14002),
+));
+
 //IDEA TITLE
-echo '<h1>' . $i['i__title'] . '</h1>';
+echo '<h1>' . $is[0]['i__title'] . '</h1>';
 
 
 //MESSAGES
 foreach($this->X_model->fetch(array(
     'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
     'x__type' => 4231, //IDEA NOTES Messages
-    'x__right' => $i['i__id'],
+    'x__right' => $is[0]['i__id'],
 ), array(), 0, 0, array('x__spectrum' => 'ASC')) as $count => $x) {
     echo $this->X_model->message_view( $x['x__message'], true);
 }
