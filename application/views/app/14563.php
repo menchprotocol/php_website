93e0a33aa2c6767727d2ac4cc3d26d6dd4fa2eed
Shervin Enayati <shervin@mench.co>
2020-12-23 11:33:31 -0600

    Iterations Towards v170 - Reorganize Mench Apps #3047

--- application/views/app/14563.php
+++ application/views/app/14563.php
@@ -4,7 +4,7 @@ $e___11035 = $this->config->item('e___11035'); //MENCH NAVIGATION
 
 echo '<div class="container">';
 echo '<h1>🙈 PAGE NOT FOUND</h1>';
-echo '<p>'.view_shuffle_message(12694).'</p>';
+echo '<p class="msg">'.view_shuffle_message(12694).'</p>';
 echo '<div class="margin-top-down"><a href="/" class="css__title underline">Back to Home</a></div>';
 echo '</div>';
 
