2ac1fbb6eee5e64f8282abf8a0b47e2d38a2d1ce
Shervin Enayati <shervin@mench.co>
2020-12-23 11:15:02 -0600

    Iterations Towards v170 - Reorganize Mench Apps #3047

--- application/views/app_frame.php
+++ application/views/app_frame.php
@@ -8,9 +8,11 @@ if($memory_detected){
     $e___11035 = $this->config->item('e___11035'); //MENCH NAVIGATION
 
     //Apps, Header & Source:
+    /*
     echo '<a href="/app/" class="inline-block"><span class="icon-block">'.view_e__icon($e___11035[6287]['m__icon']).'</span></a>';
     echo '<span class="icon-block">'.view_e__icon($e___6287[$app_e__id]['m__icon']).'</span>';
     echo '<h1 class="inline-block">'.$e___6287[$app_e__id]['m__title'].'</h1>';
+    */
 
     //Optional Description:
     if(strlen($e___6287[$app_e__id]['m__message']) > 0){
