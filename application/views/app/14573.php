fc845626d7ca0dce5191f9c2e21e6ca89aaf13df
Shervin Enayati <shervin@mench.co>
2020-12-23 19:55:35 -0600

    v170 - Iterations Towards v171

--- application/views/app/14573.php
+++ application/views/app/14573.php
@@ -9,11 +9,9 @@ if($user_e){
         'i__type NOT IN (' . join(',', $this->config->item('n___7356')) . ')' => null, //ACTIVE
     )) as $deleted_i){
         $deleted_ideas++;
-        $this_deleted = $this->I_model->remove($deleted_i['i__id'], $user_e['e__id']);
-        echo '#'.$deleted_i['i__id'].' : '.$this_deleted.' Deleted<br />';
-        $deleted_links += $this_deleted;
+        $deleted_links += $this->I_model->remove($deleted_i['i__id'], $user_e['e__id']);
     }
 }
 
 
-echo '<br />Deleted '.$deleted_links.' Links from '.$deleted_ideas.' deleted ideas';
+echo 'Deleted '.$deleted_links.' Links from '.$deleted_ideas.' deleted ideas';
