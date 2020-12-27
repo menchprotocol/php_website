7aa9bd658f1465184b04f20913dde564cf81634d
Shervin Enayati <shervin@mench.co>
2020-12-27 15:58:18 -0600

    Iterations Towards v171

--- application/views/app/4269.php
+++ application/views/app/4269.php
@@ -124,7 +124,7 @@ if(superpower_unlocked()) {
                         'i__id' => $sign_i__id,
                     ));
                     if (count($sign_i)) {
-                        $intro_message = str_replace('%s','<a href="/' . $sign_i__id . '"><u>'.$sign_i[0]['i__title'].'</u></a>', $e___4269[7560]['m__message']);
+                        $intro_message = str_replace('%s','<br /><a href="/' . $sign_i__id . '"><u>'.$sign_i[0]['i__title'].'</u></a>', $e___4269[7560]['m__message']);
                     }
                 }
 
