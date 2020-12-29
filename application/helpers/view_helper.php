268785c9dcf38110504c0c7a79aa7b8df01d23ad
Shervin Enayati <shervin@mench.co>
2020-12-28 23:59:07 -0600

    Iterations Towards v171

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1695,8 +1695,8 @@ function view_i($x__type, $i, $control_enabled = false, $message_input = null, $
             ), array('x__left'), 0, 0, array('i__spectrum' => 'DESC'));
 
             if(count($is_previous)){
-                $ui .= '<div class="dropdown" title="'.$e___11035[11019]['m__title'].'">';
-                $ui .= '<button type="button" class="btn no-left-padding no-right-padding idea" id="nextIdeas'.$i['i__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="icon-block-xs">'.count($is_previous).'</span></button>';
+                $ui .= '<div class="dropdown inline-block" title="'.$e___11035[11019]['m__title'].'">';
+                $ui .= '<button type="button" class="btn no-left-padding no-right-padding idea icon-block-xs" id="nextIdeas'.$i['i__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="">'.count($is_previous).'</span></button>';
                 $ui .= '<div class="dropdown-menu btn-idea" aria-labelledby="nextIdeas'.$i['i__id'].'">';
                 foreach($is_previous as $previous_i) {
                     $ui .= '<a href="/~'.$previous_i['i__id'].'" class="dropdown-item css__title '.( $previous_i['i__id']==$current_i ? ' active ' : '' ).'"><span class="icon-block i__type_'.$previous_i['i__id'].'" title="'.$e___4737[$previous_i['i__type']]['m__title'].'">'.$e___4737[$previous_i['i__type']]['m__icon'].'</span>'.view_i_title($previous_i).'</a>';
@@ -1720,7 +1720,7 @@ function view_i($x__type, $i, $control_enabled = false, $message_input = null, $
                 'x__left' => $i['i__id'],
             ), array('x__right'), 0, 0, array('x__spectrum' => 'ASC'));
             if(count($is_next)){
-                $ui .= '<div class="dropdown" title="'.$e___11035[13542]['m__title'].'">';
+                $ui .= '<div class="dropdown inline-block" title="'.$e___11035[13542]['m__title'].'">';
                 $ui .= '<button type="button" class="btn no-left-padding no-right-padding idea" id="nextIdeas'.$i['i__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="icon-block-xs">'.count($is_next).'</span></button>';
                 $ui .= '<div class="dropdown-menu btn-idea" aria-labelledby="nextIdeas'.$i['i__id'].'">';
                 foreach($is_next as $next_i) {
