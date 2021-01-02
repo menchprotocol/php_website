54b67d67dde924b2eaf9e8c55ec1035334f71378
Shervin Enayati <shervin@mench.co>
2021-01-02 14:58:31 -0600

    Iterations Towards v171

--- application/views/app/14709.php
+++ application/views/app/14709.php
@@ -13,10 +13,7 @@ if(!count($is) || !$member_e){
 
     echo $is[0]['i__title'];
 
-    foreach($this->config->item('e___14709') as $e__id => $m) {
-        echo '<div class="headline top-margin"><span class="icon-block">'.$m['m__icon'].'</span>'.$m['m__title'].'</div>';
-        echo view_e_settings($e__id, true);
-    }
+    echo view_e_settings(14709, false);
 
 }
 
