80fa47f3665c2b3ce1bf1cb1426965e85c1b5be7
Shervin Enayati <shervin@mench.co>
2021-01-10 21:21:26 -0600

    Iterations Towards v171

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1716,6 +1716,9 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
             //Idea Toolbar
             $ui .= '<div style="text-align: center;">';
 
+            $ui .= view_coins_i(12274,  $i).'&nbsp;';
+            $ui .= view_coins_i(6255,  $i).'&nbsp;';
+
             if(isset($i['x__id'])){
 
                 $x__metadata = unserialize($i['x__metadata']);
@@ -1743,12 +1746,6 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
 
             }
 
-            $ui .= '</div>';
-
-            //IDEA COins
-            $ui .= '<div style="text-align: center;">';
-            $ui .= view_coins_i(12274,  $i).'&nbsp;&nbsp;';
-            $ui .= view_coins_i(6255,  $i);
             $ui .= '</div>';
 
         }
