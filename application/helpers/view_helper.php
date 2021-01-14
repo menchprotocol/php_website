1ec0caf13cc878232037a9b069616c97d080cb8a
Shervin Enayati <shervin@mench.co>
2021-01-14 00:41:33 -0600

    Iterations Towards v171

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -34,7 +34,7 @@ function view_i_time($i_stats, $show_icon = false, $micro_sign = false){
     //Has Time
     $CI =& get_instance();
     $e___13544 = $CI->config->item('e___13544'); //IDEA TREE COUNT
-    $ui = '<div class="css__title doupper grey inline-block" style="font-size:0.8em;">';
+    $ui = '<div class="css__title doupper grey inline-block" style="font-size:0.89em;">';
 
     if(!$micro_sign && $i_stats['i___6170']>0){
         $ui .= ( $has_any_diff && !$micro_sign ? $i_stats['i___6169'].'-' : '' ).view_number($i_stats['i___6170']).' '.$e___13544[12273]['m__title'].' &middot; ';
