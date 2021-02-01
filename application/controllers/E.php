97f36aa0f6c972dba6a51c044ec4d2cbef29a57e
Shervin Enayati <shervin@mench.co>
2021-01-31 21:26:03 -0800

    Iterations Towards v171

--- application/controllers/E.php
+++ application/controllers/E.php
@@ -1480,11 +1480,11 @@ class E extends CI_Controller
 
     function e_modify_load(){
 
-        $member_e = superpower_unlocked(10939);
+        $member_e = superpower_unlocked();
         if (!$member_e) {
             return view_json(array(
                 'status' => 0,
-                'message' => view_unauthorized_message(10939),
+                'message' => view_unauthorized_message(),
             ));
         } elseif(!source_of_e($_POST['e__id'], $member_e)){
             return view_json(array(
