f2ceac7dc689c2ea2053fce1e70e61055ea7f5bd
Shervin Enayati <shervin@mench.co>
2021-02-03 23:39:34 -0800

    Iterations Towards v171

--- application/views/header.php
+++ application/views/header.php
@@ -17,7 +17,13 @@ $basic_header_footer = isset($basic_header_footer) && intval($basic_header_foote
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?= ( isset($title) ? $title : 'MENCH' ) ?></title>
 
-
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "59riunqvfm");
+    </script>
     <script type="text/javascript">
         <?php
         //MEMBER VARIABLES
