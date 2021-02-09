3ffc9514370afbeee1a3ad6a5c5ad96a848006a3
Shervin Enayati <shervin@mench.co>
2021-02-09 15:48:49 -0800

    Iterations Towards v172

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  |
  */
 
-$config['base_url']                 = 'https://'.get_domain('m__message');
+$config['base_url']                 = 'https://'.$_SERVER['HTTP_HOST'];
 
 
 /*
