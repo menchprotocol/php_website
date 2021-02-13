67d2dcf0eabe475a13dda9a7c937d28e0d7f4f61
Shervin Enayati <shervin@mench.co>
2021-02-12 16:52:59 -0800

    Iterations Towards v172

--- application/views/i_layout.php
+++ application/views/i_layout.php
@@ -103,9 +103,9 @@ if(isset($_GET['load__e']) && $superpower_14005){
 echo '<table class="table table-sm table-striped maxout" style="text-align: left;">';
 
 echo '<tr style="font-weight: bold;">';
-echo '<td width="34%" style="text-align: left;"><div class="inline-block">'.view_input_dropdown(4737, $i_focus['i__type'], 'idea', $e_of_i, true, $i_focus['i__id'], 0).'</div></td>';
-echo '<td width="33%" style="text-align: center;"><div class="inline-block left-half-margin '.superpower_active(12700).'">'.view_input_text(4356, $i_focus['i__duration'], $i_focus['i__id'], $e_of_i && $is_active, 0).'</div></td>';
-echo '<td width="33%" style="text-align: right;"><a href="javascript:void(0);" onclick="$(\'.toggle_12589\').toggleClass(\'hidden\');" title="'.$e___11035[12589]['m__title'].'" data-toggle="tooltip" data-placement="top">'.$e___11035[12589]['m__icon'].'</a></td>';
+echo '<td width="50%" style="text-align: left;"><div class="inline-block">'.view_input_dropdown(4737, $i_focus['i__type'], 'idea', $e_of_i, true, $i_focus['i__id'], 0).'</div></td>';
+echo '<td width="25%" style="text-align: right;"><div class="inline-block left-half-margin '.superpower_active(12700).'">'.view_input_text(4356, $i_focus['i__duration'], $i_focus['i__id'], $e_of_i && $is_active, 0).'</div><span class="icon-block">'.$e___11035[4356]['m__icon'].'</span></td>';
+echo '<td width="25%" style="text-align: right;"><a href="javascript:void(0);" onclick="$(\'.toggle_12589\').toggleClass(\'hidden\');" title="'.$e___11035[12589]['m__title'].'" data-toggle="tooltip" data-placement="top">'.$e___11035[12589]['m__icon'].'</a></td>';
 echo '</tr>';
 
 echo '</table>';
