2766acb674110dc2406f0ed6fb6a1a966a24ad44
Shervin Enayati <shervin@mench.co>
2021-02-18 14:52:51 -0800

    Iterations Towards v172

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1689,7 +1689,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
         //IDEA TYPE
         if($idea_editing) {
 
-            $ui .= '<div class="row" '.( $idea_editing ? ' style="margin-top:-19px;margin-bottom:-24px;" ' : '' ).'>';
+            $ui .= '<div class="row coin-block">';
             $ui .= '<div class="col-4 center" style="margin-top: 9px;">'.view_coins_i(12274,  $i).'</div>';
             $ui .= '<div class="col-4 center">';
 
