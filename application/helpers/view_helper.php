5402849a525e46283e6866fcd4adffab4b2c6e4d
Shervin Enayati <shervin@mench.co>
2021-03-02 16:55:41 -0800

    Iterations Towards v179

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1757,7 +1757,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
 
             //UNLINK
             if($control_enabled && isset($i['x__id']) && in_array($x__type, $CI->config->item('n___6155'))){
-                $action_buttons .= '<div class="dropdown-item"><div class="x_remove css__title" i__id="'.$i['i__id'].'" x__id="'.$i['x__id'].'" title="'.$e___11035[6155]['m__title'].'"><span class="icon-block">'.$e___11035[6155]['m__cover'].'</span>'.$e___11035[6155]['m__title'].'</div></div>';
+                $action_buttons .= '<div class="dropdown-item"><div class="x_remove css__title" i__id="'.$i['i__id'].'" x__id="'.$i['x__id'].'"><span class="icon-block">'.$e___11035[6155]['m__cover'].'</span>'.$e___11035[6155]['m__title'].'</div></div>';
             }
 
             //UNLINK
