a327b91f61cab96b00f27be9a4deb6a042b8e189
Shervin Enayati <shervin@mench.co>
2021-03-07 17:05:40 -0800

    Iterations Towards v179

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -888,8 +888,8 @@ class X_model extends CI_Model
             $edit_btn = null;
             if(!$is_discovery_mode && source_of_e($es[0]['e__id'])){
                 $e___11035 = $this->config->item('e___11035');
-                $tooltip_class .= ' class="ignore-click grey trigger_coincover_edit" coin__type="12274" coin__id="' . $es[0]['e__id'] . '" ';
-                $edit_btn = '<span class="ignore-click icon-block-img" title="'.$e___11035[13571]['m__title'].'">'.view_cover($es[0]['e__cover']).'</span> ';
+                $tooltip_class .= ' class="ignore-click trigger_coincover_edit" coin__type="12274" coin__id="' . $es[0]['e__id'] . '" ';
+                $edit_btn = '<span class="ignore-click icon-block-img mini_6197_'.$es[0]['e__id'].'" title="'.$e___11035[13571]['m__title'].'">'.view_cover($es[0]['e__cover']).'</span> ';
             }
 
             $on_its_own_line = false;
