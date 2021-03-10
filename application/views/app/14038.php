9eaafca6697295898d0a10f7d6ce7e7861f2e58f
Shervin Enayati <shervin@mench.co>
2021-03-09 19:37:04 -0800

    Iterations Towards v179

--- application/views/app/14038.php
+++ application/views/app/14038.php
@@ -10,10 +10,21 @@ foreach($emojis as $count => $emoji_html){
     }
     $emoji_icon = one_two_explode('','</td>',$emoji_html);
     $emoji_name = ucwords(one_two_explode('<td class=\'name\'>','</td>',$emoji_html));
-    foreach(array('Flag: ','"',':') as $remove){
+    foreach(array('Flag: ','"',':',',') as $remove){
         $emoji_name = str_replace($remove, '', $emoji_name);
     }
-    echo $count.') ['.$emoji_icon.'] '.$emoji_name.'<br />';
+
+    //See if we have it:
+    $es = $this->E_model->fetch(array(
+        'LOWER(e__title)' => strtolower($emoji_name),
+        'e__cover' => $emoji_icon,
+        'e__type IN (' . join(',', $this->config->item('n___7358')) . ')' => null, //ACTIVE
+    ));
+
+
+    echo $count.') ['.$emoji_icon.'] '.$emoji_name.( count($es) ? '[FOUND '.count($es).': @'.$es[0]['e__id'].']' : '' ).'<br />';
+
+
 }
 
 
