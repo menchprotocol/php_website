1d47cef66dfe3b2a8dd191b862f120e2c7f97a25
Shervin Enayati <shervin@mench.co>
2021-03-09 19:25:40 -0800

    Iterations Towards v179

--- application/views/app/14038.php
+++ application/views/app/14038.php
@@ -2,7 +2,14 @@
 
 //Update Emojis
 $emoji_list = file_get_contents('https://unicode.org/emoji/charts/full-emoji-list.html');
+$emojis = explode('<td class=\'chars\'>',$emoji_list);
 
-echo substr_count($emoji_list, '<td class=\'chars\'>');
+foreach($emojis as $count => $emoji_html){
+    $emoji_icon = one_two_explode('','</td>',$emoji_html);
+    $emoji_name = ucwords(one_two_explode('<td class=\'name\'>','</td>',$emoji_html));
+    echo $count.') '.$emoji_icon.' '.$emoji_name.'<br />';
+}
+
+
 
 
