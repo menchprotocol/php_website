3b1f0317bb3c9d75328e433da19b1020e7278789
Shervin Enayati <shervin@mench.co>
2021-03-15 20:36:24 -0700

    Iterations Towards v179

--- application/views/app/13577.php
+++ application/views/app/13577.php
@@ -1,6 +1,10 @@
 <?php
 
+$processed = false;
+
 if(isset($_GET['fa_regular']) && strlen($_GET['fa_regular'])){
+
+    $processed = true;
     $detected = 0;
     $added = 0;
 
@@ -12,26 +16,37 @@ if(isset($_GET['fa_regular']) && strlen($_GET['fa_regular'])){
     echo 'REGULAR: Detected '.$detected.' icons and added '.$added.' of them.<br />';
 }
 
+if(isset($_GET['fa_brand']) && strlen($_GET['fa_brand'])) {
 
-if(isset($_GET['fa_brand']) && strlen($_GET['fa_brand'])){
+    $processed = true;
+    $detected = 0;
+    $added = 0;
+}
+
+
+
+if(!$processed){
+
+    //SHow Form:
+    echo '<form method="post" action="">';
+
+    //Regular
+    echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/regular" target="_blank">Regular Font Awesome Cheatsheet</a> here:</p>';
+    echo '<textarea class="form-control text-edit border no-padding" name="fa_regular" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
+
+
+    //Brands
+    echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/brands" target="_blank">Brand Font Awesome Cheatsheet</a> here:</p>';
+    echo '<textarea class="form-control text-edit border no-padding" name="fa_brand" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
+
+    //Apply
+    echo '<button type="submit" class="controller-nav btn btn-lrg btn-6255 go-next top-margin" value="GO">UPDATE</button>';
+
+
+    echo '</form>';
 
 }
 
 
 
-echo '<form method="get" action="">';
 
-//Regular
-echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/regular" target="_blank">Regular Font Awesome Cheatsheet</a> here:</p>';
-echo '<textarea class="form-control text-edit border no-padding" name="fa_regular" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
-
-
-//Brands
-echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/brands" target="_blank">Brand Font Awesome Cheatsheet</a> here:</p>';
-echo '<textarea class="form-control text-edit border no-padding" name="fa_brand" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
-
-//Apply
-echo '<button type="submit" class="controller-nav btn btn-lrg btn-6255 go-next top-margin" value="GO">UPDATE</button>';
-
-
-echo '</form>';
