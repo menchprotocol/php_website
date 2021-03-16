4417b56792e256dc826803acb2c52867b9b867bf
Shervin Enayati <shervin@mench.co>
2021-03-15 20:31:30 -0700

    Iterations Towards v179

--- application/views/app/13577.php
+++ application/views/app/13577.php
@@ -1,53 +1,34 @@
 <?php
 
+if(strlen($_GET['fa_regular'])){
+    $detected = 0;
+    $added = 0;
 
-echo '<div class="submit_feedback">';
+    $detected = substr_count($_GET['fa_regular'], "\n");
 
-if(count($was_sibmitted)){
-    //Editing, let them know:
-    echo '<div class="padded">You first submitted your feedback for this discover ' . view_time_difference(strtotime($was_sibmitted[0]['x__time'])) . ' Ago</div>';
+    echo 'REGULAR: Detected '.$detected.' icons and added '.$added.' of them.<br />';
 }
 
 
-//100% COMPLETE
-echo '<div class="headline top-margin"><span class="icon-block">&nbsp;</span>'.$e___14709[14730]['m__title'].'</div>';
-echo '<div class="padded">'.str_replace('%s', $is[0]['i__title'], $e___14709[14730]['m__message']).'</div>';
-echo '<div class="padded">'.view_i(14730, $is[0]['i__id'], null, $is[0]).'</div>';
+if(strlen($_GET['fa_brand'])){
 
-
-
-//Continious Updates
-echo '<div class="headline top-margin"><span class="icon-block">&nbsp;</span>'.$e___14709[14343]['m__title'].'</div>';
-echo '<div class="padded">'.str_replace('%s', $is[0]['i__title'], $e___14709[14343]['m__message']).'</div>';
-
-
-
-//Rate
-echo '<div class="headline top-margin"><span class="icon-block">&nbsp;</span>'.$e___14709[14712]['m__title'].'</div>';
-echo '<div class="padded hideIfEmpty">'.$e___14709[14712]['m__message'].'</div>';
-foreach($this->config->item('e___14712') as $x__type => $m){
-    echo '<div class="form-check">
-            <input class="form-check-input" type="radio" '.( count($was_sibmitted) && $was_sibmitted[0]['x__up']==$x__type ? ' checked="checked" ' : '' ) .' name="feedback_rating_14712" id="formRadio'.$x__type.'" value="'.$x__type.'">
-            <label class="form-check-label" for="formRadio'.$x__type.'"><span class="icon-block">' . $m['m__cover'] . '</span>' . $m['m__title'] . '</label>
-        </div>';
 }
 
 
-//Paste Code
-echo '<div class="padded"><textarea class="form-control text-edit border no-padding" data-lpignore="true" placeholder="Paste FontAwesome Cheatsheet here"></textarea></div>';
+
+echo '<form method="get" action="">';
+
+//Regular
+echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/regular" target="_blank">Regular Font Awesome Cheatsheet</a> here:</p>';
+echo '<textarea class="form-control text-edit border no-padding" name="fa_regular" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
 
 
+//Brands
+echo '<p>Copy <a href="https://fontawesome.com/cheatsheet/pro/brands" target="_blank">Brand Font Awesome Cheatsheet</a> here:</p>';
+echo '<textarea class="form-control text-edit border no-padding" name="fa_brand" data-lpignore="true" placeholder="Paste Cheatsheet..."></textarea><br />';
 
 //Apply
 echo '<div class="nav-controller"><div><button type="submit" class="controller-nav btn btn-lrg btn-6255 go-next top-margin" value="GO">UPDATE</button></div></div>';
 
 
-echo '</div>';
-
-
-
-echo '<div class="saving_feedback hidden top-margin">';
-echo '<div class="text-center platform-large">'.get_domain('m__cover').'</div>';
-echo '<p style="margin-top:13px; text-align: center;">'.view_shuffle_message(12694).'</p>';
-echo '</div>';
-
+echo '</form>';
