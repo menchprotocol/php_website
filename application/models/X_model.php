2a729199a818f6599e8dad47f8858cd82a689650
Shervin Enayati <shervin@mench.co>
2021-03-18 16:56:04 -0700

    Iterations Towards v179

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -875,14 +875,14 @@ class X_model extends CI_Model
 
             //Append any appendix generated:
             $identifier_string = '@' . $referenced_e.($string_references['ref_time_found'] ? one_two_explode('@' . $referenced_e,' ',$message_input) : '' );
-            $can_trigger_modal = ( !$is_discovery_mode && source_of_e($es[0]['e__id']) ? ' trigger_coincover_edit ' : false );
-            $tooltip_class = ( $tooltip_info ? ' title="'.$tooltip_info.'" data-toggle="tooltip" data-placement="bottom" class="ignore-click underdot '.$can_trigger_modal.'" ' : ' class="ignore-click '.$can_trigger_modal.'" ' );
+            $tooltip_class = ( $tooltip_info ? ' title="'.$tooltip_info.'" data-toggle="tooltip" data-placement="bottom" ' : null );
+            $tooltip_underdot = ( $tooltip_info ? ' underdot ' : null );
 
 
             $edit_btn = null;
-            if($can_trigger_modal){
+            if(!$is_discovery_mode && source_of_e($es[0]['e__id'])){
                 $e___11035 = $this->config->item('e___11035');
-                $tooltip_class .= ' coin__type="12274" coin__id="' . $es[0]['e__id'] . '" ';
+                $tooltip_class .= ' class="ignore-click trigger_coincover_edit" coin__type="12274" coin__id="' . $es[0]['e__id'] . '" ';
                 $edit_btn = '<span class="ignore-click icon-block-img mini_6197_'.$es[0]['e__id'].'" title="'.$e___11035[13571]['m__title'].'">'.view_cover(12274,$es[0]['e__cover']).'</span> ';
             }
 
@@ -909,12 +909,12 @@ class X_model extends CI_Model
             //Display:
             if($on_its_own_line){
                 if($new_lines <= 1){
-                    $output_body_message = $e_appendix.str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="subtle-line mini-grey text__6197_'.$es[0]['e__id'].'">' . $es[0]['e__title'] . '</span></span> ', $output_body_message).$e_dropdown;
+                    $output_body_message = $e_appendix.str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="subtle-line mini-grey text__6197_'.$es[0]['e__id'].$tooltip_underdot.'">' . $es[0]['e__title'] . '</span>'.$e_dropdown.'</span> ', $output_body_message);
                 } else {
-                    $output_body_message = str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="subtle-line mini-grey text__6197_'.$es[0]['e__id'].'">' . $es[0]['e__title'] . '</span></span> ', $output_body_message).$e_dropdown.$e_appendix;
+                    $output_body_message = str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="subtle-line mini-grey text__6197_'.$es[0]['e__id'].$tooltip_underdot.'">' . $es[0]['e__title'] . '</span>'.$e_dropdown.'</span> ', $output_body_message).$e_appendix;
                 }
             } else {
-                $output_body_message = str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="text__6197_'.$es[0]['e__id'].'">' . $es[0]['e__title'] . '</span></span>', $output_body_message).$e_dropdown.$e_appendix;
+                $output_body_message = str_replace($identifier_string, '<span '.$tooltip_class.'>'.$edit_btn.'<span class="text__6197_'.$es[0]['e__id'].$tooltip_underdot.'">' . $es[0]['e__title'] . '</span>'.$e_dropdown.'</span>', $output_body_message).$e_appendix;
             }
 
         }
