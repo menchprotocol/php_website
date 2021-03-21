bb7dd273d0e18a148bca9bb1649970255eee5c56
Shervin Enayati <shervin@mench.co>
2021-03-21 13:39:33 -0700

    Iterations Towards v179

--- application/views/app/13207.php
+++ application/views/app/13207.php
@@ -5,9 +5,9 @@ $show_max_14435 = view_memory(6404,14435);
 $group_by = 'e__id, e__title, e__cover, e__metadata, e__type, e__spectrum';
 
 //SOURCE LEADERBOARD
-foreach($this->config->item('e___13207') as $e__id => $m) {
+foreach($this->config->item('e___13207') as $x__type => $m) {
 
-    if($e__id==14874){
+    if($x__type==14874){
         echo view_coins();
         continue;
     }
@@ -17,25 +17,26 @@ foreach($this->config->item('e___13207') as $e__id => $m) {
         'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
         'x__type IN (' . join(',', $this->config->item('n___13550')) . ')' => null, //SOURCE IDEAS
         'x__up > 0' => null,
-        ' EXISTS (SELECT 1 FROM table__x WHERE e__id=x__down AND x__up='.$e__id.' AND x__type IN (' . join(',', $this->config->item('n___4592')) . ') AND x__status IN ('.join(',', $this->config->item('n___7359')) /* PUBLIC */.')) ' => null,
+        ' EXISTS (SELECT 1 FROM table__x WHERE e__id=x__down AND x__up='.$x__type.' AND x__type IN (' . join(',', $this->config->item('n___4592')) . ') AND x__status IN ('.join(',', $this->config->item('n___7359')) /* PUBLIC */.')) ' => null,
     ), array('x__up'), 0, 0, array('totals' => 'DESC'), 'COUNT(x__id) as totals, '.$group_by, $group_by);
 
     if(!count($e_list)){
         continue;
     }
 
-    echo '<div class="headline top-margin"><span class="icon-block">'.$m['m__cover'].'</span>'.$m['m__title'].'</div>';
-    echo '<div class="row justify-content-center" style="padding-bottom:41px;">';
-
+    $ui = '<div class="row justify-content-center">';
     $has_more = count($e_list)>($show_max_14435+1);
     foreach($e_list as $count=>$e) {
         if($count==$show_max_14435 && $has_more){
-            echo view_show_more(14435, 'see_more_who'.$e__id);
+            $ui .= view_show_more(14435, 'see_more_who'.$x__type);
         }
-        echo view_e(13207, $e, ( $count>=$show_max_14435 && $has_more ? ' see_more_who'.$e__id.' hidden ' : ''), true);
+        $ui .= view_e(13207, $e, ( $count>=$show_max_14435 && $has_more ? ' see_more_who'.$x__type.' hidden ' : ''), true);
     }
 
-    echo '</div>';
+    $ui .= '</div>';
+
+    echo view_headline($x__type, null, $m, $ui);
+
 }
 
 ?>
