ae6878743be7440eaaad437d4b347f6b024a4780
Shervin Enayati <shervin@mench.co>
2021-03-21 19:45:11 -0700

    Iterations Towards v179

--- application/views/footer.php
+++ application/views/footer.php
@@ -204,7 +204,8 @@ if($member_e && ( !isset($basic_header_footer) || !$basic_header_footer )){
                 <div class="modal-footer ">
 
                     <div class="<?= superpower_active(14003) ?>"><input type="text" id="coin__cover" value="" data-lpignore="true" placeholder="Emoji, Image URL or Icon Code" class="form-control border-dotted" style="margin-top: 5px;"></div>
-
+                    <input type="hidden" id="coin__type" value="0" />
+                    <input type="hidden" id="coin__id" value="0" />
 
                     <table style="width: 100%;">
                         <tr>
@@ -213,9 +214,7 @@ if($member_e && ( !isset($basic_header_footer) || !$basic_header_footer )){
 
                             </td>
                             <td>
-                                <div style="padding: 10px; text-align: center;"><button type="button" onclick="coin__save()" class="btn btn-default">SAVE</button></div>
-                                <input type="hidden" id="coin__type" value="0" />
-                                <input type="hidden" id="coin__id" value="0" />
+                                <button type="button" onclick="coin__save()" class="btn btn-default">SAVE</button>
                             </td>
                         </tr>
                     </table>
