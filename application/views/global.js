cc532b709015521d9694eefb4082ad440d7f7ba9
Shervin Enayati <shervin@mench.co>
2021-03-26 13:19:04 -0700

    Iterations Towards v179

--- application/views/global.js
+++ application/views/global.js
@@ -782,6 +782,7 @@ $(document).ready(function () {
                     //Members can filter search with first word:
                     var search_only_e = $("#top_search").val().charAt(0) == '@';
                     var search_only_in = $("#top_search").val().charAt(0) == '#';
+                    $("#container_search .row").html(''); //Reset results view
 
                     //Do not search if specific command ONLY:
                     if (( search_only_in || search_only_e ) && !isNaN($("#top_search").val().substr(1)) ) {
@@ -1458,7 +1459,7 @@ function toggle_search(){
         search_on = true; //Reverse
         $('.top_nav, #container_content').addClass('hidden');
         $('.search_nav, #container_search').removeClass('hidden');
-        $("#container_search .row").html('') //Reset results view
+        $("#container_search .row").html(''); //Reset results view
         $('#top_search').focus();
 
         setTimeout(function () {
