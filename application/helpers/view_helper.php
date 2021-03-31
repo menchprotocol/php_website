472c4c6bd94a090e8fcfc30ad9d30274f32062f8
Shervin Enayati <shervin@mench.co>
2021-03-31 07:47:03 -0700

    Iterations Towards v179

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1840,7 +1840,7 @@ function view_e_line($e)
     $href = '/@'.$e['e__id'];
 
     $ui = '<div class="row">';
-    $ui .= '<div class="coinface-12274 edge-coin col-md-2 col-3 coin_cover no-padding coin-12274 coin___12274_'.$e['e__id'].'">';
+    $ui .= '<div class="coinface-12274 mini-coin edge-coin col-md-1 col-2 no-padding coin-12274 coin___12274_'.$e['e__id'].'">';
     $ui .= '<div class="cover-wrapper">';
     $ui .= '<a href="'.$href.'" class="coinType12274 black-background cover-link" '.( $has_valid_url ? 'style="background-image:url(\''.$e['e__cover'].'\');"' : '' ).'>';
     $ui .= '<div class="cover-btn">'.( !$has_valid_url && $e['e__cover'] ? view_cover(12274,$e['e__cover']) : '' ).'</div>';
@@ -1848,7 +1848,7 @@ function view_e_line($e)
     $ui .= '</div>';
     $ui .= '</div>';
 
-    $ui .= '<div class="col-md-10 col-9">';
+    $ui .= '<div class="col-md-11 col-10">';
     $ui .= '<div><a href="'.$href.'" class="css__title">'.$e['e__title'].'</a><span class="grey" style="padding-left: 5px;">' . view_time_difference(strtotime($e['x__time'])) . '</span></div>';
     $ui .= '</div>';
     $ui .= '</div>';
