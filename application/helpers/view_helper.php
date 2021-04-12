8246bbce68369bdc65a1d76e2fd0eabeac6c861a
Shervin Enayati <shervin@mench.co>
2021-04-11 19:25:56 -0700

    Iterations Towards v179

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1790,7 +1790,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
 
         $ui .= '<div class="'.( !$linkbar_visible ? ' coin-hover ' : '' ).'">';
         $ui .= '<table class="coin_coins"><tr>';
-        $ui .= '<td width="33%" class="push_down '.superpower_active(10939).'" style="text-align: right;"><div>'.( $discovery_mode ? '' : ( i_is_startable($i) ? '<span data-toggle="tooltip" data-placement="top" title="'.$e___11035[26124]['m__title'].'">'.$e___11035[26124]['m__cover'].'</span>' : '<span data-toggle="tooltip" data-placement="top" title="'.$e___11035[26130]['m__title'].'">'.$e___11035[26130]['m__cover'].'</span>' ) ).'</div></td>';
+        $ui .= '<td width="33%" class="push_down '.superpower_active(12700).'" style="text-align: right;"><div>'.( $discovery_mode ? '' : ( i_is_startable($i) ? '<span data-toggle="tooltip" data-placement="top" title="'.$e___11035[26124]['m__title'].'">'.$e___11035[26124]['m__cover'].'</span>' : '<span data-toggle="tooltip" data-placement="top" title="'.$e___11035[26130]['m__title'].'">'.$e___11035[26130]['m__cover'].'</span>' ) ).'</div></td>';
         $ui .= '<td width="34%" class="center"><div>'.( $discovery_mode || !superpower_active(12700, true) ? '&nbsp;' : view_input_text(4356, $i['i__duration'], $i['i__id'], $e_of_i, 0).' '.$e___11035[4356]['m__cover'] ).'</div></td>';
         $ui .= '<td width="33%" class="push_down '.superpower_active(10939).'" style="text-align: left;"><div>';
         if($discovery_mode){
