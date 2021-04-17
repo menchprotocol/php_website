34b1e73fc1bf5b9c4f31e8a75825c79f175e98ae
Shervin Enayati <shervin@mench.co>
2021-04-17 13:06:12 -0700

    Bug Fixes

--- application/views/app/14436.php
+++ application/views/app/14436.php
@@ -1,18 +1,35 @@
 <?php
 
-//This page is loaded after member successfully authenticates via Auth0
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+$client_id = get_domain_setting(14881);
+$client_secret = get_domain_setting(14882);
+$server_name = get_server('SERVER_NAME');
 
-require 'vendor/autoload.php';
-use Auth0\SDK\Auth0;
-$auth0 = new Auth0([
-    'domain' => 'mench.auth0.com',
-    'client_id' => get_domain_setting(14881),
-    'client_secret' => get_domain_setting(14882),
-    'redirect_uri' => 'https://'.get_server('SERVER_NAME').'/-14564',
-    'scope' => 'openid profile email',
-]);
+if($client_id && $client_secret && $server_name){
 
-$auth0->login();
+    //This page is loaded after member successfully authenticates via Auth0
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
+    require 'vendor/autoload.php';
+
+    use Auth0\SDK\Auth0;
+
+    $auth0 = new Auth0([
+        'domain' => 'mench.auth0.com',
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'redirect_uri' => 'https://'.$server_name.'/-14564',
+        'scope' => 'openid profile email',
+    ]);
+
+    $auth0->login();
+
+} else {
+
+    js_redirect('/', 13);
+
+}
+
+
+
