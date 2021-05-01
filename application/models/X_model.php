6c86f42e75bff295711dda802af15f14265e607a
Shervin Enayati <shervin@mench.co>
2021-05-01 14:51:21 -0700

    Idea/Source Owner Displayed under Sources

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -912,7 +912,7 @@ class X_model extends CI_Model
                 } else {
 
                     //List all links:
-                    $e_dropdown .= '<div class="dropdown inline-block inline-dropdown"><button type="button" class="btn no-left-padding no-right-padding css__title ignore-click" id="externalRef'.$es[0]['e__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-external-link"></i></button><div class="dropdown-menu" aria-labelledby="externalRef'.$es[0]['e__id'].'">';
+                    $e_dropdown .= '<div class="dropdown inline-block inline-dropdown"><button type="button" class="btn no-left-padding no-right-padding css__title ignore-click" id="externalRef'.$es[0]['e__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-angle-down"></i></button><div class="dropdown-menu" aria-labelledby="externalRef'.$es[0]['e__id'].'">';
                     foreach($e_links as $e_link){
                         $e_dropdown .= '<a href="'.$e_link['x__message'].'" target="_blank" class="dropdown-item move_away css__title ignore-click"><span class="icon-block">'.view_cover(12274,$e_link['e__cover']).'</span>'.$e_link['e__title'].'</a>';
                     }
