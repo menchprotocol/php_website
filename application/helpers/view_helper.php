7fb321489ab04bcd5ced584336f156d1b391e350
Shervin Enayati <shervin@mench.co>
2021-05-04 08:23:33 -0700

    Improve message preview position

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1789,14 +1789,14 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $control_enabl
             $messages .= $mes['x__message'].' ';
         }
         if($messages){
-            $message_tooltip = '<span class="icon-block" title="'.$messages.'">'.$e___11035[4231]['m__cover'].'</span>';
+            $message_tooltip = '<span class="icon-block-xs" title="'.$messages.'">'.$e___11035[4231]['m__cover'].'</span>';
         }
     }
     $view_i_time = view_i_time($i_stats);
     $ui .= '<div class="cover-text"><div class="'.( !$linkbar_visible ? ' coin-hover ' : '' ).' grey">' . ( $view_i_time ? $view_i_time : '&nbsp;' ).$message_tooltip . '</div>';
 
     if(count($minter)){
-        $ui .= '<div class="coin-hover grey mini-font">Minted <span title="'.$minter[0]['x__time'].' PST">'.view_time_difference(strtotime($minter[0]['x__time'])).' ago</span> & owned by <a href="/@'.$minter[0]['e__id'].'"><u>'.$minter[0]['e__title'].'</u></a></div>';
+        $ui .= '<div class="coin-hover grey mini-font">Minted <span title="'.$minter[0]['x__time'].' PST">'.view_time_difference(strtotime($minter[0]['x__time'])).' ago</span> by <a href="/@'.$minter[0]['e__id'].'"><u>'.$minter[0]['e__title'].'</u></a></div>';
 
     }
     $ui .= '</div>';
@@ -2157,7 +2157,7 @@ function view_e($x__type, $e, $extra_class = null, $source_of_e = false)
         'x__down' => $e['e__id'],
     ), array('x__source')) : array());
 
-    $ui .= ( count($minter) ? '<div class="cover-text"><div class="coin-hover grey mini-font" style="padding-top:5px;">Minted <span title="'.$minter[0]['x__time'].' PST">'.view_time_difference(strtotime($minter[0]['x__time'])).' ago</span> & owned by <a href="/@'.$minter[0]['e__id'].'"><u>'.$minter[0]['e__title'].'</u></a></div></div>' : '' );
+    $ui .= ( count($minter) ? '<div class="cover-text"><div class="coin-hover grey mini-font" style="padding-top:5px;">Minted <span title="'.$minter[0]['x__time'].' PST">'.view_time_difference(strtotime($minter[0]['x__time'])).' ago</span> by <a href="/@'.$minter[0]['e__id'].'"><u>'.$minter[0]['e__title'].'</u></a></div></div>' : '' );
 
 
     //Message
