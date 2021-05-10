cf2b7e39d1bb2c71d6c295517c834d6e311aa827
Shervin Enayati <shervin@mench.co>
2021-05-10 13:18:36 -0700

    Fixing inline linking for sources in messages

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -54,7 +54,7 @@ function view_i_time($i_stats, $give_right_space = false, $micro_sign = false){
     $ui = '<div class="inline-block '.( $give_right_space ? ' css__title grey ' : ' mini-font ' ).'">';
 
     if(!$micro_sign && $i_stats['i___6170']>0){
-        $ui .= ( $has_any_diff && !$micro_sign ? number_format(intval($i_stats['i___6169']), 0).'<span class="mid-range">-</span>' : '' ).number_format(intval($i_stats['i___6170']), 0).' '.$e___13544[26155]['m__title'].view__s(intval($i_stats['i___6170'])).'<span class="mid-range">&middot;</span>';
+        $ui .= ( $has_any_diff && !$micro_sign ? number_format(intval($i_stats['i___6169']), 0).'<span class="mid-range">-</span>' : '' ).number_format(intval($i_stats['i___6170']), 0).' '.$e___13544[26155]['m__title'].view__s(intval($i_stats['i___6170'])).' in '; //<span class="mid-range">&middot;</span>
     }
 
     if($has_micro){
