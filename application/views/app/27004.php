790d48afd880d2dac3b020cd4ecf4419b922655b
Shervin Enayati <shervin@mench.co>
2021-07-15 17:51:02 -0700

    Paypal Payment Dashboard

--- application/views/app/27004.php
+++ application/views/app/27004.php
@@ -2,125 +2,133 @@
 
 $commission_rate = doubleval(view_memory(6404,27017))/100;
 
-if(!isset($_GET['i__id']) || !intval($_GET['i__id'])){
+$gross_units = 0;
+$gross_revenue = 0;
+$gross_paypal_fee = 0;
+$gross_commission = 0;
+$gross_payout = 0;
+$gross_currencies = array();
 
+$query_filters = array(
+    'i__type IN (' . join(',', $this->config->item('n___27005')) . ')' => null, //Payment Idea
+);
 
-    $gross_units = 0;
-    $gross_revenue = 0;
-    $gross_paypal_fee = 0;
-    $gross_commission = 0;
-    $gross_payout = 0;
-    $gross_currencies = array();
+if (isset($_GET['i__id']) && substr_count($_GET['i__id'], ',') > 0) {
 
-    //List all payment Ideas and their total earnings
-    $body_content = '';
-    foreach($this->I_model->fetch(array(
-        'i__type IN (' . join(',', $this->config->item('n___27005')) . ')' => null, //Payment Idea
-    )) as $i){
+    //This is multiple:
+    $query_filters['( i__id IN (' . $_GET['i__id'] . '))'] = null;
 
-        //Total earnings:
-        $total_units = 0;
-        $total_revenue = 0;
-        $total_paypal_fee = 0;
-        $currencies = array();
+} elseif(isset($_GET['i__id']) && intval($_GET['i__id']) > 0) {
 
-        foreach($this->X_model->fetch(array(
-            'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //DISCOVERY COIN
-            'x__left' => $i['i__id'],
-        ), array(), 0) as $x){
-            $x__metadata = unserialize($x['x__metadata']);
-            if(doubleval($x__metadata['mc_gross']) <= 0){
-                continue;
-            }
-            $total_units++;
-            $total_paypal_fee += doubleval($x__metadata['mc_fee']);
-            $total_revenue += doubleval($x__metadata['mc_gross']);
-            if(!in_array($x__metadata['mc_currency'], $currencies) && strlen($x__metadata['mc_currency'])>0){
-                array_push($currencies, $x__metadata['mc_currency']);
-            }
-            if(!in_array($x__metadata['mc_currency'], $gross_currencies) && strlen($x__metadata['mc_currency'])>0){
-                array_push($gross_currencies, $x__metadata['mc_currency']);
-            }
-
-        }
-        $total_commission = ( $commission_rate * $total_revenue );
-        $payout = $total_revenue-$total_commission-$total_paypal_fee;
-
-
-        $gross_units += $total_units;
-        $gross_revenue += $total_revenue;
-        $gross_paypal_fee += $total_paypal_fee;
-        $gross_commission += $total_commission;
-        $gross_payout += $payout;
-
-
-        $body_content .= '<tr>';
-
-        $body_content .= '<td><a href="/~'.$i['i__id'].'" style="font-weight:bold;"><u>'.$i['i__title'].'</u></a></td>';
-        $body_content .= '<td style="text-align: right;">'.$total_units.'x</td>';
-        $body_content .= '<td style="text-align: right;">$'.number_format(( $total_units > 0 ? $total_revenue / $total_units : 0 ), 2).'</td>';
-        $body_content .= '<td style="text-align: right;">+$'.number_format($total_revenue, 2).'</td>';
-        $body_content .= '<td style="text-align: right;" title="'.($commission_rate*100).'%">-$'.number_format($total_commission, 2).'</td>';
-        $body_content .= '<td style="text-align: right;" title="'.(( $total_revenue>0 ? $total_paypal_fee/$total_revenue : 0 )*100).'%">-$'.number_format($total_paypal_fee, 2).'</td>';
-        $body_content .= '<td style="text-align: right;" title="'.(( $total_revenue>0 ? $payout/$total_revenue : 0 )*100).'%"><b>$'.number_format($payout, 2).'</b></td>';
-        $body_content .= '<td style="text-align: right;">'.join(', ',$currencies).'</td>';
-        $body_content .= '</tr>';
-
-    }
-
-
-    $table_sortable = array('#th_primary','#th_average','#th_rev');
-
-    echo '<table id="sortable_table" class="table table-sm table-striped image-mini">';
-    echo '<tr style="vertical-align: baseline;">';
-    echo '<th id="th_primary">Paid Ideas</th>';
-    echo '<th style="text-align: right;" id="th_paid">Unit</th>';
-    echo '<th style="text-align: right;" id="th_average">Average</th>';
-    echo '<th style="text-align: right;" id="th_rev">Revenue</th>';
-    echo '<th style="text-align: right;" id="th_payout">Commission</th>';
-    echo '<th style="text-align: right;" id="th_payout">Paypal Fee</th>';
-    echo '<th style="text-align: right;" id="th_payout">Payout</th>';
-    echo '<th style="text-align: right;" id="th_currency">Currency</th>';
-    echo '</tr>';
-    echo $body_content;
-
-    echo '<tr>';
-    echo '<th style="text-align: right;" id="th_primary">Totals</th>';
-    echo '<th style="text-align: right;">'.$gross_units.'x</th>';
-    echo '<th style="text-align: right;">$'.number_format(( $gross_units > 0 ? $gross_revenue / $gross_units : 0 ), 2).'</th>';
-    echo '<th style="text-align: right;">+$'.number_format($gross_revenue, 2).'</th>';
-    echo '<th style="text-align: right;" title="'.($commission_rate*100).'%">-$'.number_format($gross_commission, 2).'</th>';
-    echo '<th style="text-align: right;" title="'.(( $gross_revenue>0 ? $gross_paypal_fee/$gross_revenue : 0 )*100).'%">-$'.number_format($gross_paypal_fee, 2).'</th>';
-    echo '<th style="text-align: right;" title="'.(( $gross_revenue>0 ? $gross_payout/$gross_revenue : 0 )*100).'%"><b>$'.number_format($gross_payout, 2).'</b></th>';
-    echo '<th style="text-align: right;">'.join(', ',$gross_currencies).'</th>';
-    echo '</tr>';
-    echo '</table>';
-
-} else {
-
-    //Make sure right idea type:
-    if(!count($this->I_model->fetch(array(
-        'i__id' => $_GET['i__id'],
-        'i__type IN (' . join(',', $this->config->item('n___27005')) . ')' => null, //Payment Idea
-    )))){
-        echo 'Error: Idea must be a payment type';
-    } else {
-
-        //All good, load transactions:
-        foreach($this->X_model->fetch(array(
-            'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //DISCOVERY COIN
-            'x__left' => $_GET['i__id'],
-        ), array('x__source'), 0) as $x_progress){
-
-        }
-
-    }
-
-    //List a specific idea with its discovery transactions
+    $query_filters['i__id'] = intval($_GET['i__id']);
 
 }
+
+//List all payment Ideas and their total earnings
+$body_content = '';
+foreach($this->I_model->fetch($query_filters) as $i){
+
+    //Total earnings:
+    $transaction_content = '';
+    $total_units = 0;
+    $total_revenue = 0;
+    $total_paypal_fee = 0;
+    $currencies = array();
+
+    foreach($this->X_model->fetch(array(
+        'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+        'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //DISCOVERY COIN
+        'x__left' => $i['i__id'],
+    ), array(), 0) as $x){
+
+        $x__metadata = unserialize($x['x__metadata']);
+        if(doubleval($x__metadata['mc_gross']) <= 0){
+            continue;
+        }
+        $total_units++;
+        $total_paypal_fee += doubleval($x__metadata['mc_fee']);
+        $total_revenue += doubleval($x__metadata['mc_gross']);
+        if(!in_array($x__metadata['mc_currency'], $currencies) && strlen($x__metadata['mc_currency'])>0){
+            array_push($currencies, $x__metadata['mc_currency']);
+        }
+        if(!in_array($x__metadata['mc_currency'], $gross_currencies) && strlen($x__metadata['mc_currency'])>0){
+            array_push($gross_currencies, $x__metadata['mc_currency']);
+        }
+
+
+        $item_parts = explode('-',$x__metadata['item_number']);
+        $es = $this->E_model->fetch(array(
+            'e__id' => $item_parts[3],
+        ));
+        $this_commission = $x__metadata['mc_gross']*$commission_rate;
+        $this_payout = $x__metadata['mc_gross']-$x__metadata['mc_fee']-$this_commission;
+
+
+        $transaction_content .= '<tr class="tr_row transactions_'.$i['i__id'].' hidden">';
+        $transaction_content .= '<td>'.$x__metadata['first_name'].' '.$x__metadata['last_name'].'<br />'.( count($es) ? '<a href="/@'.$es[0]['e__id'].'" style="font-weight:bold;"><u>'.$es[0]['e__title'].'</u></a>' : 'Unknown user @'.$item_parts[3] ).'</td>';
+        $transaction_content .= '<td style="text-align: right;">1x</td>';
+        $transaction_content .= '<td style="text-align: right;"></td>';
+        $transaction_content .= '<td style="text-align: right;">+$'.number_format($x__metadata['mc_gross'], 2).'</td>';
+        $transaction_content .= '<td style="text-align: right;" title="'.($commission_rate*100).'%">-$'.number_format($this_commission, 2).'</td>';
+        $transaction_content .= '<td style="text-align: right;" title="'.($x__metadata['mc_fee']/$x__metadata['mc_gross']*100).'%">-$'.number_format($x__metadata['mc_fee'], 2).'</td>';
+        $transaction_content .= '<td style="text-align: right;" title="'.(( $x__metadata['mc_gross']>0 ? $this_payout/$x__metadata['mc_gross'] : 0 )*100).'%"><b>$'.number_format($this_payout, 2).'</b></td>';
+        $transaction_content .= '<td style="text-align: right;">'.$x__metadata['mc_currency'].'</td>';
+        $transaction_content .= '</tr>';
+
+    }
+    $total_commission = ( $commission_rate * $total_revenue );
+    $payout = $total_revenue-$total_commission-$total_paypal_fee;
+
+
+    $gross_units += $total_units;
+    $gross_revenue += $total_revenue;
+    $gross_paypal_fee += $total_paypal_fee;
+    $gross_commission += $total_commission;
+    $gross_payout += $payout;
+
+
+
+    $body_content .= '<tr>';
+    $body_content .= '<td><a href="javascript:void(0)" onclick="$(\'.transactions_'.$i['i__id'].'\').toggleClass(\'hidden\');" style="font-weight:bold;"><u>'.$i['i__title'].'</u></a></td>';
+    $body_content .= '<td style="text-align: right;">'.$total_units.'x</td>';
+    $body_content .= '<td style="text-align: right;">$'.number_format(( $total_units > 0 ? $total_revenue / $total_units : 0 ), 2).'</td>';
+    $body_content .= '<td style="text-align: right;">+$'.number_format($total_revenue, 2).'</td>';
+    $body_content .= '<td style="text-align: right;" title="'.($commission_rate*100).'%">-$'.number_format($total_commission, 2).'</td>';
+    $body_content .= '<td style="text-align: right;" title="'.(( $total_revenue>0 ? $total_paypal_fee/$total_revenue : 0 )*100).'%">-$'.number_format($total_paypal_fee, 2).'</td>';
+    $body_content .= '<td style="text-align: right;" title="'.(( $total_revenue>0 ? $payout/$total_revenue : 0 )*100).'%"><b>$'.number_format($payout, 2).'</b></td>';
+    $body_content .= '<td style="text-align: right;">'.join(', ',$currencies).'</td>';
+    $body_content .= '</tr>';
+    $body_content .= $transaction_content;
+
+}
+
+
+$table_sortable = array('#th_primary','#th_average','#th_rev');
+
+echo '<table id="sortable_table" class="table table-sm table-striped image-mini">';
+echo '<tr style="vertical-align: baseline;">';
+echo '<th id="th_primary">Paid Ideas</th>';
+echo '<th style="text-align: right;" id="th_paid">Unit</th>';
+echo '<th style="text-align: right;" id="th_average">Average</th>';
+echo '<th style="text-align: right;" id="th_rev">Revenue</th>';
+echo '<th style="text-align: right;" id="th_payout">Commission</th>';
+echo '<th style="text-align: right;" id="th_payout">Paypal Fee</th>';
+echo '<th style="text-align: right;" id="th_payout">Payout</th>';
+echo '<th style="text-align: right;" id="th_currency">Currency</th>';
+echo '</tr>';
+echo $body_content;
+
+echo '<tr>';
+echo '<th style="text-align: right;" id="th_primary">Totals</th>';
+echo '<th style="text-align: right;">'.$gross_units.'x</th>';
+echo '<th style="text-align: right;">$'.number_format(( $gross_units > 0 ? $gross_revenue / $gross_units : 0 ), 2).'</th>';
+echo '<th style="text-align: right;">+$'.number_format($gross_revenue, 2).'</th>';
+echo '<th style="text-align: right;" title="'.($commission_rate*100).'%">-$'.number_format($gross_commission, 2).'</th>';
+echo '<th style="text-align: right;" title="'.(( $gross_revenue>0 ? $gross_paypal_fee/$gross_revenue : 0 )*100).'%">-$'.number_format($gross_paypal_fee, 2).'</th>';
+echo '<th style="text-align: right;" title="'.(( $gross_revenue>0 ? $gross_payout/$gross_revenue : 0 )*100).'%"><b>$'.number_format($gross_payout, 2).'</b></th>';
+echo '<th style="text-align: right;">'.join(', ',$gross_currencies).'</th>';
+echo '</tr>';
+echo '</table>';
 
 
 ?>
@@ -153,6 +161,10 @@ if(!isset($_GET['i__id']) || !intval($_GET['i__id'])){
        padding: 8px 0 !important;
         font-weight: bold;
         font-size: 1.2em;
+    }
+    tr td {
+        padding: 5px 0 !important;
+        font-size: 1.1em;
     }
     th:hover, th:active{
         background-color: #FFF;
