3d66c97bf550b029426639147ba33344552380b4
Shervin Enayati <shervin@mench.co>
2021-10-06 19:57:59 -0700

    Link Tree App

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -1012,7 +1012,7 @@ class X_model extends CI_Model
 
             //Display:
             if($simple_version){
-                $output_body_message = str_replace($identifier_string, $the_title, $output_body_message);
+                $output_body_message = str_replace($identifier_string, $es[0]['e__title'], $output_body_message);
             } elseif($on_its_own_line){
                 $the_title = '<span class="subtle-line mini-grey text__6197_'.$es[0]['e__id'].$tooltip_underdot.'">' . $es[0]['e__title'] . '</span>';
                 if($new_lines <= 1){
