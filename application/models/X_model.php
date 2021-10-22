23bf64d7970439eedeae7c85c587177028efd4c0
Shervin Enayati <shervin@mench.co>
2021-10-21 20:29:21 -0700

    Idea link bug fix

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -551,7 +551,7 @@ class X_model extends CI_Model
 
 
         //Breakup into smaller SMS friendly messages
-        $sms_message = $subject.': '.$plain_message;
+        $sms_message = $subject.( preg_match("/[a-z]/i", substr(strtolower($subject), -1)) ? ': ' : ' ' ).$plain_message;
         if(strlen($sms_message)<=view_memory(6404,27891)){
 
             //Send SMS
