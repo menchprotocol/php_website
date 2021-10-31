0c8143c457d9583f4c3fb0fc1a6affd403840be7
Shervin Enayati <shervin@mench.co>
2021-10-30 20:42:00 -0700

    Text breakdown

--- application/views/app/13790.php
+++ application/views/app/13790.php
@@ -315,7 +315,7 @@ if(!isset($_GET['i__id']) || !$_GET['i__id']){
         $('.editable').click(function (e) {
 
             //alert($(this).attr('i__id') + '/'+ $(this).attr('e__id') +'/'+ $(this).attr('x__source') +'/'+ $(this).attr('x__id'));
-
+            $('.x__source_' + $(this).attr('e__id') + '_' + $(this).attr('x__source')).html('<i class="far fa-yin-yang fa-spin"></i>');
 
             //Check email and validate:
             $.post("/e/e_toggle_e", {
@@ -330,7 +330,7 @@ if(!isset($_GET['i__id']) || !$_GET['i__id']){
                 if (data.status) {
 
                     //Update source id IF existed previously:
-                    $('.x__source_' + $(this).attr('e__id') + '_' + $(this).attr('x__source')).html(data.message);
+                    $('.x__source_' + $(this).attr('e__id') + '_' + $(this).attr('x__source')).text(data.message);
 
                 } else {
                     alert('ERROR:' + data.message);
