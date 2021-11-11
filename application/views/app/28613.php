e7b9655476f9bac28fe0f94a46c3c742b5b3df0a
Shervin Enayati <shervin@mench.co>
2021-11-11 15:04:09 -0800

    Contact Us app

--- application/views/app/28613.php
+++ application/views/app/28613.php
@@ -3,4 +3,4 @@
 $from_email = get_domain_setting(28614);
 $from_phone = get_domain_setting(28615);
 
-echo 'You can call us at <a href="tel:'.$from_phone.'"><u><b>'.$from_phone.'</b></u></a> or email <a href="mailto:'.$from_email.'"><u><b>'.$from_email.'</b></u></a> to get in touch.';
+echo '<p>You can call us at <a href="tel:'.$from_phone.'"><u><b>'.$from_phone.'</b></u></a> or email <a href="mailto:'.$from_email.'"><u><b>'.$from_email.'</b></u></a> to get in touch.</p>';
