00cf90442c4345dd803e909831fac22e16ed9acb
Shervin Enayati <shervin@mench.co>
2021-11-27 22:42:01 -0800

    Home Page background

--- application/views/header.php
+++ application/views/header.php
@@ -169,7 +169,8 @@ foreach($this->config->item('e___13890') as $e__id => $m){
             echo 'background: transparent !important; ';
             echo '}';
 
-            echo ' .halfbg, .mid-title, .msg span, .fixed-top { background: rgba(0, 0, 0, 0.5) !important; } ';
+            echo ' .halfbg, .msg span, .fixed-top { background: rgba(0, 0, 0, 0.5) !important; } ';
+            echo ' .mid-title { display:none !important; } ';
             echo ' .halfbg, .msg span, .msg { line-height: 125% !important; } ';
             echo ' .msg span u, .msg span a { line-height: 100% !important; padding:0 !important; } ';
 
