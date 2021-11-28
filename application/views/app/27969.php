972f3237db1e0567880e000f50b22bfad6820f35
Shervin Enayati <shervin@mench.co>
2021-11-28 14:24:48 -0800

    Home Page background

--- application/views/app/27969.php
+++ application/views/app/27969.php
@@ -4,7 +4,7 @@ function build_item($e__id, $i__id, $s__title, $s__cover, $link, $desc = null, $
 
     return '<a href="/-27970?e__id='.$e__id.'&i__id='.$i__id.'&go_to='.urlencode($link).'" class="list-group-item list-group-item-action flex-column align-items-start">
     <div class="d-flex justify-content-between">
-      <h4 class="css__title"><b><span class="icon-block-lg">'.view_cover(($e__id>0 ? 12274 : 12273),$s__cover).'</span>'.$s__title.'</b></h4>
+      <h4 class="css__title"><b><span class="icon-block-lg title-left">'.view_cover(($e__id>0 ? 12274 : 12273),$s__cover).'</span><span class="title-right">'.$s__title.'</span></b></h4>
       <small style="padding: 11px 1px 0 0;"><i class="far fa-chevron-right"></i></small>
     </div>
     '.( strlen($desc) ? '<p>'.$desc.'</p>' : '' ) .'
