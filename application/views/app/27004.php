22f1bce83ea7d0c9b817e9a0a751211b889188dc
Shervin Enayati <shervin@mench.co>
2021-12-15 12:51:40 -0800

    Payment chart

--- application/views/app/27004.php
+++ application/views/app/27004.php
@@ -235,6 +235,39 @@ if(count($i_query)){
 
     ksort($daily_sales);
     print_r($daily_sales);
+    echo '<div id="chart_div"></div>';
+    ?>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages':['corechart']});
+
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Day', 'Sales'],
+                <?php
+                foreach($daily_sales as $day => $sales){
+                    echo "['".$day."', ".number_format($sales, 0)."],";
+                }
+                ?>
+            ]);
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, {'title':'Sales by Day',
+                'width':400,
+                'height':300});
+        }
+    </script>
+    <?php
 
 }
 
