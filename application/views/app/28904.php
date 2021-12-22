82c3d863165a98205e00ec965041f09e9dc7929b
Shervin Enayati <shervin@mench.co>
2021-12-21 20:36:14 -0800

    Expandable Idea App

--- application/views/app/28904.php
+++ application/views/app/28904.php
@@ -2,4 +2,4 @@
 
 //Notification Settings
 
-echo '<div style="max-width: 540px;">'.view_radio_e(28904, $member_e['e__id'], 0).'</div>';
+echo '<div style="max-width: 540px; margin: 0 auto; text-align: center; padding: 0 13px;">'.view_radio_e(28904, $member_e['e__id'], 0).'</div>';
