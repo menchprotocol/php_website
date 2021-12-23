b167fa0283aa85295b6eca4c2b339fee8829e210
Shervin Enayati <shervin@mench.co>
2021-12-22 18:02:35 -0800

    Source Layout

--- application/views/app/28904.php
+++ application/views/app/28904.php
@@ -2,4 +2,4 @@
 
 //Notification Settings
 
-echo '<div style="max-width: 540px; margin: 0 auto;" class="msg">'.view_radio_e(28904, $member_e['e__id'], 0).'</div>';
+echo '<div style="max-width: 540px; margin: 0 auto;">'.view_radio_e(28904, $member_e['e__id'], 0).'</div>';
