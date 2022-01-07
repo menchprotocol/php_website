d26a162f812b3eb18a39ddd22db5661569a7e19a
Shervin Enayati <shervin@mench.co>
2022-01-07 15:20:31 -0800

    Sources & Idea Boxes

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1837,7 +1837,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $message_input
         if($messages){
             $message_tooltip = '<span class="mini-font">'.$messages.'</span>';
         }
-    } elseif($discovery_mode && $can_click && isset($i['x__message']) && strlen($i['x__message'])>0){
+    } elseif($discovery_mode && isset($i['x__message']) && strlen($i['x__message'])>0){ //&& $can_click
         if(superpower_active(12701, true)){
             $message_tooltip = '<div class="mini-font">'.$CI->X_model->message_view( $i['x__message'], true).'</div>';
         } else {
@@ -1850,7 +1850,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $message_input
 
     $view_i_time = null;
     if($message_tooltip || $view_i_time){
-        $ui .= '<div class="grey">' . $message_tooltip . ( $view_i_time ? $view_i_time : '' ) . '</div>';
+        $ui .= '<div class="">' . $message_tooltip . ( $view_i_time ? $view_i_time : '' ) . '</div>'; //grey
     }
 
     if(count($minter)){
