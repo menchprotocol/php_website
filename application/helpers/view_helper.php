58932de537c8f8f6be2ccc1601f718b11b7c3da7
Shervin Enayati <shervin@mench.co>
2022-01-13 17:06:56 -0800

    Dynamic Loading

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1090,6 +1090,7 @@ function view_coins_e($x__type, $e__id, $page_num = 0, $append_coin_icon = true,
 
         $query = $CI->X_model->fetch($query_filters, $join_objects, 1, 0, array(), 'COUNT(x__id) as totals');
         $count_query = $query[0]['totals'];
+        print_r($CI->db->last_query());
 
         if($append_coin_icon){
 
