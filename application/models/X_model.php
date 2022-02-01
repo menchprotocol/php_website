9a868dfcab3e64640390f555d0304453aa89f8eb
Shervin Enayati <shervin@mench.co>
2022-01-31 18:02:55 -0800

    Reverted power editor 2

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -962,12 +962,22 @@ class X_model extends CI_Model
             //Add Dropdown frame IF any:
             $e_dropdown = '';
             if(count($e_links)){
-                //List all links:
-                $e_dropdown .= '<div class="dropdown inline-block inline-dropdown"><button type="button" class="btn-transparent no-left-padding no-right-padding ignore-click" id="externalRef'.$es[0]['e__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.( $is_discovery_mode ? '<span class="icon-block-xxs">' . view_cover(12274,$es[0]['e__cover']).'</span><u>'.$es[0]['e__title'].'</u>' : '' ).'<span class="icon-block-xs" style="font-size:0.89em;"><i class="far fa-angle-down"></i></span></button><div class="dropdown-menu" aria-labelledby="externalRef'.$es[0]['e__id'].'">';
-                foreach($e_links as $e_link){
-                    $e_dropdown .= '<a href="'.$e_link['x__message'].'" target="_blank" class="dropdown-item move_away css__title ignore-click should-click"><span class="icon-block">'.view_cover(12274,$e_link['e__cover']).'</span>'.$e_link['e__title'].'</a>';
+
+                if(count($e_links)==1){
+
+                    //Just show one:
+                    $e_dropdown .= '<a href="'.$e_links[0]['x__message'].'" target="_blank" class="ignore-click should-click" title="'.$e_links[0]['e__title'].'"><span class="icon-block-xxs">' . view_cover(12274,$es[0]['e__cover']).'</span><u>'.$es[0]['e__title'].'</u></a>';
+
+                } else {
+
+                    //List all links:
+                    $e_dropdown .= '<div class="dropdown inline-block inline-dropdown"><button type="button" class="btn-transparent no-left-padding no-right-padding ignore-click" id="externalRef'.$es[0]['e__id'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.( $is_discovery_mode ? '<span class="icon-block-xxs">' . view_cover(12274,$es[0]['e__cover']).'</span><u>'.$es[0]['e__title'].'</u>' : '' ).'<span class="icon-block-xs" style="font-size:0.89em;"><i class="far fa-angle-down"></i></span></button><div class="dropdown-menu" aria-labelledby="externalRef'.$es[0]['e__id'].'">';
+                    foreach($e_links as $e_link){
+                        $e_dropdown .= '<a href="'.$e_link['x__message'].'" target="_blank" class="dropdown-item move_away css__title ignore-click should-click"><span class="icon-block">'.view_cover(12274,$e_link['e__cover']).'</span>'.$e_link['e__title'].'</a>';
+                    }
+                    $e_dropdown .= '</div></div>';
+
                 }
-                $e_dropdown .= '</div></div>';
             }
 
 
