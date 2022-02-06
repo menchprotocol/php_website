983ef24bacd0a0314dca79779eba295a3dde39fa
Shervin Enayati <shervin@mench.co>
2022-02-06 13:22:00 -0800

    Alignment fixes

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1856,7 +1856,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $message_input
                 $action_buttons .= '<a href="javascript:void(0);" onclick="coin__load(12273,'.$i['i__id'].')" class="dropdown-item css__title">'.$anchor.'</a>'; //COIN COVER
             } elseif($e__id==12589){
                 $action_buttons .= '<a href="javascript:void(0);" onclick="apply_all_load(12589,'.$i['i__id'].')" class="dropdown-item css__title">'.$anchor.'</a>';
-            } elseif($e__id==6155 && $editing_enabled && isset($i['x__id']) && in_array($x__type, $CI->config->item('n___6155')) && ($x__type!=6255 || $superpower_10939)){
+            } elseif($e__id==6155 && isset($i['x__id']) && in_array($x__type, $CI->config->item('n___6155')) && ($x__type!=6255 || $superpower_10939)){
                 $action_buttons .= '<a href="javascript:void(0);" class="dropdown-item css__title x_remove" i__id="'.$i['i__id'].'" x__id="'.$i['x__id'].'">'.$anchor.'</a>'; //UNLINK
             } elseif($e__id==26001){
                 //Reset discoveries
