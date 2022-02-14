1a10bceb554a3f06521175ccecae13b98311735e
Shervin Enayati <shervin@mench.co>
2022-02-14 14:07:02 -0800

    Expire Time Cron Job

--- application/views/app/28199.php
+++ application/views/app/28199.php
@@ -26,8 +26,28 @@ foreach($this->X_model->fetch(array(
         ));
         $seconds_left = intval($expires['x__message']) - (time()-strtotime($x_progress['x__time']));
 
+        if(!count($answered) && !$seconds_left){
+            //Answer not yet completed and no time left, delete response:
+            $to_delete = 0;
+            foreach($this->X_model->fetch(array(
+                'x__status IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
+                'x__type IN (' . join(',', $this->config->item('n___12227')) . ')' => null,
+                'x__left' => $x_progress['x__right'],
+                'x__source' => $x_progress['e__id'],
+            ), array(), 0) as $delete){
+                $to_delete++;
+            }
+
+            /*
+            $this->X_model->update($e_data['x__id'], array(
+                'x__status' => 6173, //Transaction Deleted
+            ), $e__id, 27890);
+            */
+
+        }
+
         //Now see if they have responded and completed the answer to this question:
-        echo '<div style="padding-left: 21px;"><a href="/@'.$x_progress['e__id'].'">'.$x_progress['e__title'].'</a>: '.$x_progress['x__time'].' ? '.$x_progress['x__message'].' / <a href="/-12722?x__id=' . $x_progress['x__id'] . '">'.$x_progress['x__id'].' / Answer: '.count($answered).'</a> '.( !count($answered) ? ( $seconds_left <= 0 ? ' DELETE ' : '['.$seconds_left.'] SEcs left' ) : '' ).'</div>';
+        echo '<div style="padding-left: 21px;"><a href="/@'.$x_progress['e__id'].'">'.$x_progress['e__title'].'</a>: '.$x_progress['x__time'].' ? '.$x_progress['x__message'].' / <a href="/-12722?x__id=' . $x_progress['x__id'] . '">'.$x_progress['x__id'].' / Answer: '.count($answered).'</a> '.( !count($answered) ? ( !$seconds_left ? ' DELETE '.$to_delete.' TRS ' : '['.$seconds_left.'] SEcs left' ) : '' ).'</div>';
 
     }
 
