5f9c226a258224731b693f3006835c7ac84c4a41
Shervin Enayati <shervin@mench.co>
2022-02-14 16:27:00 -0800

    Expire Time Cron Job

--- application/views/app/28199.php
+++ application/views/app/28199.php
@@ -11,14 +11,14 @@ if($member_e && isset($_GET['i__id']) && intval($_GET['i__id'])>0){
     $filters['x__right'] = intval($_GET['i__id']);
     $buffer_time = 0;
 } else {
-    $buffer_time = 30;
+    $buffer_time = 120;
 }
 
 $links_deleted = 0;
 //Go through all expire seconds ideas:
 foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
 
-    //echo '<hr /><div><a href="/~'.$expires['i__id'].'">'.$expires['i__title'].'</a></div>';
+    echo '<hr /><div><a href="/~'.$expires['i__id'].'">'.$expires['i__title'].'</a></div>';
 
     //Now go through everyone who discovered this selection:
     $counter = 0;
@@ -29,7 +29,7 @@ foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
     ), array('x__source'), 0) as $x_progress){
 
         //Now see if the answer is completed:
-        $answered = $this->X_model->fetch(array(
+        $answer_completed = $this->X_model->fetch(array(
             'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PRIVATE
             'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //Discoveries
             'x__left' => $x_progress['x__right'],
@@ -37,7 +37,7 @@ foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
         ));
         $seconds_left = intval( intval( $expires['x__message']) + $buffer_time - time() - strtotime($x_progress['x__time'] ));
 
-        if(!count($answered) && $seconds_left <= 0){
+        if(!count($answer_completed) && $seconds_left <= 0){
 
             //Answer not yet completed and no time left, delete response:
             foreach($this->X_model->fetch(array(
@@ -59,7 +59,7 @@ foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
 
         //Now see if they have responded and completed the answer to this question:
         $counter++;
-        //echo '<div style="padding-left: 21px;">'.$counter.') <a href="/@'.$x_progress['e__id'].'">'.$x_progress['e__title'].'</a>: '.$x_progress['x__time'].' ? '.$x_progress['x__message'].' / <a href="/-12722?x__id=' . $x_progress['x__id'] . '">'.$x_progress['x__id'].' / Answer: '.count($answered).'</a> '.( !count($answered) ? ( $seconds_left <= 0 ? ' DELETE ' : '['.$seconds_left.'] SEcs left' ) : '' ).'</div>';
+        echo '<div style="padding-left: 21px;">'.$counter.') <a href="/@'.$x_progress['e__id'].'">'.$x_progress['e__title'].'</a>: '.$x_progress['x__time'].' ? '.$x_progress['x__message'].' / <a href="/-12722?x__id=' . $x_progress['x__id'] . '">'.$x_progress['x__id'].' / Answer: '.count($answer_completed).'</a> '.( !count($answer_completed) ? ( $seconds_left <= 0 ? ' DELETE ' : '['.$seconds_left.'] SEcs left' ) : '' ).' ('.intval( $expires['x__message']) .'+'. $buffer_time .'-'. time() .'-'. strtotime($x_progress['x__time'] ).' = '.$seconds_left.')</div>';
 
     }
 
