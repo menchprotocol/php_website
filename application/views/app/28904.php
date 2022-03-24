7af1843fa6a3cc3721e1ed529412fc6d3612951a
Shervin Enayati <shervin@mench.co>
2022-03-24 11:19:51 -0700

    Unsubscribe streamlined

--- application/views/app/28904.php
+++ application/views/app/28904.php
@@ -1,4 +1,12 @@
 <?php
 
-//Notification Settings
-echo '<div style="max-width: 540px; margin: 0 auto;">'.view_radio_e(28904, ( isset($_GET['e__id']) && isset($_GET['e__hash']) && md5($_GET['e__id'].$this->config->item('cred_password_salt'))==$_GET['e__hash'] ? $_GET['e__id'] : $member_e['e__id'] ), 0).'</div>';
+$e__id = ( isset($_GET['e__id']) && isset($_GET['e__hash']) && md5($_GET['e__id'].$this->config->item('cred_password_salt'))==$_GET['e__hash'] ? $_GET['e__id'] : $member_e['e__id'] );
+
+if($e__id > 0){
+    //Notification Settings
+    echo '<div style="max-width: 540px; margin: 0 auto;">'.view_radio_e(28904, , 0).'</div>';
+} else {
+    js_redirect('/-4269?url='.urlencode($_SERVER['REQUEST_URI']), 13);
+}
+
+
