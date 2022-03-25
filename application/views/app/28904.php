855d954614a129ad4862f85e917c609851fbdfe5
Shervin Enayati <shervin@mench.co>
2022-03-25 11:30:45 -0700

    Unsubscribe streamlined

--- application/views/app/28904.php
+++ application/views/app/28904.php
@@ -4,6 +4,11 @@ $e__id = ( isset($_GET['e__id']) && isset($_GET['e__hash']) && md5($_GET['e__id'
 
 if($e__id > 0){
     //Notification Settings
+    foreach($this->E_model->fetch(array(
+        'e__id' => $e__id,
+    )) as $e){
+        echo '<div style="max-width: 540px; margin: 0 auto; text-align: center; font: 1.4em;">'.view_cover(12274,$e['e__cover']).' '.$e['e__title'].'</div>';
+    }
     echo '<div style="max-width: 540px; margin: 0 auto;">'.view_radio_e(28904, $e__id, 0).'</div>';
     echo '<input type="hidden" id="member__id_override" value="'.$e__id.'" />';
 } else {
