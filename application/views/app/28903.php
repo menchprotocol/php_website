821f549127d9826bc448aefb27aaad3e43c4d0c6
Shervin Enayati <shervin@mench.co>
2022-04-14 22:30:11 -0700

    Source Type NFT

--- application/views/app/28903.php
+++ application/views/app/28903.php
@@ -37,10 +37,10 @@ if(!isset($_GET['i__id'])){
             'x__left' => $_GET['i__id'],
         ), array('x__right'), 0, 0, array('x__spectrum' => 'ASC')) as $i) {
 
-            echo '<h3 style="margin:13px 0; border-bottom: 1px solid #000;"><a href="javascript:void(0);" onclick="$(\'.i_msg_'.$i['i__id'].'\').toggleClass(\'hidden\');" class="inner-content doblock">' . $i['i__title'] . '</a></h3>';
+            echo '<h3 style="margin:13px 0;"><a href="javascript:void(0);" onclick="$(\'.i_msg_'.$i['i__id'].'\').toggleClass(\'hidden\');" class="inner-content doblock">' . $i['i__title'] . '</a></h3>';
 
             //MESSAGES
-            echo '<div class="i_msg_'.$i['i__id'].' hidden" style="padding:5px 0 34px 41px;">';
+            echo '<div class="i_msg_'.$i['i__id'].' hidden" style="padding:5px 0 34px 41px; border-bottom: 1px solid #000;">';
             foreach ($this->X_model->fetch(array(
                 'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //ACTIVE
                 'x__type' => 4231, //IDEA NOTES Messages
