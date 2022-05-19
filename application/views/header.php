20bbfc9f825da18f77fb35e372ebf0cb40d54c0a
Shervin Enayati <shervin@mench.co>
2022-05-18 20:13:36 -0700

    Scheduled Messaging V2

--- application/views/header.php
+++ application/views/header.php
@@ -144,11 +144,12 @@ foreach($this->config->item('e___13890') as $e__id => $m){
                 //Is Video:
                 $bgVideo = '<video autoplay muted loop id="myVideo"><source src="'.$domain_background.'" type="video/mp4"></video>';
                 echo '#bgVideo {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
+    height: 100%;
+    border-radius: 0;
+    position: fixed;
+    z-index: -1;
+    top: 0;
+    left: 0;
 }';
 
             } else {
