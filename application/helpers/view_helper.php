ae24027e25a395a97db52b9831a8a916559b51c1
Shervin Enayati <shervin@mench.co>
2022-05-18 21:12:48 -0700

    Scheduled Messaging V2

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1115,7 +1115,7 @@ function view_coins_e($x__type, $e__id, $page_num = 0, $append_coin_icon = true,
             $coins1 = view_coins_e(11029, $e__id, 0, false);
             $coins2 = view_coins_e(11030, $e__id, 0, false);
             $count_query = $coins1 + $coins2;
-            $visual_counter = $coins1.'/'.$coins2;
+            $visual_counter = $coins2.'/'.$coins1;
 
         } else {
             $query = $CI->X_model->fetch($query_filters, $join_objects, 1, 0, array(), 'COUNT(x__id) as totals');
