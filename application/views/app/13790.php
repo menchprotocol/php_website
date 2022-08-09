64820011b1038e920d225362fada89e2440cc891
shervine <shervin@mench.com>
2022-08-09 06:26:40 -0700

    Tweaks

--- application/views/app/13790.php
+++ application/views/app/13790.php
@@ -80,7 +80,7 @@ if(isset($_GET['xaxis_e']) && strlen($_GET['xaxis_e'])){
         'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
         'x__type IN (' . join(',', $this->config->item('n___13550')) . ')' => null, //SOURCE IDEAS
         'x__up' => $_GET['xaxis_e'], //ACTIVE
-        'i__type IN (' . join(',', $this->config->item('n___7355')) . ')' => null, //PUBLIC
+        'i__type IN (' . join(',', $this->config->item('n___7356')) . ')' => null, //ACTIVE
     ), array('x__right'), 0, 0, array('x__spectrum' => 'ASC', 'i__title' => 'ASC')) as $link_i){
         array_push($column_ideas, $link_i);
     }
