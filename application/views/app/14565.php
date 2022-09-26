54ec9543e81d523ca31d5ee7e2607cd73d6b97e6
shervine <shervin@mench.com>
2022-09-25 17:21:36 -0700

    Home Page Updates

--- application/views/app/14565.php
+++ application/views/app/14565.php
@@ -244,10 +244,10 @@ if($social_id){
 }
 
 
-$domain_year_started = intval(get_domain_setting(30379));
-if($domain_year_started){
-    $domain_company_name = intval(get_domain_setting(30380));
-    echo '<p style="font-size: 0.8em; text-align: center;" class="social-footer '.( $domain_background ? ' halfbg ' : '' ).'">'.( $domain_company_name ? $domain_company_name.' | ' : '' ).$domain_year_started.' - '.date('Y').'</p>';
+$domain_year_started = get_domain_setting(30379);
+$domain_company_name = get_domain_setting(30380);
+if($domain_year_started || $domain_company_name){
+    echo '<p style="font-size: 0.8em; text-align: center;" class="social-footer '.( $domain_background ? ' halfbg ' : '' ).'">'.( $domain_company_name ? $domain_company_name.' | ' : '' ).( $domain_year_started ? $domain_year_started.' - '.date('Y') : '' ).'</p>';
 }
 
 
