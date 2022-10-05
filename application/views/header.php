075fa2c1557c488fe31692318ce877e4a0638146
shervine <shervin@mench.com>
2022-10-04 17:30:10 -0700

    Google Analytics Setup

--- application/views/header.php
+++ application/views/header.php
@@ -281,36 +281,33 @@ echo $bgVideo;
 $live_chat_page_id = get_domain_setting(12899);
 if(strlen($live_chat_page_id)>10){
     ?>
-    <!-- Load Facebook SDK for JavaScript -->
+    <!-- Messenger Chat Plugin Code -->
     <div id="fb-root"></div>
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat" ref="<?= ( $member_e ? $member_e['e__id'] : '' ) ?>">
+    </div>
+    <script>
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "<?= $live_chat_page_id ?>");
+        chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+    <!-- Your SDK code -->
     <script>
         window.fbAsyncInit = function() {
             FB.init({
                 xfbml            : true,
-                version          : 'v12.0'
+                version          : 'v15.0'
             });
         };
-
         (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
             js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
             fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
-
-    <!-- Your Chat App code -->
-    <div class="fb-customerchat"
-         page_id="<?= $live_chat_page_id ?>"
-         greeting_dialog_display="hide"
-         welcome_screen_greeting="Hi"
-         ref="<?= ( $member_e ? $member_e['e__id'] : '' ) ?>"
-         theme_color="#000000">
-    </div>
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     <?php
-
-    /*<div class="chat-title"><span><?= $e___11035[12899]['m__title'] ?></span></div>*/
-
 }
 
 
