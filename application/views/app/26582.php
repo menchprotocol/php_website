8639f8ad562dd42f6837d3595361b5eafe33e428
shervine <shervin@mench.com>
2022-10-04 23:12:42 -0700

    Message Scheduling

--- application/views/app/26582.php
+++ application/views/app/26582.php
@@ -89,8 +89,7 @@ echo '<div id="charNum"></div>';
 
 echo '</div>';
 
-echo date('Y-m-d\TH:i');
-echo '<input type="datetime-local" id="meeting-time" value="'.''.'">';
+echo '<input type="datetime-local" id="meeting-time" value="'.date('Y-m-d\TH:i').'">';
 
 echo '<div id="send_message_btn" style="padding: 10px;"><a class="btn btn-default" href="javascript:void(0);"  onclick="send_message();">Schedule Message for '.$message_list['unique_users_count'].' Members <i class="fas fa-arrow-right"></i></a></div>';
 
