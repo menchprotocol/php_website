2ca1bb426ab0a532d385557106346a0bec08f534
shervine <shervin@mench.com>
2022-10-18 12:35:35 -0700

    Bug Fix

--- application/views/app/27004.php
+++ application/views/app/27004.php
@@ -159,7 +159,7 @@ foreach($i_query as $i){
 
         $transaction_content .= '</tr>';
 
-        $date = date("m/d", strtotime($x['x__time']));
+        $date = date("md", strtotime($x['x__time']));
         if(isset($daily_sales[$date])){
             $daily_sales[$date] += $x__metadata['mc_gross'];
         } else {
