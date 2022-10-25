379689273f7bfa7a03ac4c82411ecd84982a5b93
shervine <shervin@mench.com>
2022-10-25 11:45:25 -0700

    New Payment Engine

--- application/views/app/26595.php
+++ application/views/app/26595.php
@@ -12,33 +12,35 @@ if(isset($_POST)){
             'x__message' => 'Unexpected Paypal Call',
             'x__metadata' => $_POST,
         ));
-    }
-
-    $item_numbers = explode('-',$_POST['item_number']);
-    $top_i__id = intval($item_numbers[0]);
-    $i__id = intval($item_numbers[1]);
-    $currency_type = intval($item_numbers[2]);
-    $x__source = intval($item_numbers[3]);
-    $pay_amount = doubleval(( $_POST['payment_gross']>$_POST['mc_gross'] ? $_POST['payment_gross'] : $_POST['mc_gross'] ));
-
-    //Seems like a valid Paypal IPN Call:
-    $next_is = $this->I_model->fetch(array(
-        'i__id' => $i__id,
-        'i__type IN (' . join(',', $this->config->item('n___7355')) . ')' => null, //PUBLIC
-    ));
-
-    if($top_i__id>0 && $i__id>0 && in_array($currency_type, $this->config->item('n___26661')) && $x__source>0 && count($next_is) && $pay_amount>0){
-
-        //Mark idea as complete if not already:
-        $this->X_model->mark_complete($top_i__id, $next_is[0], array(
-            'x__type' => 26595,
-            'x__source' => $x__source,
-            'x__up' => $currency_type,
-            'x__metadata' => $_POST,
-        ));
+        echo 'Bad Paypal Post Data!';
 
     } else {
-        echo 'Invalid item number';
+        $item_numbers = explode('-',$_POST['item_number']);
+        $top_i__id = intval($item_numbers[0]);
+        $i__id = intval($item_numbers[1]);
+        $currency_type = intval($item_numbers[2]);
+        $x__source = intval($item_numbers[3]);
+        $pay_amount = doubleval(( $_POST['payment_gross']>$_POST['mc_gross'] ? $_POST['payment_gross'] : $_POST['mc_gross'] ));
+
+        //Seems like a valid Paypal IPN Call:
+        $next_is = $this->I_model->fetch(array(
+            'i__id' => $i__id,
+            'i__type IN (' . join(',', $this->config->item('n___7355')) . ')' => null, //PUBLIC
+        ));
+
+        if($top_i__id>0 && $i__id>0 && in_array($currency_type, $this->config->item('n___26661')) && $x__source>0 && count($next_is) && $pay_amount>0){
+
+            //Mark idea as complete if not already:
+            $this->X_model->mark_complete($top_i__id, $next_is[0], array(
+                'x__type' => 26595,
+                'x__source' => $x__source,
+                'x__up' => $currency_type,
+                'x__metadata' => $_POST,
+            ));
+
+        } else {
+            echo 'Invalid item number';
+        }
     }
 
 } else {
