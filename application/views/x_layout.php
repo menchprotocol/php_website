7145cc1a1442e41c8bd93d9840f28e05b98f61eb
shervine <shervin@mench.com>
2022-10-25 17:34:41 -0700

    New Payment Engine

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -70,7 +70,7 @@ if($is_payment){
             //Break down amount & currency
             $currency_parts = explode(' ',$total_dues[0]['x__message'],2);
             $unit_price = number_format($currency_parts[1], 2);
-            $unit_fee = number_format($currency_parts[1] * ( count($digest_fees) || !is_new() ? 0 : ( doubleval(view_memory(6404,27017)) + doubleval(view_memory(6404,30590)) + doubleval(view_memory(6404,30612)) )/100 ), 2);
+            $unit_fee = number_format($currency_parts[1] * ( count($digest_fees) ? 0 : ( doubleval(view_memory(6404,27017)) + doubleval(view_memory(6404,30590)) + doubleval(view_memory(6404,30612)) )/100 ), 2);
             $unit_total = number_format($unit_fee+$currency_parts[1], 2);
             $max_allowed = ( count($multi_selectable) && is_numeric($multi_selectable[0]['x__message']) && $multi_selectable[0]['x__message']>1 ? intval($multi_selectable[0]['x__message']) : view_memory(6404,29651) );
 
