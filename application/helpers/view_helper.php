6f25e7adbdd7e4b1082b7abe4f9132091e98c9ba
shervine <shervin@mench.com>
2022-10-26 19:23:02 -0700

    New Payment Engine

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2277,8 +2277,8 @@ function view_e($x__type, $e, $extra_class = null, $source_of_e = false)
     $ui .= '<td width="20%"><div>'.($source_of_e && $superpower_13422 && !$cache_app ? view_input_dropdown(6177, $e['e__type'], null, $source_of_e && $superpower_13422, false, $e['e__id']) : '').'</div></td>';
     $ui .= '<td width="20%">'.($source_of_e && $superpower_13422 && !$cache_app && $x__id ? ( in_array($e['x__type'], $CI->config->item('n___13550')) ? view_input_dropdown(13550, $e['x__type'], null, $source_of_e && $superpower_13422, false, $e['e__id'], $x__id) : '<a href="javascript:void(0);" onclick="x_message_load(' . $e['x__id'] . ')" class="icon-block">'.view_cache(4593, $e['x__type']).'</a>' ) : '').'</td>';
     $ui .= '<td width="20%"><div class="show-on-hover">'.($has_sortable ? '<span class="sort_e hidden" title="'.$e___11035[4603]['m__title'].'"><span class="icon-block">'.$e___11035[4603]['m__cover'].'</span></span>' : '').'</div></td>';
-    $ui .= '<td width="20%"><div class="show-on-hover">'.$dropdown_ui.'</div></td>';
     $ui .= '<td width="20%"><div class="show-on-hover">'.( $can_click && $show_text_editor ? '<a href="'.$href.'"><i class="fas fa-arrow-right"></i></a>' : '' ).'</div></td>';
+    $ui .= '<td width="20%"><div class="show-on-hover">'.$dropdown_ui.'</div></td>';
     $ui .= '</tr></table>';
 
 
