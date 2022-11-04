0372ee8dd705cd05f621de5506f3d82c36479faa
shervine <shervin@mench.com>
2022-11-04 16:12:39 -0700

    Streamlining Idea/Source Links

--- application/views/e_layout.php
+++ application/views/e_layout.php
@@ -12,9 +12,9 @@ $this->X_model->create(array(
 
 
 //Top Notes
-$e___11018 = $this->config->item('e___11018');
+$e___11035 = $this->config->item('e___11035'); //NAVIGATION
 $following_count = view_coins_e(11030, $e['e__id'], 0, false);
-echo view_pill(11030,  $following_count, $e___11018[11030], view_body_e(11030, $following_count, $e['e__id']), false);
+echo view_pill(11030,  $following_count, $e___11035[11030], view_body_e(11030, $following_count, $e['e__id']), false);
 
 
 
