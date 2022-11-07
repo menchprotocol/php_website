7999654566a437aee939760a6061c54a11b9d9e0
shervine <shervin@mench.com>
2022-11-06 22:56:24 -0800

    Streamlining Idea/Source Links

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('general_helper','view_helper','cookie');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array('mench_memory', 'credentials');
+$autoload['config'] = array('credentials');
 
 /*
 | -------------------------------------------------------------------
