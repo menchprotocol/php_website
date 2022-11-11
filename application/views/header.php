378ffc7273c628f230fa1a10fcb296dd037a00aa
shervine <shervin@mench.com>
2022-11-11 13:05:35 -0800

    Starting Idea

--- application/views/header.php
+++ application/views/header.php
@@ -332,7 +332,7 @@ if(!$basic_header_footer){
 
     //Do not show for /sign view
     ?>
-    <div class="container fixed-top" style="padding-bottom: 0 !important;">
+    <div class="container fixed-top" style="padding-bottom: 0 !important; min-height: 38px;">
         <div class="row justify-content">
             <table class="platform-navigation">
                 <tr>
