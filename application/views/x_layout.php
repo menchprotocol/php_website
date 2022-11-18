6a2ce0bc19998d7a3b4e82643b65f89d6b7bda9e
shervine <shervin@mench.com>
2022-11-17 19:06:03 -0800

    New/Simplified Idea Format

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -470,7 +470,7 @@ if($top_i__id) {
 
                 echo '<tr>';
                 echo '<td class="table-btn first_btn" style="text-align: right;">Fee:&nbsp;&nbsp;</td>';
-                echo '<td class="table-btn first_btn">'.$unit_fee.' '.$currency_parts[0].'</td>';
+                echo '<td class="table-btn first_btn" title="hi">'.$unit_fee.' '.$currency_parts[0].'</td>';
                 echo '</tr>';
             }
 
