8d99883cb562415bd9693f81d7cc306a48b77ed6
shervine <shervin@mench.com>
2022-11-18 13:26:48 -0800

    Email Server down

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1459,13 +1459,16 @@ function email_send($to_emails, $subject, $email_body, $e__id = 0, $x_data = arr
         $email_message .= '<div><a href="https://'.get_domain('m__message', $e__id, $x__domain).'/-28904?e__id='.$e__id.'&e__hash='.md5($e__id.view_memory(6404,30863)).'" style="font-size:10px;">'.$e___6287[28904]['m__title'].'</a></div>';
     }
 
+    $aws_key = $CI->config->item('cred_aws');
+    putenv("AWS_ACCESS_KEY_ID=" . $aws_key->key);
+    putenv("AWS_SECRET_ACCESS_KEY=" . $aws_key->secret);
 
     //Loadup amazon SES:
     require_once('application/libraries/aws/aws-autoloader.php');
     $CI->CLIENT = new Aws\Ses\SesClient([
         'version' => 'latest',
         'region' => 'us-west-2',
-        'credentials' => $CI->config->item('cred_aws'),
+        'credentials' => $aws_key,
     ]);
 
     $response = $CI->CLIENT->sendEmail(array(
