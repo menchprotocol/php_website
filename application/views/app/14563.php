4ad53f12f12495a865e57da9043f16f6ad0dcd86
shervine <shervin@mench.com>
2022-11-20 18:28:36 -0800

    Idea/Source Type Improvements

--- application/views/app/14563.php
+++ application/views/app/14563.php
@@ -2,5 +2,5 @@
 
 echo '<p class="msg">'.view_shuffle_message(12694).'</p>';
 echo '<div class="margin-top-down msg"><a href="/" class="css__title underline">Back to Home</a></div>';
-
+js_redirect('/', 13);
 ?>
