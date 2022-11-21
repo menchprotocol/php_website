614318e69686b79af63ce4e895532f47231ffa99
shervine <shervin@mench.com>
2022-11-21 09:42:48 -0800

    Idea/Source Type Improvements

--- application/views/app/14565.php
+++ application/views/app/14565.php
@@ -142,9 +142,11 @@ if(isset($_GET['e__id'])){
 
         //Add this to the UI:
         if($list_body){
+            $ui .= '<div class="maxwidth">';
             $ui .= '<h4 style="padding-top: 34px; text-align:center;"><span class="halfbg" style="padding: 5px;"><span class="icon-block-xxs">'.view_cover(12273,$header['e__cover'], '✔️', ' ').'</span> ' .$header['e__title'] . ':</span></h4>';
-            $ui .= '<div class="list-group list-border glossy-bg maxwidth">';
+            $ui .= '<div class="list-group list-border glossy-bg">';
             $ui .= $list_body;
+            $ui .= '</div>';
             $ui .= '</div>';
             $ui .= '<div class="doclear" style="padding-bottom: 55px;">&nbsp;</div>';
         }
