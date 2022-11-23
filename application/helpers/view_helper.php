181ab423b543473440f6ad06ee93b121c9e54797
shervine <shervin@mench.com>
2022-11-22 19:55:45 -0800

    Idea/Source Type Improvements

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2039,10 +2039,8 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e = fal
         if($superpower_10939 && !$focus_coin && $show_coins){
 
             $ui .= '<div class="coin_coins">';
-            $ui .= '<span class="hideIfEmpty">'.view_coins_i(12274,  $i['i__id']).'</span>';
             $ui .= '<span class="hideIfEmpty">'.view_coins_i(12273,  $i['i__id']).'</span>';
-            //$ui .= '<span class="hideIfEmpty">'.view_coins_i(11019,  $i['i__id']).'</span>';
-            //$ui .= '<span class="hideIfEmpty">'.view_coins_i(13542,  $i['i__id']).'</span>';
+            $ui .= '<span class="hideIfEmpty">'.view_coins_i(12274,  $i['i__id']).'</span>';
             $ui .= '<span class="hideIfEmpty">'.view_coins_i(6255,  $i['i__id']).'</span>';
             $ui .= '</div>';
 
@@ -2343,10 +2341,8 @@ function view_e($x__type, $e, $extra_class = null, $source_of_e = false)
     //Coin Block
     if($superpower_10939 && !$is_app && !$focus_coin){
         $ui .= '<div class="coin_coins">';
-        //$ui .= '<span class="hideIfEmpty">'.view_coins_e(11030,  $e['e__id']).'</span>';
-        //$ui .= '<span class="hideIfEmpty">'.view_coins_e(11029,  $e['e__id']).'</span>';
-        $ui .= '<span class="hideIfEmpty">'.view_coins_e(12274,  $e['e__id']).'</span>';
         $ui .= '<span class="hideIfEmpty">'.view_coins_e(12273,  $e['e__id']).'</span>';
+        $ui .= '<span class="hideIfEmpty">'.view_coins_e(12274,  $e['e__id']).'</span>';
         $ui .= '<span class="hideIfEmpty">'.view_coins_e(6255,  $e['e__id']).'</span>';
         $ui .= '</div>';
     }
---
+++ application/views/app/26560.php
@@ -0,0 +1,53 @@
+<?php
+$member_e = superpower_unlocked();
+$superpower_31000 = $member_e && superpower_active(31000, true);
+
+
+if(isset($_GET['x__id']) && strlen($_GET['x__id']) > 0 && ( $superpower_31000 || (isset($_GET['x__time']) && strlen($_GET['x__time']) > 0))){
+
+    $x = $this->X_model->fetch(array(
+        'x__id' => $_GET['x__id'],
+    ));
+
+    if(!count($x)){
+        die('Invalid Ticket ID URL');
+    }
+    $tr_time = strtotime($x[0]['x__time']);
+
+    if(isset($_GET['x__time']) && $_GET['x__time']==$tr_time){
+        echo 'Verified URL<br />';
+    } else {
+        echo 'Unverified<br />';
+    }
+
+
+
+
+
+    if($superpower_31000){
+        echo 'CECKED IN 1 TICKET<br />';
+        echo 'CECKED IN OTHER 2 IN GROUP?<br />';
+    } else {
+        echo 'Unverified<br />';
+
+    }
+
+
+    $url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-26560?x__id='.$x[0]['x__id'].'&x__time='.$tr_time;
+    echo $url;
+    echo '<hr />';
+
+    echo(generateQR($url));
+
+} else {
+
+    if($superpower_31000){
+
+        //Show list of recent tickets sold.
+
+    } else {
+        echo 'Missing ticket ID & timestamp. Make sure to click on the link that was emailed to you to manage your ticket.';
+    }
+
+
+}
