ef0fbffe3e4972a6f1ee12211555d19f7a45d4fc
shervine <shervin@mench.com>
2022-11-22 16:37:31 -0800

    Idea/Source Type Improvements

--- application/views/app/30840.php
+++ application/views/app/30840.php
@@ -9,9 +9,16 @@ if(isset($_GET['x__id']) && strlen($_GET['x__id']) > 0){
     if(!count($x)){
         die('Invalid Ticket ID URL');
     }
+    $tr_time = strtotime($x[0]['x__time']);
+
+    if(isset($_GET['x__time']) && $_GET['x__time']==$tr_time){
+        echo 'Verified URL<br />';
+    } else {
+        echo 'Unverified<br />';
+    }
 
     $member_e = superpower_unlocked();
-    $url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-30840?x__id='.$x[0]['x__id'].'&x__time='.$x[0]['x__time'];
+    $url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-30840?x__id='.$x[0]['x__id'].'&x__time='.$tr_time;
     echo $url;
     echo '<hr />';
 
