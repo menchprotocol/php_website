b7a969eb6388dd8aad04760df52bea122c3d1160
shervine <shervin@mench.com>
2022-11-22 20:13:50 -0800

    Idea/Source Type Improvements

--- application/views/app/26560.php
+++ application/views/app/26560.php
@@ -10,44 +10,43 @@ if($superpower_31000 || (isset($_GET['x__id']) && strlen($_GET['x__id']) > 0 && 
         $x = $this->X_model->fetch(array(
             'x__id' => $_GET['x__id'],
         ));
+
+        if(!count($x)){
+
+            if($superpower_31000){
+                //Show list of recent tickets sold.
+
+            } else {
+                die('Invalid Ticket ID URL');
+            }
+
+        } else {
+
+            //FOcus on a specific Ticket:
+            $tr_time = strtotime($x[0]['x__time']);
+
+            if(isset($_GET['x__time']) && $_GET['x__time']==$tr_time){
+                echo 'Verified URL<br />';
+            } else {
+                echo 'Unverified<br />';
+            }
+
+            if($superpower_31000){
+                echo 'CECKED IN 1 TICKET<br />';
+                echo 'CECKED IN OTHER 2 IN GROUP?<br />';
+            } else {
+                echo 'Unverified<br />';
+
+            }
+
+            $url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-26560?x__id='.$x[0]['x__id'].'&x__time='.$tr_time;
+            echo $url;
+            echo '<hr />';
+
+            echo(generateQR($url));
+
+        }
     }
-
-    if(isset($_GET['x__id']) && !count($x)){
-
-        if($superpower_31000){
-            //Show list of recent tickets sold.
-
-        } else {
-            die('Invalid Ticket ID URL');
-        }
-
-    } else {
-
-        //FOcus on a specific Ticket:
-        $tr_time = strtotime($x[0]['x__time']);
-
-        if(isset($_GET['x__time']) && $_GET['x__time']==$tr_time){
-            echo 'Verified URL<br />';
-        } else {
-            echo 'Unverified<br />';
-        }
-
-        if($superpower_31000){
-            echo 'CECKED IN 1 TICKET<br />';
-            echo 'CECKED IN OTHER 2 IN GROUP?<br />';
-        } else {
-            echo 'Unverified<br />';
-
-        }
-
-        $url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-26560?x__id='.$x[0]['x__id'].'&x__time='.$tr_time;
-        echo $url;
-        echo '<hr />';
-
-        echo(generateQR($url));
-
-    }
-
 
 }
 
