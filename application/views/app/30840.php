6befc7b39252978bcfc30bc9414c1b2b5f6fc1d0
shervine <shervin@mench.com>
2022-11-22 21:24:46 -0800

    Idea/Source Type Improvements

--- application/views/app/30840.php
+++ application/views/app/30840.php
@@ -1,14 +1,3 @@
-
-<div style="position: fixed; z-index: -99; width: 100%; height: 100%">
-    <iframe
-        frameborder="0"
-        height="100%"
-        width="100%"
-        src="https://youtube.com/embed/ybiXR2WCFQ?autoplay=1&controls=0&showinfo=0&autohide=1"
-    >
-    </iframe>
-</div>
-
 <?php
 
 
