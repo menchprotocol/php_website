04bf0f9aa711f758024fc859bb2060ce1e3f2f47
shervine <shervin@mench.com>
2022-11-22 21:23:44 -0800

    Idea/Source Type Improvements

--- application/views/app/30840.php
+++ application/views/app/30840.php
@@ -1,4 +1,16 @@
+
+<div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+    <iframe
+        frameborder="0"
+        height="100%"
+        width="100%"
+        src="https://youtube.com/embed/ybiXR2WCFQ?autoplay=1&controls=0&showinfo=0&autohide=1"
+    >
+    </iframe>
+</div>
+
 <?php
+
 
 if(isset($_GET['x__id']) && strlen($_GET['x__id']) > 0){
 
