30b7e94a853d322189ba0dffa9ed41f0e3913ca5
shervine <shervin@mench.com>
2022-11-24 22:24:36 -0800

    Idea/Source Type Improvements

---
+++ application/views/app/28917.php
@@ -0,0 +1,12 @@
+<?php
+
+
+foreach($this->config->item('e___28917') as $x__type => $m) {
+    if(isset($m['m__message']) && intval($m['m__message'])>0){
+
+        echo $m['m__title'].': '.intval($m['m__message']).' Days<hr />';
+
+        //Count Members, and any who are eligible to come back as a subscriber?
+
+    }
+}
