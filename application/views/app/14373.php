789996ae441221d238f6d067b252f0c5faacf099
shervine <shervin@mench.com>
2022-11-28 03:22:02 -0800

    The Big Dump fixes

--- application/views/app/14373.php
+++ application/views/app/14373.php
@@ -7,36 +7,8 @@ if(!count($website_terms)){
 }
 
 foreach($website_terms as $i_item) {
-
-    //MESSAGES
-    foreach($this->X_model->fetch(array(
-        'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-        'x__type' => 4231, //IDEA NOTES Messages
-        'x__right' => $i_item['i__id'],
-    ), array(), 0, 0, array('x__spectrum' => 'ASC')) as $x) {
-        echo $this->X_model->message_view( $x['x__message'], true);
-    }
-
-    //Next level Ideas:
-    foreach($is_next = $this->X_model->fetch(array(
-        'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-        'i__type IN (' . join(',', $this->config->item('n___7355')) . ')' => null, //PUBLIC
-        'x__type IN (' . join(',', $this->config->item('n___12840')) . ')' => null, //IDEA LINKS TWO-WAY
-        'x__left' => $i_item['i__id'],
-    ), array('x__right'), 0, 0, array('x__spectrum' => 'ASC')) as $i){
-
-        echo '<h2>'.$i['i__title'].'</h2>';
-
-        //MESSAGES
-        foreach($this->X_model->fetch(array(
-            'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type' => 4231, //IDEA NOTES Messages
-            'x__right' => $i['i__id'],
-        ), array(), 0, 0, array('x__spectrum' => 'ASC')) as $x) {
-            echo $this->X_model->message_view( $x['x__message'], true);
-        }
-
-    }
+    js_redirect('/:'.$i_item['i__id'], 13);
+    break;
 }
 
 
