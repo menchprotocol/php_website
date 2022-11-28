7d79d9662b9eaef16bc426c841f0ab1f6b2a24d4
shervine <shervin@mench.com>
2022-11-28 02:59:24 -0800

    The Big Dump fixes

--- application/views/app/28903.php
+++ application/views/app/28903.php
@@ -40,7 +40,7 @@ if(!isset($_GET['i__id'])){
             echo '<h3 style="margin:13px 0 21px; padding-left:5px; font-size:1.15em;"><a href="javascript:void(0);" onclick="$(\'.i_msg_'.$i['i__id'].'\').toggleClass(\'hidden\');" class="inner-content doblock css__title">' . $i['i__title'] . '</a></h3>';
 
             //MESSAGES
-            echo '<div class="i_msg_'.$i['i__id'].' hidden" style="padding:5px 5px 13px 21px;">';
+            echo '<div class="i_msg_'.$i['i__id'].' hidden" style="padding:5px;">';
             foreach ($this->X_model->fetch(array(
                 'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
                 'x__type' => 4231, //IDEA NOTES Messages
