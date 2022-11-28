309ae9c7b3e46a33c4e661d3eda81a6167feed2e
shervine <shervin@mench.com>
2022-11-28 03:24:04 -0800

    The Big Dump fixes

--- application/views/app/14373.php
+++ application/views/app/14373.php
@@ -1,5 +1,7 @@
 <?php
 
+//Offers a permanent /-14373 Website URL for Terms which redirects to the Website Term...
+
 $website_terms = $this->E_model->scissor_i(website_setting(0), 14373); //Website Terms
 if(!count($website_terms)){
     //Default terms:
