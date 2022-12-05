ae496f3d32ae9b4e389134ef96507fcdc312b791
shervine <shervin@mench.com>
2022-12-05 11:49:03 -0800

    UI Reframing

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1499,7 +1499,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e = fal
                 } elseif($e__id==12589){
                     //Mass Apply
                     $action_buttons .= '<a href="javascript:void(0);" onclick="apply_all_load(12589,'.$i['i__id'].')" class="dropdown-item css__title">'.$anchor.'</a>';
-                } elseif($e__id==30795 && !$discovery_mode){
+                } elseif($e__id==30795 && !$discovery_mode && $superpower_10939){
                     //Discovery Mode
                     $action_buttons .= '<a href="/'.$i['i__id'].'" class="dropdown-item css__title">'.$anchor.'</a>';
                 } elseif($e__id==6155 && isset($i['x__id']) && in_array($x__type, $CI->config->item('n___6155')) && ($e_of_i || $superpower_10939)){
