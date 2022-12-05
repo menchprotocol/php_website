fed72de9b0b9f62f7beba52757d85176bfee92bc
shervine <shervin@mench.com>
2022-12-04 20:02:43 -0800

    UI Reframing

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -957,23 +957,10 @@ function view_coins_i($x__type, $i__id, $page_num = 0, $append_coin_icon = true)
     } else {
 
         $e___11035 = $CI->config->item('e___11035'); //COINS
-
-        if(0 && $x__type==12273){
-
-            $coins1 = view_coins_i(11019, $i__id, 0, false);
-            $coins2 = view_coins_i(12273, $i__id, 0, false);
-            $count_query = $coins1 + $coins2;
-            $visual_counter = view_number($count_query);
-            $title_desc = number_format($coins1, 0).' '.$e___11035[11019]['m__title'].' & '.number_format($coins2, 0).' '.$e___11035[12273]['m__title'];
-
-        } else {
-
-            $query = $CI->X_model->fetch($query_filters, $join_objects, 1, 0, array(), 'COUNT(x__id) as totals');
-            $count_query = $query[0]['totals'];
-            $visual_counter = view_number($count_query);
-            $title_desc = number_format($count_query, 0).( isset($e___11035[$x__type]['m__title']) ? ' '.$e___11035[$x__type]['m__title'] : '' );
-
-        }
+        $query = $CI->X_model->fetch($query_filters, $join_objects, 1, 0, array(), 'COUNT(x__id) as totals');
+        $count_query = $query[0]['totals'];
+        $visual_counter = view_number($count_query);
+        $title_desc = number_format($count_query, 0).( isset($e___11035[$x__type]['m__title']) ? ' '.$e___11035[$x__type]['m__title'] : '' );
 
         if($append_coin_icon){
 
