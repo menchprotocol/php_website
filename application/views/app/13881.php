110befb9cbd28c4224a43505f2b8be297bb60d3a
shervine <shervin@mench.com>
2022-12-06 16:31:02 -0800

    UI Reframing

--- application/views/app/13881.php
+++ application/views/app/13881.php
@@ -1,11 +1,88 @@
 <?php
 
+$guide = 'First Column Full Name, 2nd Column Email, 3rd Column Phone Number (If any)... Also no headline row, start with data! ';
+
 if(isset($_POST['import_sources'])){
-    echo strlen($_POST['import_sources']);
+
+    echo 'Begind Processing Import Data:<hr />';
+
+    //Guide:
+    $stats = array(
+        'new_lines' => 0,
+        'unique_lines' => 0,
+        'dup_index' => array(),
+    );
+
+    foreach(explode(PHP_EOL, $_POST['skuList']) as $count => $new_line){
+
+        $e__id = 0; //We must first identify this at the first volumn using e__title to them import the rest
+
+        //Go through each column of this new line:
+        $tabs = preg_split('/\s+/', $new_line);
+
+        print_r($tabs);
+        break;
+
+
+        $md5 = md5($tabs[1]);
+        if(!isset($stats['dup_index'][$md5])){
+
+            //This is new:
+            $stats['unique_lines']++;
+
+            //Import into DB & map ID for next columns:
+            $stats['dup_index'][$md5] = 1;
+
+            /*
+
+            $focus_e = $this->E_model->create(array(
+                'e__title' => $e__title_validate['e__title_clean'],
+                'e__cover' => $e__cover,
+                'e__type' => 6181, //Private
+            ), true, $x__source);
+
+             * */
+
+        }
+
+
+
+        foreach( as $count=>$column_data){
+
+            if(!isset($stats['commands'][$count])){
+                //First line is the command line:
+                echo 'COmmand Column '.$count.' is: '.$column_data.'<hr />';
+                $stats['commands'][$count] = $column_data;
+                continue;
+            }
+
+            //Ready to apply command:
+            $stats['new_lines']++;
+
+            if($stats['commands'][$count]=='e__title'){
+
+
+
+            } elseif(substr($stats['commands'][$count], 0, 1)=='@' && is_numeric(substr($stats['commands'][$count], 1))){
+
+                $follow_e = intval(substr($stats['commands'][$count], 1));
+                //Add as follower to this source based on the content:
+
+
+            }
+
+
+        }
+    }
+
+    print_r($stats);
+
     echo '<hr />';
+
 }
 
+echo $guide;
 echo '<form method="POST" action="">';
-echo '<textarea class="border padded" placeholder="Paste List Here" name="import_sources"></textarea>';
-echo '<button type="submit" class="btn btn-lrg top-margin">UPDATE</button>';
+echo '<textarea class="border padded" placeholder="Paste List Here" name="import_sources" style="width: 100%; height: 200px;"></textarea>';
+echo '<button type="submit" class="btn btn-lrg top-margin"><i class="fas fa-plus-circle zq12274"></i> IMPORT</button>';
 echo '</form>';
