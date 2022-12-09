0bebdccb34b519b37cc832f38fa8678341bebc9f
shervine <shervin@mench.com>
2022-12-09 08:08:22 -0800

    UI Reframing

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1464,7 +1464,7 @@ function view_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e = fal
                 } elseif($e__id==30795 && !$discovery_mode && $superpower_10939){
                     //Discovery Mode
                     $action_buttons .= '<a href="/'.$i['i__id'].'" class="dropdown-item css__title">'.$anchor.'</a>';
-                } elseif($e__id==6155 && $x__id && in_array($x__type, $CI->config->item('n___6155')) && ($e_of_i || $superpower_10939 || ( $member_e && $member_e['e__id']==$i['x__source'] ))){
+                } elseif($e__id==6155 && $x__id && !in_array($i['x__type'], $CI->config->item('n___31776')) && ($e_of_i || $superpower_10939 || ( $member_e && $member_e['e__id']==$i['x__source'] ))){
                     //Remove/Unlink
                     $action_buttons .= '<a href="javascript:void(0);" class="dropdown-item css__title x_remove" i__id="'.$i['i__id'].'" x__id="'.$x__id.'">'.$anchor.'</a>';
                 } elseif($e__id==30873){
