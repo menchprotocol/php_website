cda641e3bfecf137109a3b442eca5a27e03b7abe
shervine <shervin@mench.com>
2022-12-10 19:51:19 -0800

    Source Recursive Followers

--- application/views/app/27004.php
+++ application/views/app/27004.php
@@ -68,7 +68,7 @@ if(!isset($_GET['e__id']) || $_GET['e__id']<1){
         $total_quantity = 0;
 
         //$member_e
-        foreach($this->E_model->recursive_es($member_e['e__id'], array(27004), array(), array('x__id' => 'DESC')) as $e){
+        foreach($this->E_model->recursive_es($member_e['e__id'], array(27004), array(), array('x__id' => 'ASC')) as $e){
 
             //See if this Source has any paymen ideas:
             $payment_is = array();
@@ -112,7 +112,7 @@ if(!isset($_GET['e__id']) || $_GET['e__id']<1){
         }
 
         //Totals
-        echo '<tr class="css__title" style="font-weight: 1.3em;">';
+        echo '<tr class="css__title" style="font-size: 1.3em;">';
         echo '<td>Totals</td>';
         echo '<td style="text-align: right;">'.$total_tickets.'</td>';
         echo '<td style="text-align: right;">'.number_format($total_quantity, 0).'</td>';
