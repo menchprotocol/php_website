6c331ee674c0a6c47c59530e7d0b7fa9ffc4a772
shervine <shervin@mench.com>
2022-12-18 19:14:26 -0800

    Source Recursive Followers

--- application/views/app/31079.php
+++ application/views/app/31079.php
@@ -4,12 +4,15 @@ $_GET['i__id'] = 19120; //Tech House 2023 New Years Celebration
 
 echo '<div class="list-group">';
 foreach(view_coins_i(11019, $_GET['i__id'], 1, false) as $referral_i){
+    $views = view_coins_i(6255, $referral_i['i__id'], 0, false);
+    $income = 45;
+
     echo '<a href="/~'.$referral_i['i__id'].'" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">'.$referral_i['i__title'].'</h5>
       <small></small>
     </div>
-    <p class="mb-1"><span class="icon-block"><i class="fas fa-dollar"></i></span> 42.1 | <span class="icon-block"><i class="fas fa-eye"></i></span> '.view_coins_i(6255, $referral_i['i__id'], 0, false).'</p>
+    <p class="mb-1"><span class="icon-block"><i class="fas fa-eye"></i></span> '.$views.( $income>0 ? : ' | <span class="icon-block"><i class="fas fa-dollar-sign"></i></span> '.$income ).'</p>
   </a>';
 }
 
