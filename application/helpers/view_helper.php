8638e16d5cb2177b7af6a7f9036f22d615c6cdc2
shervine <shervin@mench.com>
2022-12-19 13:47:31 -0800

    Source Recursive Followers

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1575,56 +1575,17 @@ function view_featured_source($x__source, $x){
     return '<div class="source-info">'
         . '<span class="icon-block">'.view_cover(12274,$x['e__cover'], true) . '</span>'
         . '<span>'.$x['e__title'] . '</span>'
-        . '<div class="payment_box">'. ( $x['e__id']==30976 /* Hack: Location loads with Google Maps */ ? '<a href="https://www.google.com/maps/search/'.urlencode($x['x__message']).'" target="_blank" style="text-decoration:underline;" class="sub_note css__title">'.$x['x__message'].'</a><div id="load_map"></div><script type="text/javascript">
+        . '<div class="payment_box">'. ( $x['e__id']==30976 /* Hack: Location loads with Google Maps */ ? '<a href="https://www.google.com/maps/search/'.urlencode($x['x__message']).'" target="_blank" style="text-decoration:underline;" class="sub_note css__title">'.$x['x__message'].'</a><div id="load_map" style="width:100%;height:200px;"></div><script type="text/javascript">
 
 $(document).ready(function () {
-    let map;
-    let service;
-    let infowindow;
-    
-    
-function initMap() {
-
-      const sydney = new google.maps.LatLng(-33.867, 151.195);
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("load_map"), {
-          center: sydney,
-    zoom: 15,
-  });
-
-  const request = {
-    query: "'.$x['x__message'].'",
-    fields: ["name", "geometry"],
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+    function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("load_map"),mapProp);
 }
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-
-
-
-    window.initMap = initMap;
 });
 
 </script>' : '<div class="sub_note css__title">'.nl2br($x['x__message']).'</div>' ) . '</div>'
