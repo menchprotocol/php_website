f39b6d5b3ad0b57c2abe848bdf5524eb8afb6fc2
shervine <shervin@mench.com>
2022-12-18 19:22:57 -0800

    Source Recursive Followers

--- application/views/app/31079.php
+++ application/views/app/31079.php
@@ -10,17 +10,17 @@ foreach(view_coins_i(11019, $_GET['i__id'], 1, false) as $referral_i){
         'x__type' => 7610, //MEMBER VIEWED DISCOVERY
         'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
     ), array(), 0, 0, array(), 'COUNT(x__id) as total_count');
-    $income = 45;
+    $income = 0;
 
     echo '<a href="/~'.$referral_i['i__id'].'" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">'.$referral_i['i__title'].'</h5>
-      <small></small>
+      <small>' . view_time_difference(strtotime($referral_i['x__time'])) . ' Ago <i class="fas fa-chevron-right"></i></small>
     </div>
     <p class="mb-1">'.
-        '<span class="data-block"><span class="icon-block"><i class="fal fa-eye"></i></span> '.$x_count[0]['total_count'].'</span>'.
-        '<span class="data-block"><span class="icon-block"><i class="fal fa-play"></i></span> '.$plays.'</span>'.
-        '<span class="data-block"><span class="icon-block"><i class="fal fa-dollar-sign"></i></span> '.$income.'</span>'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-eye"></i></span> '.$x_count[0]['total_count'].'</span>'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-play"></i></span> '.$plays.'</span>'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-dollar-sign"></i></span> '.$income.'</span>'.
         '</p>
   </a>';
 }
