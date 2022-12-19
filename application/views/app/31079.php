4bbabe9ac0d798a4b4ea1b33fe63f5722e9ebd5a
shervine <shervin@mench.com>
2022-12-18 20:09:21 -0800

    Source Recursive Followers

--- application/views/app/31079.php
+++ application/views/app/31079.php
@@ -11,6 +11,29 @@ if(count($is)){
     echo '<h2 style="text-align: center; padding-bottom:34px; margin-top: -55px; font-size: 1.4em;"><a href="/~'.$is[0]['i__id'].'">' . $is[0]['i__title'] . '</a></h2>';
 
     echo '<div class="list-group">';
+
+
+    $plays = view_coins_i(6255, $is[0]['i__id'], 0, false);
+    $x_count = $this->X_model->fetch(array(
+        'x__right' => $is[0]['i__id'],
+        'x__type' => 7610, //MEMBER VIEWED DISCOVERY
+        'x__status IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+    ), array(), 0, 0, array(), 'COUNT(x__id) as total_count');
+    $income = 0;
+
+    echo '<a href="/~'.$is[0]['i__id'].'" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">'.$is[0]['i__title'].'<span style="font-size: 0.75em; color: #999999; padding-left: 10px;">/'.$is[0]['i__id'].'</span></h5>
+      <small></small>
+    </div>
+    <p class="mb-1">'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-eye"></i></span> '.$x_count[0]['total_count'].'</span>'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-play"></i></span> '.$plays.'</span>'.
+        '<span class="data-block"><span class="icon-block-xs"><i class="fal fa-dollar-sign"></i></span> '.$income.'</span>'.
+        '</p>
+  </a><br /><br />';
+
+
     foreach(view_coins_i(11019, $_GET['i__id'], 1, false) as $referral_i){
         $plays = view_coins_i(6255, $referral_i['i__id'], 0, false);
         $x_count = $this->X_model->fetch(array(
