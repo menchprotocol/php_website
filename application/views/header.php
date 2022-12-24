5de53da94613947ffc069212006f9a2d9659191d
shervine <shervin@mench.com>
2022-12-24 03:13:47 -0800

    Source Recursive Followers

--- application/views/header.php
+++ application/views/header.php
@@ -343,8 +343,6 @@ if(!$basic_header_footer){
                     $domain_logo = one_two_explode('"','"', $domain_cover);
                     if(filter_var($domain_logo, FILTER_VALIDATE_URL)){
                         $padding_hack = 0; //For URL
-                    } elseif(string_is_icon($domain_logo)){
-                        $padding_hack = 3; //For Icon
                     } else {
                         $padding_hack = 7; //For Emoji
                     }
