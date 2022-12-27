00237b48f1a79098de831978063c22140035769e
shervine <shervin@mench.com>
2022-12-27 02:30:12 -0800

    Source Recursive Followers

--- application/views/global.js
+++ application/views/global.js
@@ -1451,9 +1451,9 @@ function load_tab(x__type){
             counter:$('.headline_body_'+x__type).attr('read-counter'),
             e__id:fetch_val('#focus_id')
         }, function (data) {
-            $('.headline_body_'+x__type).html(data);
+            $('.headline_body_'+x__type).html(data).scrollTop($('.focus-coin').offset().top - 54);
             if(js_n___14686.includes(x__type)){
-                $('html, body').scrollTop($('.focus-coin').offset().top - 54);
+                //$('html, body').scrollTop($('.focus-coin').offset().top - 54);
             }
         });
 
