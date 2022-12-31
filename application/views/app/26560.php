b8964108e5ee2960b68f8c2b328facdbb1dbbaf9
shervine <shervin@mench.com>
2022-12-31 09:57:22 -0800

    Source Recursive Followers

--- application/views/app/26560.php
+++ application/views/app/26560.php
@@ -45,7 +45,7 @@ if(isset($_GET['x__id']) && strlen($_GET['x__id']) > 0 && isset($_GET['x__source
 
         if(count($ticket_checked_in)){
 
-            echo '<div class="msg alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Ticket Scanned by <a href="/@'.$ticket_checked_in[0]['e__id'].'">'.$ticket_checked_in[0]['e__title'].'</a> at '.substr($ticket_checked_in[0]['x__time'], 0, 19).' PST (' . view_time_difference(strtotime($ticket_checked_in[0]['x__time'])) . ' Ago)</div>';
+            echo '<div class="msg alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Ticket Already Checked-In by <a href="/@'.$ticket_checked_in[0]['e__id'].'">'.$ticket_checked_in[0]['e__title'].'</a> <span class="underdot" title="'.substr($ticket_checked_in[0]['x__time'], 0, 19).' PST">' . view_time_difference(strtotime($ticket_checked_in[0]['x__time'])) . ' Ago</span>.</div>';
 
         } else {
 
@@ -76,7 +76,7 @@ if(isset($_GET['x__id']) && strlen($_GET['x__id']) > 0 && isset($_GET['x__source
                         'x__reference' => $x[0]['x__id'],
                     ));
 
-                    echo '<div class="msg alert alert-success" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Successfully checkin for '.$quantity.' Ticket'.view__s($quantity).'</div>';
+                    echo '<div class="msg alert alert-success" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>Successful checkin for '.$quantity.' Ticket'.view__s($quantity).'</div>';
 
                 }
             } else {
