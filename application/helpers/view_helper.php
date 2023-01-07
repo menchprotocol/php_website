b69d937ba63d5d013e2ea30dc6c97b758c09d4b9
shervine <shervin@mench.com>
2023-01-07 11:12:36 -0800

    Source Recursive Followers

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1540,29 +1540,26 @@ function view_i_card($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e 
             }
             if($e__id_bottom_bar==31892 && !$can_click && $member_e && !$focus_coin && !$discovery_mode){
                 $active_bars++;
-                $bottom_bar_ui .= '<td><div class="show-on-hover">';
                 $bottom_bar_ui .= '<span class="hideIfEmpty">';
                 $bottom_bar_ui .= '<a href="'.$href.'" class="right-btn" title="'.$m_bottom_bar['m__title'].'">'.$m_bottom_bar['m__cover'].'</a>';
                 $bottom_bar_ui .= '</span>';
-                $bottom_bar_ui .= '</div></td>';
             } else {
                 $coins_ui = view_coins_i($e__id_bottom_bar,  $i['i__id']);
                 if(strlen($coins_ui)){
                     $active_bars++;
-                    $bottom_bar_ui .= '<td><div class="show-on-hover">';
                     $bottom_bar_ui .= '<span class="hideIfEmpty">';
                     $bottom_bar_ui .= $coins_ui;
                     $bottom_bar_ui .= '</span>';
-                    $bottom_bar_ui .= '</div></td>';
                 }
             }
         }
 
         if($bottom_bar_ui){
-            $ui .= '<table class="coin_coins active_bars_'.$active_bars.'"><tr>';
+            $ui .= '<div class="coin_coins"><div class="show-on-hover">';
             $ui .= $bottom_bar_ui;
-            $ui .= '</tr></table>';
+            $ui .= '</div></div>';
         }
+
     }
 
 
