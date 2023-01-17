63cb15cb49c383b42aa66766f9c15c61ebc85e20
shervine <shervin@mench.com>
2023-01-17 14:51:32 -0800

    Updates

--- application/views/app/6287.php
+++ application/views/app/6287.php
@@ -1,11 +1,12 @@
 <?php
 
-$already_loaded = array();
+$already_loaded = array(6287);
 
 //Show Featured Apps
 echo '<div class="row">';
 foreach($this->X_model->fetch(array(
     'x__up' => 30841, //Featured Apps
+    'x__down NOT IN (' . join(',', $already_loaded) . ')' => null, //SOURCE LINKS
     'x__type IN (' . join(',', $this->config->item('n___4592')) . ')' => null, //SOURCE LINKS
     'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
     'e__privacy IN (' . join(',', $this->config->item('n___7357')) . ')' => null, //PUBLIC
