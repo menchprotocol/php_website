f8be2c1eae7c7e070c08ace35ebcc2b25c4591d7
shervine <shervin@mench.com>
2023-01-29 01:07:49 -0800

    Updates

--- application/controllers/E.php
+++ application/controllers/E.php
@@ -122,7 +122,8 @@ class E extends CI_Controller
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
                 foreach(view_covers_e($_POST['x__type'], $_POST['e__id'], 1, false) as $source_e) {
                     if(isset($source_e['e__id'])){
-                        $ui .= view_card('/@'.$source_e['e__id'], $source_e['e__id']==$current_e, $e___4593[$source_e['x__type']]['m__cover'], $e___6177[$source_e['e__privacy']]['m__cover'], view_cover(12274,$source_e['e__cover'], true), $source_e['e__title'], preview_x__message($source_e['x__message'],$source_e['x__type']));
+                        //$ui .= view_card('/@'.$source_e['e__id'], $source_e['e__id']==$current_e, $e___4593[$source_e['x__type']]['m__cover'], $e___6177[$source_e['e__privacy']]['m__cover'], view_cover(12274,$source_e['e__cover'], true), $source_e['e__title'], preview_x__message($source_e['x__message'],$source_e['x__type']));
+                        $ui .= view_card_e(31970, $source_e);
                         $listed_items++;
                     }
                 }
@@ -136,7 +137,8 @@ class E extends CI_Controller
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
                 foreach(view_covers_e($_POST['x__type'], $_POST['e__id'], 1, false) as $next_i) {
                     if(isset($next_i['i__id'])){
-                        $ui .= view_card('/i/i_go/'.$next_i['i__id'], $next_i['i__id']==$current_i, $e___4593[$next_i['x__type']]['m__cover'], $e___31004[$next_i['i__privacy']]['m__cover'], $e___4737[$next_i['i__type']]['m__cover'], view_i_title($next_i), preview_x__message($next_i['x__message'],$next_i['x__type']));
+                        //$ui .= view_card('/i/i_go/'.$next_i['i__id'], $next_i['i__id']==$current_i, $e___4593[$next_i['x__type']]['m__cover'], $e___31004[$next_i['i__privacy']]['m__cover'], $e___4737[$next_i['i__type']]['m__cover'], view_i_title($next_i), preview_x__message($next_i['x__message'],$next_i['x__type']));
+                        $ui .= view_card_i(31970, 0, null, $next_i);
                         $listed_items++;
                     }
                 }
