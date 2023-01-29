cff984d303a2ecf21f481b7343203b94561d02b1
shervine <shervin@mench.com>
2023-01-29 01:16:58 -0800

    Updates

--- application/controllers/E.php
+++ application/controllers/E.php
@@ -120,6 +120,8 @@ class E extends CI_Controller
                 $current_e = ( substr($_POST['first_segment'], 0, 1)=='@' ? intval(substr($_POST['first_segment'], 1)) : 0 );
                 $e___6177 = $this->config->item('e___6177'); //Source Status
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
+
+                $ui .= '<div class="row justify-content">';
                 foreach(view_covers_e($_POST['x__type'], $_POST['e__id'], 1, false) as $source_e) {
                     if(isset($source_e['e__id'])){
                         //$ui .= view_card('/@'.$source_e['e__id'], $source_e['e__id']==$current_e, $e___4593[$source_e['x__type']]['m__cover'], $e___6177[$source_e['e__privacy']]['m__cover'], view_cover(12274,$source_e['e__cover'], true), $source_e['e__title'], preview_x__message($source_e['x__message'],$source_e['x__type']));
@@ -127,6 +129,8 @@ class E extends CI_Controller
                         $listed_items++;
                     }
                 }
+                $ui .= '</div>';
+
 
             } elseif($_POST['x__type']==12273 || $_POST['x__type']==6255){
 
@@ -135,6 +139,8 @@ class E extends CI_Controller
                 $e___31004 = $this->config->item('e___31004'); //Idea Status
                 $e___4737 = $this->config->item('e___4737'); //Idea Types
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
+
+                $ui .= '<div class="row justify-content">';
                 foreach(view_covers_e($_POST['x__type'], $_POST['e__id'], 1, false) as $next_i) {
                     if(isset($next_i['i__id'])){
                         //$ui .= view_card('/i/i_go/'.$next_i['i__id'], $next_i['i__id']==$current_i, $e___4593[$next_i['x__type']]['m__cover'], $e___31004[$next_i['i__privacy']]['m__cover'], $e___4737[$next_i['i__type']]['m__cover'], view_i_title($next_i), preview_x__message($next_i['x__message'],$next_i['x__type']));
@@ -142,6 +148,7 @@ class E extends CI_Controller
                         $listed_items++;
                     }
                 }
+                $ui .= '</div>';
 
             }
 
--- application/controllers/I.php
+++ application/controllers/I.php
@@ -250,7 +250,7 @@ class I extends CI_Controller {
                 $e___6177 = $this->config->item('e___6177'); //Source Types
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
                 $current_e = ( substr($_POST['first_segment'], 0, 1)=='@' ? intval(substr($_POST['first_segment'], 1)) : 0 );
-
+                $ui .= '<div class="row justify-content">';
                 foreach(view_covers_i($_POST['x__type'], $_POST['i__id'], 1, false) as $source_e) {
                     if(isset($source_e['e__id'])){
                         //$ui .= view_card('/@'.$source_e['e__id'], $source_e['e__id']==$current_e, $e___4593[$source_e['x__type']]['m__cover'], $e___6177[$source_e['e__privacy']]['m__cover'], view_cover(12274,$source_e['e__cover'], true), $source_e['e__title'], preview_x__message($source_e['x__message'],$source_e['x__type']));
@@ -258,6 +258,7 @@ class I extends CI_Controller {
                         $listed_items++;
                     }
                 }
+                $ui .= '</div>';
 
             } elseif($_POST['x__type']==11019 || $_POST['x__type']==12273){
 
@@ -267,6 +268,8 @@ class I extends CI_Controller {
                 $e___4593 = $this->config->item('e___4593'); //Transaction Types
                 $superpower_10939 = superpower_active(10939, true);
                 $current_i = ( substr($_POST['first_segment'], 0, 1)=='~' ? intval(substr($_POST['first_segment'], 1)) : 0 );
+
+                $ui .= '<div class="row justify-content">';
                 foreach(view_covers_i($_POST['x__type'], $_POST['i__id'], 1, false) as $next_i) {
                     if(isset($next_i['i__id'])){
                         //$ui .= view_card('/~'.$next_i['i__id'], $next_i['i__id']==$current_i, $e___4593[$next_i['x__type']]['m__cover'], $e___31004[$next_i['i__privacy']]['m__cover'], $e___4737[$next_i['i__type']]['m__cover'], view_i_title($next_i), preview_x__message($next_i['x__message'],$next_i['x__type']));
@@ -274,6 +277,8 @@ class I extends CI_Controller {
                         $listed_items++;
                     }
                 }
+                $ui .= '</div>';
+
 
             }
 
