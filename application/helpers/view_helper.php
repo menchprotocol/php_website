d06f2c2b7a64cd0094d738556d33c97be85a3fc0
shervine <shervin@mench.com>
2023-01-29 05:06:48 -0800

    Update

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1255,6 +1255,7 @@ function view_card_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e 
     $linkbar_visible = in_array($x__type, $CI->config->item('n___20410'));
     $focus_cover = in_array($x__type, $CI->config->item('n___12149')); //NODE COIN
     $has_self = $member_e && $focus_e && $member_e['e__id']==$focus_e['e__id'];
+    $self_link = isset($i['x__creator']) && $i['x__creator']==$member_e['e__id'];
 
     if(!$focus_e){
         $focus_e = $member_e;
@@ -1438,7 +1439,7 @@ function view_card_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e 
                         //Delete Permanently
                         $action_buttons .= '<a href="javascript:void();" current-selected="'.$i['i__privacy'].'" onclick="update_dropdown(31004, 6182, '.$i['i__id'].', '.$x__id.', 0)" class="dropdown-item dropi_31004_'.$i['i__id'].'_'.$x__id.' css__title optiond_6182_'.$i['i__id'].'_'.$x__id.'">'.$anchor.'</a>';
 
-                    } elseif($e__id_dropdown==26560 && isset($i['x__type']) && in_array($i['x__type'], $CI->config->item('n___32014'))){
+                    } elseif($e__id_dropdown==26560 && isset($i['x__type']) && in_array($i['x__type'], $CI->config->item('n___32014')) && $self_link){
 
                         //Ticket Details
                         $action_buttons .= '<a href="/-26560?x__id='.$i['x__id'].'&x__creator='.$i['x__creator'].'" class="dropdown-item css__title">'.$anchor.'</a>';
@@ -1487,7 +1488,12 @@ function view_card_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e 
 
     //Coin Cover
     $ui .= '<div class="cover-wrapper cover_wrapper12273">';
-    $ui .= ( !$can_click ? '<div' : '<a href="'.$href.'"' ).' class="coinType12273 black-background-obs cover-link">';
+    $ui .= ( !$can_click ? '<div' : '<a href="'.$href.'"' ).' class="'.( $discovery_mode && $link_type_id==6255 && $self_link && count($CI->X_model->fetch(array(
+            'x__privacy IN (' . join(',', $CI->config->item('n___7359')) . ')' => null, //PUBLIC
+            'x__type IN (' . join(',', $CI->config->item('n___6255')) . ')' => null, //DISCOVERIES
+            'x__creator' => $member_e['e__id'],
+            'x__left' => $i['i__id'],
+        ))) ? 'coinType6255' : 'coinType12273' ).' black-background-obs cover-link">';
     $ui .= ( !$can_click ? '</div>' : '</a>' );
     $ui .= '</div>';
 
@@ -1880,7 +1886,7 @@ function view_card_e($x__type, $e, $extra_class = null)
                         //Reset Alphabetic order
                         $action_buttons .= '<a href="javascript:void(0);" onclick="sort_e_handle_reset()" class="dropdown-item css__title">'.$anchor.'</a>';
 
-                    } elseif($e__id_dropdown==26560 && isset($e['x__type']) && in_array($e['x__type'], $CI->config->item('n___32014'))){
+                    } elseif($e__id_dropdown==26560 && isset($e['x__type']) && in_array($e['x__type'], $CI->config->item('n___32014')) && $e['x__creator']==$member_e['e__id']){
 
                         //Ticket Details
                         $action_buttons .= '<a href="/-26560?x__id='.$e['x__id'].'&x__creator='.$e['x__creator'].'" class="dropdown-item css__title">'.$anchor.'</a>';
