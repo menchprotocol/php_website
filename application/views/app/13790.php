89c686180a85b2665838cfcf6d1d530cc56e3433
shervine <shervin@mench.com>
2023-01-30 22:10:12 -0800

    Update

--- application/views/app/13790.php
+++ application/views/app/13790.php
@@ -218,7 +218,7 @@ if(strlen($_GET['i__id'])){
 
 
         $plus_info = ' '.( $this_quantity > 0 ? '+'.$this_quantity : '' );
-        $body_content .= '<td>'.( strlen($name) ? $name.$plus_info.' [<a href="/@'.$x['e__id'].'" style="font-weight:bold;">'.$x['e__title'].'</a>]' : $x['e__title'].$plus_info ).'</td>';
+        $body_content .= '<td>'.( strlen($name) ? $name.$plus_info.' [<a href="/@'.$x['e__id'].'" style="font-weight:bold;">'.$x['e__title'].'</a>]' : '<a href="/@'.$x['e__id'].'" style="font-weight:bold;">'.$x['e__title'].'</a>'.$plus_info ).'</td>';
 
 
 
