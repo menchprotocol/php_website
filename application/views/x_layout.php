afe3e5588ba352fa802e572fe1ab760c17d07b29
shervine <shervin@mench.com>
2023-01-30 23:42:26 -0800

    Update

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -240,8 +240,8 @@ if($top_i__id){
     $go_next_url = ( $top_completed ? '/x/x_completed_next/' : '/x/x_next/' ) . $top_i__id . '/' . $i['i__id'];
 
     if(isset($tree_progress['fixed_completed_percentage']) && $tree_progress['fixed_completed_percentage']>0){
-        echo '<div class="progress">
-<div class="progress-bar progress-bar-striped bg-danger" role="progressbar" title="'.$tree_progress['fixed_discovered'].' / '.$tree_progress['fixed_total'].' Ideas Discovered" style="width: '.$tree_progress['fixed_completed_percentage'].'%" aria-valuenow="'.$tree_progress['fixed_completed_percentage'].'" aria-valuemin="0" aria-valuemax="100"></div>
+        echo '<div class="progress" style="height: 5px; margin: -21px 0 13px;">
+<div class="progress-bar bg6255" role="progressbar" title="'.$tree_progress['fixed_discovered'].' / '.$tree_progress['fixed_total'].' Ideas Discovered" style="width: '.$tree_progress['fixed_completed_percentage'].'%" aria-valuenow="'.$tree_progress['fixed_completed_percentage'].'" aria-valuemin="0" aria-valuemax="100"></div>
 </div>';
     }
 
