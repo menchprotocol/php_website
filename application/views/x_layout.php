033c0fc40d4983c04aa6982a5e1170a99598ab6d
shervine <shervin@mench.com>
2023-02-04 20:11:30 -0800

    Update

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -497,7 +497,7 @@ if($top_i__id) {
                 'x__right' => $i['i__id'],
                 'x__up' => 30615, //Is Refundable
             )))){
-                echo 'Final sale: no refunds. ';
+                echo 'Final sale. ';
             }
 
             echo 'You Do Not need to create a Paypal account: You can pay by only entering your credit card details & checkout as a guest. Once paid, click "<span style="color: #990000;">Return to Merchant</span>" to continue back here. By paying you agree to our <a href="/-14373" target="_blank"><u>Terms of Use</u></a>.';
