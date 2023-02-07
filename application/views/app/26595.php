276cb580a5652048feb96dafd2c49496fe0acb5e
shervine <shervin@mench.com>
2023-02-07 13:41:39 -0800

    Update

--- application/views/app/26595.php
+++ application/views/app/26595.php
@@ -8,13 +8,8 @@ foreach($this->X_model->fetch(array(
     'x__weight' => 0,
 ), array(), 0) as $fix){
     $x__metadata = unserialize($fix['x__metadata']);
-    if(isset($x__metadata['quantity']) && $x__metadata['quantity']>0) {
-        //$res = $this->X_model->update($fix['x__id'], array('x__weight' => intval($x__metadata['quantity'])));
-        echo '<div>-'.intval($x__metadata['quantity']).' ('.$res.')</div>';
-    } else {
-        echo '<div>ERROR:'.$fix['x__id'].'</div>';
-        //$this->db->query("DELETE FROM table__x WHERE x__id=".$fix['x__id'].";");
-    }
+    $res = $this->X_model->update($fix['x__id'], array('x__weight' => -1 ));
+    echo '<div>-'.intval($x__metadata['quantity']).' ('.$res.')</div>';
 }
 
 //Called when the paypal payment is complete:
@@ -57,7 +52,6 @@ if(isset($_POST['payment_status']) && ($_POST['payment_status']=='Refunded' || $
             //Find original payment:
             $original_payment = $this->X_model->fetch(array(
                 'x__type' => 26595,
-                'x__weight' => (-1 * intval($_POST['quantity'])),
                 'x__creator' => $x__creator,
                 'x__left' => $next_is[0]['i__id'],
             ));
@@ -65,6 +59,7 @@ if(isset($_POST['payment_status']) && ($_POST['payment_status']=='Refunded' || $
             //Log Refund:
             $new_x = $this->X_model->mark_complete($top_i__id, $next_is[0], array(
                 'x__type' => 31967,
+                'x__weight' => (-1 * ( isset($original_payment[0]['x__weight']) ? $original_payment[0]['x__weight'] : 1 )),
                 'x__creator' => $x__creator,
                 'x__metadata' => $_POST,
                 'x__reference' => ( isset($original_payment[0]['x__id']) ? $original_payment[0]['x__id'] : 0 ),
