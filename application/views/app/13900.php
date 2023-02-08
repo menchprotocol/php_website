babd1723b77725752a361a082dbe3f60a3fbc556
shervine <shervin@mench.com>
2023-02-07 21:28:14 -0800

    Tree App

--- application/views/app/13900.php
+++ application/views/app/13900.php
@@ -13,10 +13,12 @@ if(strlen($_GET['i__id'])){
         echo '<h2><a href="/i/i_go/'.$loaded_i['i__id'].'">'.$loaded_i['i__title'].'</a> '.count($all_ids).' IDEAS</h2>';
 
         echo '<div class="row justify-content">';
-        foreach($this->I_model->fetch(array(
-            'i__id IN (' . join(',', $all_ids) . ')' => null, //Active Member
-        ), 0) as $this_i){
-            echo view_card_i(12273, 0, null, $this_i);
+        foreach($all_ids as $recursive_down_id){
+            foreach($this->I_model->fetch(array(
+                'i__id' => $recursive_down_id,
+            ), 0) as $this_i){
+                echo view_card_i(12273, 0, null, $this_i);
+            }
         }
         echo '</div>';
 
