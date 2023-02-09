b7fe016892f326c5ebcd05d7cbc77b78b7429eb7
shervine <shervin@mench.com>
2023-02-09 08:40:35 -0800

    Updates

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -386,10 +386,10 @@ if($top_i__id) {
                 $quantity = ( isset($x__metadata['quantity']) && $x__metadata['quantity']>1 ? $x__metadata['quantity'] : ( $x_complete['x__weight'] > 0 ? $x_complete['x__weight'] : 1 ) );
                 $ticket_ui .= '<div class="msg alert alert-success" role="alert"><span class="icon-block"><i class="fas fa-check-circle"></i></span>'.( $x__metadata['mc_gross']>0 ? 'Received your payment of ' : 'Refunded you a total of ' ).$x__metadata['mc_currency'].' '.$x__metadata['mc_gross'].( $quantity>1 ? ' for '.$quantity.' tickets' : '' ).'</div>';
                 if($x__metadata['mc_gross']>0){
-                    for($t=1;$t<=$quantity;$t--){
+                    for($t=1;$t<=$quantity;$t++){
                         $checkin_url = 'https://'.get_domain('m__message', ( isset($member_e['e__id']) ? $member_e['e__id'] : 0 )).'/-26560?x__id='.$x_complete['x__id'].'&x__creator='.$x_complete['x__creator'].'&checkin_32016=1';
                         $ticket_ui .= '<div>Ticket #'.$t.'</div>';
-                        //$ticket_ui .= '<div>'.qr_code($checkin_url).'</div>';
+                        $ticket_ui .= '<div>'.qr_code($checkin_url).'</div>';
                     }
                 }
             }
