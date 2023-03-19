f88e6bdec4e19d8e4eade1097fd46003b0dbe8ba
shervine <shervin@mench.com>
2023-03-19 16:39:56 -0700

    New Login

--- application/views/global.js
+++ application/views/global.js
@@ -2441,7 +2441,7 @@ function isNormalInteger(str) {
 }
 
 function final_logout(){
-    var r = confirm("FINAL WARNING: You are about to permanently lose access to your anonymous account since you have not yet added a phone or email. Are you sure you want to continue?");
+    var r = confirm("FINAL WARNING: You are about to permanently lose access to your anonymous account since you have not yet added your email. Are you sure you want to continue?");
     if (r == true) {
         //Redirect:
         js_redirect('/-7291');
