aa0cb5884d9cd26a708195fa5589464a9b75451b
shervine <shervin@mench.com>
2023-03-24 19:42:20 -0700

    Updates

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -26,7 +26,7 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
     } elseif($e__id1==6287) {
 
         //App Store
-        echo '<div class="row justify-content list-covers" style="font-size: 1.8em; padding-top: 55px; line-height: 130%;"><div style="min-height:40px; width: 100%; text-align: center;"><b class="card_count_x css__title">'.number_format(count__apps(), 0).'</b></div><div class="css__title" style="text-align: center;"><a href="/-'.$e__id1.'" title="'.( strlen($m1['m__message']) ? $m1['m__title'] : '' ).'"><u>'.( strlen($m1['m__message']) ? $m1['m__message'] : $m1['m__title'] ).'</u></a></div></div>';
+        echo '<div class="row justify-content list-covers" style="font-size: 1.8em; padding-top: 55px; line-height: 130%;"><div style="min-height:40px; width: 100%; text-align: center;"><b class="card_count_app css__title">'.number_format(count__apps(), 0).'</b></div><div class="css__title" style="text-align: center;"><a href="/-'.$e__id1.'" title="'.( strlen($m1['m__message']) ? $m1['m__title'] : '' ).'"><u>'.( strlen($m1['m__message']) ? $m1['m__message'] : $m1['m__title'] ).'</u></a></div></div>';
 
     }
 }
