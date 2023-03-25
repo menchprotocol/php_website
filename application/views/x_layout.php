ce8a021c25028b7d590678277197f7c2e53ef74f
shervine <shervin@mench.com>
2023-03-24 22:46:45 -0700

    Updates

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -884,7 +884,7 @@ if(!$top_i__id){
             //NEXT
             $control_btn = '<div style="padding-left: 8px;" id="next_div"><a class="controller-nav round-btn go-next main-next" href="javascript:void(0);" onclick="go_next()">'.$m2['m__cover'].'</a><span class="nav-title css__title">'.$m2['m__title'].'</span></div>';
 
-        } elseif($x__type==28239 && $can_skip && !count($x_completes)){
+        } elseif($x__type==28239 && $can_skip){
 
             //SKIP
             $control_btn = '<div style="padding-left: 13px;"><a class="controller-nav round-btn" href="javascript:void(0);" onclick="go_next()">'.$m2['m__cover'].'</a><span class="nav-title css__title">'.$m2['m__title'].'</span></div>';
