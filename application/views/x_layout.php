0d09ca16b4471611159c5236aa986a7a1b59803c
shervine <shervin@mench.com>
2023-03-24 22:47:24 -0700

    Updates

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -879,12 +879,12 @@ if(!$top_i__id){
             //Ideas
             $control_btn = '<a class="controller-nav round-btn" href="javascript:void(0);" onclick="toggle_headline(12211)">'.$m2['m__cover'].'</a><span class="nav-title css__title">'.count($is_next).' '.$m2['m__title'].'</span>';
 
-        } elseif($x__type==12211){
+        } elseif($x__type==12211){ // && !$top_completed
 
             //NEXT
             $control_btn = '<div style="padding-left: 8px;" id="next_div"><a class="controller-nav round-btn go-next main-next" href="javascript:void(0);" onclick="go_next()">'.$m2['m__cover'].'</a><span class="nav-title css__title">'.$m2['m__title'].'</span></div>';
 
-        } elseif($x__type==28239 && $can_skip){
+        } elseif($x__type==28239 && $can_skip){ // && !count($x_completes)
 
             //SKIP
             $control_btn = '<div style="padding-left: 13px;"><a class="controller-nav round-btn" href="javascript:void(0);" onclick="go_next()">'.$m2['m__cover'].'</a><span class="nav-title css__title">'.$m2['m__title'].'</span></div>';
