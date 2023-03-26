c5d0ce56b5f52692f71a4cd822e626c12edece1c
shervine <shervin@mench.com>
2023-03-25 20:49:03 -0700

    Updates

---
+++ application/views/app/13572.php
@@ -0,0 +1,62 @@
+<?php
+
+if(isset($_POST)){
+    echo 'POST FOUND';
+    print_r($_POST);
+}
+
+?>
+
+
+<form enctype="multipart/form-data">
+    <input name="file" type="file" />
+    <input type="button" value="Upload" />
+</form>
+
+<script type="text/javascript">
+
+    $(':file').on('change', function () {
+        var file = this.files[0];
+
+        if (file.size > 1024) {
+            alert('max upload size is 1k');
+        }
+
+        // Also see .name, .type
+    });
+
+    $(':button').on('click', function () {
+        $.ajax({
+            // Your server script to process the upload
+            url: '/-13572',
+            type: 'POST',
+
+            // Form data
+            data: new FormData($('form')[0]),
+
+            // Tell jQuery not to process data or worry about content-type
+            // You *must* include these options!
+            cache: false,
+            contentType: false,
+            processData: false,
+
+            // Custom XMLHttpRequest
+            xhr: function () {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    // For handling the progress of the upload
+                    myXhr.upload.addEventListener('progress', function (e) {
+                        if (e.lengthComputable) {
+                            $('progress').attr({
+                                value: e.loaded,
+                                max: e.total,
+                            });
+                        }
+                    }, false);
+                }
+                return myXhr;
+            }
+        });
+    });
+
+</script>
