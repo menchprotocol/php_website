6031f2bb815061ea3dc3142854b68a3da0db772f
shervine <shervin@mench.com>
2023-03-27 09:17:42 -0700

    Updates

--- application/views/app/26595.php
+++ application/views/app/26595.php
@@ -62,7 +62,7 @@ if(isset($_POST['payment_status']) && ($_POST['payment_status']=='Refunded' || $
 }
 
 
-if(!$is_good){
+if(!$is_good && isset($_POST) && count($_POST)){
     $this->X_model->create(array(
         'x__type' => 4246, //Platform Bug Reports
         'x__message' => 'Invalid item number',
