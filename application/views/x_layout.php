2d7b0326660d587cd4626b928d2f92ba622a8972
shervine <shervin@mench.com>
2023-04-12 14:13:17 -0700

    Advanced Editors

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -783,7 +783,10 @@ if($top_i__id) {
     } elseif ($i['i__type'] == 32603) {
 
         //Sign Agreement
-        
+
+        //Text response
+        $message_ui = '<h3>' . $e___4737[$i['i__type']]['m__title'] . '</h3>';
+        $message_ui .= '<input type="text" class="border greybg custom_ui_14506_34281 main__title itemsetting" placeholder="Full Legal Name" id="x_sign" />';
 
     } elseif ($i['i__type'] == 7637) {
 
