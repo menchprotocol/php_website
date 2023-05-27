db6f3ef6318766a63edc331acee2bf974a290486
shervine <shervin@mench.com>
2023-05-27 10:23:43 -0700

    Updates

--- application/views/app/32603.php
+++ application/views/app/32603.php
@@ -27,7 +27,7 @@ foreach($this->I_model->fetch(array(
             //Input validated, process signature:
             $signed_idea = true;
 
-            echo '<div class="msg alert alert-success" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle zq6255"></i></span> Agreement signed successfully for "'.$_POST['x_write'].'". Show your matching ID at the door.</div>';
+            echo '<div class="msg alert alert-success" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle zq6255"></i></span> Waver signed for "'.$_POST['x_write'].'". Show matching ID at the door to enter.</div>';
 
 
         }
