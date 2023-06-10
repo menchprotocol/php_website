4578e80bd4e63cd33c12a73d75cd1286a19e1902
shervine <shervin@mench.com>
2023-06-10 15:47:18 -0700

    Updates

--- application/views/app/28199.php
+++ application/views/app/28199.php
@@ -27,14 +27,14 @@ foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
         'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
         'x__type IN (' . join(',', $this->config->item('n___12326')) . ')' => null, //Discovery Expansions
         'x__left' => $expires['i__id'],
-    ), array('x__source'), 0) as $x_progress){
+    ), array('x__creator'), 0) as $x_progress){
 
         //Now see if the answer is completed:
         $answer_completed = $this->X_model->fetch(array(
             'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
             'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //Discoveries
             'x__left' => $x_progress['x__right'],
-            'x__source' => $x_progress['e__id'],
+            'x__creator' => $x_progress['e__id'],
         ));
         $seconds_left = intval( intval( $expires['x__message']) + $buffer_time - (time() - strtotime($x_progress['x__time'])));
 
@@ -45,7 +45,7 @@ foreach($this->X_model->fetch($filters, array('x__right'), 0) as $expires){
                 'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
                 'x__type IN (' . join(',', $this->config->item('n___12227')) . ')' => null,
                 'x__left' => $expires['i__id'],
-                'x__source' => $x_progress['e__id'],
+                'x__creator' => $x_progress['e__id'],
             ), array(), 0) as $delete){
 
                 /*
