0ced2b7eaae51604d975cf98e13fa6a559d43373
shervine <shervin@mench.com>
2023-08-13 12:42:14 -0700

    Updates

--- application/views/app/13790.php
+++ application/views/app/13790.php
@@ -270,7 +270,7 @@ if(strlen($_GET['i__id'])){
                 }
             }
 
-            $idea_content .= '<td>'.( count($discoveries) ? '<span title="'.$discoveries[0]['x__time'].'">'.( strlen($discoveries[0]['x__message']) > 0 ? ( isset($_GET['expand']) ? '<p title="'.$i['i__title'].': '.$discoveries[0]['x__message'].'" data-placement="top" '.$underdot_class.'>'.convertURLs($discoveries[0]['x__message']).'</p>' : '<span title="'.$i['i__title'].': '.$discoveries[0]['x__message'].'" '.$underdot_class.'>✔️</span>'  ) : '<span title="'.$i['i__title'].'">✔️</span>' ).'</span>'  : '').'</td>';
+            $idea_content .= '<td>'.( count($discoveries) ? ( strlen($discoveries[0]['x__message']) > 0 ? ( isset($_GET['expand']) ? '<p title="'.$i['i__title'].': '.$discoveries[0]['x__message'].'" data-placement="top" '.$underdot_class.'>'.convertURLs($discoveries[0]['x__message']).'</p>' : '<span title="'.$i['i__title'].': '.$discoveries[0]['x__message'].' ['.$discoveries[0]['x__time'].']" '.$underdot_class.'>✔️</span>'  ) : '<span title="'.$i['i__title'].' ['.$discoveries[0]['x__time'].']">✔️</span>' )  : '').'</td>';
 
             if(count($discoveries)){
                 if(!isset($count_totals['i'][$i['i__id']])){
