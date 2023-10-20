7da922793b061cd720707452ad723586bbee20a7
shervine <shervin@mench.com>
2023-10-20 10:58:47 -0700

    Updates

--- application/views/app/40947.php
+++ application/views/app/40947.php
@@ -10,12 +10,12 @@ $list_settings = list_settings($_GET['i__id'], true);
 echo '<h1>' . view_i_title($list_settings['i']) . '</h1>';
 echo $list_settings['filters_ui'];
 
+echo count($list_settings['query_string']) . ' Contacts<br />';
+echo $list_settings['contact_details']['email_count'] . ' Emails<br />';
+echo $list_settings['contact_details']['phone_count'] . ' Phones<br />';
 
 //Generate the contact list of the input idea:
 echo '<textarea class="mono-space subscriber_data" style="background-color:#FFFFFF; color:#000 !important; padding:3px; font-size:0.8em; height:218px; width: 100%; border-radius: 21px;">'.$list_settings['contact_details']['full_list'].'</textarea>';
 echo '<textarea class="mono-space subscriber_data" style="background-color:#FFFFFF; color:#000 !important; padding:3px; font-size:0.8em; height:218px; width: 100%; border-radius: 21px;">'.$list_settings['contact_details']['email_list'].'</textarea>';
 
 echo '<textarea class="mono-space subscriber_data" style="background-color:#FFFFFF; color:#000 !important; padding:3px; font-size:0.8em; height:218px; width: 100%; border-radius: 21px;">'.$list_settings['contact_details']['email_list'].'</textarea>';
-
-echo $list_settings['contact_details']['email_count'] . 'Emails<br />';
-echo $list_settings['contact_details']['phone_count'] . 'Phones<br />';
