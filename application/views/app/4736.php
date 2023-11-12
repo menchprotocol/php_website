0eab90aea83600486a62f2a6e6b6743624092980
shervine <shervin@mench.com>
2023-11-11 22:22:42 -0800

    Updates

--- application/views/app/4736.php
+++ application/views/app/4736.php
@@ -4,6 +4,7 @@
 
 $count = 0;
 $message_included = 0;
+$updated = 0;
 foreach($this->I_model->fetch(array(
     'i__id > 0' => null,
 ), 0, 0, array('i__id' => 'ASC')) as $loaded_i){
@@ -18,12 +19,17 @@ foreach($this->I_model->fetch(array(
 
     $message_included = $message_included + ( count($messages) ? 1 : 0 );
 
-    $message_simple = '';
+    $message_simple = $loaded_i['i__title']."\n";
     foreach($messages as $x) {
         $message_simple .= $x['x__message']."\n";
     }
 
+    $updated += $this->I_model->update($loaded_i['i__id'], array(
+        'i__message' => trim($message_simple),
+    ));
+
 }
 
-echo '['.$count.' COUNT]<br />';
-echo '['.$message_included.' Messaged]';
+echo '['.$count.' COUNT]';
+echo '<br />['.$message_included.' Messaged]';
+echo '<br />['.$updated.' Updated]';
