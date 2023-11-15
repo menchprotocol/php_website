66a31cbba1e8a6db427176a8154c4df113aa2a6d
shervine <shervin@mench.com>
2023-11-15 15:15:08 -0800

    Updates

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -17,11 +17,11 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
         $inner_stats .= '<div class="main__title large_title"><a href="'.( in_array($e__id, $this->config->item('n___6287')) ? '/-' : '/@' ).$e__id.'" title="'.( strlen($m['m__message']) ? $m['m__title'] : '' ).'">'.( strlen($m['m__message']) ? $m['m__message'] : $m['m__title'] ).'</a> <a href="javascript:void(0);" onclick="$(\'.card_subcat_'.$e__id.'\').toggleClass(\'hidden\');"><i class="fas fa-sort"></i></a></div>';
 
         //Sub Categories
-        $inner_stats .= '<div class="card_subcat card_subcat_'.$e__id.' hidden">';
+        $inner_stats .= '<table class="card_subcat card_subcat_'.$e__id.' hidden">';
         foreach($this->config->item('e___'.( $e__id==12273 ? 4737 /* Idea Type */ : ( $e__id==12274 ? 7358 /* Source Active Access */ : $e__id /* Link It-self */ ) )) as $e__id2 => $m2) {
-            $inner_stats .= '<div><span class="icon-block">'.$m['m__cover'].'</span> '.number_format(count_interactions($e__id2), 0).' '.$m['m__title'].'</div>';
+            $inner_stats .= '<tr><td style="text-align: right;" width="34%">'.number_format(count_interactions($e__id2), 0).'</td><td><span class="icon-block">'.$m2['m__cover'].'</span>'.$m2['m__title'].'</td></tr>';
         }
-        $inner_stats .= '</div>';
+        $inner_stats .= '</table>';
 
 
         $inner_stats .= '</div>';
