467cacc50be0ee7074982d64fec78c427323d105
shervine <shervin@mench.com>
2023-11-15 15:24:40 -0800

    Updates

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -17,9 +17,35 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
         $inner_stats .= '<div class="main__title large_title"><a href="'.( in_array($e__id, $this->config->item('n___6287')) ? '/-' : '/@' ).$e__id.'" title="'.( strlen($m['m__message']) ? $m['m__title'] : '' ).'">'.( strlen($m['m__message']) ? $m['m__message'] : $m['m__title'] ).'</a> <a href="javascript:void(0);" onclick="$(\'.card_subcat_'.$e__id.'\').toggleClass(\'hidden\');"><i class="fas fa-sort"></i></a></div>';
 
         //Sub Categories
+        $cat_id = ( $e__id==12273 ? 4737 /* Idea Type */ : ( $e__id==12274 ? 7358 /* Source Active Access */ : $e__id /* Link It-self */ ) );
         $inner_stats .= '<table class="card_subcat card_subcat_'.$e__id.' hidden">';
-        foreach($this->config->item('e___'.( $e__id==12273 ? 4737 /* Idea Type */ : ( $e__id==12274 ? 7358 /* Source Active Access */ : $e__id /* Link It-self */ ) )) as $e__id2 => $m2) {
-            $inner_stats .= '<tr><td style="text-align: right;" width="34%">'.number_format(count_interactions($e__id2), 0).'</td><td style="text-align: left;"><span class="icon-block">'.$m2['m__cover'].'</span>'.$m2['m__title'].'</td></tr>';
+        foreach($this->config->item('e___'.$cat_id) as $e__id2 => $m2) {
+
+            if($e__id==12273){
+
+                $sub_counter = $this->I_model->fetch(array(
+                    'i__type IN (' . join(',', $this->config->item('n___'.$cat_id)) . ')' => null, //SELECT IDEA
+                ), array(), 0, 0, array(), 'COUNT(i__id) as totals');
+
+
+            } elseif($e__id==12274){
+
+                $sub_counter = $this->E_model->fetch(array(
+                    'e__access IN (' . join(',', $this->config->item('n___'.$cat_id)) . ')' => null, //ACTIVE
+                ), array(), 0, 0, array(), 'COUNT(e__id) as totals');
+
+            } else {
+
+                $list_e_count = $this->X_model->fetch(array(
+                    'x__type' => $e__id2,
+                    'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
+                ), array(), 0, 0, array(), 'COUNT(x__id) as totals');
+
+            }
+
+            if($list_e_count[0]['totals'] > 0){
+                $inner_stats .= '<tr><td style="text-align: right;" width="34%">'.number_format($list_e_count[0]['totals'], 0).'</td><td style="text-align: left;"><span class="icon-block">'.$m2['m__cover'].'</span>'.$m2['m__title'].'</td></tr>';
+            }
         }
         $inner_stats .= '</table>';
 
