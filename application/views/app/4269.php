c62f3705ae642afbe1e036229a8dad85eb1c6e76
shervine <shervin@mench.com>
2023-11-16 12:17:20 -0800

    Updates

--- application/views/app/4269.php
+++ application/views/app/4269.php
@@ -282,7 +282,7 @@ if(superpower_unlocked()) {
     <div class="center-info">
 
         <div class="text-center platform-large"><?= get_domain('m__cover') ?></div>
-        <div class="text-center main__title"><?= get_domain('m__title') ?></div>
+        <div class="text-center main__title" style="padding-top: 21px;"><?= get_domain('m__title') ?></div>
 
         <div class="login-content" style="margin-top:40px;">
 
