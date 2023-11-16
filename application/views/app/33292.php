aec7f1538c699a2eb5f4c2a4d448c075e9a874de
shervine <shervin@mench.com>
2023-11-16 12:45:25 -0800

    Updates

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -55,7 +55,7 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
 
                     $total_sub_count += $sub_counter[0]['totals'];
 
-                    $inner_stats .= '<tr class="mobile-shrink" title="'.$m2['m__message'].' ('.number_format(($sub_counter[0]['totals']/$this_count*100),3).'% Of Total)"><td style="text-align: right;" width="28%">'.number_format($sub_counter[0]['totals'], 0).'</td><td style="text-align: left;"><span class="icon-block-xs">'.$m2['m__cover'].'</span>'.$m2['m__title'].'</td>'.( $cat_id==4737 ? '<td style="text-align: left;" width="21%">'.( in_array($e__id2, $this->config->item('n___7712')) ? $e___10891[7712]['m__cover'] : $e___10891[41933]['m__cover'] ).'</td>' : '' ).'</tr>';
+                    $inner_stats .= '<tr class="mobile-shrink" title="'.$m2['m__message'].' ('.number_format(($sub_counter[0]['totals']/$this_count*100),3).'% Of Total)"><td style="text-align: right;" width="28%">'.number_format($sub_counter[0]['totals'], 0).'</td><td style="text-align: left;"><span class="icon-block-xs">'.$m2['m__cover'].'</span>'.$m2['m__title'].'</td>'.( $cat_id==4737 ? '<td style="text-align: left;" width="13%">'.( in_array($e__id2, $this->config->item('n___7712')) ? $e___10891[7712]['m__cover'] : $e___10891[41933]['m__cover'] ).'</td>' : '' ).'</tr>';
 
                 }
 
