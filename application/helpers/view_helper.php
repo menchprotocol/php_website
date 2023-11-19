55dd4c0e66e9d814674dad4de24d9f3f7224d57e
shervine <shervin@mench.com>
2023-11-19 10:57:21 -0800

    Updates

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1327,7 +1327,7 @@ function view_card_i($x__type, $top_i__id = 0, $previous_i = null, $i, $focus_e 
             //Determine hover state:
             $always_see = in_array($x__type_top_bar, $CI->config->item('n___32172'));
 
-            if($x__type_top_bar==31770 && $link_type_ui && ($write_access_i || $link_creator)){
+            if($x__type_top_bar==31770 && $link_type_ui){
 
                 //Links
                 $active_bars++;
