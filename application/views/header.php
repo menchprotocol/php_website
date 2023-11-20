7aea7ab9685ee08b0b1ac971d1fcc38a361028b9
shervine <shervin@mench.com>
2023-11-19 22:22:05 -0800

    Updates

--- application/views/header.php
+++ application/views/header.php
@@ -257,8 +257,8 @@ foreach($this->config->item('e___13890') as $e__id => $m){
                 //echo ' .halfbg, .msg>span, .msg { line-height: 150% !important; } ';
                 echo ' .msg>span u, .msg>span a { line-height: 100% !important; padding:0 !important; } ';
 
-                echo '.list-border, .msg { max-width:768px; margin: 0 auto !important; }';
-                echo ' @media (max-width:768px) { .list-border, .msg { max-width:100%; margin: 0 auto; } }';
+                echo '.list-border, .msg { max-width:600px; margin: 0 auto !important; }';
+                echo ' @media (max-width:600px) { .list-border, .msg { max-width:100%; margin: 0 auto; } }';
 
                 //Force Dark Mode:
                 $body_class = str_replace('custom_ui_13884_13885','custom_ui_13884_13886', $body_class);
