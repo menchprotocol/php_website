d19054b3782412c65583d7069d4aa6513b25b24a
shervine <shervin@mench.com>
2023-11-19 21:41:39 -0800

    Updates

--- application/views/e_layout.php
+++ application/views/e_layout.php
@@ -17,7 +17,7 @@ $this->X_model->create(array(
 //Load Top:
 $counter_top = view_e_covers(11030, $e['e__id'], 0, false);
 
-echo '<div class="hideIfEmpty headline_body_11030" read-counter="'.$counter_top.'"><div class="tab_content"></div>'.( $write_access_e ? '<div class="new-list-11030"><div class="col-md-8 col-sm-10 col-12 container-center"><div class="dropdown_11030 list-adder">
+echo '<div class="hideIfEmpty headline_body_11030" read-counter="'.$counter_top.'"><div class="tab_content"></div>'.( $write_access_e ? '<div class="new-list-11030"><div class="col-md-6 col-sm-8 col-10 container-center"><div class="dropdown_11030 list-adder">
                     <div class="input-group border">
                         <input type="text"
                                class="form-control form-control-thick algolia_search dotransparent add-input"
@@ -47,7 +47,7 @@ foreach($this->config->item('e___41091') as $x__type => $m) {
         if($x__type==12273){
 
             //IDEAS
-            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-md-8 col-sm-10 col-12 container-center"><div class="list-group"><div class="list-group-item dropdown_'.$x__type.' list-adder">
+            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-md-6 col-sm-8 col-10 container-center"><div class="list-group"><div class="list-group-item dropdown_'.$x__type.' list-adder">
                 <div class="input-group border">
                     <input type="text"
                            class="form-control form-control-thick algolia_search dotransparent add-input"
@@ -57,7 +57,7 @@ foreach($this->config->item('e___41091') as $x__type => $m) {
 
         } elseif($x__type==12274){
 
-            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-md-8 col-sm-10 col-12 container-center"><div class="dropdown_'.$x__type.' list-adder">
+            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-md-6 col-sm-8 col-10 container-center"><div class="dropdown_'.$x__type.' list-adder">
                     <div class="input-group border">
                         <input type="text"
                                class="form-control form-control-thick algolia_search dotransparent add-input"
