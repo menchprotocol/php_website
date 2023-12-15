3d92014f3336be068255b3b66f30d6c73dbed749
shervine <shervin@mench.com>
2023-12-15 08:45:25 -0800

    New Updates :)

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,12 +10,10 @@ $route['stats'] = "app/load/33292";
 $route['routes'] = "app/load/42006";
 $route['cache'] = "app/load/4527";
 
-
 //$route['-(:any)']                             = "app/load/$1";
-$route['@(:alphanum)']                          = "e/e_layout/$1"; //Source
-$route['~(:alphanum)@(:alphanum)']              = "i/i_layout/$1/$2"; //Append Source (To be deprecated soon & merged into mass apply function)
-$route['~(:alphanum)']                          = "i/i_layout/$1"; //Ideate
+$route['@(:any)']                               = "e/e_layout/$1"; //Source
+$route['~(:any)@(:any)']                        = "i/i_layout/$1/$2"; //Append Source (To be deprecated soon & merged into mass apply function)
+$route['~(:any)']                               = "i/i_layout/$1"; //Ideate
 $route['(:alphanum)/(:alphanum)/@(:alphanum)']  = "x/x_layout/$1/$2/$3"; //Discovery Started
 $route['(:alphanum)/(:alphanum)']               = "x/x_layout/$1/$2/0"; //Discovery Started
-$route['(:segment)']                           = "x/x_layout/0/$1/0"; //Discovery Preview
-//$route['(:any)']                                = "x/x_layout/0/$1/0"; //Discovery Preview
+$route['(:any)']                                = "x/x_layout/0/$1/0"; //Discovery Preview
