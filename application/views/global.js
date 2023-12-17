44b01801393b1a2833bf1b3a22a9361a3e88a71c
shervine <shervin@mench.com>
2023-12-17 15:39:07 -0800

    New Updates :)

--- application/views/global.js
+++ application/views/global.js
@@ -642,6 +642,7 @@ function x_create(add_fields){
 function update__cover(new_cover){
     $('#modal31912 .save_e__cover').val( new_cover );
     update_cover_main(new_cover, '.demo_cover');
+    has_unsaved_changes = true;
 }
 function image_cover(cover_preview, cover_apply, new_title){
     return '<a href="#preview_cover" onclick="update__cover(\''+cover_apply+'\')">' + view_s_mini_js(cover_preview, new_title) + '</a>';
