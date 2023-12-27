b8909349de7b24b35b30f7e3d42d0415e5b14503
shervine <shervin@mench.com>
2023-12-27 15:57:03 -0800

    New Updates :)

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1366,7 +1366,7 @@ function view_card_i($x__type, $top_i__hashtag = 0, $previous_i = null, $i, $foc
                 $active_bars++;
                 $top_bar_ui .= $link_type_ui;
 
-            } elseif($x__type_top_bar==4362 && 0 && isset($i['x__time']) && strtotime($i['x__time']) > 0){
+            } elseif($x__type_top_bar==4362 && isset($i['x__time']) && strtotime($i['x__time']) > 0){
 
                 //Creation Time:
                 $active_bars++;
