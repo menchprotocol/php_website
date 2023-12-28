62a78bc683cc1c393d232bf07e72ebc7eebf2333
shervine <shervin@mench.com>
2023-12-28 10:34:54 -0800

    New Updates :)

--- application/views/e_layout.php
+++ application/views/e_layout.php
@@ -14,6 +14,13 @@ $this->X_model->create(array(
 ));
 
 
+//Website redirect hack:
+if(!$member_e['e__id'] && in_array($e['e__id'], $this->config->item('n___14870'))){
+    $e___14870 = $this->config->item('e___14870'); //Website Partner
+    js_php_redirect('https://'.$e___14870[$e['e__id']]['m__message'], 13);
+}
+
+
 /*
 
 //Load Top:
