1f8f1ace88cee557ff7f47aef5da42c478d0f23a
shervine <shervin@mench.com>
2024-01-05 12:28:53 -0800

    New Updates :)

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -50,7 +50,7 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
             foreach(array_intersect($m3['m__following'], $this->config->item('n___42263')) as $found_link_group){
                 if(!$focus_link_group || $focus_link_group!=$found_link_group){
                     echo '<tr class="mobile-shrink">';
-                    echo '<td style="text-align: left; font-weight: bold;" colspan="2"><span class="icon-block-xs">'.$e___42263[$found_link_group]['m__cover'].'</span>'.$e___42263[$found_link_group]['m__title'].':</td>';
+                    echo '<td style="text-align: left;" colspan="2"><span class="icon-block-xxs">'.$e___42263[$found_link_group]['m__cover'].'</span><b>'.$e___42263[$found_link_group]['m__title'].'</b></td>';
                     echo '</tr>';
                     $focus_link_group = $found_link_group;
                 }
