78df67bbfc14803a381cca6784e954c682bb9191
shervine <shervin@mench.com>
2024-01-05 15:06:43 -0800

    New Updates :)

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -6,181 +6,185 @@ if(!in_array($focus_i['i__access'], $this->config->item('n___31871')) && !write_
 
 } else {
 
-$e___11035 = $this->config->item('e___11035'); //NAVIGATION
-$e___4737 = $this->config->item('e___4737'); //Idea Types
-$is_or_7712 = in_array($focus_i['i__type'], $this->config->item('n___7712'));
-$is_single_click = in_array($focus_i['i__type'], $this->config->item('n___40680'));
+    $e___11035 = $this->config->item('e___11035'); //NAVIGATION
+    $e___4737 = $this->config->item('e___4737'); //Idea Types
+    $is_or_7712 = in_array($focus_i['i__type'], $this->config->item('n___7712'));
+    $is_single_click = in_array($focus_i['i__type'], $this->config->item('n___40680'));
 
-//NEXT IDEAS
-$is_next = $this->X_model->fetch(array(
-    'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-    'i__access IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
-    'x__type IN (' . join(',', $this->config->item('n___12840')) . ')' => null, //IDEA LINKS TWO-WAY
-    'x__left' => $focus_i['i__id'],
-), array('x__right'), 0, 0, array('x__weight' => 'ASC'));
-
-//Filter Next Ideas:
-foreach($is_next as $in_key => $in_value){
-    $i_is_available = i_is_available($in_value['i__id'], false);
-    if(!$i_is_available['status']){
-        //Remove this option:
-        unset($is_next[$in_key]);
-    }
-}
-
-
-
-$focus_i['i__message'] = str_replace('"','',$focus_i['i__message']);
-$x__creator = ( $member_e ? $member_e['e__id'] : 0 );
-$top_i__id = ( count($top_i) ? $top_i['i__id'] : 0 );
-    $top_i__id = ( count($top_i) && $x__creator ? $top_i['i__id'] : 0 );
-    $top_i__hashtag = ( count($top_i) && $x__creator ? $top_i['i__hashtag'] : null );
-$top_completed = false; //Assume main intent not yet completed, unless proven otherwise...
-$can_skip = in_array($focus_i['i__type'], $this->config->item('n___42211')) || count($this->X_model->fetch(array(
-    'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-    'x__type IN (' . join(',', $this->config->item('n___42256')) . ')' => null, //Authored
-    'x__right' => $focus_i['i__id'],
-    'x__up' => 28239, //Can Skip
-)));
-
-
-if(isset($_GET['delete'])){
-
-    boost_power();
-
-    foreach($this->X_model->fetch(array(
-        'x__up' => $_GET['delete'],
-        'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
-        'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
-    ), array('x__down'), 0) as $x) {
-
-        if(count($this->X_model->fetch(array(
-                'x__down' => $x['e__id'],
-                'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
-                'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
-            ), array(), 0))<=2 && !count($this->X_model->fetch(array(
-                'x__up' => $x['e__id'],
-                'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
-                'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
-            ), array(), 0))){
-            //Good to delete:
-
-            //Delete all transactions:
-            $links_removed = $this->E_model->remove($x['e__id'], $member_e['e__id'], 0);
-            $this->E_model->update($x['e__id'], array(
-                'e__access' => 6178,
-            ), true, $member_e['e__id']);
-
-        } else {
-            //Issues:
-            echo '<div>Cannot delete <a href="/@'.$x['e__handle'].'">@'.$x['e__handle'].'</a>.</div>';
-        }
-
-    }
-
-}
-
-/*
- *
- * //TODO Remove at the end
-
-if(isset($_GET['go1'])){
-
-    boost_power();
-
-    //Sync Ideas & Sources
-    $stats = array(
-        'cached_ideas' => 0,
-        'active_ideas' => 0,
-        'old_links_removed' => 0,
-        'old_links_kept' => 0,
-        'new_links_added' => 0,
-        'missing_creation' => 0,
-    );
-
-    $edited = 0;
-    $edited_sources = 0;
-    foreach($this->I_model->fetch(array(
+    //NEXT IDEAS
+    $is_next = $this->X_model->fetch(array(
+        'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
         'i__access IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
-    ), 0) as $i_fix){
+        'x__type IN (' . join(',', $this->config->item('n___12840')) . ')' => null, //IDEA LINKS TWO-WAY
+        'x__left' => $focus_i['i__id'],
+    ), array('x__right'), 0, 0, array('x__weight' => 'ASC'));
 
-        $view_sync_links = view_sync_links($i_fix['i__message'], true);
-
-        echo '<a href="/~'.$i_fix['i__hashtag'].'">#'.$i_fix['i__hashtag'].'</a><br />';
-        echo nl2br(htmlentities($i_fix['i__message'])).'<br />';
-
-        if(count($view_sync_links['replace_from'])){
-
-            //Show all:
-            $starting_message = $i_fix['i__message'];
-
-            foreach($view_sync_links['replace_from'] as $index=>$val){
-                $edited_sources++;
-                if(substr_count($starting_message, $view_sync_links['replace_from'][$index].' ')){
-                    $starting_message = str_replace($view_sync_links['replace_from'][$index].' ',$view_sync_links['replace_to'][$index].' ',$starting_message);
-                    echo '<div>['.$view_sync_links['replace_from'][$index].' ] Replaced to ['.$view_sync_links['replace_to'][$index].' ]</div>';
-                } else {
-                    $starting_message = str_replace($view_sync_links['replace_from'][$index],$view_sync_links['replace_to'][$index],$starting_message);
-                    echo '<div>['.$view_sync_links['replace_from'][$index].'] Replaced to ['.$view_sync_links['replace_to'][$index].']</div>';
-                }
-            }
-
-            if($starting_message!=$i_fix['i__message']){
-                //view_sync_links($starting_message, true, $i_fix['i__id']);
-                $edited++;
-            }
-
+    //Filter Next Ideas:
+    foreach($is_next as $in_key => $in_value){
+        $i_is_available = i_is_available($in_value['i__id'], false);
+        if(!$i_is_available['status']){
+            //Remove this option:
+            unset($is_next[$in_key]);
         }
-        $stats['old_links_removed'] += $view_sync_links['sync_stats']['old_links_removed'];
-        $stats['old_links_kept'] += $view_sync_links['sync_stats']['old_links_kept'];
-        $stats['new_links_added'] += $view_sync_links['sync_stats']['new_links_added'];
     }
 
-    echo '<hr />Edited ['.$edited.']['.$edited_sources.']<br />';
 
-}
-
-if(isset($_GET['go2'])) {
-
-    boost_power();
-
-
-    $updated = 0;
-    $already = 0;
-    foreach ($this->X_model->fetch(array(
+    $focus_i['i__message'] = str_replace('"','',$focus_i['i__message']);
+    $x__creator = ( $member_e ? $member_e['e__id'] : 0 );
+    $top_i__id = ( count($top_i) ? $top_i['i__id'] : 0 );
+        $top_i__id = ( count($top_i) && $x__creator ? $top_i['i__id'] : 0 );
+        $top_i__hashtag = ( count($top_i) && $x__creator ? $top_i['i__hashtag'] : null );
+    $top_completed = false; //Assume main intent not yet completed, unless proven otherwise...
+    $can_skip = in_array($focus_i['i__type'], $this->config->item('n___42211')) || count($this->X_model->fetch(array(
         'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
         'x__type IN (' . join(',', $this->config->item('n___42256')) . ')' => null, //Authored
-        //'x__right' => $focus_i['i__id'],
-        'x__up' => 26562, //Total Due
-    ), array(), 0) as $ticket) {
+        'x__right' => $focus_i['i__id'],
+        'x__up' => 28239, //Can Skip
+    )));
 
-        //linked to any currency?
-        if(count($this->X_model->fetch(array(
-            'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type' => 4983, //Co-Author
-            'x__right' => $ticket['x__right'],
-            'x__up IN (' . join(',', $this->config->item('n___26661')) . ')' => null, //PUBLIC
-        )))){
-            //Already linked to currency:
-            $already++;
-          continue;
+
+    if(isset($_GET['delete'])){
+
+        boost_power();
+
+        foreach($this->X_model->fetch(array(
+            'x__up' => $_GET['delete'],
+            'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
+            'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
+        ), array('x__down'), 0) as $x) {
+
+            if(count($this->X_model->fetch(array(
+                    'x__down' => $x['e__id'],
+                    'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
+                    'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
+                ), array(), 0))<=2 && !count($this->X_model->fetch(array(
+                    'x__up' => $x['e__id'],
+                    'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
+                    'x__access IN (' . join(',', $this->config->item('n___7360')) . ')' => null, //ACTIVE
+                ), array(), 0))){
+                //Good to delete:
+
+                //Delete all transactions:
+                $links_removed = $this->E_model->remove($x['e__id'], $member_e['e__id'], 0);
+                $this->E_model->update($x['e__id'], array(
+                    'e__access' => 6178,
+                ), true, $member_e['e__id']);
+
+            } else {
+                //Issues:
+                echo '<div>Cannot delete <a href="/@'.$x['e__handle'].'">@'.$x['e__handle'].'</a>.</div>';
+            }
+
         }
-
-        $updated++;
-        $this->X_model->create(array(
-            'x__creator' => $member_e['e__id'],
-            'x__type' => 4983, //Co-Author
-            'x__up' => 26092,
-            'x__right' => $ticket['x__right'],
-        ));
 
     }
 
-    echo 'Currency updated for ' . $updated . ' ideas & '.$already.' already.';
-    echo '<br /><br /><br /><br />';
 
-}
-*/
+
+    if(isset($_GET['go1'])){
+
+        boost_power();
+
+        //Sync Ideas & Sources
+        $stats = array(
+            'cached_ideas' => 0,
+            'active_ideas' => 0,
+            'old_links_removed' => 0,
+            'old_links_kept' => 0,
+            'new_links_added' => 0,
+            'missing_creation' => 0,
+        );
+
+        $edited = 0;
+        $edited_sources = 0;
+        foreach($this->I_model->fetch(array(
+            'i__access IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
+        ), 0) as $i_fix){
+
+            $view_sync_links = view_sync_links($i_fix['i__message'], true, $i_fix['i__id']);
+
+            /*
+            echo '<a href="/~'.$i_fix['i__hashtag'].'">#'.$i_fix['i__hashtag'].'</a><br />';
+            echo nl2br(htmlentities($i_fix['i__message'])).'<br />';
+
+            if(count($view_sync_links['replace_from'])){
+
+                //Show all:
+                $starting_message = $i_fix['i__message'];
+
+                foreach($view_sync_links['replace_from'] as $index=>$val){
+                    $edited_sources++;
+                    if(substr_count($starting_message, $view_sync_links['replace_from'][$index].' ')){
+                        $starting_message = str_replace($view_sync_links['replace_from'][$index].' ',$view_sync_links['replace_to'][$index].' ',$starting_message);
+                        echo '<div>['.$view_sync_links['replace_from'][$index].' ] Replaced to ['.$view_sync_links['replace_to'][$index].' ]</div>';
+                    } else {
+                        $starting_message = str_replace($view_sync_links['replace_from'][$index],$view_sync_links['replace_to'][$index],$starting_message);
+                        echo '<div>['.$view_sync_links['replace_from'][$index].'] Replaced to ['.$view_sync_links['replace_to'][$index].']</div>';
+                    }
+                }
+
+                if($starting_message!=$i_fix['i__message']){
+                    //view_sync_links($starting_message, true, $i_fix['i__id']);
+                    $edited++;
+                }
+
+            }
+            $stats['old_links_removed'] += $view_sync_links['sync_stats']['old_links_removed'];
+            $stats['old_links_kept'] += $view_sync_links['sync_stats']['old_links_kept'];
+            $stats['new_links_added'] += $view_sync_links['sync_stats']['new_links_added'];
+
+            */
+        }
+
+        echo '<hr />Edited ['.$edited.']['.$edited_sources.']<br />';
+
+    }
+
+    /*
+     *
+     * //TODO Remove at the end
+
+    if(isset($_GET['go2'])) {
+
+        boost_power();
+
+
+        $updated = 0;
+        $already = 0;
+        foreach ($this->X_model->fetch(array(
+            'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+            'x__type IN (' . join(',', $this->config->item('n___42256')) . ')' => null, //Authored
+            //'x__right' => $focus_i['i__id'],
+            'x__up' => 26562, //Total Due
+        ), array(), 0) as $ticket) {
+
+            //linked to any currency?
+            if(count($this->X_model->fetch(array(
+                'x__access IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+                'x__type' => 4983, //Co-Author
+                'x__right' => $ticket['x__right'],
+                'x__up IN (' . join(',', $this->config->item('n___26661')) . ')' => null, //PUBLIC
+            )))){
+                //Already linked to currency:
+                $already++;
+              continue;
+            }
+
+            $updated++;
+            $this->X_model->create(array(
+                'x__creator' => $member_e['e__id'],
+                'x__type' => 4983, //Co-Author
+                'x__up' => 26092,
+                'x__right' => $ticket['x__right'],
+            ));
+
+        }
+
+        echo 'Currency updated for ' . $updated . ' ideas & '.$already.' already.';
+        echo '<br /><br /><br /><br />';
+
+    }
+    */
 
 
 
