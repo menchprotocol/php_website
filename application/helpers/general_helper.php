682c0016f37d5c8d85736b269d017506e3a6bb15
shervine <shervin@mench.com>
2024-01-04 20:01:37 -0800

    New Updates :)

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1841,12 +1841,7 @@ function write_access_e($e__handle, $member_e = array()){
         || superpower_unlocked(13422)
 
         //If Source Follows this Member
-        || count($CI->X_model->fetch(array(
-            'x__type IN (' . join(',', $CI->config->item('n___41944')) . ')' => null, //Source Authors
-            'x__access IN (' . join(',', $CI->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__up' => $member_e['e__id'],
-            'LOWER(e__handle)' => strtolower($e__handle),
-        ), array('x__down')))
+
 
     );
 
