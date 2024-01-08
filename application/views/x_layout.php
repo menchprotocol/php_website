00b63b11c964e320d01130c4d66ced01778ad644
shervine <shervin@mench.com>
2024-01-08 10:36:39 -0800

    New Updates :)

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -151,16 +151,19 @@ if(!in_array($focus_i['i__access'], $this->config->item('n___31871')) && !write_
             'x__type IN (' . join(',', $this->config->item('n___42256')) . ')' => null, //Authored
             'x__up IN (' . join(',', $this->config->item('n___4318')) . ')' => null, //Authored
         ), array('x__up'), 0) as $target) {
+
+            foreach(array('mon','tue','wed','thu','fri','sat','sun') as $day){
+                $target['x__message'] = str_replace($day.'.','',$target['x__message']);
+                $target['x__message'] = str_replace($day,'',$target['x__message']);
+            }
+
             if(strlen($target['x__message']) && !substr_count($target['x__message'], '2023')){
                 $target['x__message'] = $target['x__message'].' 2023';
             }
             $target['x__message'] = strtolower($target['x__message']);
             $target['x__message'] = str_replace('p ','pm ',$target['x__message']);
             $target['x__message'] = str_replace('a ','am ',$target['x__message']);
-            foreach(array('mon','tue','wed','thu','fri','sat','sun') as $day){
-                $target['x__message'] = str_replace($day.'.','',$target['x__message']);
-                $target['x__message'] = str_replace($day,'',$target['x__message']);
-            }
+
             if(strtotime($target['x__message'])>0){
                 echo '<div>@'.$target['e__handle'].' | '.$target['x__message'].' => '.date('Y-m-d\TH:i:sP', strtotime($target['x__message'])).'</div>';
             } else {
