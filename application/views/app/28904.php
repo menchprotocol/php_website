05c10df7bdf095ee16f578e3c4e58599eaca3185
shervine <shervin@mench.com>
2024-01-14 15:16:54 -0800

    Source Editing Modal Updates

--- application/views/app/28904.php
+++ application/views/app/28904.php
@@ -1,17 +1,23 @@
 <?php
 
+
+
 $e__handle = ( isset($_GET['e__handle']) && isset($_GET['e__hash']) && view_e__hash($_GET['e__handle'])==$_GET['e__hash'] ? $_GET['e__handle'] : $member_e['e__handle'] );
 
-if($e__handle > 0){
+if(strlen($e__handle)){
+
     //Notification Settings
     foreach($this->E_model->fetch(array(
-        'e__id' => $e__handle,
+        'e__handle' => $e__handle,
     )) as $e){
         echo '<h3 style="text-align: center; margin: -10px 0 21px 0;">'.$e['e__title'].'</h3>';
     }
     echo '<div style="max-width: 540px; margin: 0 auto;">'.view_radio_e(28904, $e['e__id'], 0).'</div>';
+
 } else {
+
     js_php_redirect(view_app_link(4269).'?url='.urlencode($_SERVER['REQUEST_URI']), 13);
+
 }
 
 
