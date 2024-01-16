e0aa0a6d1ce96a7f5ca47e21920c987024640807
shervine <shervin@mench.com>
2024-01-16 09:56:57 -0800

    Cloudinary Upload library

--- application/views/global.js
+++ application/views/global.js
@@ -680,7 +680,8 @@ $(document).ready(function () {
         uploadPreset: 'upload_file_e_cover'
 
         }, (error, result) => {
-            if (!error && result && result.status === "success") {
+            console.log(result);
+            if (!error && result && result.event === "success") {
                 console.log('Done! Here is the image info: ', result.info);
             }
         }
