433b0370f50de6eea0be261e2e92a3656334f0c2
shervine <shervin@mench.com>
2024-01-18 20:58:08 -0800

    New Email Templates based on ideas

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1140,7 +1140,7 @@ function view_sync_links($str, $return_array = false, $save_i__id = 0) {
             }
         }
 
-        $i__cache .= '<div class="line '.(!$line_index ? ' first_line ' : '').( $save_i__id && $word_count>=$word_limit ? ' hidden ' : '' ).'" >';
+        $i__cache .= '<div class="line '.(!$line_index ? 'first_line' : '').( $save_i__id && $word_count>=$word_limit ? ' hidden ' : '' ).'">';
         $i__cache .= $i__cache_line;
         $i__cache .= '</div>';
 
