1b3155dd0f72f68ff44a62b6ea7b4f8b48213a2e
shervine <shervin@mench.com>
2024-01-19 01:39:46 -0800

    Source Editor Modal

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -956,14 +956,14 @@ if($top_i__hashtag){
 
 <!-- Example split danger button -->
 <div class="btn-group">
-    <button type="button" class="btn btn-danger">Action</button>
+    <button type="button" class="btn btn-danger">FOLLOW</button>
     <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-        <span class="visually-hidden">Toggle Dropdown</span>
+        OR
     </button>
     <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><a class="dropdown-item" href="#">MUTE</a></li>
+        <li><a class="dropdown-item" href="#">SUBSCRIBE</a></li>
+        <li><a class="dropdown-item" href="#">REPORT</a></li>
         <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item" href="#">Separated link</a></li>
     </ul>
