8785f1811702ef57430c60ebc712c0741cee6d5e
shervine <shervin@mench.com>
2024-01-22 12:04:40 -0800

    Dynamic Drop Down fixes

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2358,7 +2358,7 @@ function view_dropdown($cache_e__id, $selected_e__id, $write_privacy_i = true, $
 
     $ui .= '<button type="button" '.( $write_privacy_i ? 'class="btn no-left-padding '.( $show_full_name ? 'dropdown-toggle' : 'no-right-padding dropdown-lock' ).'" id="dropdownMenuButton'.$cache_e__id.'_'.$o__id.'_'.$x__id.'" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : 'class="btn adj-btn '.( !$show_full_name ? 'no-padding' : '' ).' edit-locked" ' ).'>';
 
-    $ui .= '<span class="current_content">'.( isset($e___this[$selected_e__id]['m__cover']) ? '<span class="icon-block-xs">'.$e___this[$selected_e__id]['m__cover'].'</span>'.( $show_full_name ?  $e___this[$selected_e__id]['m__title'] : '' ) : '' ).'</span><span class="icon-block-xs"><i class="fal fa-angle-down"></i></span>';
+    $ui .= '<span class="current_content">'.( isset($e___this[$selected_e__id]['m__cover']) ? '<span class="icon-block-xs">'.$e___this[$selected_e__id]['m__cover'].'</span>'.( $show_full_name ?  $e___this[$selected_e__id]['m__title'] : '' ) : '' ).'</span>'.( $show_full_name ? '<span class="icon-block-xs"><i class="fal fa-angle-down"></i></span>' : '' );
 
     $ui .= '</button>';
 
