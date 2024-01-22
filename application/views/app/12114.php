07c53a4f34269edab44faafed1cca2d5b355bca4
shervine <shervin@mench.com>
2024-01-22 07:36:31 -0800

    Link counter fix

--- application/views/app/12114.php
+++ application/views/app/12114.php
@@ -24,7 +24,7 @@ foreach($this->config->item('e___42263') as $x__type => $m) {
     $growth = ( $growth >= 0 ? '+' : '-' ).$growth.'%';
 
     //Add to UI:
-    $html_message .= '<div class="line"><span style="display:inline-block; width: 55px; text-align: right;">'.$growth.'</span><span class="icon-block">'.$m['m__cover'].'</span>'.view_number($unique).' '.$m['m__title'].'</div>';
+    $html_message .= '<div class="line"><span style="display:inline-block; width: 55px; text-align: right;">'.$growth.'</span><span style="width:34px !important; display: inline-block; text-align: center;">'.$m['m__cover'].'</span>'.view_number($unique).' '.$m['m__title'].'</div>';
 
     //Primary Coin?
     if(in_array($x__type, $this->config->item('n___6404'))){
