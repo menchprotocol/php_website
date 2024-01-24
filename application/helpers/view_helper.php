236247c16217410ff121fe2aeaf88722ce322451
shervine <shervin@mench.com>
2024-01-24 09:45:05 -0800

    added btn class

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2171,7 +2171,7 @@ function view_card_e($x__type, $e, $extra_class = null)
             array_push($social_listed, $e__id);
 
             //Append to links:
-            $social_ui .= '<li class="icon-block"><a '.$social_url.' data-toggle="tooltip" data-placement="top" title="'.$m['m__title'].'">'.$m['m__cover'].'</a></li>';
+            $social_ui .= '<li class="icon-block-xx"><a '.$social_url.' data-toggle="tooltip" data-placement="top" title="'.$m['m__title'].'">'.$m['m__cover'].'</a></li>';
 
         }
     }
