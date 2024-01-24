94d27996b35ec5437f68816467e33ce6160ef471
shervine <shervin@mench.com>
2024-01-23 22:24:56 -0800

    added btn class

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -710,7 +710,7 @@ function view_i_covers($x__type, $i__id, $page_num = 0, $append_card_icon = true
                 return null;
             }
 
-            $card_icon = '<span class="icon-block-xx">'.$e___11035[$x__type]['m__cover'].'</span>';
+            $card_icon = '<span class="icon-block-xs">'.$e___11035[$x__type]['m__cover'].'</span>';
 
             $ui = '<div class="dropdown inline-block">';
             $ui .= '<button type="button" class="btn no-left-padding no-right-padding main__title load_i_covers button_of_'.$i__id.'_'.$x__type.'" id="card_group_i_'.$x__type.'_'.$i__id.'" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" load_x__type="'.$x__type.'" load_i__id="'.$i__id.'" load_counter="'.$count_query.'" load_first_segment="'.$first_segment.'"><span title="'.$title_desc.'" data-toggle="tooltip" data-placement="top">'.$card_icon.$visual_counter.'</span></button>';
