7e89723d48759f3e40d8bd62f08d70f26b117ff0
shervine <shervin@mench.com>
2024-01-24 09:49:18 -0800

    added btn class

--- application/views/e_layout.php
+++ application/views/e_layout.php
@@ -40,7 +40,7 @@ foreach($this->config->item('e___31916') as $x__type => $m) {
         if(in_array($x__type, $this->config->item('n___42261'))){
 
             //ADD IDEAS
-            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-12 container-center"><div class="list-group"><div class="list-group-item dropdown_'.$x__type.' list-adder">
+            $input_content .= '<div class="new_list new-list-'.$x__type.'"><div class="col-12 container-center"><div class="list-group"><div class="list-group-item dropdown_'.$x__type.' list-adder">
                 <div class="input-group border">
                     <input type="text"
                            class="form-control form-control-thick algolia_finder dotransparent add-input"
@@ -50,7 +50,7 @@ foreach($this->config->item('e___31916') as $x__type => $m) {
         } elseif(in_array($x__type, $this->config->item('n___11028'))){
 
             //ADD SOURCES
-            $input_content .= '<div class="new-list-'.$x__type.'"><div class="col-12 container-center"><div class="dropdown_'.$x__type.' list-adder">
+            $input_content .= '<div class="new_list new-list-'.$x__type.'"><div class="col-12 container-center"><div class="dropdown_'.$x__type.' list-adder">
                     <div class="input-group border">
                         <input type="text"
                                class="form-control form-control-thick algolia_finder dotransparent add-input"
