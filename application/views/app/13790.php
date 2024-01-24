5402ce12a9050fd3eeb4b3c9932d375789993e52
shervine <shervin@mench.com>
2024-01-23 20:02:53 -0800

    Borders removed :)

--- application/views/app/13790.php
+++ application/views/app/13790.php
@@ -217,7 +217,7 @@ echo '</table>';
         cursor: ns-resize !important;
     }
     #sortable_table th, #sortable_table td{
-        border: 1px solid #999999 !important;
+        border: 1px solid #000000 !important;
     }
 
     #sortable_table th:hover, #sortable_table th:active{
