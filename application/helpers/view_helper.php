e87cfd3661fc662b8c7a48fec58a095c3bc82cd8
shervine <shervin@mench.com>
2024-01-24 21:25:01 -0800

    added btn class

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2121,8 +2121,8 @@ function view_card_e($x__type, $e, $extra_class = null)
 
     $ui .= '<div class="creator_headline grey mini-frame">@<span class="ui_e__handle_'.$e['e__id'].'" title="ID '.$e['e__id'].'">'.$e['e__handle'].'</span></div>';
 
-    if ($grant_privacy && $x__id && !$is_app) {
-        $ui .= '<span class="icon-block-sm ui_x__message_' . $x__id . '" data-title="'.htmlentities($e['x__message']).'" data-toggle="tooltip" data-placement="top" data-bs-html="true">c'.$e___11035[4372]['m__cover'].'</span>';
+    if ($grant_privacy && $x__id && !$is_app && strlen($e['x__message'])) {
+        $ui .= '<span class="icon-block-sm ui_x__message_' . $x__id . '" data-title="'.htmlentities($e['x__message']).'" data-toggle="tooltip" data-placement="top" data-bs-html="true">'.$e___11035[4372]['m__cover'].'</span>';
     } elseif($is_app && isset($e['x__message']) && strlen($e['x__message'])){
         $ui .= '<span class="icon-block-sm" data-toggle="tooltip" data-placement="top" title="'.$e['x__message'].'"><i class="far fa-info-circle"></i></span>';
     }
