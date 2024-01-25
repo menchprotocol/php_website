b4205edbc865be28d5122dd6246f8f8f5c5bd452
shervine <shervin@mench.com>
2024-01-25 09:23:17 -0800

    stats

--- application/views/app/33292.php
+++ application/views/app/33292.php
@@ -102,7 +102,7 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
 
     $(document).ready(function () {
 
-        $("h1").append('<a class="icon-block" href="javascript:void(0);" onclick="$(\'.advanced-stats\').toggleClass(\'hidden\');"><i class="fas fa-search-plus advanced-stats" style="font-size: 0.34em !important;"></i><i class="fas fa-search-minus advanced-stats hidden" style="font-size: 0.34em !important;"></i></a>').append('<a class="icon-block" href="javascript:void(0);" onclick="$(\'.points_frame\').toggleClass(\'hidden\');"><span class="points_frame"><?= $e___11035[42225]['m__cover'] ?></span><span class="points_frame hidden"><i class="fas fa-hexagon fa-spin-slow zq6255" aria-hidden="true"></i></span></a>');
+        $("h1").append('<a class="icon-block" href="javascript:void(0);" onclick="$(\'.advanced-stats\').toggleClass(\'hidden\');"><i class="fas fa-search-plus advanced-stats" style="font-size: 0.34em !important;"></i><i class="fas fa-search-minus advanced-stats hidden" style="font-size: 0.34em !important;"></i></a>').append('<a class="icon-block-xs advanced-stats hidden" href="javascript:void(0);" onclick="$(\'.points_frame\').toggleClass(\'hidden\');"><span class="points_frame"><?= $e___11035[42225]['m__cover'] ?></span><span class="points_frame hidden"><i class="fas fa-hexagon fa-spin-slow zq6255" aria-hidden="true"></i></span></a>');
 
         //Load initial stats:
         load_stats_33292();
