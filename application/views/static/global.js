58f1ca29236d33291f09f2fd149d83014e622ec0
shervine <shervin@mench.com>
2024-01-24 21:37:27 -0800

    added btn class

--- application/views/static/global.js
+++ application/views/static/global.js
@@ -1100,8 +1100,8 @@ function editor_load_i(i__id, x__id, link_i__id = 0, quote_i__id = 0){
         $('#modal31911 .save_x__id').val(x__id);
 
         //Idea<>Idea links do not have an interaction message
-        if(fetch_int_val('#focus_card')!=12273 || $('.ui_x__message_'+x__id+':first').attr('title').length>0){
-            $('#modal31911 .save_x__message').val($('.ui_x__message_'+x__id+':first').attr('title'));
+        if(fetch_int_val('#focus_card')!=12273 || $('.ui_x__message_'+x__id+':first').attr("title").length>0){
+            $('#modal31911 .save_x__message').val($('.ui_x__message_'+x__id+':first').attr("title"));
             $('#modal31911 .save_x__frame').removeClass('hidden');
         }
     }
@@ -1280,7 +1280,7 @@ function editor_save_i(){
             }
 
             if(modify_data['save_x__id'] && modify_data['save_x__message']!='IGNORE_INPUT'){
-                $('.ui_x__message_'+modify_data['save_x__id']).attr('title', modify_data['save_x__message']);
+                $('.ui_x__message_'+modify_data['save_x__id']).attr("title", modify_data['save_x__message']);
                 if(modify_data['save_x__message'].length){
                     $('.ui_x__message_'+modify_data['save_x__id']).removeClass('hidden');
                 } else {
@@ -1455,8 +1455,8 @@ function editor_load_e(e__id, x__id){
 
 
     if(x__id){
-        console.log('What:'+$('.ui_x__message_'+x__id).attr('title'));
-        $('#modal31912 .save_x__message').val($('.ui_x__message_'+x__id).attr('title'));
+        console.log('What:'+$('.ui_x__message_'+x__id).attr("title"));
+        $('#modal31912 .save_x__message').val($('.ui_x__message_'+x__id).attr("title"));
         $('#modal31912 .save_x__frame').removeClass('hidden');
         setTimeout(function () {
             set_autosize($('#modal31912 .save_x__message'));
@@ -1615,7 +1615,7 @@ function editor_save_e(){
             update_cover_main(modify_data['save_e__cover'], '.s__12274_'+modify_data['save_e__id']);
 
             if( modify_data['save_x__id'] && modify_data['save_x__message']!='IGNORE_INPUT'){
-                $('.ui_x__message_'+ modify_data['save_x__id']).attr('title',modify_data['save_x__message']);
+                $('.ui_x__message_'+ modify_data['save_x__id']).attr("title",modify_data['save_x__message']);
                 if(modify_data['save_x__message'].length){
                     $('.ui_x__message_'+modify_data['save_x__id']).removeClass('hidden');
                 } else {
