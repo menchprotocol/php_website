b21ad59dedaaf8622e9ccf8d2d69d6561ea871ff
shervine <shervin@mench.com>
2024-01-26 12:58:47 -0800

    Idea/Source Editors

--- application/views/header.php
+++ application/views/header.php
@@ -1134,7 +1134,7 @@ if($member_e && ( !isset($basic_header_footer) || !$basic_header_footer )){
                             <div class="dynamic_editing_input">
                                 <h3 class="mini-font"><?= '<span class="icon-block-xs">'.$e___11035[6198]['m__cover'].'</span>'.$e___11035[6198]['m__title'].': '. ( in_array(6198, $this->config->item('n___42174')) ? ' <b title="Required Field" style="color:#FF0000;">*</b>' : '' );  ?></h3>
                                 <!-- Cover HIDDEN Input (Editable for font awesome icons only) -->
-                                <input type="text" class="form-control unsaved_warning save_e__cover " data-lpignore="true" placeholder="Emoji, Image URL or Cover Code">
+                                <input type="text" class="form-control unsaved_warning save_e__cover superpower__13758" data-lpignore="true" placeholder="Emoji, Image URL or Cover Code">
                                 <div>
                                     <!-- Cover Settings/Selectors -->
                                     <div class="icons_small font_awesome hidden section_subframe">
