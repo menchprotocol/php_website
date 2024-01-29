c0a39b36d01ebfdd90d7ca55212df57b987213cb
shervine <shervin@mench.com>
2024-01-28 18:06:53 -0800

    Idea/Source Editors

--- application/views/global.js
+++ application/views/global.js
@@ -1180,11 +1180,11 @@ function editor_load_i(i__id, x__id, link_i__id = 0, quote_i__id = 0){
 
                     $("#modal31911 .dynamic_"+i+" .radio_frame").remove();
                     if(data.return_inputs[index_i]["d__is_radio"]){
-                        $("#modal31911 .dynamic_"+i+" .text_content").addClass('hidden');
                         $("#modal31911 .dynamic_"+i).prepend( '<div class="radio_frame hideIfEmpty">' + data.return_inputs[index_i]["d__profile_header"] + data.return_inputs[index_i]["d__html"] + '</div>' );
+                        $("#modal31911 .dynamic_"+i+" .text_content").addClass('hidden');
                     } else {
-                        $("#modal31911 .dynamic_"+i+" .text_content").removeClass('hidden');
                         $("#modal31911 .dynamic_"+i).prepend( '<div class="radio_frame hideIfEmpty">' + data.return_inputs[index_i]["d__profile_header"] + '</div>' );
+                        $("#modal31911 .dynamic_"+i+" .text_content").removeClass('hidden');
                         $("#modal31911 .dynamic_"+i+" h3").html(data.return_inputs[index_i]["d__html"]);
                         $("#modal31911 .dynamic_"+i+" input").attr('placeholder',data.return_inputs[index_i]["d__placeholder"]).attr('type',data.return_inputs[index_i]["d__type_name"]).attr('d__id',data.return_inputs[index_i]["d__id"]).attr('d_x__id',data.return_inputs[index_i]["d_x__id"]).val(data.return_inputs[index_i]["d__value"]);
 
@@ -1543,11 +1543,11 @@ function editor_load_e(e__id, x__id){
 
                 $("#modal31912 .dynamic_"+i+" .radio_frame").remove();
                 if(data.return_inputs[index_i]["d__is_radio"]){
+                    $("#modal31912 .dynamic_"+i).prepend( '<div class="radio_frame hideIfEmpty">' + data.return_inputs[index_i]["d__profile_header"] + data.return_inputs[index_i]["d__html"] + '</div>' );
                     $("#modal31912 .dynamic_"+i+" .text_content").addClass('hidden');
-                    $("#modal31912 .dynamic_"+i).prepend( '<div class="radio_frame hideIfEmpty">' + data.return_inputs[index_i]["d__html"] + '</div>' );
                 } else {
+                    $("#modal31912 .dynamic_"+i).prepend( '<div class="radio_frame hideIfEmpty">' + data.return_inputs[index_i]["d__profile_header"] + '</div>' );
                     $("#modal31912 .dynamic_"+i+" .text_content").removeClass('hidden')
-                    $("#modal31912 .dynamic_"+i).prepend( '<div class="radio_frame">' + data.return_inputs[index_i]["d__profile_header"] + '</div>' );
                     $("#modal31912 .dynamic_"+i+" h3").html(data.return_inputs[index_i]["d__html"]);
                     $("#modal31912 .dynamic_"+i+" input").attr('placeholder',data.return_inputs[index_i]["d__placeholder"]).attr('type',data.return_inputs[index_i]["d__type_name"]).attr('d__id',data.return_inputs[index_i]["d__id"]).attr('d_x__id',data.return_inputs[index_i]["d_x__id"]).val(data.return_inputs[index_i]["d__value"]);
 
