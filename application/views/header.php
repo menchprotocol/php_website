bfae96edcf28c4b4096705ac568c9844d98bf135
shervine <shervin@mench.com>
2024-01-31 12:40:47 -0800

    Idea/Source Editors

--- application/views/header.php
+++ application/views/header.php
@@ -970,13 +970,13 @@ if($member_e && ( !isset($basic_header_footer) || !$basic_header_footer )){
                         <div class="inner_message">
 
                             <!-- Upload -->
-                            <div class="dynamic_editing_input" style="margin: 0 !important;">
+                            <div class="dynamic_editing_input solo_input" style="margin: 0 !important;">
                                 <a class="uploader_13572 icon-block-xs" href="javascript:void(0);" title="<?= $e___11035[13572]['m__title'] ?>"><?= $e___11035[13572]['m__cover'] ?></a>
                             </div>
 
                             <!-- EMOJI -->
                             <div class="dynamic_editing_input" style="margin: 0 !important;">
-                                <div class="dropdown emoji_selector">
+                                <div class="dropdown solo_input">
                                     <button type="button" class="btn no-left-padding no-right-padding icon-block-xs" id="emoji_i" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-face-smile"></i></button>
                                     <div class="dropdown-menu emoji_i" aria-labelledby="emoji_i"></div>
                                 </div>
