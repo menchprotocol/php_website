ad3b87ecbdf795ef2658ca3899306e0fe0536b0a
shervine <shervin@mench.com>
2024-02-01 17:15:37 -0800

    Idea/Source Editors

--- application/views/global.js
+++ application/views/global.js
@@ -1438,7 +1438,7 @@ function cloudinary_add_pending(info){
 function cloudinary_add_uploaded(info){
 
     //Assume its a file unless proven otherwise:
-    var view_template = '<h6>'+info.original_filename+'</h6>';
+    var view_template = '<h6>'+info.original_filename+'</h6><span><i class="fas fa-file"></i></span>';
     var media_type = 42185; //File
 
     //See if we can find a Video, Image or Audio file:
@@ -1446,15 +1446,15 @@ function cloudinary_add_uploaded(info){
         if(js_e___42641[4258]['m__message'].split(' ').includes(info.format) && info.video){
             //Video
             media_type = 4258;
-            view_template = '<video class="play_video" onclick="this.play()" controls poster="'+info.thumbnail_url.replace('c_limit,h_60,w_90','c_fill,h_233,w_233')+'"><source src="'+info.secure_url+'" type="video/'+info.format+'"></video>';
+            view_template = '<video class="play_video" onclick="this.play()" controls poster="'+info.thumbnail_url.replace('c_limit,h_60,w_90','c_fill,h_233,w_233')+'"><source src="'+info.secure_url+'" type="video/'+info.format+'"></video><h6>'+info.original_filename+'</h6>';
         } else if(js_e___42641[4260]['m__message'].split(' ').includes(info.format) && info.resource_type=='image'){
             //Image
             media_type = 4260;
-            view_template = '<img src="'+info.thumbnail_url.replace('c_limit,h_60,w_90','c_fill,h_233,w_233')+'" />';
+            view_template = '<img src="'+info.thumbnail_url.replace('c_limit,h_60,w_90','c_fill,h_233,w_233')+'" /><h6>'+info.original_filename+'</h6>';
         } else if(js_e___42641[4259]['m__message'].split(' ').includes(info.format) && info.audio){
             //Audio
             media_type = 4259;
-            view_template = '<audio controls src="'+info.secure_url+'"></audio><span><i class="fas fa-file"></i></span>';
+            view_template = '<audio controls src="'+info.secure_url+'"></audio><h6>'+info.original_filename+'</h6>';
         }
     }
 
