92376719e4c1d368a82904c926a0b7c1eae8cfaa
shervine <shervin@mench.com>
2024-02-07 14:01:36 -0800

    Cloudinary Integration

--- application/views/global.js
+++ application/views/global.js
@@ -1642,8 +1642,9 @@ function cloudinary_load_source(uploader_id, info_id, media_e__id, playback_code
     if(media_e__id == 4258){
 
         //Video
-        $('#'+info_id).html('<input type="text" value="'+e__title+'" placeholder="Source Title" /><span title="'+js_e___42294[media_e__id]['m__title']+'">'+js_e___42294[media_e__id]['m__cover']+'</span><video id="video_player_'+playback_code+'" controls class="cld-video-player vjs-fade-out cld-fluid cld-video-player-skin-light" poster="'+e__cover+'"></video><a href="javascript:void(0)" onclick="delete_media(\''+uploader_id+'\',\''+info_id+'\')"><i class="fas fa-xmark"></i></a>');
-        play_video(playback_code);
+        $('#'+info_id).html('<input type="text" value="'+e__title+'" placeholder="Source Title" /><span title="'+js_e___42294[media_e__id]['m__title']+'">'+js_e___42294[media_e__id]['m__cover']+'</span><img src="'+e__cover+'" /><a href="javascript:void(0)" onclick="delete_media(\''+uploader_id+'\',\''+info_id+'\')"><i class="fas fa-xmark"></i></a>');
+        //<video id="video_player_'+playback_code+'" controls class="cld-video-player vjs-fade-out cld-fluid cld-video-player-skin-light" poster="'+e__cover+'"></video>
+        //play_video(playback_code);
 
     } else if(media_e__id == 4260){
 
