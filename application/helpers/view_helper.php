d3e59c7d6662e58d8ac4ef3b6006eea4f59be0f1
shervine <shervin@mench.com>
2024-02-14 14:09:55 -0800

    Idea Linking Bug Fixes

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1678,8 +1678,8 @@ function view_card_i($x__type, $i, $previous_i = null, $top_i__hashtag = null, $
                 )))){
 
                 //Reply Inverse / Quote
-                $bottom_bar_ui .= '<span><div class="'.( $always_see ? '' : 'show-on-hover' ).'">';
-                $bottom_bar_ui .= '<a href="/'.$i['i__hashtag'].'/start" class="main__title"><span class="icon-block-xs">'.$m_top_bar['m__cover'].'</span>'.$m_top_bar['m__title'].'</a>';
+                $bottom_bar_ui .= '<span class="icon-block-xs"><div class="'.( $always_see ? '' : 'show-on-hover' ).'">';
+                $bottom_bar_ui .= '<a href="/'.$i['i__hashtag'].'/start">'.$m_top_bar['m__cover'].'</a>';
                 $bottom_bar_ui .= '</div></span>';
 
             } elseif($x__type_top_bar==13909 && $write_privacy_i && $has_sortable){
