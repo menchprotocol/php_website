20ae4fc097c3c9df380790b29bf26a050969a643
shervine <shervin@mench.com>
2024-02-17 11:58:35 -0800

    Idea Modal Updates

--- application/views/x_layout.php
+++ application/views/x_layout.php
@@ -937,7 +937,7 @@ if($top_i__hashtag){
         $('[data-toggle="popover"]').popover({
             html: true,
             content: function () {
-                alert($('.popover-header').text());
+                alert($('.popover-title').text());
                 $.post("/ajax/load_popover", {
                     handle_string:'@shervin',
                 }, function (data) {
