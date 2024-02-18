33abca27945efbcaa0b856d1aa99cfeac81b6514
shervine <shervin@mench.com>
2024-02-17 20:08:37 -0800

    URL Routes Simplified

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1171,9 +1171,9 @@ function view_sync_links($str, $return_array = false, $save_i__id = 0) {
 
     $ui_template = array(
         4256 => '<spanaa href="%s" target="_blank" class="ignore-click"><span class="url_truncate">%s</span></spanaa>',
-        31834 => '<span href="/%s" data-toggle="popover" class="ref_idea">%s</span>',
-        42337 => '<span href="/%s" data-toggle="popover" class="ref_idea">%s</span>',
-        31835 => '<span href="/@%s" data-toggle="popover" class="ref_source">%s</span>',
+        31834 => '<spanaa href="/%s" data-toggle="popover" class="ref_idea">%s</spanaa>',
+        42337 => '<spanaa href="/%s" data-toggle="popover" class="ref_idea">%s</spanaa>',
+        31835 => '<spanaa href="/@%s" data-toggle="popover" class="ref_source">%s</spanaa>',
     );
 
     $replace_from = array();
