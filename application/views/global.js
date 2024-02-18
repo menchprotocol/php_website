eef55761645cd3e938acfb66838aaafe55871974
shervine <shervin@mench.com>
2024-02-18 14:43:04 -0800

    Discovery Mode

--- application/views/global.js
+++ application/views/global.js
@@ -799,9 +799,8 @@ function random_animal(basic_style = false){
 }
 
 function activate_popover(){
-    var glower = $('.glow-red');
     window.setInterval(function() {
-        glower.toggleClass('active');
+        $('.glow-red').toggleClass('active');
     }, 987);
 
     $('[data-toggle="tooltip"]').tooltip();
