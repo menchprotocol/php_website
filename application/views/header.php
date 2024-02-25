7644614cb2017e3fdd26d3ff0cd1290adbecc0b0
shervine <shervin@mench.com>
2024-02-25 08:16:41 -0800

    App Framework

--- application/views/header.php
+++ application/views/header.php
@@ -479,11 +479,17 @@ if(!$basic_header_footer){
                     }
 
 
-                    //Always give option to ideate:
-                    if($player_e){
-                        echo '<td class="block-x"><a href="javascript:void(0);" onclick="i_editor_load()" title="'.$e___11035[33532]['m__title'].'">'.$e___11035[33532]['m__cover'].'</a></td>'; //Add New Idea
+
+                    //Add Source
+                    if(superpower_unlocked(13422)){
+                        echo '<td class="block-x"><a href="javascript:void(0);" onclick="e_editor_load()" title="'.$e___11035[42819]['m__title'].'">'.$e___11035[42819]['m__cover'].'</a></td>';
                     }
 
+
+                    //Add Idea:
+                    if($player_e){
+                        echo '<td class="block-x"><a href="javascript:void(0);" onclick="i_editor_load()" title="'.$e___11035[33532]['m__title'].'">'.$e___11035[33532]['m__cover'].'</a></td>';
+                    }
 
 
                     //MENU
@@ -560,11 +566,6 @@ if(!$basic_header_footer){
                     echo '</div>';
                     echo '</div>';
                     echo '</td>';
-
-                    //Add Source
-                    if(superpower_unlocked(13422)){
-                        echo '<td class="block-x"><a href="javascript:void(0);" onclick="e_editor_load()" title="'.$e___11035[42819]['m__title'].'">'.$e___11035[42819]['m__cover'].'</a></td>';
-                    }
 
                     ?>
                 </tr>
