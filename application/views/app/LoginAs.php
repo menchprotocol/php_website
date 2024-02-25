1dd6d92e0d7f5697d5fd052dee4fda5e3ba4c228
shervine <shervin@mench.com>
2024-02-25 15:55:31 -0800

    App Framework

--- application/views/app/LoginAs.php
+++ application/views/app/LoginAs.php
@@ -1,47 +1,25 @@
 <?php
 
-if(!isset($_GET['e__handle']) || !strlen($_GET['e__handle'])) {
+//Make sure member:
+if(!count($this->X_model->fetch(array(
+    'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+    'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
+    'x__following IN (' . join(',', $this->config->item('n___32537')) . ')' => null, //Interested Member
+    'x__follower' => $focus_e['e__id'],
+)))){
+
     return view_json(array(
         'status' => 0,
-        'message' => 'Missing e__handle',
+        'message' => 'Source is not an interested member',
     ));
+
 } else {
 
-    //Login as user:
-    $es = $this->E_model->fetch(array(
-        'LOWER(e__handle)' => strtolower($_GET['e__handle']),
-    ));
+    session_delete();
 
-    if (!count($es) || !in_array($es[0]['e__privacy'], $this->config->item('n___7358'))) {
-        return view_json(array(
-            'status' => 0,
-            'message' => 'Source is not active.',
-        ));
-    } else {
+    //Assign session & log transaction:
+    $this->E_model->activate_session($focus_e);
 
-        //Make sure member:
-        if(!count($this->X_model->fetch(array(
-            'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
-            'x__following IN (' . join(',', $this->config->item('n___32537')) . ')' => null, //Interested Member
-            'x__follower' => $es[0]['e__id'],
-        )))){
+    js_php_redirect(view_memory(42903,42902).$focus_e['e__handle'], 1597);
 
-            return view_json(array(
-                'status' => 0,
-                'message' => 'Source is not an interested member',
-            ));
-
-        } else {
-
-            session_delete();
-
-            //Assign session & log transaction:
-            $this->E_model->activate_session($es[0]);
-
-            js_php_redirect(view_memory(42903,42902).$es[0]['e__handle']);
-        }
-
-
-    }
 }
