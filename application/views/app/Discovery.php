2b165b52c0458b8efe821193965913fb4b9b7726
shervine <shervin@mench.com>
2024-03-08 15:01:15 -0800

    App Framework

--- application/views/app/Discovery.php
+++ application/views/app/Discovery.php
@@ -103,7 +103,7 @@ if($player_e){
 
 
 //Focus Discovery:
-echo '<div class="main_item view_6255 row justify-content">';
+echo '<div class="main_item row justify-content">';
 echo view_card_i(43007, $focus_i);
 echo '</div>';
 
