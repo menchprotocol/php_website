90ebde13080b49e5d2ce733957a067978a09cff3
shervine <shervin@mench.com>
2024-03-09 19:24:16 -0800

    App Framework

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1493,6 +1493,9 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
     $access_level_i = access_level_i($i['i__hashtag'], 0, $i);
     $i_startable = i_startable($i);
     $discovery_mode = ( (isset($_POST['js_request_uri']) && substr_count($_POST['js_request_uri'], '/')==2) || (!isset($_POST['js_request_uri']) && strlen($CI->uri->segment(2))) );
+    if($discovery_mode && !$target_i__hashtag){
+        $target_i__hashtag = $CI->uri->segment(1);
+    }
 
     $focus__node = in_array($x__type, $CI->config->item('n___12149')); //NODE COIN
     $x__player = ( $x__id && isset($i['x__player']) ? $i['x__player'] : ( $focus_e && $focus_e['e__id'] ? $focus_e['e__id'] : ( $player_e && $player_e['e__id'] ? $player_e['e__id'] : 0 ) ) );
@@ -1507,7 +1510,7 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
     if($discovery_mode || $cache_app) {
         if(($goto_start || !$superpower_10939) && $i_startable){
             $href = view_memory(42903,30795).$i['i__hashtag'].'/'.view_memory(6404,4235);
-        } elseif($link_creator && $target_i__hashtag){
+        } elseif($target_i__hashtag){ //$link_creator &&
             $href = view_memory(42903,30795).$target_i__hashtag.'/'.$i['i__hashtag'];
         } else {
             $href = view_memory(42903,33286).$i['i__hashtag'];
