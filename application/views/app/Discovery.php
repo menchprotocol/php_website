00071e2d41291b1049953dfb964b1f58b840a3d1
shervine <shervin@mench.com>
2024-03-10 11:09:32 -0700

    App Framework

--- application/views/app/Discovery.php
+++ application/views/app/Discovery.php
@@ -41,21 +41,18 @@ if($x__player && $target_i__hashtag!=$focus_i['i__hashtag']){
             $level++;
 
             //Does this have a follower list?
-            /*
             $query_subset = $this->X_model->fetch(array(
                 'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
                 'i__privacy IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
                 'x__type IN (' . join(',', $this->config->item('n___42267')) . ')' => null, //Sequence Down
                 'x__previous' => $followings_i['i__id'],
             ), array('x__next'), 0, 0, array('x__weight' => 'ASC'), '*', null, true);
-            && count($query_subset) >= 2
-            */
 
             $breadcrum_content .= '<li class="breadcrumb-item">';
             $breadcrum_content .= '<a href="'.view_memory(42903,30795).$target_i__hashtag.'/'.$followings_i['i__hashtag'].'"><u>'.view_i_title($followings_i).'</u></a>';
 
             //Do we have more sub-items in this branch? Must have more than 1 to show, otherwise the 1 will be included in the main branch:
-            if(0){
+            if(count($query_subset) >= 2){
                 //Show other branches:
                 $breadcrum_content .= '<div class="dropdown inline-block">';
                 $breadcrum_content .= '<button type="button" class="btn no-side-padding" id="dropdown_instant_'.$followings_i['i__id'].'" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
@@ -63,7 +60,19 @@ if($x__player && $target_i__hashtag!=$focus_i['i__hashtag']){
                 $breadcrum_content .= '</button>';
                 $breadcrum_content .= '<div class="dropdown-menu" aria-labelledby="dropdown_instant_'.$followings_i['i__id'].'">';
                 foreach ($query_subset as $i_subset) {
-                    $breadcrum_content .= '<a href="'.view_memory(42903,30795).$target_i__hashtag.'/'.$i_subset['i__hashtag'].'" class="dropdown-item main__title '.( in_array($i_subset['i__id'], $main_branch) ? ' active ' : '' ).'">'.view_i_title($i_subset).'</a>';
+
+                    if(count($this->X_model->fetch(array(
+                        'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+                        'x__type IN (' . join(',', $this->config->item('n___6255')) . ')' => null, //DISCOVERIES
+                        'x__player' => $x__player,
+                        'x__previous' => $i_subset['i__id'],
+                    )))){
+                        $breadcrum_content .= '<a href="'.view_memory(42903,30795).$target_i__hashtag.'/'.$i_subset['i__hashtag'].'" class="dropdown-item main__title '.( in_array($i_subset['i__id'], $main_branch) ? ' active ' : '' ).'">'.view_i_title($i_subset).'</a>';
+                    } else {
+                        //Undiscovered
+                        $breadcrum_content .= '<a href="javascript:void(0)" class="dropdown-item main__title '.( in_array($i_subset['i__id'], $main_branch) ? ' active ' : '' ).'">'.view_i_title($i_subset).'<span class="icon-block-sm">'.$e___11035[43010]['m__cover'].'</span></a>';
+                    }
+
                 }
                 $breadcrum_content .= '</div>';
                 $breadcrum_content .= '</div>';
