babcbc6cec51abbebd579bd8f12d961ca7ac39da
shervine <shervin@mench.com>
2024-03-16 13:20:38 -0700

    Interactive Discovery

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -68,7 +68,7 @@ function is_valid_date($str)
 }
 
 function target_disccovery(){
-    return ( isset($_POST['js_request_uri']) && substr_count($_POST['js_request_uri'], '/')==2 ? one_two_explode('/','/',$_POST['js_request_uri']).'|' : null );
+    return ( isset($_POST['js_request_uri']) && substr($_POST['js_request_uri'], 0, 1)=='/' && substr_count($_POST['js_request_uri'], '/')==2 ? strtok(substr($_POST['js_request_uri'], 1), '/').'/' : null );
 }
 function e_pinned($e__id, $first_pin_only, $return_itself = false){
     $CI =& get_instance();
