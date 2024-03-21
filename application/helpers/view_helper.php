540bc8a60249f706e6d3491341460d40ae7ef6a8
shervine <shervin@mench.com>
2024-03-21 00:11:31 -0700

    Interactive Discovery

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -2090,8 +2090,10 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
 
                  * */
 
+                $bottom_bar_ui .= '<script> $(document).ready(function () { $(\'.cache_frame_'.$i['i__id'].' .first_line\').append(\'<span title="'.$m_target_bar['m__title'].'" data-toggle="tooltip" data-placement="top"><span class="icon-block-sm">'.$m_target_bar['m__cover'].'</span></span>\') }); </script>';
+
                 //Locked
-                $bottom_bar_ui .= '<span title="'.$m_target_bar['m__title'].'" data-toggle="tooltip" data-placement="top"><span class="icon-block-sm">'.$m_target_bar['m__cover'].'</span></span>';
+                //$bottom_bar_ui .= '<span title="'.$m_target_bar['m__title'].'" data-toggle="tooltip" data-placement="top"><span class="icon-block-sm">'.$m_target_bar['m__cover'].'</span></span>';
 
             } elseif($x__type_target_bar==4235 && !$discovery_mode && $i_startable && $access_level_i>=1){
 
