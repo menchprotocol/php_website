438b12573e906066f8229bf4a3b1175bea5c2326
shervine <shervin@mench.com>
2024-03-24 21:01:55 -0700

    Interactive Discovery

--- application/views/includes/global.js
+++ application/views/includes/global.js
@@ -867,9 +867,11 @@ function load_card_clickers(){
     });
 
     if(typeof focus_i__type !== 'undefined' && focus_i__type>0){
+        console.log('ACTIVATED');
         var is_single_choice = ( focus_i__type==6684 );
         $(".this_selector").unbind();
         $(".this_selector").click(function (e) {
+            console.log('CLICKED');
             if($('.this_selector_'+$(this).attr('selection_i__id')+' i').hasClass('fa-square-check')){
                 //Already selected, so unselect:
                 $('.this_selector_'+$(this).attr('selection_i__id')+' i').removeClass('fas').removeClass('fa-square-check').addClass('far').addClass('fa-square');
