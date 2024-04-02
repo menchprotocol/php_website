dfed54f96948a455ed6bf374d45c148beae8f21d
shervine <shervin@mench.com>
2024-04-02 11:52:00 -0700

    Interactive Discovery

--- application/models/X_model.php
+++ application/models/X_model.php
@@ -1529,10 +1529,19 @@ class X_model extends CI_Model
             return false;
         }
 
-        $input__selection = in_array($i['i__type'], $this->config->item('n___7712'));
-        $recursive_down_ids = $this->I_model->recursive_down_ids($i, ( $input__selection ? 'OR' : 'AND' ));
+        $recursive_down_ids = $this->I_model->recursive_down_ids($i, 'AND');
         if(!isset($recursive_down_ids['recursive_i_ids']) || !count($recursive_down_ids['recursive_i_ids'])){
-            return false;
+            $input__selection = in_array($i['i__type'], $this->config->item('n___7712'));
+            if($input__selection){
+                return array(
+                    'fixed_total' => 1,
+                    'list_total' => array($i['i__id']),
+                    'fixed_discovered' => 1,
+                    'list_discovered' => array($i['i__hashtag']),
+                );
+            } else {
+                return false;
+            }
         }
 
         $current_level++;
