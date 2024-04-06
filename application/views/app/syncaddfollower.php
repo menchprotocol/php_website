9663262d9515ef06df07fbf36b548ee530af92bb
shervine <shervin@mench.com>
2024-04-05 18:02:23 -0700

    Interactive Discovery

--- application/views/app/syncaddfollower.php
+++ application/views/app/syncaddfollower.php
@@ -15,37 +15,38 @@ foreach($this->X_model->fetch(array(
     ), array('x__player'), 0) as $dicovered) {
 
         //Does this user already following the source?
-        if(!count($this->X_model->fetch(array(
+        if(count($this->X_model->fetch(array(
             'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
-            'x__type IN (' . join(',', $this->config->item('n___32292')) . ')' => null, //SOURCE LINKS
+            'x__type' => 4251, //SOURCE LINKS
             'x__following' => $addition_sync['x__following'],
             'x__follower' => $dicovered['x__player'],
         )))){
-
-            $counter++;
-
-            echo $counter.') @'.$dicovered['e__handle'].' not following @'.$addition_sync['e__handle'].' even though discovered #'.$addition_sync['i__hashtag'].'<hr />';
-
-            /*
-            //No, lets sync it:
-            $this->X_model->create(array(
-                'x__type' => 4251, //Creator
-                'x__message' => $dicovered['x__message'],
-                'x__player' => $dicovered['x__player'],
-                'x__following' => $addition_sync['x__following'],
-                'x__follower' => $dicovered['x__player'],
-            ));
-
-            $this->X_model->create(array(
-                'x__type' => 43047, //Sync Add Follower
-                'x__message' => $dicovered['x__message'],
-                'x__player' => $dicovered['x__player'],
-                'x__following' => $addition_sync['x__following'],
-                'x__follower' => $dicovered['x__player'],
-                'x__previous' => $addition_sync['i__id'],
-            ));
-
-            */
+            continue;
         }
+
+        $counter++;
+
+        echo $counter.') @'.$dicovered['e__handle'].' not following @'.$addition_sync['e__handle'].' even though discovered #'.$addition_sync['i__hashtag'].'<hr />';
+
+        /*
+        //No, lets sync it:
+        $this->X_model->create(array(
+            'x__type' => 4251, //Creator
+            'x__message' => $dicovered['x__message'],
+            'x__player' => $dicovered['x__player'],
+            'x__following' => $addition_sync['x__following'],
+            'x__follower' => $dicovered['x__player'],
+        ));
+
+        $this->X_model->create(array(
+            'x__type' => 43047, //Sync Add Follower
+            'x__message' => $dicovered['x__message'],
+            'x__player' => $dicovered['x__player'],
+            'x__following' => $addition_sync['x__following'],
+            'x__follower' => $dicovered['x__player'],
+            'x__previous' => $addition_sync['i__id'],
+        ));
+
+        */
     }
 }
