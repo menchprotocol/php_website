7592b21dce7cff87cb15f237a9e6434fc47b2d6d
shervine <shervin@mench.com>
2024-04-05 17:57:35 -0700

    Interactive Discovery

--- application/views/app/syncaddfollower.php
+++ application/views/app/syncaddfollower.php
@@ -1,10 +1,11 @@
 <?php
 
 //Sync All Adding followers:
+$counter = 0;
 foreach($this->X_model->fetch(array(
     'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
     'x__type' => 7545,
-), array('x__following','x__next'), 0) as $addition_sync){
+), array('x__following'), 0) as $addition_sync){
 
     //Fetch everyone who has discovered this idea:
     foreach($this->X_model->fetch(array(
@@ -21,7 +22,9 @@ foreach($this->X_model->fetch(array(
             'x__follower' => $dicovered['x__player'],
         )))){
 
-            echo '@'.$dicovered['e__handle'].' not following @'.$addition_sync['e__handle'].' even though discovered #'.$addition_sync['i__hashtag'].'<hr />';
+            $counter++;
+
+            echo $counter.') @'.$dicovered['e__handle'].' not following @'.$addition_sync['e__handle'].' even though discovered<hr />';
 
             /*
             //No, lets sync it:
