56982daea5e80043b357064203c9cdc0d3987084
shervine <shervin@mench.com>
2024-04-09 01:24:57 -0700

    Interactive Discovery

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1371,7 +1371,7 @@ function view_i_nav($discovery_mode, $focus_i){
     $ideation_pen = superpower_unlocked(10939);
     $e___loading_order = $CI->config->item('e___'.( $discovery_mode ? 26005 : 26005 ));
 
-    if(!$player_e){
+    if(!superpower_unlocked(10939)){
         return null;
     }
 
