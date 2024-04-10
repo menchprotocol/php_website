5e41fc4e368ec659e58abfeebcf30c12bc03ab1a
shervine <shervin@mench.com>
2024-04-09 19:01:47 -0700

    Interactive Discovery

--- application/views/app/discovery.php
+++ application/views/app/discovery.php
@@ -141,7 +141,7 @@ if($player_e){
         $(document).ready(function() {
             // Check if body height is higher than window height :)
             if ($("body").height() < $(window).height()) {
-                console.log('NO SCROLL: '+$$("body").height()+'/'+$(window).height());
+                console.log('NO SCROLL: '+$("body").height()+'/'+$(window).height());
                 $(".fixed-bottom").removeClass('hidden');
             } else {
                 $(window).scroll(function() {
