62fd4bdc4f62501b975385a0d72bb45d1b148389
Shervin Enayati <shervin@mench.co>
2024-07-14 18:19:58 -0700

    Bug fixes before launch

--- application/views/includes/global.js
+++ application/views/includes/global.js
@@ -949,7 +949,7 @@ function load_card_clickers(){
                         $(this).removeClass('fas').removeClass('fa-square-check').addClass('far').addClass('fa-square');
                     });
                     //Go Next:
-                    if(!$('.input_ui_'+$(this).attr('selection_i__id'))[0] || $('.input_ui_'+$(this).attr('selection_i__id')).count()==1){
+                    if(!$('.input_ui_'+$(this).attr('selection_i__id'))[0] || $('.input_ui_'+$(this).attr('selection_i__id')).length==1){
                         //Since there is no input for this single select, we can instantly go next:
                         setTimeout(function () {
                             go_next(0);
