be8daae2f92550d2d2b34e047d8a7af954dee269
Shervin Enayati <shervin@mench.co>
2024-07-14 17:47:33 -0700

    Bug fixes before launch

--- application/views/app/paypalpayment.php
+++ application/views/app/paypalpayment.php
@@ -11,7 +11,6 @@ if(isset($_POST['payment_status'])){
     $idea_refs = explode('#', $item_numbers[0]);
     $hash_target = count($idea_refs)>=3 && strlen($idea_refs[2]);
 
-
     $player_es = $this->E_model->fetch(array(
         'LOWER(e__handle)' => strtolower($item_numbers[1]),
     ));
@@ -39,8 +38,6 @@ if(isset($_POST['payment_status'])){
         $is_pending = ($_POST['payment_status']=='Pending');
         $is_good = true;
 
-        //, $x__player, $target_i__id, $i
-
         if($pay_amount > 0){
 
             //Paid:
@@ -48,7 +45,7 @@ if(isset($_POST['payment_status'])){
             $x__type = ( $is_pending ? 35572 /* Pending Payment */ : 26595 );
 
             //Log Payment:
-            $completion_status = $this->X_model->mark_complete($x__type, $x__player, $target_i__id, $next_is[0], array(), array(
+            $completion_status = $this->X_model->mark_complete($x__type, $x__player, ( isset($target_is[0]['i__id']) ? $target_is[0]['i__id'] : 0 ), $next_is[0], array(), array(
                 'x__weight' => intval($_POST['quantity']),
                 'x__metadata' => $_POST,
             ));
@@ -66,7 +63,7 @@ if(isset($_POST['payment_status'])){
             ));
 
             //Log Refund:
-            $completion_status = $this->X_model->mark_complete($x__type, $x__player, $target_i__id, $next_is[0], array(), array(
+            $completion_status = $this->X_model->mark_complete($x__type, $x__player, ( isset($target_is[0]['i__id']) ? $target_is[0]['i__id'] : 0 ), $next_is[0], array(), array(
                 'x__weight' => (-1 * ( isset($original_payment[0]['x__weight']) ? $original_payment[0]['x__weight'] : 1 )),
                 'x__metadata' => $_POST,
                 'x__reference' => ( isset($original_payment[0]['x__id']) ? $original_payment[0]['x__id'] : 0 ),
