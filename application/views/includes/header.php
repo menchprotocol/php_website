5aa9ea0c6e36daa773ab8b3fdaa3463920f9cdfb
Shervin Enayati <shervin@mench.co>
2024-07-14 18:53:51 -0700

    Bug fixes before launch

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -299,6 +299,79 @@ if($website_id==39599){
 
 <?php
 
+
+//Called when the paypal payment is complete:
+if(isset($_POST['payment_status']) && isset($_POST['item_number'])){
+
+    //Log New Payment:
+    $item_numbers = explode('@', $_POST['item_number']);
+    $idea_refs = explode('#', $item_numbers[0]);
+    $hash_target = count($idea_refs)>=3 && strlen($idea_refs[2]);
+
+    $player_es = $this->E_model->fetch(array(
+        'LOWER(e__handle)' => strtolower($item_numbers[1]),
+    ));
+    $website_es = $this->E_model->fetch(array(
+        'LOWER(e__handle)' => strtolower($item_numbers[2]),
+    ));
+    $next_is = $this->I_model->fetch(array(
+        'LOWER(i__hashtag)' => strtolower(( $hash_target ? $idea_refs[2] : $idea_refs[1] )),
+        'i__privacy IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
+    ));
+    $target_is = ($hash_target ? $this->I_model->fetch(array(
+        'LOWER(i__hashtag)' => strtolower($idea_refs[1]),
+        'i__privacy IN (' . join(',', $this->config->item('n___31871')) . ')' => null, //ACTIVE
+    )) : false);
+
+
+
+    $x__player = intval($item_numbers[3]);
+    $pay_amount = doubleval(( strlen($_POST['payment_gross']) ? $_POST['payment_gross'] : $_POST['mc_gross']));
+
+
+
+    if(count($player_es) && count($next_is)) {
+
+        $is_pending = ($_POST['payment_status']=='Pending');
+
+        if($pay_amount > 0){
+
+            //Paid:
+
+            $x__type = ( $is_pending ? 35572 /* Pending Payment */ : 26595 );
+
+            //Log Payment:
+            $completion_status = $this->X_model->mark_complete($x__type, $x__player, ( isset($target_is[0]['i__id']) ? $target_is[0]['i__id'] : 0 ), $next_is[0], array(), array(
+                'x__weight' => intval($_POST['quantity']),
+                'x__metadata' => $_POST,
+            ));
+
+        } else {
+
+            $x__type = ( $is_pending ? 39597 /* Pending Refund */ : 31967 );
+
+
+            //Find issued tickets:
+            $original_payment = $this->X_model->fetch(array(
+                'x__type' => 26595,
+                'x__player' => $x__player,
+                'x__previous' => $next_is[0]['i__id'],
+            ));
+
+            //Log Refund:
+            $completion_status = $this->X_model->mark_complete($x__type, $x__player, ( isset($target_is[0]['i__id']) ? $target_is[0]['i__id'] : 0 ), $next_is[0], array(), array(
+                'x__weight' => (-1 * ( isset($original_payment[0]['x__weight']) ? $original_payment[0]['x__weight'] : 1 )),
+                'x__metadata' => $_POST,
+                'x__reference' => ( isset($original_payment[0]['x__id']) ? $original_payment[0]['x__id'] : 0 ),
+                'x__website' => ( isset($original_payment[0]['x__website']) && $original_payment[0]['x__website']>0 ? $original_payment[0]['x__website'] : 0 ),
+            ));
+
+        }
+    }
+}
+
+
+
 $i_view = 0;
 $quick_id = 0;
 $discovery_i__hashtag = ( strlen($first_segment) ? ( strlen($second_segment) ? $second_segment : $first_segment ) : 0 );
