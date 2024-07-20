1c538c541e489ce5424a2cb1b3571243b982cc94
Shervin Enayati <shervin@mench.co>
2024-07-19 18:54:50 -0700

    Bug fixes before launch

--- application/views/app/spreadsheet.php
+++ application/views/app/spreadsheet.php
@@ -26,7 +26,7 @@ $count_totals = array(
 $list_settings = list_settings($_GET['i__hashtag']);
 
 echo '<h1>' . view_i_title($list_settings['i']) . '</h1>';
-echo '<div class="center-frame hide-subline hideIfEmpty remove_first_line">' . view_i__links($list_settings['i']) . '</div>';
+echo '<div class="center-frame hide-subline maxwidth hideIfEmpty remove_first_line">' . view_i__links($list_settings['i']) . '</div>';
 
 foreach($list_settings['query_string'] as $x){
 
@@ -203,6 +203,10 @@ echo '</table>';
 
     #sortable_table a {
         text-decoration: underline !important;
+    }
+
+    .maxwidth {
+        max-width: 1200px;
     }
 
     /* CSS Adjustments for Printing View */
