9cf8221d2c3315488fa4ae78c1bbfa548f6b1c01
Shervin Enayati <shervin@mench.co>
2024-07-25 23:02:05 -0700

    Include/Exclude Any/All

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1998,6 +1998,8 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
 
     foreach($CI->config->item('e___31904') as $x__type_target_bar => $m_target_bar) {
 
+        break;
+
         //See if missing superpower?
         $superpowers_required = array_intersect($CI->config->item('n___10957'), $m_target_bar['m__following']);
         if(count($superpowers_required) && (!superpower_unlocked(end($superpowers_required)) || $is_cache)){
@@ -2217,7 +2219,7 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
     }
 
     //Bottom Bar menu
-    if(!$focus__node && !$is_locked && !$is_cache){
+    if(!$focus__node && !$is_locked && !$is_cache && 0){
         foreach($CI->config->item('e___'.( $discovery_mode ? 42877 : 31890 )) as $e__id_bottom_bar => $m_bottom_bar) {
 
             $superpowers_required = array_intersect($CI->config->item('n___10957'), $m_bottom_bar['m__following']);
@@ -2241,7 +2243,7 @@ function view_card_i($x__type, $i, $previous_i = null, $target_i__hashtag = null
 
 
 
-    if($bottom_bar_ui){
+    if($bottom_bar_ui && 0){
         $ui .= '<div class="'.( $focus__node && $discovery_mode ? ' container fixed-bottom hidden ' : '' ).'">';
         $ui .= '<div class="card_covers">';
         $ui .= $bottom_bar_ui;
