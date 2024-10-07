decb6794c38e571b352a1a4aed983ea3cc7139c0
shervinesta <shervin@mench.com>
2024-10-07 13:44:46 -0700

    updates

--- application/views/app/discovery.php
+++ application/views/app/discovery.php
@@ -120,6 +120,18 @@ if($player_e || isset($_GET['open'])){
 }
 
 
+echo '<hr />';
+
+echo preg_replace('/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.
+    '|[\x00-\x7F][\x80-\xBF]+'.
+    '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.
+    '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.
+    '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
+    '?', view_i_title($i, true) );
+echo '<hr />';
+echo preg_replace('/\xE0[\x80-\x9F][\x80-\xBF]'.
+    '|\xED[\xA0-\xBF][\x80-\xBF]/S','', view_i_title($i, true) );
+echo '<hr />';
 
 ?>
 
