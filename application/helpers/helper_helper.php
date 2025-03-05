55afba132f084a3aea0aa8bdf8bcd39a303d4871
shervinesta <shervin@mench.com>
2025-03-05 08:24:58 -0800

    Discotique 25 Launch

--- application/helpers/helper_helper.php
+++ application/helpers/helper_helper.php
@@ -4446,7 +4446,7 @@ function view__card_i($x__type, $i, $previous_i = null, $target_i__hashtag = nul
             $e___7712 = $CI->config->item('e___7712');
             $ui .= '<script> $(document).ready(function () { $(\'.nav__'.$i['i__id'].' .thepill12840 .xtypecounter12840\').remove(); $(\'.nav__'.$i['i__id'].' .thepill12840 .xtypetitle_12840\').text(\''.$e___7712[$i['i__type']]['m__message'].':\'); }); </script>';
 
-        } elseif (in_array($i['i__type'], $CI->config->item('n___41055')) && $focus__node) {
+        } elseif (in_array($i['i__type'], $CI->config->item('n___41055')) && ($focus__node || $i['i__type']==43758)) {
 
             //PAYMENT TICKET
             if(isset($_GET['cancel_pay']) && !count($x_completes)){
