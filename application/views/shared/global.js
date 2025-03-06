1e701dcfebb684f69598a6ac0cd6a2575c3966cd
shervinesta <shervin@mench.com>
2025-03-05 20:25:02 -0800

    Discotique 25 Launch

--- application/views/shared/global.js
+++ application/views/shared/global.js
@@ -1178,7 +1178,11 @@ $(document).ready(function () {
                     return view__s_js_line(suggestion,'#');
                 },
                 replace: function (suggestion) {
-                    return ' #' + suggestion.s__handle + ' ';
+                    setTimeout(function () {
+                        //One more time to make sure it also works in mobile:
+                        set_autosize($('.save_i__message'));
+                    }, 144);
+                    return "\n" + '#' + suggestion.s__handle + "\n"+'#';
                 }
             },
             {
@@ -1207,6 +1211,34 @@ $(document).ready(function () {
                         set_autosize($('.save_i__message'));
                     }, 144);
                     return "\n" + '\\' + suggestion.s__handle + "\n"+'\\';
+                }
+            },
+            {
+                match: /(^|\s)!(\w*(?:\s*\w*))$/,
+                search: function (q, callback) {
+                    index_algolia.search(q, {
+                        hitsPerPage: js_e___6404[31112]['m__message'],
+                        filters: 's__type=12273' + search_and_filter,
+                    })
+                        .then(function searchSuccess(content) {
+                            if (content.query === q) {
+                                callback(content.hits);
+                            }
+                        })
+                        .catch(function searchFailure(err) {
+                            console.error(err);
+                        });
+                },
+                template: function (suggestion) {
+                    //Triger hieght adjust
+                    return view__s_js_line(suggestion,'!');
+                },
+                replace: function (suggestion) {
+                    setTimeout(function () {
+                        //One more time to make sure it also works in mobile:
+                        set_autosize($('.save_i__message'));
+                    }, 144);
+                    return "\n" + '!' + suggestion.s__handle + "\n"+'!';
                 }
             },
         ]);
