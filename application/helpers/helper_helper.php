fc50c4e09b62c6f69009ec9688ad6110aa061260
shervinesta <shervin@mench.com>
2025-03-08 12:55:04 -0800

    Discotique 25 Launch

--- application/helpers/helper_helper.php
+++ application/helpers/helper_helper.php
@@ -4927,7 +4927,7 @@ function view__card_i($x__type, $i, $previous_i = null, $target_i__hashtag = nul
     }
 
 
-    $i_popup_url = i_popup_url($focus_i);
+    $i_popup_url = i_popup_url($i);
     if($i_popup_url){
         $ui .= '<div class="">';
         $ui .= '<div class="link_click hideIfEmpty"><a href="'.$i_popup_url.'" class="hideIfEmpty" target="_blank" onclick="link_clicked()">'.$i_popup_url.'</a></div>';
