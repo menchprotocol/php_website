4f28e97423975f3716e11180a0a869754945b4c2
shervinesta <shervin@mench.com>
2025-03-07 20:58:32 -0800

    Discotique 25 Launch

--- application/helpers/helper_helper.php
+++ application/helpers/helper_helper.php
@@ -5059,7 +5059,7 @@ function view__card_i($x__type, $i, $previous_i = null, $target_i__hashtag = nul
                 if($max_allowed > 0 || $min_allowed > 0){
                     $input_ui .= '<div class="sale_controller sale_controller_'.$i['i__id'].'" unitprice="'.$unit_price.'" unitcurrency="'.$unit_currency.'" i__id="'.$i['i__id'].'">';
                     $input_ui .= '<a href="javascript:void(0);" onclick="sale_increment(-1,'.$i['i__id'].','.$max_allowed.','.$min_allowed.','.($unit_fee+$unit_price).','.$unit_fee.')" class="sale_increment sale_down"><i class="fas fa-minus '.( $current_value==$min_allowed ? ' hidden ' : '' ).'"></i></a>';
-                    $input_ui .= '<span class="main__title current_count" style="display: inline-block; min-width:34px; text-align: center;">'.$current_value.'</span>';
+                    $input_ui .= '<span class="main__title current_count">'.$current_value.'</span>';
                     $input_ui .= '<a href="javascript:void(0);" onclick="sale_increment(1,'.$i['i__id'].','.$max_allowed.','.$min_allowed.','.($unit_fee+$unit_price).','.$unit_fee.')" class="sale_increment sale_up">'.( $max_allowed==$min_allowed ? '<i class="fas fa-lock islocked"></i>' : '<i class="fas fa-plus"></i>' ).'</a>';
                     $input_ui .= '</div>';
                 } else {
