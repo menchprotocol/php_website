2157ddf39f1939a9bfff26d0695164e1b60a3f0e
shervinesta <shervin@mench.com>
2025-03-08 12:44:56 -0800

    Discotique 25 Launch

--- application/views/app/discovery.php
+++ application/views/app/discovery.php
@@ -118,9 +118,9 @@ if($player_e){
 //Focus Discovery:
 echo '<div class="main_item row justify-content">';
 echo view__card_i(43007, $focus_i, null, null, 0, $x_completes);
+echo '<div class="link_click hideIfEmpty"><a href="" class="hideIfEmpty" target="_blank" onclick="link_clicked()"></a></div>';
 echo '</div>';
 
-echo '<div class="link_click hideIfEmpty"><a href="" class="hideIfEmpty" target="_blank" onclick="link_clicked()"></a></div>';
 
 
 //Main Navigation
