0daeed7ff839baed0b61270cabc203cf500e87b4
shervinesta <shervin@mench.com>
2025-03-09 12:53:12 -0700

    Discotique 25 Launch

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -3196,7 +3196,12 @@ class App extends CI_Controller
                 $i_redirect_url = i_redirect_url($primary_i);
             }
             if(!$i_redirect_url){
-                $find_next = $this->Mench_ledger->find_next($player_e['e__id'], $_POST['target_i__hashtag'], $focus_i);
+                foreach($this->Idea_cache->fetch(array(
+                    'i__id' => $_GET['target_i__id'],
+                )) as $i_target){
+                    $find_next = $this->Mench_ledger->find_next($player_e['e__id'], $_POST['target_i__hashtag'], $i_target);
+                }
+                //$find_next = $this->Mench_ledger->find_next($player_e['e__id'], $_POST['target_i__hashtag'], $focus_i);
             }
 
             //All good:
