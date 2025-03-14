383ed3b81bda6d049e1d7fd7dd35af1be919272f
shervinesta <shervin@mench.com>
2025-03-14 05:59:56 -0700

    Discotique 25 Launch

--- application/models/Mench_ledger.php
+++ application/models/Mench_ledger.php
@@ -1572,6 +1572,7 @@ class Mench_ledger extends CIdea_cache
         if($input__text){
             foreach($this->Mench_ledger->fetch(array(
                 'x__privacy IN (' . join(',', $this->config->item('n___7359')) . ')' => null, //PUBLIC
+                'x__player' => $e__id,
                 'x__type' => 6144, //Written Response
                 'x__previous' => $i['i__id'],
             ), array('x__next'), 1) as $x) {
