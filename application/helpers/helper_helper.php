e8ad13f5e60f24702913d2c00f7494050241bbf7
shervinesta <shervin@mench.com>
2025-03-14 05:34:09 -0700

    Discotique 25 Launch

--- application/helpers/helper_helper.php
+++ application/helpers/helper_helper.php
@@ -574,7 +574,7 @@ function round_minutes($seconds){
 
 function view_tree($i){
 
-    echo '<div style="padding-left:'.($i['i__level']*21).'px;">'.view__i_title($i).( count($i['i__response']) ? ' ['.$i['i__response']['x__message'].']' : '' ).'</div>';
+    echo '<div class="slim_title" style="padding-left:'.($i['i__level']*21).'px;">'.view__i_title($i).( count($i['i__response']) ? ' ['.$i['i__response']['x__message'].']' : '' ).'</div>';
 
     foreach($i['i__next'] as $next_i){
         view_tree($next_i);
--- application/views/app/history.php
+++ application/views/app/history.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<h3><a href="/@' . $focus_e['e__handle'] . '">' . $focus_e['e__title'] . '</a> History of <a href="/' . $focus_i['i__hashtag'] . '">' . view__i_title($focus_i) . '</a></h3>';
+echo '<h3><a href="/@' . $focus_e['e__handle'] . '"><u>' . $focus_e['e__title'] . '</u></a> History:</h3>';
 
 //Display idea info:
 $recursive_down_ids = $this->Mench_ledger->tree_history($focus_i, $focus_e['e__id']);
@@ -11,6 +11,3 @@ echo '<div class="row justify-content">';
 view_tree($recursive_down_ids);
 echo '</div>';
 
-
-
-//print_r($recursive_down_ids);
