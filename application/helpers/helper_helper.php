48de719e877856f0fe434bd6b04cdc7ce4732fa7
shervinesta <shervin@mench.com>
2025-03-14 06:23:11 -0700

    Discotique 25 Launch

--- application/helpers/helper_helper.php
+++ application/helpers/helper_helper.php
@@ -577,7 +577,7 @@ function view_tree($i){
     echo '<div class="slim_title">';
 
     echo '<div>';
-    echo ( isset($i['i__discover']['x__weight']) && strlen($i['i__discover']['x__weight']) ? $i['i__discover']['x__weight'].'x ' : '' );
+    echo ( isset($i['i__discover']['x__weight']) && intval($i['i__discover']['x__weight'])>1 ? $i['i__discover']['x__weight'].'x ' : '' );
     echo '<span class="'.( count($i['i__discover']) ? ' main__title ' : '' ).'">'.view__i_title($i, true).'</span>';
     //echo ( count($i['i__discover']) ? ' ['.$i['i__response']['x__message'].']' : '' );
     echo ( isset($i['i__response']['i__message']) && strlen($i['i__response']['i__message']) ? ': '.$i['i__response']['i__message'] : '' );
