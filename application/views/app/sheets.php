2c3fe0c7cd36816195109c08974e694759e4f12c
shervinesta <shervin@mench.com>
2025-03-16 15:23:01 -0700

    Discotique 25 Launch

--- application/views/app/sheets.php
+++ application/views/app/sheets.php
@@ -136,7 +136,7 @@ foreach($list_settings['query_string_filtered'] as $x){
                 'i__privacy IN (' . join(',', $this->config->item('n___42948')) . ')' => null, //Public Ideas
             ), array('x__previous'), 1) as $target_i){
                 //See History for this user:
-                $message_clean = '<a href="'.view__app_link(44328).'/'.$target_i['i__hashtag'].'@'.$x['e__handle'].'" target="_blank"><span class="icon-block">'.$e___11035[44328]['m__cover'].'</span>'.$e___11035[44328]['m__title'].'</a>';
+                $message_clean = '<a href="'.view__app_link(44328).'/'.$target_i['i__hashtag'].'@'.$x['e__handle'].'" target="_blank"><span class="icon-block-sm">'.$e___11035[44328]['m__cover'].'</span>'.$e___11035[44328]['m__title'].'</a>';
             }
         }
 
