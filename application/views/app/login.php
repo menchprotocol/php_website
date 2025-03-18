6e296b36d91fedd3e6cf1b0a562cd9cf6b1a8fd6
shervinesta <shervin@mench.com>
2025-03-18 07:13:09 -0700

    Discotique 25 Launch

--- application/views/app/login.php
+++ application/views/app/login.php
@@ -287,7 +287,7 @@ if(superpower_unlocked()) {
             <!-- Step 1: Enter Email -->
             <div id="step2" class="signup-steps hidden">
 
-                <span class="main__title" style="padding-bottom: 3px; display:block;"><?= '<span class="icon-block">'.$e___4269[32079]['m__cover'].'</span>'.$e___4269[32079]['m__title'] ?></span>
+                <span class="main__title" style="padding-bottom: 3px; display:block;"><?= '<span class="icon-block">'.$e___4269[32079]['m__cover'].'</span>' ?></span>
 
                 <div class="form-group"><input type="text" autocapitalize="none" placeholder="<?= $e___4269[32079]['m__message'] ?>" id="account_email_phone" <?= isset($_GET['account_email_phone']) ? ' value="'.$_GET['account_email_phone'].'" ' : '' ?> class="form-control border input_border"></div>
 
