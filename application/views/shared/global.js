b8ad45b764ec13eb1505fca3932840c334ac5f3d
shervinesta <shervin@mench.com>
2025-03-19 21:23:34 -0700

    Discotique 25 Launch

--- application/views/shared/global.js
+++ application/views/shared/global.js
@@ -3452,7 +3452,7 @@ function go_next(do_skip){
                 if (data.status) {
                     //Go to redirect message:
                     alert(data.message);
-                    js_redirect(data.next__url);
+                    //js_redirect(data.next__url);
                 } else {
                     //Show error:
                     $('.go_next_btn').html(original_html);
