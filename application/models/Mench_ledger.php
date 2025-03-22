2c841e5c58e43b72dd27b49f1611ca8d68066199
shervinesta <shervin@mench.com>
2025-03-22 07:16:15 -0700

    Discotique 25 Launch

--- application/models/Mench_ledger.php
+++ application/models/Mench_ledger.php
@@ -1568,6 +1568,9 @@ class Mench_ledger extends CIdea_cache
         unset($i['x__player']);
         unset($i['x__previous']);
         unset($i['x__next']);
+        unset($i['x__id']);
+        unset($i['x__message']);
+
         $input__selection = in_array($i['i__type'], $this->config->item('n___7712'));
         $input__text = in_array($i['i__type'], $this->config->item('n___43002'));
         $i['uploaded_media'] = array();
@@ -1598,6 +1601,8 @@ class Mench_ledger extends CIdea_cache
             unset($x['x__previous']);
             unset($x['x__next']);
             unset($x['x__player']);
+            unset($x['x__message']);
+            unset($x['x__id']);
 
             $i['user_discovered'] = $x;
 
