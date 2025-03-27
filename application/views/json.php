253e0382ceeb73cc1dcc96f6204fa9b6ccd66f2d
shervinesta <shervin@mench.com>
2025-03-27 03:37:28 -0700

    Simplifications

--- application/views/json.php
+++ application/views/json.php
@@ -2,10 +2,8 @@
 
 //view__json($this->Mench_ledger->tree_full_history($focus_i, $focus_e['e__id']));
 
-
-$count = 0;
-foreach($this->Idea_cache->fetch(array()) as $i){
-    $count++;
+foreach($this->Idea_cache->fetch(array(
+    'LOWER(i__hashtag)' => 'kazbah25',
+)) as $i){
+    echo print_r($i, true);
 }
-
-echo $count.' ideas';
