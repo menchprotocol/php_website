988c09bbc15d63e772ed9633d487e550837344be
shervinesta <shervin@mench.com>
2025-04-01 12:32:54 -0700

    Simplifications

--- application/views/json.php
+++ application/views/json.php
@@ -3,56 +3,34 @@
 boost_power();
 
 $link_count = 0;
-$link_full_duplicate = 0;
-$previous_x = array();
 
 echo '<table width="100%" border="1px">';
 foreach($this->Mench_ledger->fetch(array(
-    'link_id >' => '0',
-    'link_type IN (4983,4230,4251,7545)' => null,
+    'link_type !=' => 4250,
 ), array(), 0, 0, array(
-    'link_type' => 'ASC',
-    'link_up' => 'ASC',
-    'link_down' => 'ASC',
-    'link_left' => 'ASC',
-    'link_right' => 'ASC',
-    'LENGTH(link_text)' => 'ASC',
-    'link_id' => 'DESC',
+    'link_id' => 'ASC',
 )) as $x){
 
     //echo $x['link_id']."<hr />";
 
+    /*
+    $this->db->insert('menchledger', array(
+        'linkplayer' => $x['link_player'],
+        'linktime' => $x['link_time'],
+        'linkdomain' => $x['link_domain'],
+        'linkdown' => $x['link_down'],
+        'linkleft' => ( $x['link_left']>0 ? 100000+$x['link_left'] : 0 ),
+        'linkright' => ( $x['link_right']>0 ? 100000+$x['link_right'] : 0 ),
+        'linktext' => $x['link_text'],
+        'linktype' => ( $x['link_type']==4251 ? 4230 : $x['link_type'] ),
+    ));
+    */
+
     $link_count++;
-    if(count($previous_x) && $previous_x['link_type']==$x['link_type']){
-        //Check duplicate with previous link:
-        if(($previous_x['link_left']>0 || $previous_x['link_right']>0) && $previous_x['link_left']==$x['link_left'] && $previous_x['link_right']==$x['link_right'] && (($previous_x['link_text']==$x['link_text']) || !strlen($previous_x['link_text']) || !strlen($x['link_text']))){
-
-            echo '<tr '.( strlen($previous_x['link_text']) ? '' : 'style="background-color: #EFEFEF;"' ).'><td>ID '.$previous_x['link_id'].'</td><td>TP '.$previous_x['link_type'].'</td><td>PL '.$previous_x['link_player'].'</td><td>LF '.$previous_x['link_left'].'</td><td>RT '.$previous_x['link_right'].'</td><td> </td><td> </td><td>'.$previous_x['link_text'].'</td></tr>';
-            echo '<tr '.( strlen($x['link_text']) ? '' : 'style="background-color: #EFEFEF;"' ).'><td>ID '.$x['link_id'].'</td><td>TP '.$x['link_type'].'</td><td>PL '.$x['link_player'].'</td><td>LF '.$x['link_left'].'</td><td>RT '.$x['link_right'].'</td><td> </td><td> </td><td>'.$x['link_text'].'</td></tr>';
-
-            $this->db->query("DELETE FROM mench_ledger WHERE link_id=".( strlen($previous_x['link_text']) ? $x['link_id'] : $previous_x['link_id'] ).";");
-
-            //What about content?
-            $link_full_duplicate++;
-
-        } elseif(($previous_x['link_up']>0 || $previous_x['link_down']>0) && $previous_x['link_up']==$x['link_up'] && $previous_x['link_down']==$x['link_down'] && (($previous_x['link_text']==$x['link_text']) || !strlen($previous_x['link_text']) || !strlen($x['link_text']))){
-
-            echo '<tr '.( strlen($previous_x['link_text']) ? '' : 'style="background-color: #EFEFEF;"' ).'><td>ID '.$previous_x['link_id'].'</td><td>TP '.$previous_x['link_type'].'</td><td>PL '.$previous_x['link_player'].'</td><td> </td><td> </td><td>UP '.$previous_x['link_up'].'</td><td>DW '.$previous_x['link_down'].'</td><td>'.$previous_x['link_text'].'</td></tr>';
-            echo '<tr '.( strlen($x['link_text']) ? '' : 'style="background-color: #EFEFEF;"' ).'><td>ID '.$x['link_id'].'</td><td>TP '.$x['link_type'].'</td><td>PL '.$x['link_player'].'</td><td> </td><td> </td><td>UP '.$x['link_up'].'</td><td>DW '.$x['link_down'].'</td><td>'.$x['link_text'].'</td></tr>';
-
-            $this->db->query("DELETE FROM mench_ledger WHERE link_id=".( strlen($previous_x['link_text']) ? $x['link_id'] : $previous_x['link_id'] ).";");
-
-            //What about content?
-            $link_full_duplicate++;
-
-        }
-    }
-
-    $previous_x = $x;
 }
 echo '</table>';
 
-echo $link_full_duplicate.' duplicate out of '.$link_count.' TOTAL';
+echo $link_count.' LINKS';
 
 
 
