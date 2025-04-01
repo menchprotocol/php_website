76f5ed3e5db29dfa0e226769f777a8eac37108c0
shervinesta <shervin@mench.com>
2025-04-01 12:18:50 -0700

    Simplifications

--- application/views/json.php
+++ application/views/json.php
@@ -16,7 +16,7 @@ foreach($this->Mench_ledger->fetch(array(
     'link_down' => 'ASC',
     'link_left' => 'ASC',
     'link_right' => 'ASC',
-    'link_text' => 'ASC',
+    'LENGTH(link_text)' => 'DESC',
     'link_id' => 'DESC',
 )) as $x){
 
