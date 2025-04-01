6da6d0243adcc197871871438dd9f3ae4a894c46
shervinesta <shervin@mench.com>
2025-03-31 19:18:17 -0700

    Simplifications

--- application/views/json.php
+++ application/views/json.php
@@ -1,8 +1,63 @@
 <?php
 
+$link_count = 0;
+$link_full_duplicate = 0;
+$link_half_duplicate = 0;
+$previous_x = array();
+
+echo '<table>';
+foreach($this->Mench_ledger->fetch(array(
+    'link_id >' => '0',
+    'link_type NOT IN (4250,4251)' => null,
+), array(), 1000, 0, array(
+    'link_type' => 'ASC',
+    'link_up' => 'ASC',
+    'link_down' => 'ASC',
+    'link_left' => 'ASC',
+    'link_right' => 'ASC',
+    'LENGTH(link_text)' => 'DESC',
+)) as $x){
+    //echo $x['link_id']."<hr />";
+
+    $link_count++;
+    if(count($previous_x)){
+        //Check duplicate with previous link:
+        if($previous_x['link_up']==$x['link_up'] && $previous_x['link_down']==$x['link_down']){
+
+            echo '<tr><td>ID '.$previous_x['link_id'].'</td><td>PL '.$previous_x['link_player'].'</td><td>UP '.$previous_x['link_up'].'</td><td>DW '.$previous_x['link_down'].'</td><td>'.$previous_x['link_text'].'</td></tr>';
+            echo '<tr><td>ID '.$x['link_id'].'</td><td>PL '.$x['link_player'].'</td><td>UP '.$x['link_up'].'</td><td>DW '.$x['link_down'].'</td><td>'.$x['link_text'].'</td></tr>';
+
+            //What about content?
+            if($previous_x['link_text']==$x['link_text']){
+                $link_full_duplicate++;
+            } else {
+                $link_half_duplicate++;
+            }
+        } elseif($previous_x['link_left']==$x['link_left'] && $previous_x['link_right']==$x['link_right']){
+
+            echo '<tr><td>ID '.$previous_x['link_id'].'</td><td>PL '.$previous_x['link_player'].'</td><td>LF '.$previous_x['link_left'].'</td><td>RT '.$previous_x['link_right'].'</td><td>'.$x['link_text'].'</td></tr>';
+            echo '<tr><td>ID '.$x['link_id'].'</td><td>PL '.$x['link_player'].'</td><td>LF '.$x['link_left'].'</td><td>RT '.$x['link_right'].'</td><td>'.$x['link_text'].'</td></tr>';
+
+            //What about content?
+            if($previous_x['link_text']==$x['link_text']){
+                $link_full_duplicate++;
+            } else {
+                $link_half_duplicate++;
+            }
+        }
+    }
+
+    $previous_x = $x;
+}
+echo '</table>';
+
+echo $link_half_duplicate.' HALF & '.$link_full_duplicate.' FULL duplicate out of '.$link_count.' TOTAL';
+
+
+
 
 //view__json($this->Mench_ledger->tree_full_history($focus_i, $focus_e['e__id']));
-if(1){
+if(0){
 if(1){
     $count = 0;
     $found = 0;
@@ -126,45 +181,5 @@ foreach($this->Idea_cache->fetch(array(
 
 }
 echo $found.'/'.$count.' Found';
-
-}
-
-
-if(0){
-
-    $link_count = 0;
-    $link_full_duplicate = 0;
-    $link_half_duplicate = 0;
-    $previous_x = array();
-    foreach($this->Mench_ledger->fetch(array(
-        'link_id >' => '0',
-        'link_type NOT IN (4250,4251)' => null,
-    ), array(), 500, 0, array(
-        'link_type' => 'ASC',
-        'link_up' => 'ASC',
-        'link_down' => 'ASC',
-        'link_left' => 'ASC',
-        'link_right' => 'ASC',
-        'LENGTH(link_text)' => 'DESC',
-    )) as $x){
-        //echo $x['link_id']."<hr />";
-
-        $link_count++;
-        if(count($previous_x)){
-            //Check duplicate with previous link:
-            if($previous_x['link_up']==$x['link_up'] && $previous_x['link_down']==$x['link_down']){
-                //What about content?
-                if($previous_x['link_text']==$x['link_text']){
-                    $link_full_duplicate++;
-                } else {
-                    $link_half_duplicate++;
-                }
-            }
-        }
-
-        $previous_x = $x;
-    }
-
-    echo $link_half_duplicate.' HALF & '.$link_full_duplicate.' FULL duplicate out of '.$link_count.' TOTAL';
 
 }
