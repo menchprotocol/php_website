a80e146ae23b9e67d0e7ebb6e34055020d6ebee0
shervinesta <shervin@mench.com>
2025-03-31 17:26:43 -0700

    Simplifications

--- application/views/json.php
+++ application/views/json.php
@@ -2,7 +2,7 @@
 
 //view__json($this->Mench_ledger->tree_full_history($focus_i, $focus_e['e__id']));
 
-if(0){
+if(1){
     $count = 0;
     $found = 0;
     foreach($this->Source_cache->fetch(array(
@@ -12,13 +12,30 @@ if(0){
         $count++;
         //echo $count.') @'.$e['e__handle'].' @'.$e['e__id'].'<hr />';
 
-        if(count($this->Mench_ledger->read(array(
-            'linkid' => $e['e__id'],
-            'linktype' => 4251, //New Source Created
-        )))){
-            continue;
+        $this->db->insert('menchledger', array(
+            'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
+            'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
+            'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
+            'linkup' => 32338, //Player Handle
+            'linktext' => $e['e__handle'],
+            'linkdown' => $e['e__id'],
+            'linktype' => 4230, //Follow
+        ));
+
+        if(strlen($e['e__cover'])){
+            $this->db->insert('menchledger', array(
+                'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
+                'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
+                'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
+                'linkup' => 6198, //Player Cover
+                'linktext' => $e['e__cover'],
+                'linkdown' => $e['e__id'],
+                'linktype' => 4230, //Follow
+            ));
         }
 
+
+        continue;
         $creators = $this->Mench_ledger->fetch(array(
             'link_down' => $e['e__id'],
             'link_type IN (4230,4251)' => null, //Idea References
@@ -70,6 +87,19 @@ foreach($this->Idea_cache->fetch(array(
 
     //Lets log:
     $new_i_id = intval($i['i__id'])+100000;
+
+    $this->db->insert('menchledger', array(
+        'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
+        'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
+        'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
+        'linkup' => 32337, //Idea Hashtag
+        'linkright' => $new_i_id,
+        'linktext' => $i['i__hashtag'],
+        'linktype' => 4983, //CO-author
+    ));
+
+    continue;
+
     $this->db->insert('menchledger', array(
         'linkid' => $new_i_id,
         'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : 1 ),
