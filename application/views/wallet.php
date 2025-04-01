f6be2a20ec993995a445d52ed3f4237b200836a3
shervinesta <shervin@mench.com>
2025-04-01 15:37:39 -0700

    Ledger V12

--- application/views/wallet.php
+++ application/views/wallet.php
@@ -40,27 +40,24 @@ foreach($this->config->item('players___33292') as $playerid1 => $m1) {
         echo '<div class="main__title large_title" title="@'.$playerid2.' @'.$m2['m__handle'].'">'.$m2['m__title'].'</div>';
         echo '</div>';
 
-        if($playerid2==12273 || $playerid2==12274){
-            continue;
+
+        if($playerid2!=12273 && $playerid2!=12274){
+            echo '<table class="table table-striped card_subcat card_subcat_'.$playerid2.' hidden" style="width:100%; margin-top:13px;">';
+            $focus_link_group = 0;
+            $player_pinned = e_pinned($playerid2, true);
+            if(!$player_pinned || !is_array($this->config->item('players___'.$player_pinned)) || !count($this->config->item('players___'.$player_pinned)) ){
+                continue;
+            }
+            foreach($this->config->item('players___'.$player_pinned) as $playerid3 => $m3) {
+
+                echo '<tr class="mobile-shrink" title="'.$m3['m__message'].'" data-toggle="tooltip" data-placement="top">';
+                echo '<td style="text-align: left;" title="@'.$playerid3.' @'.$m3['m__handle'].'"><a href="'.view__memory(42903,42902).$m3['m__handle'].'"><span class="icon-block-sm">'.$m3['m__cover'].'</span>'.$m3['m__title'].'</a><span class="last-right-col"><b class="card_count_'.$playerid3.'"><i class="fas fa-yin-yang fa-spin"></i></b></span></td>';
+                echo '</tr>';
+
+            }
+            echo '</table>';
         }
 
-        echo '<table class="table table-striped card_subcat card_subcat_'.$playerid2.' hidden" style="width:100%; margin-top:13px;">';
-
-        $focus_link_group = 0;
-        $player_pinned = e_pinned($playerid2, true);
-        if(!$player_pinned || !is_array($this->config->item('players___'.$player_pinned)) || !count($this->config->item('players___'.$player_pinned)) ){
-            continue;
-        }
-        foreach($this->config->item('players___'.$player_pinned) as $playerid3 => $m3) {
-
-            echo '<tr class="mobile-shrink" title="'.$m3['m__message'].'" data-toggle="tooltip" data-placement="top">';
-            echo '<td style="text-align: left;" title="@'.$playerid3.' @'.$m3['m__handle'].'"><a href="'.view__memory(42903,42902).$m3['m__handle'].'"><span class="icon-block-sm">'.$m3['m__cover'].'</span>'.$m3['m__title'].'</a><span class="last-right-col"><b class="card_count_'.$playerid3.'"><i class="fas fa-yin-yang fa-spin"></i></b></span></td>';
-            echo '</tr>';
-
-        }
-
-
-        echo '</table>';
 
         echo '</div>';
         echo '</div>';
