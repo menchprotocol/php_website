05459371a5554def803e0ea86e9f12ce0c03a5af
shervinesta <shervin@mench.com>
2025-04-01 14:25:23 -0700

    Ledger V12

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -25,7 +25,7 @@ class App extends CI_Controller
 
     function load($app_playerid = 14563 /* Error if none provided */, $focus_handle = 0, $focus_hashtag = 0, $target_hashtag = 0){
 
-        if(!isset($_SERVER['REMOTE_ADDR']) || $_SERVER['REMOTE_ADDR']!='73.15.62.97'){ die('We will be back up shortly... IP '.$_SERVER['REMOTE_ADDR']); }
+        if(!isset($_SERVER['REMOTE_ADDR']) || $_SERVER['REMOTE_ADDR']!='73.15.62.97'){ die('We will be back up shortly... IP '); }
 
         $memory_detected = is_array($this->config->item('playerids___6287')) && count($this->config->item('playerids___6287'));
         if(!$memory_detected){
