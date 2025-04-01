16ed60f23e81e4843d5fad4e5f17a87d1ce29012
shervinesta <shervin@mench.com>
2025-04-01 14:30:31 -0700

    Ledger V12

--- application/views/json.php
+++ application/views/json.php
@@ -1,20 +1,3 @@
 <?php
 
-//view__json($this->Menchledger->tree_full_history($focus_i, $focus_e['playerid']));
-
-$count = 0;
-foreach($this->Cacheideas->fetchold(array(
-    'i__id >' => 0,
-), 0) as $i){
-
-    $count++;
-    //echo $count.') #'.$i['i__type']."<hr />";
-
-    //Lets log:
-    $new_i_id = intval($i['i__id'])+100000;
-
-    $this->db->query("UPDATE cacheideas SET ideatype=".$i['i__type']." WHERE ideaid=".$new_i_id.";");
-
-}
-
-echo $count.' TOTAL';
+view__json($this->Menchledger->tree_full_history($focus_i, $focus_e['playerid']));
