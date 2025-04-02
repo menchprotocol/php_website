56285b7f5e700d2a069a9cb1455a045d2536a87d
shervinesta <shervin@mench.com>
2025-04-02 08:58:42 -0700

    Ledger V12

--- application/views/json.php
+++ application/views/json.php
@@ -2,16 +2,39 @@
 
 //view_json($this->Menchledger->tree_full_history($focus_i, $focus_e['playerid']));
 
-
+$is_ideation = array(4228, 31834, 42337, 33532, 44161, 40791, 44162, 40793, 32247, 32304, 33344, 30901, 42244, 42243);
+$is_sourcing = array(41011, 4251, 44399, 33335, 42659, 42849, 44176, 44179, 42897, 32486, 4230, 32489, 42579, 42581, 42580, 42283, 42335, 42516, 42554, 42570, 42427, 42518, 42440, 42791);
+$is_contribution = array(34513, 4250, 4983, 44396, 42292, 33600, 42272, 42274, 31840, 42275, 42282, 32488, 4258, 4260, 4259, 31835, 41949, 43941, 4256, 12896, 10573, 27984, 43513, 43514, 26600, 7545, 26599, 32235);
+$is_discovery = array(44397, 44245, 43798, 43142, 42995, 42916, 42402, 42397, 29393, 42332, 41940, 39597, 35572, 32016, 4559, 31967, 31810, 31798, 31797, 7712, 6144, 31022, 6157, 26595, 4235, 12117, 4235,7712,27676,27678,29399);
+$count_ideation = 0;
+$count_sourcing = 0;
+$count_contribution = 0;
+$count_discovery = 0;
+$count_other = 0;
 
 $count = 0;
 $missing = 0;
 foreach ($this->Menchledger->fetchold(array(
     'link_type' => 4983,
 )) as $x) {
+
+    if(in_array($x['link_type'], $is_ideation)) {
+        $count_ideation++;
+    } elseif(in_array($x['link_type'], $is_sourcing)) {
+        $count_sourcing++;
+    } elseif(in_array($x['link_type'], $is_contribution)) {
+        $count_contribution++;
+    } elseif(in_array($x['link_type'], $is_discovery)) {
+        $count_discovery++;
+    } else {
+        $count_other++;
+        echo print_r($x, true);
+        echo 'WAS OTHER<hr />';
+    }
+
     $count++;
     if(!count($this->Menchledger->fetch(array(
-        'linktype' => 4983,
+        'linktype' => $x['link_type'],
         'linkplayerup' => $x['link_up'],
         'linkplayerdown' => $x['link_down'],
         'linkidealeft' => ( $x['link_left']>0 ? intval($x['link_left'])+100000 : 0 ),
@@ -19,8 +42,15 @@ foreach ($this->Menchledger->fetchold(array(
     )))){
         $missing++;
         echo print_r($x, true);
-        echo '<hr />';
+        echo 'WAS MISSING <hr />';
     }
 }
 
-echo $missing.'/'.$count.' Missing';
+echo $count.' Total:<br /><br />';
+
+echo $missing.' MISSING<br />';
+echo $count_ideation.' $count_ideation<br />';
+echo $count_sourcing.' $count_sourcing<br />';
+echo $count_contribution.' $count_contribution<br />';
+echo $count_discovery.' $count_discovery<br />';
+echo $count_other.' $count_other<br />';
