3da10c37843110bf03aa678bd91498489f7b4074
shervinesta <shervin@mench.com>
2025-04-02 05:54:04 -0700

    Ledger V12

--- application/views/json.php
+++ application/views/json.php
@@ -24,7 +24,7 @@ foreach($this->Menchledger->fetch(array(
         echo '<tr><td>'.$previous['linktype'].'</td><td>'.$previous['linktime'].'</td><td>'.$previous['linkcreator'].'</td><td>'.$previous['linkright'].'</td><td>'.$previous['linkleft'].'</td><td>'.$previous['linktext'].'</td><td>'.$previous['linktype'].'</td><td>'.$previous['linktype'].'</td></tr>';
         echo '<tr style="background-color: #CCC;"><td>'.$discover['linktype'].'</td><td>'.$discover['linktime'].'</td><td>'.$discover['linkcreator'].'</td><td>'.$discover['linkright'].'</td><td>'.$discover['linkleft'].'</td><td>'.$discover['linktext'].'</td><td>'.$discover['linktype'].'</td><td>'.$discover['linktype'].'</td></tr>';
 
-        //$this->db->query("DELETE FROM menchledger WHERE linkid=".$discover['linkid'].";");
+        $this->db->query("DELETE FROM menchledger WHERE linkid=".$discover['linkid'].";");
 
     }
 
