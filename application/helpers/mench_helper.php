4c74d2bab123f03403f0115fe1b4e3ef257e9dc4
shervinesta <shervin@mench.com>
2025-04-02 22:33:12 -0700

    Ledger V12

--- application/helpers/mench_helper.php
+++ application/helpers/mench_helper.php
@@ -3122,7 +3122,7 @@ function view_player_body($linkplayertype, $counter, $playerid, $js_request_uri)
     $es = $CI->Nodeplayers->fetch(array(
         'playerid' => $playerid,
     ));
-    if (!count($es) || !count($list_results)) {
+    if (!count($es) || !$list_results) {
         return false;
     }
     $ui = '';
