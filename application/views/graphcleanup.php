aadbc2da9635d1392d219f2288676a406dc56de5
shervinesta <shervin@mench.com>
2025-04-03 22:24:05 -0700

    Ledger V12

--- application/views/graphcleanup.php
+++ application/views/graphcleanup.php
@@ -1,177 +1,101 @@
 <?php
 
+//view_json($this->Menchledger->tree_full_history($focus_i, $focus_e['playerid']));
 
+$is_ideation = array(4228, 31834, 42337, 33532, 44161, 40791, 44162, 40793, 32247, 32304, 33344, 30901, 42244, 42243);
+$is_sourcing = array(41011, 4251, 44399, 33335, 42659, 42849, 44176, 44179, 42897, 32486, 4230, 32489, 42579, 42581, 42580, 42283, 42335, 42516, 42554, 42570, 42427, 42518, 42440, 42791);
+$is_contribution = array(34513, 4250, 4983, 44396, 42292, 33600, 42272, 42274, 31840, 42275, 42282, 32488, 4258, 4260, 4259, 31835, 41949, 43941, 4256, 12896, 10573, 27984, 43513, 43514, 26600, 7545, 26599, 32235);
+$is_discovery = array(44397, 44245, 43798, 43142, 42995, 42916, 42402, 42397, 29393, 42332, 41940, 39597, 35572, 32016, 4559, 31967, 31810, 31798, 31797, 7712, 6144, 31022, 6157, 26595, 4235, 12117, 4235, 7712, 27676, 27678, 29399);
+$count_ideation = 0;
+$count_sourcing = 0;
+$count_contribution = 0;
+$count_discovery = 0;
+$count_other = 0;
+$missing_ideation = 0;
+$missing_sourcing = 0;
+$missing_contribution = 0;
+$missing_discovery = 0;
+$missing_other = 0;
 boost_power();
 
+$count = 0;
+$missing = 0;
+foreach ($this->Menchledger->fetchold(array(
+    'link_type IN (' . join(',', $is_contribution) . ')' => null, //Active Writes
+), array(), 0, 0, array('link_id' => 'DESC')) as $x) {
 
+    $count++;
+    $is_missing = !count($this->Menchledger->fetch(array(
+        'linkplayertype' => $x['link_type'],
+        'linkplayerup' => $x['link_up'],
+        'linkplayerdown' => $x['link_down'],
+        'linkidealeft' => ($x['link_left'] > 0 ? intval($x['link_left']) + 100000 : 0),
+        'linkidearight' => ($x['link_right'] > 0 ? intval($x['link_right']) + 100000 : 0),
+    )));
+    if ($is_missing) {
 
-$link_count = 0;
-$link_success = 0;
+        $missing++;
+        echo print_r($x, true);
+        echo 'WAS MISSING <hr />';
 
-echo '<table width="100%" border="1px">';
-foreach($this->Link->readalt(array(
-    'link_type !=' => 4250,
-), array(), 0, 0, array(
-    'link_id' => 'ASC',
-)) as $x){
-
-    $new_x = $this->db->insert('menchledger', array(
-        'linkplayer' => $x['link_player'],
-        'linktime' => $x['link_time'],
-        'linkdomain' => $x['link_domain'],
-        'linkup' => intval($x['link_up']),
-        'linkdown' => intval($x['link_down']),
-        'linkleft' => ( $x['link_left']>0 ? 100000+$x['link_left'] : 0 ),
-        'linkright' => ( $x['link_right']>0 ? 100000+$x['link_right'] : 0 ),
-        'linknumber' => $x['link_number'],
-        'linktext' => $x['link_text'],
-        'linktype' => $x['link_type'],
-        'linkvoid' => 0,
-    ));
-
-    $insert_id = $this->db->insert_id();
-
-    if($insert_id>0){
-        $this->db->query("DELETE FROM mench_ledger WHERE link_id=".$x['link_id'].";");
-        $link_success++;
+        /*
+        $this->Menchledger->create(array(
+            'linktime' => $x['link_time'],
+            'linkplayercreator' => $x['link_player'],
+            'linkplayertype' => $x['link_type'],
+            'linkplayerdomain' => $x['link_domain'],
+            'linkplayerup' => $x['link_up'],
+            'linkplayerdown' => $x['link_down'],
+            'linkidealeft' => ($x['link_left'] > 0 ? intval($x['link_left']) + 100000 : 0),
+            'linkidearight' => ($x['link_right'] > 0 ? intval($x['link_right']) + 100000 : 0),
+            'linknumber' => $x['link_number'],
+            'linktext' => $x['link_text'],
+        ));
+        */
     }
-    $link_count++;
 
-}
-echo '</table>';
-
-echo $link_success.'/'.$link_count.' SUCCESS';
-
-
-
-
-//view__json($this->Mench_ledger->tree_full_history($focus_i, $focus_e['e__id']));
-if(0){
-    if(1){
-        $count = 0;
-        $found = 0;
-        foreach($this->Source_cache->fetch(array(
-            'e__id >' => 0,
-        ), 0, 0, array('e__id' => 'ASC')) as $e){
-
-            $count++;
-            //echo $count.') @'.$e['e__handle'].' @'.$e['e__id'].'<hr />';
-
-            $creators = $this->Mench_ledger->fetch(array(
-                'link_down' => $e['e__id'],
-                'link_type IN (4230,4251)' => null, //Idea References
-                'link_void' => 0, //Not Void
-            ));
-
-            $this->db->insert('menchledger', array(
-                'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
-                'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
-                'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
-                'linkup' => 32338, //Player Handle
-                'linktext' => $e['e__handle'],
-                'linkdown' => $e['e__id'],
-                'linktype' => 4230, //Follow
-            ));
-
-            if(strlen($e['e__cover'])){
-                $this->db->insert('menchledger', array(
-                    'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
-                    'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
-                    'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
-                    'linkup' => 6198, //Player Cover
-                    'linktext' => $e['e__cover'],
-                    'linkdown' => $e['e__id'],
-                    'linktype' => 4230, //Follow
-                ));
-            }
-
-
-
-
-            if(0){
-                //Lets log:
-                $this->db->insert('menchledger', array(
-                    'linkid' => $e['e__id'],
-                    'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
-                    'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
-                    'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
-                    'linktext' => $e['e__title'],
-                    'linktype' => 4251, //New Source Created
-                ));
-
-                //Add to cache:
-                $this->db->insert('cacheplayers', array(
-                    'playerid' => $e['e__id'],
-                    'playerexternal' => intval($e['e__external']),
-                    'playernumber' => intval($e['e__weight']),
-                    'playerhandle' => $e['e__handle'],
-                    'playercover' => $e['e__cover'],
-                    'playertext' => $e['e__title'],
-                ));
-            }
-
+    if (in_array($x['link_type'], $is_ideation)) {
+        $count_ideation++;
+        if ($is_missing) {
+            $missing_ideation++;
         }
-        echo $found.' Updated Found';
-
+    } elseif (in_array($x['link_type'], $is_sourcing)) {
+        $count_sourcing++;
+        if ($is_missing) {
+            $missing_sourcing++;
+        }
+    } elseif (in_array($x['link_type'], $is_contribution)) {
+        $count_contribution++;
+        if ($is_missing) {
+            $missing_contribution++;
+        }
+    } elseif (in_array($x['link_type'], $is_discovery)) {
+        $count_discovery++;
+        if ($is_missing) {
+            $missing_discovery++;
+        }
+    } else {
+        $count_other++;
+        if ($is_missing) {
+            $missing_other++;
+        }
+        echo print_r($x, true);
+        echo 'WAS OTHER<hr />';
     }
-
-
-
-
-    $count = 0;
-    $found = 0;
-    foreach($this->Idea_cache->fetch(array(
-        'i__id >' => 0,
-    ), 0) as $i){
-
-        $count++;
-        //echo $count.') #'.$i['i__id']."<hr />";
-
-        $creators = $this->Mench_ledger->fetch(array(
-            'link_right' => $i['i__id'],
-            'link_type' => 4250, //Idea References
-            'link_void' => 0, //Not Void
-        ));
-
-        //Lets log:
-        $new_i_id = intval($i['i__id'])+100000;
-
-        $this->db->insert('menchledger', array(
-            'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : $e['e__id'] ),
-            'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
-            'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
-            'linkup' => 32337, //Idea Hashtag
-            'linkright' => $new_i_id,
-            'linktext' => $i['i__hashtag'],
-            'linktype' => 4983, //CO-author
-        ));
-
-        continue;
-
-        $this->db->insert('menchledger', array(
-            'linkid' => $new_i_id,
-            'linkplayer' => ( isset($creators[0]['link_player']) ? $creators[0]['link_player'] : 1 ),
-            'linktime' => ( isset($creators[0]['link_time']) ? $creators[0]['link_time'] : date("Y-m-d H:i:s", time()) ),
-            'linkdomain' => ( isset($creators[0]['link_domain']) ? $creators[0]['link_domain'] : 0 ),
-            'linktext' => $i['i__message'],
-            'linktype' => 4250,
-        ));
-
-        //Add to cache:
-        $this->db->insert('cacheideas', array(
-            'ideaid' => $new_i_id,
-            'ideaexternal' => intval($i['i__external']),
-            'ideanumber' => intval($i['i__weight']),
-            'ideahashtag' => $i['i__hashtag'],
-            'ideatext' => $i['i__message'],
-            'ideacache' => $i['i__cache'],
-        ));
-
-
-    }
-    echo $found.'/'.$count.' Found';
 
 }
+
+echo $missing . '/' . $count . ' Total Missing:<br /><br />';
+
+echo $missing_ideation . '/' . $count_ideation . ' $count_ideation<br />';
+echo $missing_sourcing . '/' . $count_sourcing . ' $count_sourcing<br />';
+echo $missing_contribution . '/' . $count_contribution . ' $count_contribution<br />';
+echo $missing_discovery . '/' . $count_discovery . ' $count_discovery<br /><br />';
+
+echo $missing_other . '/' . $count_other . ' $count_other<br />';
+
+
+
 
 /*
 
