826b3c04537098676337e01e5168e9086235483f
shervinesta <shervin@mench.com>
2025-04-07 20:25:31 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -495,7 +495,7 @@ function idea_list_config($ideaid){
         array_push($list_config[intval($setting_link['linkplayertype'])], intval($setting_link['linkidearight']));
     }
 
-    prin_r($list_config);
+    print_r($list_config);
     die('sdasd');
 
     return $list_config;
