71b6e4c2961a2f172cff5bbd750f499fe8f0f6ec
shervinesta <shervin@mench.com>
2025-04-07 20:09:40 -0700

    Ledger V12

--- application/views/json.php
+++ application/views/json.php
@@ -1,3 +1,6 @@
 <?php
 
-view_json($this->Links->history($focus_i, $focus_e['playerid']));
+view_json(array(
+    'idea_settings' => idea_settings($focus_i['ideahashtag'], false),
+    //'history' => $this->Links->history($focus_i, $focus_e['playerid']),
+));
