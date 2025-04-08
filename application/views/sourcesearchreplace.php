7ad9b5fdae9e1f04d6e55d7b5a93a3bf8ae29e01
shervinesta <shervin@mench.com>
2025-04-07 21:30:34 -0700

    Ledger V12

--- application/views/sourcesearchreplace.php
+++ application/views/sourcesearchreplace.php
@@ -39,10 +39,10 @@ if($search_for_set){
 
                 //Do replacement:
                 $append_text = @$_GET['append_text'];
-                $en['playertext'] = Playersearchreplace . phpstr_ireplace($_GET['search_for'], $_GET['replace_with'], $en['playertext']) . $append_text;
+                $en['playertext'] = str_ireplace($_GET['search_for'],$_GET['replace_with'],$en['e__title']) . $append_text;
 
                 if($replace_with_confirmed){
-                    //Update idea:
+                    //Update player:
                     $res = $this->Players->update($en['playerid'], array(
                         'playertext' => $en['playertext'],
                     ), $player_active['playerid']);
