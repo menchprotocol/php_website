aa4245e4baf67a61b7065a11dd951b5ca512ba9b
shervinesta <shervin@mench.com>
2025-04-07 19:03:21 -0700

    Ledger V12

--- application/views/communities.php
+++ application/views/communities.php
@@ -9,12 +9,12 @@ foreach ((isset($_GET['playerhandle']) && strlen($_GET['playerhandle']) ? $this-
         'linkplayertype IN (' . join(',', $this->config->item('playerids___13548')) . ')' => null, //SOURCE LINKS
     ), array('linkplayerdown'), 0, 0, array('linknumber' => 'ASC', 'linkid' => 'DESC')) as $x) {
 
-        $total_count = players_query(11029, $x['playerid'], 0, false);
+        $total_count = players_query(42373, $x['playerid'], 0, false);
 
         if ($total_count) {
 
             $ui = '<div class="row justify-content">';
-            foreach (players_query(11029, $x['playerid'], 1, false) as $count => $e) {
+            foreach (players_query(42373, $x['playerid'], 1, false) as $count => $e) {
                 $ui .= player_view(13207, $e, null);
             }
             $ui .= '</div>';
