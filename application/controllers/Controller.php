91750bd9b2717c01fa994e083441d5ba2f15c5ee
shervinesta <shervin@mench.com>
2025-04-08 00:21:41 -0700

    Ledger V12

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -3516,7 +3516,7 @@ class Controller extends CI_Controller
                 } elseif ($has_hashtag) {
                     $void_filter['( linkidealeft = ' . $is[0]['ideaid'] . ' OR linkidearight = ' . $is[0]['ideaid'] . ' )'] = null;
                 }
-                prin_r($void_filter);
+                print_r($void_filter);
                 die();
                 $sub_counter = $this->Links->read($void_filter, array(), 0, 0, array(), 'COUNT(linkid) as totals');
                 $return_array[$linkplayertype1] = intval($sub_counter[0]['totals']);
