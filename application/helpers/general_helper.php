677be354766fcc8c38a93f71834a39a12c9e6761
shervinesta <shervin@mench.com>
2025-04-09 12:13:44 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -517,9 +517,9 @@ function idea_list_config($ideaid){
     //Now search for these settings across ideas:
     foreach ($CI->Links->read(array(
         'linkidearight >' => 0,
-        'linkidearight' => $ideaid,
+        'linkidealeft' => $ideaid,
         'linkplayertype IN (' . join(',', $CI->config->item('playerids___40792')) . ')' => null,
-    ), array('linkidealeft'), 0) as $setting_link) {
+    ), array('linkidearight'), 0) as $setting_link) {
         array_push($idea_list_config[intval($setting_link['linkplayertype'])], intval($setting_link['linkidearight']));
         array_push($idea_list_config['full_config_'.$setting_link['linkplayertype']], $setting_link);
     }
