db62fbf6188624929a23c5673aa8303bb3956c4c
shervinesta <shervin@mench.com>
2025-04-11 00:05:59 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2665,19 +2665,19 @@ function link_view($x)
             //SOURCE
             $ui .= '<td style="width:100px !important;">';
             $ui .= '<div style="width:100px !important; overflow:hidden;">';
-            $found_group = null;
+            $playerhandle_sign = '@';
             if($playerid==4593){
                 //Determine the link group:
                 foreach($CI->config->item('players___31770') as $groupid => $groupm) {
                     if(in_array($x[$m['m__handle']], $CI->config->item('playerids___'.$groupid))){
-                        $found_group = $groupm['m__cover'];
+                        $playerhandle_sign = $groupm['m__cover'];
                         break;
                     }
                 }
             }
             if (isset($x[$m['m__handle']]) && intval($x[$m['m__handle']]) > 0) {
                 foreach ($CI->Players->read(array('playerid' => $x[$m['m__handle']])) as $focus_e) {
-                    $ui .= '<a href="' . view_memory(42903, 42902) . $focus_e['playerhandle'] . '" data-toggle="popover">'.$found_group.'@' . $focus_e['playerhandle'] . '</a>';
+                    $ui .= '<a href="' . view_memory(42903, 42902) . $focus_e['playerhandle'] . '" data-toggle="popover">'.$playerhandle_sign . $focus_e['playerhandle'] . '</a>';
                 }
             }
             $ui .= '</div>';
