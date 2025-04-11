b1f2eafb339428d4f677784ced3ec54bbac439fe
shervinesta <shervin@mench.com>
2025-04-11 00:19:35 -0700

    Ledger V12

--- application/views/graph.php
+++ application/views/graph.php
@@ -47,7 +47,7 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
         echo '<div class="card_header" title="' . $m2['m__message'] . '" playerid="' . $playerid2 . '">';
         echo '<div class="large_cover">' . $m2['m__cover'] . '</div>';
-        echo '<div class="main__title large_title"><a href="'.view_app_link(4341).'?linkplayertype='.join(',',( $is_link ? $this->config->item('players___' . $playerid2) : array(( $playerid2==12273 ? 4250 : 4251 )) )).'&linkvoid=0" class="card_count_' . $playerid2 . '"><i class="fas fa-yin-yang fa-spin"></i></a></div>';
+        echo '<div class="main__title large_title"><a href="'.view_app_link(4341).'?linkplayertype='.join(',',( $is_link ? $this->config->item('playerids___' . $playerid2) : array(( $playerid2==12273 ? 4250 : 4251 )) )).'&linkvoid=0" class="card_count_' . $playerid2 . '"><i class="fas fa-yin-yang fa-spin"></i></a></div>';
         echo '<div class="main__title large_title" title="@' . $playerid2 . ' @' . $m2['m__handle'] . '">' . $m2['m__title'] . '</div>';
         echo '</div>';
 
