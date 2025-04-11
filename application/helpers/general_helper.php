c1cd0b1dfd2546c397f2e742e77066408c5f90c1
shervinesta <shervin@mench.com>
2025-04-10 22:57:38 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2663,23 +2663,27 @@ function link_view($x)
         if (in_array(6160, $m['m__following'])) {
 
             //SOURCE
-            $ui .= '<td style="width:55px !important; overflow:hidden;">';
+            $ui .= '<td style="width:55px !important;">';
+            $ui .= '<div style="width:55px !important; overflow:hidden;">';
             if (isset($x[$m['m__handle']]) && intval($x[$m['m__handle']]) > 0) {
                 foreach ($CI->Players->read(array('playerid' => $x[$m['m__handle']])) as $focus_e) {
                     $ui .= '<a href="' . view_memory(42903, 42902) . $focus_e['playerhandle'] . '">@' . $focus_e['playerhandle'] . '</a>';
                 }
             }
+            $ui .= '</div>';
             $ui .= '</td>';
 
         } elseif (in_array(6202, $m['m__following'])) {
 
             //IDEA
             $ui .= '<td style="width:55px !important; overflow:hidden;">';
+            $ui .= '<div style="width:55px !important; overflow:hidden;">';
             if (isset($x[$m['m__handle']]) && intval($x[$m['m__handle']]) > 0) {
                 foreach ($CI->Ideas->read(array('ideaid' => $x[$m['m__handle']])) as $focus_i) {
                     $ui .= '<a href="' . view_memory(42903, 33286) . $focus_i['ideahashtag'] . '">#' . $focus_i['ideahashtag'] . '</a>';
                 }
             }
+            $ui .= '</div>';
             $ui .= '</td>';
 
         } elseif (in_array(4367, $m['m__following'])) {
