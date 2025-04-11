a754b1b17337087b4ae82652bca13a5c27307b0a
shervinesta <shervin@mench.com>
2025-04-10 23:01:40 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2676,8 +2676,8 @@ function link_view($x)
         } elseif (in_array(6202, $m['m__following'])) {
 
             //IDEA
-            $ui .= '<td style="width:110px !important;">';
-            $ui .= '<div style="width:110px !important; overflow:hidden;">';
+            $ui .= '<td style="width:125px !important;">';
+            $ui .= '<div style="width:125px !important; overflow:hidden;">';
             if (isset($x[$m['m__handle']]) && intval($x[$m['m__handle']]) > 0) {
                 foreach ($CI->Ideas->read(array('ideaid' => $x[$m['m__handle']])) as $focus_i) {
                     $ui .= '<a href="' . view_memory(42903, 33286) . $focus_i['ideahashtag'] . '">#' . $focus_i['ideahashtag'] . '</a>';
