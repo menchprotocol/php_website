4ab11606c898194b99c82b5c808fdcb2d28b6f8f
shervinesta <shervin@mench.com>
2025-04-10 20:09:42 -0700

    Ledger V12

--- application/views/graph.php
+++ application/views/graph.php
@@ -83,7 +83,7 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
 
                 echo '<tr class="main__title mobile-shrink" title="' . $m3['m__message'] . '" data-toggle="tooltip" data-placement="top">';
-                echo '<td style="text-align: left;" title="@' . $playerid3 . ' @' . $m3['m__handle'] . '"><a href="' . view_memory(42903, 42902) . $m3['m__handle'] . '"><span class="icon-block-sm">' . $m3['m__cover'] . '</span>' . $m3['m__title'] . '</a><span class="last-right-col"><a href="'.view_app_link(4341).'?linkplayertype='.  $playerid3 . '" class="card_count_' . $playerid3 . '"><i class="fas fa-yin-yang fa-spin"></i></a></span></td>';
+                echo '<td style="text-align: left;" title="@' . $playerid3 . ' @' . $m3['m__handle'] . '"><a href="' . view_memory(42903, 42902) . $m3['m__handle'] . '"><span class="icon-block-sm">' . $m3['m__cover'] . '</span>' . $m3['m__title'] . '</a><span class="last-right-col"><a href="'.view_app_link(4341).'?linkplayertype='.  $playerid3 . '&linkvoid=0" class="card_count_' . $playerid3 . '"><i class="fas fa-yin-yang fa-spin"></i></a></span></td>';
                 echo '</tr>';
 
             }
