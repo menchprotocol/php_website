a59745ad6d98cb5e68ed5a28c0e0decb9d4eef0d
shervinesta <shervin@mench.com>
2025-04-10 22:51:55 -0700

    Ledger V12

--- application/views/ledger.php
+++ application/views/ledger.php
@@ -408,7 +408,7 @@ echo '<div class="overall_stats"></div>';
 echo '<table id="table_menchledger" class="table table-sm table-striped image-mini" style="font-size: 0.8em;">'; //table-layout: fixed;
 echo '<tr style="font-weight:bold; vertical-align: baseline;">';
 foreach ($this->config->item('players___4341') as $linkplayertype => $m) {
-    echo '<th class="main__title"><span class="icon-block-sm">' . $m['m__cover'] . '</span>' . $m['m__title'] . '</th>';
+    echo '<th class="main__title"><span class="icon-block-xs">' . $m['m__cover'] . '</span>' . $m['m__title'] . '</th>';
 }
 echo '</tr>';
 echo '</table>';
