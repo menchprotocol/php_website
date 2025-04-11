0ba81527b41321f093f99a935f637ab7d3a21395
shervinesta <shervin@mench.com>
2025-04-10 23:59:07 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2681,8 +2681,8 @@ function link_view($x)
             $found_group = null;
             if($playerid==4593){
                 //Determine the link group:
-                foreach($this->config->item('players___14599') as $groupid => $groupm) {
-                    if(in_array(6202, $m['m__following'])){
+                foreach($this->config->item('players___31770') as $groupid => $groupm) {
+                    if(in_array(6202, $groupm['m__following'])){
                         $found_group = $groupm;
                         break;
                     }
