df5a5c9bd3355e2c01340cd4fe89308e92658e39
shervinesta <shervin@mench.com>
2025-04-10 23:11:10 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2689,7 +2689,7 @@ function link_view($x)
         } elseif (in_array(4367, $m['m__following'])) {
 
             //Link:
-            $ui .= '<td style="width:75px !important;">';
+            $ui .= '<td style="width:65px !important;">';
             if($x[$m['m__handle']]>0){
                 $ui .= '<a href="' . view_app_link(4341) . '?linkid=' . $x[$m['m__handle']] . '">' . $x[$m['m__handle']] . '</a>';
             }
