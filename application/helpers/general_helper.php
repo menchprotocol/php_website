fc1021bf39b7215cb6f3d240c1bd08e2c3cafc95
shervinesta <shervin@mench.com>
2025-04-11 09:27:08 -0700

    Ledger V12

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2719,7 +2719,7 @@ function link_view($x)
             //TIME
             $ui .= '<td style="width:80px !important;">';
             $ui .= '<div style="width:80px !important; overflow:hidden;">';
-            $ui .= '<span data-toggle="tooltip" data-placement="top" title="' . $m['m__title'] . ': ' . $x['linktime'] . ' PST | ID ' . $x['linkid'] . '">' . view_time_difference($x['linktime']) . '</span>';
+            $ui .= '<span data-toggle="tooltip" data-placement="top" title="' . $x['linktime'] . ' PST">' . view_time_difference($x['linktime']) . '</span>';
             $ui .= '</div>';
             $ui .= '</td>';
 
