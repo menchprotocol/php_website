5b7607e4446a6d61aae52341ab077a1ecb88d152
shervinesta <shervin@mench.com>
2025-04-13 14:17:02 -0700

    Ledger V12 Immutable

--- application/views/ledger.php
+++ application/views/ledger.php
@@ -426,7 +426,7 @@ foreach ($this->config->item('players___4341') as $linkplayertype => $m) {
 }
 $row1 .= '</tr>';
 $row2 .= '</tr>';
-echo '<table id="table_menchledger" class="table table-sm table-striped image-mini" style="font-size: 0.8em;">'.$row1.$row2.'</table>';
+echo '<table id="table_menchledger" class="table table-sm image-mini" style="font-size: 0.8em;">'.$row1.$row2.'</table>';
 
 //Table Data
 echo '<div class="main__title center hidden load_message"><span class="icon-block-sm"><i class="fas fa-yin-yang fa-spin"></i></span><span class="random_message"></span></div>';
