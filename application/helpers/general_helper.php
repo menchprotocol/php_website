58c16539bf0d892e36d09329c852804e19e243c8
shervinesta <shervin@mench.com>
2025-04-14 11:24:19 -0700

    Ledger V12 Immutable

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2696,7 +2696,7 @@ function link_view($x)
         if (in_array(6160, $m['m__following'])) {
 
             //SOURCE
-            $column_value .= '<td '.( $playerid==4593 ? 'rowspan="2"' : '' ).' style="width:25px !important;"><div style="width:25px !important; overflow:hidden;">';
+            $column_value .= '<td '.( $playerid==4593 ? 'rowspan="2"' : '' ).' style="width:25px !important; vertical-align : middle;text-align:center;"><div style="width:25px !important; overflow:hidden;">';
             if (isset($x[$m['m__handle']]) && intval($x[$m['m__handle']]) > 0) {
                 foreach ($CI->Players->read(array('playerid' => $x[$m['m__handle']])) as $focus_e) {
                     $column_value .= '<a href="' . view_memory(42903, 42902) . $focus_e['playerhandle'] . '" target="_blank" data-toggle="tooltip" title="' . $focus_e['playertext'] . '" class="icon-block-sm">' . view_cover($focus_e['playercover'], '<i class="far fa-at"></i>') . '</a>';
@@ -2742,7 +2742,7 @@ function link_view($x)
         } elseif ($playerid == 44395) {
 
             //Void:
-            $column_value .= '<td rowspan="2" style="width:60px !important;">';
+            $column_value .= '<td rowspan="2" style="width:60px !important; vertical-align : middle;text-align:center;">';
             $column_value .= ($x[$m['m__handle']] > 0 ? '<a href="' . view_app_link(4341) . '?linkid=' . $x[$m['m__handle']] . '" target="_blank">' . $x[$m['m__handle']] . '</a>' : '&nbsp;' );
             $column_value .= '</td>';
 
