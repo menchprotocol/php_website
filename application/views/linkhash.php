d634431a6b77898e8135a4a1dacdb50888cfee6f
shervinesta <shervin@mench.com>
2025-04-14 09:59:41 -0700

    Ledger V12 Immutable

--- application/views/linkhash.php
+++ application/views/linkhash.php
@@ -1,7 +1,13 @@
 <?php
 
 $count = 0;
+//Fetch current last hash:
+
 $previous = '1111111111111111111111111111111111111111';
+foreach($this->Links->read(array('(linkhash IS NOT NULL) AND (linkprevioushash IS NOT NULL)' => NULL), array(), 0, 0, array('linkid' => 'DESC')) as $x){
+    $previous = $x['linkhash'];
+}
+
 foreach($this->Links->read(array('(linkhash IS NULL) OR (linkprevioushash IS NULL)' => NULL), array(), 0, 0, array('linkid' => 'ASC')) as $x){
     $hash = linkhash($x);
     $this->db->query("UPDATE menchledger SET linkprevioushash = '".$previous."', linkhash = '".$hash."' WHERE linkid=".$x['linkid'].";");
