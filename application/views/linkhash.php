ee983b1a1b40d2e421b0081968e5051931e5a7e0
shervinesta <shervin@mench.com>
2025-04-14 11:35:36 -0700

    Ledger V12 Immutable

--- application/views/linkhash.php
+++ application/views/linkhash.php
@@ -1,26 +1,24 @@
 <?php
 
-$previous = null;
+
+
+$previous = linkprevious();
 $count = 0;
 $fixed = 0;
-foreach ($this->Links->read(array('linkid >' => 1559000), array(), 0, 0, array('linkid' => 'ASC')) as $x) {
-    if(!$previous){
-        echo print_r($x);
-        $previous = $x['linkhash'];
-        continue;
-    }
+foreach ($this->Links->read(array(), array(), 0, 0, array('linkid' => 'ASC')) as $x) {
     $must_fix = false;
     if($x['linkprevious']!=$previous){
         $x['linkprevious'] = $previous;
-        $must_fix = true;
+        $must_fix = false;
     }
     $hash = linkhash($x);
-    if($x['linkhash']!=$hash || $must_fix){
-        $this->db->query("UPDATE menchledger SET linkprevious = '" . $previous . "', linkhash = '" . $hash . "' WHERE linkid=" . $x['linkid'] . ";");
+    if($x['linkhash']!=$hash || $x['linkprevious']!=$previous){
+        //$this->db->query("UPDATE menchledger SET linkprevious = '" . $previous . "', linkhash = '" . $hash . "' WHERE linkid=" . $x['linkid'] . ";");
+        echo $x['linkid'].': '.$x['linkhash'].'!='.$hash.' OR '.$x['linkprevious'].'!='.$previous.'<hr />';
         $fixed++;
     }
     $previous = $hash;
     $count++;
 }
-echo $fixed.'/'.$count . ' hashes fixed';
+echo $fixed.'/'.$count . ' hashes';
 
