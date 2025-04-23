ced8be78bedc93f24ec1c13b72e1478da03da39b
shervinesta <shervin@mench.com>
2025-04-23 00:51:09 -0700

    Ledger V12 Immutable

--- application/views/ideachain.php
+++ application/views/ideachain.php
@@ -106,10 +106,10 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
         //Legend of how nodes connect:
         echo '<table class="table table-sm maxout center" style="width: 100%; table-layout: fixed; margin-bottom: 21px;"><tr>';
         echo '<td style="width: 16.66%; text-align: center;">&nbsp;</td>';
-        echo '<td style="width: 16.66%; text-align: center;" class="appender_32292"><i class="far fa-rotate-left rotate90"></i></td>';
-        echo '<td style="width: 16.66%; text-align: center;" class="appender_31777"><i class="far fa-arrow-right-long"></i></td>';
-        echo '<td style="width: 16.66%; text-align: center;" class="appender_13550"><i class="far fa-arrow-left-long"></i></td>';
-        echo '<td style="width: 16.66%; text-align: center;" class="appender_4486"><i class="far fa-rotate-right rotate270"></i></td>';
+        echo '<td style="width: 16.66%; text-align: center;" class="appender_32292"><i class="fas fa-rotate-left rotate90"></i></td>';
+        echo '<td style="width: 16.66%; text-align: center;" class="appender_13550"><i class="fas fa-arrow-right-long"></i></td>';
+        echo '<td style="width: 16.66%; text-align: center;" class="appender_31777"><i class="fas fa-arrow-left-long"></i></td>';
+        echo '<td style="width: 16.66%; text-align: center;" class="appender_4486"><i class="fas fa-rotate-right rotate270"></i></td>';
         echo '<td style="width: 16.66%; text-align: center;">&nbsp;</td>';
         echo '</tr></table>';
     }
