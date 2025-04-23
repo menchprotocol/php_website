abce629bc5286e6c1f1b2487f20fb58e49188486
shervinesta <shervin@mench.com>
2025-04-23 09:52:56 -0700

    Ledger V12 Immutable

--- application/views/ideachain.php
+++ application/views/ideachain.php
@@ -19,22 +19,24 @@ if ($playerhandle) {
     }
 }
 
+
 //Misc Stats, if any:
 echo '<div class="center hideIfEmpty"></div>';
 
 foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
     if($playerid1==1309754){
-        echo '<div class="mid-text-line compact-midline"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkvoid=1" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . '</span></div>';
+        echo '<div class="mid-text-line compact-midline hidden headlines"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkvoid=1" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . '</span></div>';
         //Void Links
         continue;
     } elseif($playerid1==28956){
         //Nodes
-        echo '<div class="mid-text-line compact-midline"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkplayertype=4250,4251&linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . ':</span></div>';
+        echo '<div class="mid-text-line compact-midline hidden headlines"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkplayertype=4250,4251&linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . ':</span></div>';
     } elseif($playerid1==31770){
 
         //Links
-        echo '<div class="mid-text-line compact-midline"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> <a href="javascript:void(0)" onclick="$(\'.headlines\').toggleClass(\'hidden\')" class="grey">' . $m1['m__title'] . '</a>:</span></div>';
+        echo '<div class="mid-text-line compact-midline hidden headlines"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . ':</span></div>';
+
     }
 
     echo '<div class="row justify-content list-covers">';
@@ -126,6 +128,9 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
 
 }
+
+echo '<div class="center"><a href="javascript:void(0)" onclick="$(\'.headlines\').toggleClass(\'hidden\')" class="grey">Toggle</a></div>';
+
 
 ?>
 
