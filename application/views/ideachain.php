d49bec7fb15d15c9cb1428c8882ccada3e860601
shervinesta <shervin@mench.com>
2025-04-23 00:42:30 -0700

    Ledger V12 Immutable

--- application/views/ideachain.php
+++ application/views/ideachain.php
@@ -33,14 +33,6 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
         echo '<div class="mid-text-line compact-midline"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkplayertype=4250,4251&linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> ' . $m1['m__title'] . ':</span></div>';
     } elseif($playerid1==31770){
 
-        //Legend
-        echo '<div class="row center">';
-        echo '<div class="col-3 appender_32292"><i class="far fa-rotate-left"></i></div>';
-        echo '<div class="col-3 appender_4486"><i class="far arrow-right-long"></i></div>';
-        echo '<div class="col-3 appender_13550"><i class="far arrow-left-long"></i></div>';
-        echo '<div class="col-3 appender_31777"><i class="far fa-rotate-right"></i></div>';
-        echo '</div>';
-
         //Links
         echo '<div class="mid-text-line compact-midline"><span>' . $m1['m__cover'] . ' <a target="_blank" href="'.view_app_link(4341).'?linkvoid=0" class="grey card_count_' . $playerid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a> <a href="javascript:void(0)" onclick="$(\'.headlines\').toggleClass(\'hidden\')" class="grey">' . $m1['m__title'] . '</a>:</span></div>';
     }
@@ -107,6 +99,16 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
         echo '</div>';
         echo '</div>';
+
+        if($playerid1==28956){
+            //Legend of how nodes connect:
+            echo '<div class="row center">';
+            echo '<div class="col-3 appender_32292"><i class="far fa-rotate-left"></i></div>';
+            echo '<div class="col-3 appender_4486"><i class="far fa-arrow-right-long"></i></div>';
+            echo '<div class="col-3 appender_13550"><i class="far fa-arrow-left-long"></i></div>';
+            echo '<div class="col-3 appender_31777"><i class="far fa-rotate-right"></i></div>';
+            echo '</div>';
+        }
 
     }
 
