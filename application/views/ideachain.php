96d71a048e1725df1276709c00e4eaf6b2a1aa90
shervinesta <shervin@mench.com>
2025-04-23 00:44:45 -0700

    Ledger V12 Immutable

--- application/views/ideachain.php
+++ application/views/ideachain.php
@@ -100,19 +100,19 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
         echo '</div>';
         echo '</div>';
 
-        if($playerid1==28956){
-            //Legend of how nodes connect:
-            echo '<table class="table table-sm maxout center"><tr>';
-            echo '<td class="appender_32292"><i class="far fa-rotate-left"></i></td>';
-            echo '<td class="appender_4486"><i class="far fa-arrow-right-long"></i></td>';
-            echo '<td class="appender_13550"><i class="far fa-arrow-left-long"></i></td>';
-            echo '<td class="appender_31777"><i class="far fa-rotate-right"></i></td>';
-            echo '</tr></table>';
-        }
-
     }
 
     echo '</div>';
+
+    if($playerid1==28956){
+        //Legend of how nodes connect:
+        echo '<table class="table table-sm maxout center"><tr>';
+        echo '<td class="appender_32292"><i class="far fa-rotate-left"></i></td>';
+        echo '<td class="appender_4486"><i class="far fa-arrow-right-long"></i></td>';
+        echo '<td class="appender_13550"><i class="far fa-arrow-left-long"></i></td>';
+        echo '<td class="appender_31777"><i class="far fa-rotate-right"></i></td>';
+        echo '</tr></table>';
+    }
 
 }
 
