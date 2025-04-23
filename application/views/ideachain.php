74ff7fa39793da46cf0f4fd7468ce26d06ee5fdd
shervinesta <shervin@mench.com>
2025-04-23 00:47:25 -0700

    Ledger V12 Immutable

--- application/views/ideachain.php
+++ application/views/ideachain.php
@@ -102,8 +102,6 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
 
     }
 
-    echo '</div>';
-
     if($playerid1==28956){
         //Legend of how nodes connect:
         echo '<table class="table table-sm maxout center" style="width: 100%; table-layout: fixed;"><tr>';
@@ -112,8 +110,13 @@ foreach ($this->config->item('players___33292') as $playerid1 => $m1) {
         echo '<td style="width: 16.66%; text-align: center;" class="appender_4486"><i class="far fa-arrow-right-long"></i></td>';
         echo '<td style="width: 16.66%; text-align: center;" class="appender_13550"><i class="far fa-arrow-left-long"></i></td>';
         echo '<td style="width: 16.66%; text-align: center;" class="appender_31777"><i class="far fa-rotate-right"></i></td>';
+        echo '<td style="width: 16.66%; text-align: center;">&nbsp;</td>';
         echo '</tr></table>';
     }
+
+    echo '</div>';
+
+
 
 }
 
