e9b10faf1004386d26e8ce1d2fe03b318c55ca90
shervinesta <shervin@mench.com>
2025-04-25 12:37:27 -0700

    Idea Chain Immutable

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -437,23 +437,23 @@ function view_tree($i)
     foreach ($CI->config->item('players___1592660') as $playerid => $m) {
         if (isset($i['stats']) && $playerid == 12273 && $i['stats']['all_steps'] > 0) {
 
-            echo '<span data-toggle="tooltip" data-placement="top" title="' . $m['m__title'] . ( strlen($m['m__desc']) ? ': '.$m['m__desc'] : '' ) . '"><span class="icon-block-sm">' . $m['m__cover'] . '</span>' . $i['stats']['all_steps'] . '</span>';
+            echo '<span data-toggle="tooltip" data-placement="top" title="' . $m['m__title'] . ( strlen($m['m__message']) ? ': '.$m['m__message'] : '' ) . '"><span class="icon-block-sm">' . $m['m__cover'] . '</span>' . $i['stats']['all_steps'] . '</span>';
 
         } elseif (isset($i['stats']) && $playerid == 1592672 && $i['current_level'] > 0 || $i['stats']['max_level'] > 0) {
 
-            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__desc']) ? ': '.$m['m__desc'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' .$i['current_level'] . '/' . $i['stats']['max_level'] .'</span>';
+            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__message']) ? ': '.$m['m__message'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' .$i['current_level'] . '/' . $i['stats']['max_level'] .'</span>';
 
         } elseif (isset($i['stats']) && $playerid == 1592682 && $i['stats']['min_choices'] > 0 || $i['stats']['max_choices'] > 0) {
 
-            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__desc']) ? ': '.$m['m__desc'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . ($i['stats']['min_choices'] > 0 && $i['stats']['min_choices'] != $i['stats']['max_choices'] ? $i['stats']['min_choices'] . '-' : '') . $i['stats']['max_choices'].'</span>';
+            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__message']) ? ': '.$m['m__message'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . ($i['stats']['min_choices'] > 0 && $i['stats']['min_choices'] != $i['stats']['max_choices'] ? $i['stats']['min_choices'] . '-' : '') . $i['stats']['max_choices'].'</span>';
 
         } elseif (isset($i['stats']) && $playerid == 1592686 && $i['stats']['min_steps'] > 0 || $i['stats']['max_steps'] > 0) {
 
-            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__desc']) ? ': '.$m['m__desc'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . ($i['stats']['min_steps'] != $i['stats']['max_steps'] ? $i['stats']['min_steps'] . '-' : '') . $i['stats']['max_steps'].'</span>';
+            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__message']) ? ': '.$m['m__message'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . ($i['stats']['min_steps'] != $i['stats']['max_steps'] ? $i['stats']['min_steps'] . '-' : '') . $i['stats']['max_steps'].'</span>';
 
         } elseif ($playerid == 31777 && isset($i['idea_count_discovery']) && intval($i['idea_count_discovery']) > 0) {
 
-            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__desc']) ? ': '.$m['m__desc'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . $i['idea_count_discovery'].'</span>';
+            echo '<span data-toggle="tooltip" data-placement="top" title="'.$m['m__title']. ( strlen($m['m__message']) ? ': '.$m['m__message'] : '' ).'"><span class="icon-block-sm">'.$m['m__cover'].'</span>' . $i['idea_count_discovery'].'</span>';
 
         }
     }
