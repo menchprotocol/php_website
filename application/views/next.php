66a39dbbefede7ad7349a13a9d81df979445844e
shervinesta <shervin@mench.com>
2025-05-20 01:15:53 -0700

    Idea Chain Immutable

--- application/views/next.php
+++ application/views/next.php
@@ -3,4 +3,4 @@
 $next__url = $this->Chains->idea_next($source_session['sourceid'], $focus_i['ideahashtag'], $focus_i);
 
 //Go to URL:
-return get_redirected($focus_i['ideahashtag'].'/'.($next__url ? $next__url : 'start' ));
+return get_redirected('/'.$focus_i['ideahashtag'].'/'.($next__url ? $next__url : 'start' ));
