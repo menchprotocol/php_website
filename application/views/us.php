939e846b5ae7c41b6c9818d81cba2cba2a59bb94
shervinesta <shervin@mench.com>
2025-05-28 13:29:19 -0700

    Discotique us

--- application/views/us.php
+++ application/views/us.php
@@ -2,7 +2,6 @@
 
 $community_pills = '';
 $main_source_id = 0;
-$main_source_name = '';
 
 echo '<h1>'.$focus_e['sourcevalue'].'</h1>';
 
@@ -27,7 +26,6 @@ foreach ($this->Chains->read(array(
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 1, 0, source_sort()) as $group_main) {
 
-    $main_source_name = $group_main['sourcevalue'];
     $main_source_id = intval($group_main['sourceid']);
 
     foreach ($this->Chains->read(array(
@@ -67,7 +65,7 @@ foreach ($this->Chains->read(array(
     'chainsourceup' => $focus_e['sourceid'],
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 0, 0, source_sort()) as $group) {
-    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="load_group(' . $group['sourceid'] . ')">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title">'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'</span><span class="main__title '.( $group['sourceid']==$main_source_id ? '' : ' hidden ' ).' grouptitle grouptitle_'.$group['sourceid'].'">&nbsp;'.trim(str_replace($main_source_name, '', $group['sourcevalue'])).'&nbsp;</span></a></li>';
+    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="load_group(' . $group['sourceid'] . ')">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title">'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'</span><span class="main__title '.( $group['sourceid']==$main_source_id ? '' : ' hidden ' ).' grouptitle grouptitle_'.$group['sourceid'].'">&nbsp;'.trim(str_replace($focus_e['sourcevalue'], '', $group['sourcevalue'])).'&nbsp;</span></a></li>';
 }
 echo '</ul>';
 
