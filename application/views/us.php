c399178a0f3c65ab848eab97fa76f0f23186699a
shervinesta <shervin@mench.com>
2025-05-28 12:43:09 -0700

    Discotique us

--- application/views/us.php
+++ application/views/us.php
@@ -62,6 +62,6 @@ foreach ($this->Chains->read(array(
     'chainsourceup' => $focus_e['sourceid'],
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 0, 0, source_sort()) as $group) {
-    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="$(\'.html_message_' . $group['sourceid'] . '\').toggleClass(\'hidden\');">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title"></span><span class="main__title hidden xtypetitle_'.$group['sourceid'].'">&nbsp;'.$group_counts[$group['sourceid']].'&nbsp;'.$group['sourcevalue'].'&nbsp;</span></a></li>';
+    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="$(\'.html_message_' . $group['sourceid'] . '\').toggleClass(\'hidden\');">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title"></span><span class="main__title hidden xtypetitle_'.$group['sourceid'].'">&nbsp;'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'&nbsp;'.$group['sourcevalue'].'&nbsp;</span></a></li>';
 }
 echo '</ul>';
