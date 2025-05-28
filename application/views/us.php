cb48ce0503e47c676fa516953ab8f529502c4048
shervinesta <shervin@mench.com>
2025-05-28 12:54:32 -0700

    Discotique us

--- application/views/us.php
+++ application/views/us.php
@@ -15,6 +15,7 @@ foreach ($this->Chains->read(array(
 }
 
 //Load Main:
+$main_source_id = 0;
 $content_ui = '';
 $group_counts = array();
 $content_ui .= '<div class="row justify-content group_content">';
@@ -23,6 +24,7 @@ foreach ($this->Chains->read(array(
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 1, 0, source_sort()) as $group_main) {
 
+    $main_source_id = intval($group_main['sourceid']);
     foreach ($this->Chains->read(array(
         'chainsourceup' => $group_main['sourceid'],
         'chainsourcetype IN (' . join(',', $this->config->item('sourceids___33337')) . ')' => null, //SOURCE CHAINS
@@ -60,8 +62,29 @@ foreach ($this->Chains->read(array(
     'chainsourceup' => $focus_e['sourceid'],
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 0, 0, source_sort()) as $group) {
-    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="$(\'.html_message_' . $group['sourceid'] . '\').toggleClass(\'hidden\');">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title"></span><span class="main__title hidden xtypetitle_'.$group['sourceid'].'">&nbsp;'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'&nbsp;'.$group['sourcevalue'].'&nbsp;</span></a></li>';
+    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="$(\'.html_message_' . $group['sourceid'] . '\').toggleClass(\'hidden\');">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title"></span><span class="main__title '.( $group['sourceid']==$main_source_id ? '' : ' hidden ' ).' grouptitle grouptitle_'.$group['sourceid'].'">&nbsp;'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'&nbsp;'.$group['sourcevalue'].'&nbsp;</span></a></li>';
 }
 echo '</ul>';
 
 echo $content_ui;
+?>
+
+
+<script>
+
+    var main_source_id = <?= $main_source_id ?>;
+    function load_group(group_id){
+
+        //Remove all filters:
+        $('.grouptitle').addClass('hidden');
+        $('.grouptitle_'+group_id).removeClass('hidden');
+
+        if(main_source_id!=group_id){
+            $('.main_group').addClass('hidden');
+            $('.group_'+group_id).removeClass('hidden');
+        } else {
+            $('.main_group').removeClass('hidden');
+        }
+    }
+
+</script>
