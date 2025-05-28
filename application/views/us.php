6bfb616181e5aeaf9c12d61ec7db26f8c1d8ed82
shervinesta <shervin@mench.com>
2025-05-28 13:31:31 -0700

    Discotique us

--- application/views/us.php
+++ application/views/us.php
@@ -65,7 +65,7 @@ foreach ($this->Chains->read(array(
     'chainsourceup' => $focus_e['sourceid'],
     'chainsourcetype' => 4230, //SOURCE FOLLOW
 ), array('chainsourcedown'), 0, 0, source_sort()) as $group) {
-    echo '<li class="nav-item thepill42256"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="load_group(' . $group['sourceid'] . ')">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title">'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'</span><span class="main__title '.( $group['sourceid']==$main_source_id ? '' : ' hidden ' ).' grouptitle grouptitle_'.$group['sourceid'].'">&nbsp;'.trim(str_replace($focus_e['sourcevalue'], '', $group['sourcevalue'])).'&nbsp;</span></a></li>';
+    echo '<li class="nav-item '.( $group['sourceid']==$main_source_id ? ' active ' : '' ).' navgroup_'.$group['sourceid'].'"><a class="nav-chain" href="javascript:void(0);" href="javascript:void(0);" onclick="load_group(' . $group['sourceid'] . ')">&nbsp;<span class="icon-block">'.view_cover($group['sourcecover']).'</span><span class="main__title">'.( isset($group_counts[$group['sourceid']]) ? $group_counts[$group['sourceid']] : 0 ).'</span><span class="main__title '.( $group['sourceid']==$main_source_id ? '' : ' hidden ' ).' grouptitle grouptitle_'.$group['sourceid'].'">&nbsp;'.trim(str_replace($focus_e['sourcevalue'], '', $group['sourcevalue'])).'&nbsp;</span></a></li>';
 }
 echo '</ul>';
 
@@ -81,6 +81,8 @@ echo $content_ui;
         //Remove all filters:
         $('.grouptitle').addClass('hidden');
         $('.grouptitle_'+group_id).removeClass('hidden');
+        $('.nav-item').removeClass('active');
+        $('.navgroup_'+group_id).addClass('active');
 
         if(main_source_id!=group_id){
             $('.main_group').addClass('hidden');
