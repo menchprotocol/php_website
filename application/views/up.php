3b07dadee8578de7840eb74efb84a8e14c992736
shervinesta <shervin@mench.com>
2025-05-31 23:03:10 -0700

    Discotique us

--- application/views/up.php
+++ application/views/up.php
@@ -2,6 +2,10 @@
 
 echo '<h1>'.$focus_e['sourcevalue'].'</h1>';
 
-$source_up = source_up($focus_e['sourceid']);
-
-print_r($source_up);
+echo '<div class="row justify-content">';
+foreach ($this->Sources->read(array(
+    'sourceid IN (' . join(',', source_up($focus_e['sourceid'])) . ')' => null,
+)) as $e) {
+    echo source_view(6255, $e);
+}
+echo '</div>';
