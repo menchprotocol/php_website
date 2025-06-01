5f20f4e91f20570b9bd6db540a74c723955949fc
shervinesta <shervin@mench.com>
2025-05-31 23:07:44 -0700

    Discotique us

--- application/views/up.php
+++ application/views/up.php
@@ -2,10 +2,14 @@
 
 echo '<h1>'.$focus_e['sourcevalue'].'</h1>';
 
+$total_parents = source_up($focus_e['sourceid']);
+$current_total = count($total_parents);
+$current_i = 0;
 echo '<div class="row justify-content">';
 foreach ($this->Sources->read(array(
-    'sourceid IN (' . join(',', source_up($focus_e['sourceid'])) . ')' => null,
+    'sourceid IN (' . join(',', $total_parents) . ')' => null,
 )) as $e) {
-    echo source_view(6255, $e);
+    echo '<div>'.($current_total-$current_i).') <span class="icon-block">'.view_cover($e['sourcecover']).'</span><span class="main__title">'.$e['sourcevalue'].'</span><span class="grey">@'.$e['sourcehandle'].'</span></div>';
+    $current_i++;
 }
 echo '</div>';
