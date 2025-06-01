58346fe2a1597ee163ce376e40d3fd0f32342313
shervinesta <shervin@mench.com>
2025-05-31 23:39:41 -0700

    Discotique us

--- application/views/up.php
+++ application/views/up.php
@@ -2,6 +2,29 @@
 
 echo '<h1>'.$focus_e['sourcevalue'].'</h1>';
 
+
+$deleted_all = 0;
+foreach ($this->Chains->read(array(
+    'chainsourceup' => 4430,
+    'chainsourcetype IN (' . join(',', $this->config->item('sourceids___13548')) . ')' => null, //SOURCE CHAINS
+), array('chainsourcedown'), 0) as $e) {
+
+    $deleted = 0;
+    foreach($this->Chains->read(array(
+        'chainsourceup' => $e['sourceid'],
+        'chainsourcetype IN (' . join(',', $this->config->item('sourceids___13548')) . ')' => null, //SOURCE CHAINS
+    ), array('chainsourcedown'), 0) as $child){
+        $deleted++;
+    }
+
+    $deleted_all += $deleted;
+    echo '<div>'.$deleted.') <span class="icon-block">'.view_cover($e['sourcecover']).'</span><span class="main__title">'.$e['sourcevalue'].'</span><span class="grey">@'.$e['sourcehandle'].'</span></div>';
+
+}
+
+echo $deleted_all;
+
+/*
 $total_parents = source_up($focus_e['sourceid']);
 $current_total = count($total_parents);
 $current_i = 0;
@@ -13,3 +36,4 @@ foreach ($this->Sources->read(array(
     $current_i++;
 }
 echo '</div>';
+*/
