007d797f97c553fcec3ea4093cc36dd9c49261db
shervinesta <shervin@mench.com>
2025-06-03 10:28:40 -0700

    Discotique us

--- application/views/tree.php
+++ application/views/tree.php
@@ -5,7 +5,7 @@ echo '<div class="view_12274 row justify-content">';
 echo source_view(42287, $focus_e, null);
 echo '</div>';
 
-$_GET['view_all'] = true;
+$_GET['expand'] = true;
 
 //Idea Tree:
 echo '<div class="row justify-content">';
