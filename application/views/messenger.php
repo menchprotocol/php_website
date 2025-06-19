acfe5719ea8069e0f3780da5798d4eed81f10bf4
shervinesta <shervin@mench.com>
2025-06-18 20:57:23 -0700

    Discotique us

--- application/views/messenger.php
+++ application/views/messenger.php
@@ -40,7 +40,6 @@ foreach ($this->Chains->read(array(
     $total_sent = $this->Chains->broadcast($idea_settings['query_string_filtered'], $i, $i['chainsourcedomain'], true, $demo_only);
 
     echo view_idea_title($i) . ' Sent ' . $total_sent . ' Messages to ' . count($idea_settings['query_string_filtered']) . ' Members<hr />';
-    echo nl2br(print_r($idea_settings['query_string_filtered'], true));
 
     //Mark this as complete?
     if (!$demo_only && (!$end_sending || $end_sending < time())) {
