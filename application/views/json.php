40832adea5d00d0efcd605efa827049ce16e457b
shervinesta <shervin@mench.com>
2025-07-06 23:38:03 -0700

    Ideachain V1

--- application/views/json.php
+++ application/views/json.php
@@ -32,105 +32,107 @@ foreach($this->Chains->read(array(
     }
 }
 
-exit;
-
-echo '<table class="table table-sm table-striped stats-table mini-stats-table">';
+if(0){
 
 
-foreach($this->Chains->read(array(
-    'chainvoid >=' => 0, //Any Chain
-    'chainhandletype' => 12273,
-), array('chainhashtagoutput')) as $x){
-
-    //Fetch from Cache table:
-    $current_value = $x['hashtagvalue'];
-    foreach($this->Chains->read(array(
-        'chainhashtagoutput' => $x['hashtagid'],
-        'chainhandletype IN (' . join(',', array(31835)) . ')' => null,
-    ), array('chainhandleinput')) as $x2){
-        $current_value .= "\n";
-    }
-
-    $new_value = '#'.$x['hashtaghashtag']."\n".$current_value;
+    echo '<table class="table table-sm table-striped stats-table mini-stats-table">';
 
 
-
-    //HASHTAG
-    $stats['hashtags_all']++;
-    if($x['chainvoid']>0){
-        $stats['hashtags_void']++;
-    }
-
-    //Fetch from Cache table:
     foreach($this->Chains->read(array(
         'chainvoid >=' => 0, //Any Chain
-        'chainhashtagoutput' => $x['hashtagid'],
-        'chainhandletype IN (' . join(',', array(7545, 26599, 10573, 41949, 1695880, 32235, 31835, 27984, 43513, 43514, 26600)) . ')' => null, //HANDLE CHAINS
-    ), array('chainhandleinput')) as $x2){
-        $new_value .= "\n";
+        'chainhandletype' => 12273,
+    ), array('chainhashtagoutput')) as $x){
+
+        //Fetch from Cache table:
+        $current_value = $x['hashtagvalue'];
+        foreach($this->Chains->read(array(
+            'chainhashtagoutput' => $x['hashtagid'],
+            'chainhandletype IN (' . join(',', array(31835)) . ')' => null,
+        ), array('chainhandleinput')) as $x2){
+            $current_value .= "\n";
+        }
+
+        $new_value = '#'.$x['hashtaghashtag']."\n".$current_value;
+
+
+
+        //HASHTAG
+        $stats['hashtags_all']++;
+        if($x['chainvoid']>0){
+            $stats['hashtags_void']++;
+        }
+
+        //Fetch from Cache table:
+        foreach($this->Chains->read(array(
+            'chainvoid >=' => 0, //Any Chain
+            'chainhashtagoutput' => $x['hashtagid'],
+            'chainhandletype IN (' . join(',', array(7545, 26599, 10573, 41949, 1695880, 32235, 31835, 27984, 43513, 43514, 26600)) . ')' => null, //HANDLE CHAINS
+        ), array('chainhandleinput')) as $x2){
+            $new_value .= "\n";
+        }
+
+
+        echo '<tr>';
+        echo '<td>'.$x['chainid'].'</td>';
+        echo '<td>VOID '.$x['chainvoid'].'</td>';
+        echo '<td>@'.$x['chainhandletype'].'</td>';
+        echo '<td>'.$x['chainvalue'].'</td>';
+        echo '<td>'.$new_value.'</td>';
+        echo '</tr>';
+
     }
 
 
-    echo '<tr>';
-    echo '<td>'.$x['chainid'].'</td>';
-    echo '<td>VOID '.$x['chainvoid'].'</td>';
-    echo '<td>@'.$x['chainhandletype'].'</td>';
-    echo '<td>'.$x['chainvalue'].'</td>';
-    echo '<td>'.$new_value.'</td>';
-    echo '</tr>';
-
-}
-
-
-foreach($this->Chains->read(array(
-    'chainvoid >=' => 0, //Any Chain
-    'chainhandletype' => 12274,
-), array('chainhandleoutput')) as $x){
-
-    $new_value = '';
-
-    //HANDLE
-    $stats['hashtags_all']++;
-    if($x['chainvoid']>0){
-        $stats['hashtags_void']++;
-    }
-
-    $new_value = '#';
-    //Fetch from Cache table:
-    $references = $this->Chains->read(array(
+    foreach($this->Chains->read(array(
         'chainvoid >=' => 0, //Any Chain
-        'chainhandletype IN (' . join(',', array(12273,12274)) . ')' => null, //HANDLE CHAINS
-    ), array());
+        'chainhandletype' => 12274,
+    ), array('chainhandleoutput')) as $x){
 
-    echo '<tr>';
-    echo '<td>'.$x['chainid'].'</td>';
-    echo '<td>VOID '.$x['chainvoid'].'</td>';
-    echo '<td>@'.$x['chainhandletype'].'</td>';
-    echo '<td>'.$x['chainvalue'].'</td>';
-    echo '<td>'.$new_value.'</td>';
-    echo '</tr>';
+        $new_value = '';
 
-}
+        //HANDLE
+        $stats['hashtags_all']++;
+        if($x['chainvoid']>0){
+            $stats['hashtags_void']++;
+        }
+
+        $new_value = '#';
+        //Fetch from Cache table:
+        $references = $this->Chains->read(array(
+            'chainvoid >=' => 0, //Any Chain
+            'chainhandletype IN (' . join(',', array(12273,12274)) . ')' => null, //HANDLE CHAINS
+        ), array());
+
+        echo '<tr>';
+        echo '<td>'.$x['chainid'].'</td>';
+        echo '<td>VOID '.$x['chainvoid'].'</td>';
+        echo '<td>@'.$x['chainhandletype'].'</td>';
+        echo '<td>'.$x['chainvalue'].'</td>';
+        echo '<td>'.$new_value.'</td>';
+        echo '</tr>';
+
+    }
 
 //Show STats:
-echo '<tr>';
-echo '<td>#'.$stats['hashtags_all'].'</td>';
-echo '<td>VOID '.$stats['hashtags_void'].'</td>';
-echo '<td>&nbsp;</td>';
-echo '<td>&nbsp;</td>';
-echo '<td>&nbsp;</td>';
-echo '</tr>';
+    echo '<tr>';
+    echo '<td>#'.$stats['hashtags_all'].'</td>';
+    echo '<td>VOID '.$stats['hashtags_void'].'</td>';
+    echo '<td>&nbsp;</td>';
+    echo '<td>&nbsp;</td>';
+    echo '<td>&nbsp;</td>';
+    echo '</tr>';
 
 
-echo '<tr>';
-echo '<td>@'.$stats['handles_all'].'</td>';
-echo '<td>VOID '.$stats['handles_void'].'</td>';
-echo '<td>&nbsp;</td>';
-echo '<td>&nbsp;</td>';
-echo '<td>&nbsp;</td>';
-echo '</tr>';
+    echo '<tr>';
+    echo '<td>@'.$stats['handles_all'].'</td>';
+    echo '<td>VOID '.$stats['handles_void'].'</td>';
+    echo '<td>&nbsp;</td>';
+    echo '<td>&nbsp;</td>';
+    echo '<td>&nbsp;</td>';
+    echo '</tr>';
 
-echo '</table>';
+    echo '</table>';
+}
 
 
 //$_GET['skip_config'] = true;
