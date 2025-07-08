5f2ab324e3d36508c9b5cd6fe945a5520d2cdc1b
shervinesta <shervin@mench.com>
2025-07-07 21:30:46 -0700

    Ideachain V1

--- application/views/json.php
+++ application/views/json.php
@@ -23,6 +23,7 @@ echo '<table class="table table-sm table-striped stats-table mini-stats-table" b
 $count = 0;
 foreach($this->Chains->read(array(
     'chainvoid >=' => 0, //Any Chain
+    'chainid' => 8789,
     'chainhandletype' => 12273,
 ), array(), 0) as $x){
 
