0b0643071072b1c012df26a097693d8901d5328b
shervinesta <shervin@mench.com>
2025-07-28 15:27:23 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -1,16 +1,15 @@
 <?php
 
 //We must have $handle_session['handleid']
-
-echo '<h2>'.$handle_session['handlename'].' '.$focus_e['handlename'].'</h2>';
+echo '<h2>'.$focus_e['handlename'].' for '.$handle_session['handlename'].'</h2>';
 
 
 //Check this users @handle:
 $was_found = false;
 foreach($this->Chains->read(array(
     'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
-    'chainhandleoutput' => $focus_e['handleid'],
-), array('chainhandleinput'), 0, 0, array('chainkey' => 'ASC')) as $handle_output){
+    'chainhandleinput' => $focus_e['handleid'],
+), array('chainhandleoutput'), 0, 0, array('chainkey' => 'ASC')) as $handle_output){
 
     echo '<div>$handle_output: '.$handle_output['handlename'].( strlen($handle_output['chainvalue'])>0 ? ': <b class="main__title">'.$handle_output['chainvalue'].'</b>' : '' ).'</div>';
 
@@ -19,6 +18,7 @@ foreach($this->Chains->read(array(
         'chainhandleinput' => $handle_session['handleid'],
         'chainhandleoutput' => $handle_output['handleid'],
     )) as $handle_data){
+
         $was_found = true;
         echo '<div>$handle_data: '.$handle_data['handlename'].( strlen($handle_data['chainvalue'])>0 ? ': <b class="main__title">'.$handle_data['chainvalue'].'</b>' : '' ).'</div>';
     }
@@ -26,5 +26,5 @@ foreach($this->Chains->read(array(
 
 
 if(!$was_found){
-    echo '<div class="alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span>'.$focus_e['handlename'].' Not Found for '.$handle_session['handlename'].'! Contact the admin to inquire further as you are not listed here.</div>';
+    echo '<div class="alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span>'.$focus_e['handlename'].' Not Found for '.$handle_session['handlename'].'! Contact your admin to inquire further as you are not listed here.</div>';
 }
