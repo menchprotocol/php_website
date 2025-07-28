cb7756d2cdc089688755532732e4bc67c2f9daba
shervinesta <shervin@mench.com>
2025-07-28 15:35:01 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -10,9 +10,6 @@ foreach($this->Chains->read(array(
     'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
     'chainhandleinput' => $focus_e['handleid'],
 ), array('chainhandleoutput'), 0, 0, array('chainkey' => 'ASC')) as $handle_output){
-
-    //echo '<div>$handle_output: '.$handle_output['handlename'].( strlen($handle_output['chainvalue'])>0 ? ': <b class="main__title">'.$handle_output['chainvalue'].'</b>' : '' ).'</div>';
-
     foreach($this->Chains->read(array(
         'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
         'chainhandleoutput' => $handle_session['handleid'],
@@ -20,11 +17,25 @@ foreach($this->Chains->read(array(
     ), array('chainhandleinput')) as $handle_data){
 
         $was_found = true;
-        echo '<div>$handle_data: '.$handle_data['handlename'].( strlen($handle_data['chainvalue'])>0 ? ': <b class="main__title">'.$handle_data['chainvalue'].'</b>' : '' ).'</div>';
+        echo '<div>'.$handle_data['handlename'].( strlen($handle_data['chainvalue'])>0 ? ': <b class="main__title">'.$handle_data['chainvalue'].'</b>' : '' ).'</div>';
     }
 }
 
 
 if(!$was_found){
     echo '<div class="alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span>'.$focus_e['handlename'].' Not Found for '.$handle_session['handlename'].'! Contact your admin to inquire further as you are not listed here.</div>';
+} else {
+
+    //Check this users @handle:
+    foreach($this->Chains->read(array(
+        'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
+        'chainhandleinput' => 1727532, //@checkmy
+        'chainhandleoutput' => $focus_e['handleid'],
+        'LENGTH(chainvalue) > 0' => null,
+    )) as $handle_info){
+        echo '<div class="grey">'.preg_replace('/(http[s]{0,1}\:\/\/\S{4,})\s{0,}/ims', '<a href="$1" target="_blank">$1</a> ', $handle_data['chainvalue']).'</div>';
+    }
+
+
+
 }
