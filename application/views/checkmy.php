e09fd7ccf31b6058fbb43b3cfea04954cc377d4b
shervinesta <shervin@mench.com>
2025-07-28 15:19:12 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -11,13 +11,16 @@ foreach($this->Chains->read(array(
     'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
     'chainhandleinput' => $handle_session['handleid'],
 ), array('chainhandleoutput'), 0, 0, array('chainkey' => 'ASC')) as $handle_output){
+
+    echo '<div>$handle_output: '.$handle_output['handlename'].( strlen($handle_output['chainvalue'])>0 ? ': <b class="main__title">'.$handle_output['chainvalue'].'</b>' : '' ).'</div>';
+
     foreach($this->Chains->read(array(
         'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
         'chainhandleinput' => $handle_output['handleid'],
         'chainhandleoutput' => $handle_session['handleid'],
     )) as $handle_data){
         $was_found = true;
-        echo '<div>'.$handle_output['handlename'].( strlen($handle_output['chainvalue'])>0 ? ': <b class="main__title">'.$handle_output['chainvalue'].'</b>' : '' ).'</div>';
+        echo '<div>$handle_data: '.$handle_data['handlename'].( strlen($handle_data['chainvalue'])>0 ? ': <b class="main__title">'.$handle_data['chainvalue'].'</b>' : '' ).'</div>';
     }
 }
 
