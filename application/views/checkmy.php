0b911f4f1211d1bc48474f192fdafc22e7d2fd5c
shervinesta <shervin@mench.com>
2025-07-28 15:16:14 -0700

    Ideachain V1

---
+++ application/views/checkmy.php
@@ -0,0 +1,27 @@
+<?php
+
+//We must have $handle_session['handleid']
+
+echo '<h1>'.$handle_session['handlename'].' '.$focus_e['handlename'].'</h1>';
+
+
+//Check this users @handle:
+$was_found = false;
+foreach($this->Chains->read(array(
+    'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
+    'chainhandleinput' => $handle_session['handleid'],
+), array('chainhandleoutput')) as $handle_output){
+    foreach($this->Chains->read(array(
+        'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
+        'chainhandleinput' => $handle_output['handleid'],
+        'chainhandleoutput' => $handle_session['handleid'],
+    )) as $handle_data){
+        $was_found = true;
+        echo '<div>'.$handle_output['handlename'].( strlen($handle_output['chainvalue'])>0 ? ': <b class="main__title">'.$handle_output['chainvalue'].'</b>' : '' ).'</div>';
+    }
+}
+
+
+if(!$was_found){
+    echo '<div class="alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span>'.$focus_e['handlename'].' Not Found for '.$handle_session['handlename'].'! Contact the admin to inquire further as you are not listed here.</div>';
+}
