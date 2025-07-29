3e3900ef6f9f0ed27bedc6f9b171838652fc7db7
shervinesta <shervin@mench.com>
2025-07-28 20:27:43 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -3,17 +3,6 @@
 //We must have $handle_session['handleid']
 echo '<h1>'.$focus_e['handlename'].' for '.$handle_session['handlename'].'</h1>';
 
-//Check this users @handle:
-foreach($this->Chains->read(array(
-    'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
-    'chainhandleinput' => 1727532, //@checkmy
-    'chainhandleoutput' => $focus_e['handleid'],
-    'LENGTH(chainvalue) > 0' => null,
-)) as $handle_info){
-    echo '<div>'.preg_replace('/(http[s]{0,1}\:\/\/\S{4,})\s{0,}/ims', '<a href="$1" target="_blank" style="color:#0000FF">$1</a> ', nl2br($handle_info['chainvalue'])).'</div>';
-}
-
-
 echo '<table class="table table-striped" style="border: 1px solid #000;">';
 
 //Check this users @handle:
@@ -33,6 +22,18 @@ foreach($this->Chains->read(array(
     }
 }
 echo '</table>';
+
+
+//Check this users @handle:
+foreach($this->Chains->read(array(
+    'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
+    'chainhandleinput' => 1727532, //@checkmy
+    'chainhandleoutput' => $focus_e['handleid'],
+    'LENGTH(chainvalue) > 0' => null,
+)) as $handle_info){
+    echo '<div>'.preg_replace('/(http[s]{0,1}\:\/\/\S{4,})\s{0,}/ims', '<a href="$1" target="_blank" style="color:#0000FF">$1</a> ', nl2br($handle_info['chainvalue'])).'</div>';
+}
+
 
 
 if(!$was_found){
