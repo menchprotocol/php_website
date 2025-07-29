b78ca1d8ed95a0c2dc5315518389a25c342b1063
shervinesta <shervin@mench.com>
2025-07-28 20:26:17 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -3,6 +3,16 @@
 //We must have $handle_session['handleid']
 echo '<h2>'.$focus_e['handlename'].' for '.$handle_session['handlename'].'</h2>';
 
+//Check this users @handle:
+foreach($this->Chains->read(array(
+    'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
+    'chainhandleinput' => 1727532, //@checkmy
+    'chainhandleoutput' => $focus_e['handleid'],
+    'LENGTH(chainvalue) > 0' => null,
+)) as $handle_info){
+    echo '<br /><div>'.preg_replace('/(http[s]{0,1}\:\/\/\S{4,})\s{0,}/ims', '<a href="$1" target="_blank" style="color:#0000FF">$1</a> ', nl2br($handle_info['chainvalue'])).'</div>';
+}
+
 
 echo '<table class="table table-striped" style="border: 1px solid #000;">';
 
@@ -27,18 +37,4 @@ echo '</table>';
 
 if(!$was_found){
     echo '<div class="alert alert-warning" role="alert"><span class="icon-block"><i class="fas fa-exclamation-circle"></i></span>'.$focus_e['handlename'].' Not Found for '.$handle_session['handlename'].'! Contact your admin to inquire further as you are not listed here.</div>';
-} else {
-
-    //Check this users @handle:
-    foreach($this->Chains->read(array(
-        'chainhandletype IN (' . join(',', $this->config->item('handleids___13548')) . ')' => null, //HANDLE CHAINS
-        'chainhandleinput' => 1727532, //@checkmy
-        'chainhandleoutput' => $focus_e['handleid'],
-        'LENGTH(chainvalue) > 0' => null,
-    )) as $handle_info){
-        echo '<br /><div>'.preg_replace('/(http[s]{0,1}\:\/\/\S{4,})\s{0,}/ims', '<a href="$1" target="_blank" style="color:#0000FF">$1</a> ', nl2br($handle_info['chainvalue'])).'</div>';
-    }
-
-
-
 }
