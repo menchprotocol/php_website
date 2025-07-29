4995e3423db18ca1a7d30a667d21ae2caae4fdf3
shervinesta <shervin@mench.com>
2025-07-28 20:24:00 -0700

    Ideachain V1

--- application/views/checkmy.php
+++ application/views/checkmy.php
@@ -4,7 +4,7 @@
 echo '<h2>'.$focus_e['handlename'].' for '.$handle_session['handlename'].'</h2>';
 
 
-echo '<table class="table table-striped">';
+echo '<table class="table table-striped" style="border: 1px solid #000;">';
 
 //Check this users @handle:
 $was_found = false;
