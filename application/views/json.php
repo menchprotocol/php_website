2ba782c330375e79798a0059a7ebc53c4307f305
shervinesta <shervin@mench.com>
2025-08-03 19:52:41 -0700

    Ideachain V1

--- application/views/json.php
+++ application/views/json.php
@@ -189,7 +189,7 @@ if($focus_i['hashtagterm']=='Discotique2024') {
     foreach($this->Chains->read(array(
         'chainvoid >=' => 0, //Any Chain
         'chainhandletype' => 12273,
-    ), array(), 3, 0, array('chainid' => 'ASC')) as $x){
+    ), array(), 34, 0, array('chainid' => 'ASC')) as $x){
 
         $is_duplicate = in_array($x['chainhashtagoutput'], $chainhashtagoutput);
 
