1e6ebc89df7b1e6ae12f76675507b514beffb06e
shervinesta <shervin@mench.com>
2025-08-04 19:42:41 -0700

    Ideachain V1

--- application/views/json.php
+++ application/views/json.php
@@ -521,9 +521,8 @@ if($focus_i['hashtagterm']=='Discotique2024') {
             'hashtagdiscover' => $hashtag_cache['hashtagdiscover'],
             'hashtagedit' => $hashtag_cache['hashtagedit'],
         ));
-        $this->Hashtags->update($x['chainhashtagoutput'], array(
+        $this->Chain->update($x['chainhashtagoutput'], array(
             'chainvalue' => $hashtag_cache['hashtagchain'],
-            'chainvoid' => 0,
         ));
 
     }
