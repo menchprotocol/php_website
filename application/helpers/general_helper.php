e0a592910f3fffd49f4fd3287d75fb27a84aba16
shervinesta <shervin@mench.com>
2025-08-04 17:12:17 -0700

    Ideachain V1

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -3904,13 +3904,16 @@ function hashtag_cache($save_hashtagid, $hashtagtext)
                             //Valid Handle
                             $reference_type = $chainhandletype;
                             array_push($hashtag_references[$reference_type], $handle);
+                            $word_text = '@' . $newHandleTerm;
 
                             $hashtagchain = $m['m__cover'] . $handle['handleid'];
                             $hashtagtext = $word_text;
                             if(!($first_word && $only_word_in_line) && !(count($media_attachments)==1 && $x['chainhandleinput'] == 1326)){
                                 $hashtagdiscover = '<a href="' . view_memory(42903, 42902) . $handle['handleterm'] . '" data-toggle="popover" class="ref_handle">' . $word_text . '</a>' . $media_append_end;
+                                $word_text = $hashtagdiscover;
                             } elseif($media_append_end){
                                 $hashtagdiscover = $media_append_end;
+                                $word_text = $hashtagdiscover;
                             }
                             $hashtagedit = '<a href="' . view_memory(42903, 42902) . $handle['handleterm'] . '" data-toggle="popover" class="ref_handle">' . $word_text . '</a>' . $media_append_end;
 
@@ -3931,6 +3934,7 @@ function hashtag_cache($save_hashtagid, $hashtagtext)
                             $hashtagtext = $word_text;
                             if(!($first_word && $only_word_in_line)){
                                 $hashtagdiscover = '<a href="' . view_memory(42903, 33286) . $hashtag['hashtagterm'] . '" data-toggle="popover" class="ref_hashtag">' . $word_text . '</a>';
+                                $word_text = $hashtagdiscover;
                             }
                             $hashtagedit = '<a href="' . view_memory(42903, 33286) . $hashtag['hashtagterm'] . '">' . $word_text . '</a>';
 
@@ -3965,7 +3969,6 @@ function hashtag_cache($save_hashtagid, $hashtagtext)
         $hashtag_cache['hashtagedit'] .= '</div>';
 
     }
-
 
     $hashtag_cache['hashtagdiscover'] .= '</div>';
     $hashtag_cache['hashtagedit'] .= '</div>';
