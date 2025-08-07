1c492a7c8ec6496003bb7feba124072ff6a8f70d
shervinesta <shervin@mench.com>
2025-08-06 18:12:37 -0700

    Ideachain V1

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -1174,7 +1174,7 @@ class Controller extends CI_Controller
 
             return view_json(array(
                 'status' => 1,
-                'return_hashtagdiscover_chains' => view_hashtag_value($new_i, $handle_session['handleid'], $focus__node, intval($_POST['save_discoverymode'])),
+                'return_hashtagdiscover_chains' => view_hashtag_value($new_i, $handle_session['handleid'], $focus__node, ( isset($_POST['save_discoverymode']) && intval($_POST['save_discoverymode']))),
                 'return_hashtagdiscover_full' => hashtag_view($_POST['focus_group'], $new_i),
                 'save_hashtagid' => $new_i['hashtagid'],
                 'save_hashtagtext' => trim($_POST['save_hashtagtext']),
