fed4cabec25c97218f20f746c31e3db7ed94876b
shervinesta <shervin@mench.com>
2025-08-08 12:19:01 -0700

    Ideachain V1

--- application/views/json.php
+++ application/views/json.php
@@ -489,12 +489,11 @@ if($focus_i['hashtagterm']=='Discotique2024') {
                 'chainhandleinput NOT IN ('.$x['chainhandlecreator'].',1,2,32337)' => null,
                 'chainhandletype' => 4983, //Authors
             ), array('chainhandleinput')) as $x2){
-                if(substr_count($hashtagtext, "@".$x2['handleterm'])){
-                    break;
-                }
-                $core_content .= "\n"."@".$x2['handleterm'];
-                if (strlen($x2['chainvalue'] > 0)) {
-                    $core_content .= " ".$x2['chainvalue'];
+                if(!substr_count($hashtagtext, "@".$x2['handleterm'])){
+                    $core_content .= "\n"."@".$x2['handleterm'];
+                    if (strlen($x2['chainvalue'] > 0)) {
+                        $core_content .= " ".$x2['chainvalue'];
+                    }
                 }
             }
 
@@ -534,11 +533,12 @@ if($focus_i['hashtagterm']=='Discotique2024') {
                     $hashtagtext .= "\n";
                 }
                 $core_content .= "\n@".$mentions[$x2['chainhandletype']]['m__cover'].$x2['handleterm'];
-                $hashtagtext .= "\n".$mentions[$x2['chainhandletype']]['m__cover'].$x2['handleterm'];
+                $hashtagtext .= "\n@".$mentions[$x2['chainhandletype']]['m__cover'].$x2['handleterm'];
+                if(strlen($x2['chainvalue'])){
+                    $hashtagtext .= ' '.$x2['chainvalue'];
+                }
             }
-
         }
-
 
         if(!strlen(trim($core_content))){
             $stats['hashtags_empty']++;
