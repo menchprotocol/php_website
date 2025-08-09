dab397603619eaf5c80e8f0f7218469f22b9ca6e
shervinesta <shervin@mench.com>
2025-08-08 17:38:52 -0700

    Ideachain V1

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -4007,7 +4007,7 @@ function hashtag_cache($save_hashtagid, $hashtagtext, $chainhandlecreator, $repl
         $hashtag_cache['hashtagedit'] = '<div class="i_cache i_hashtagedit cache_frame_' . $save_hashtagid . '">'.$hashtag_cache['hashtagedit'].'</div>';
     }
 
-    if (!intval($chainhandlecreator)) {
+    if (!intval($save_hashtagid)) {
         //Nothing else we need to do:
         return $hashtag_cache;
     }
