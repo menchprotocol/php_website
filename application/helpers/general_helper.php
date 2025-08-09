b2cf5e2b2e6edd0212e3d03d96b187854140324d
shervinesta <shervin@mench.com>
2025-08-08 17:08:58 -0700

    Ideachain V1

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -3920,7 +3920,7 @@ function hashtag_cache($save_hashtagid, $hashtagtext, $chainhandlecreator, $repl
 
                             $hashtagchain = $m['m__cover'] . $handle['handleid'];
                             $hashtagtext = $word_text;
-                            if(!($first_word && $only_word_in_line) && !(count($media_attachments)==1 && $x['chainhandleinput'] == 1326)){
+                            if(!($first_word) && !(count($media_attachments)==1 && $x['chainhandleinput'] == 1326)){
                                 $hashtagdiscover = '<a href="' . view_memory(42903, 42902) . $handle['handleterm'] . '" data-toggle="popover" class="ref_handle">' . $word_text . '</a>' . $media_append_end;
                             } elseif($media_append_end){
                                 $hashtagdiscover = $media_append_end;
