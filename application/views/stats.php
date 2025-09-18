237575b34a7381a247adcf95bf9dd227848a1ba9
shervinesta <shervin@mench.com>
2025-09-18 11:19:45 -0700

    Ideachain V1

--- application/views/stats.php
+++ application/views/stats.php
@@ -4,7 +4,7 @@ $handleterm = (isset($_GET['handleterm']) ? $_GET['handleterm'] : null);
 $hashtagterm = (!$handleterm && isset($_GET['hashtagterm']) ? $_GET['hashtagterm'] : null);
 $handles___11035 = $this->config->item('handles___11035'); //Encyclopedia
 
-echo '<h1><a href="/ideachain"><span class="card_count_4341" style="min-width:160px; display: inline-block; text-align: center;"></span></a> Chains</h1>';
+echo '<h1><a href="/ideachain"><span class="card_count_4341" style="min-width:160px; display: inline-block; text-align: center;"><i class="fas fa-yin-yang fa-spin"></i></span></a> Chains</h1>';
 
 if ($handleterm) {
     foreach ($this->Handles->read(array(
