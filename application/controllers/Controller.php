9f3342e7945fa118bc27e1dbc33061f69312cdf7
shervinesta <shervin@mench.com>
2025-09-30 21:07:13 -0700

    Ideachain V1.0

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -3417,7 +3417,6 @@ class Controller extends CI_Controller
                 }
                 $sub_counter = $this->Chains->read($void_filter, array(), 0, 0, array(), 'COUNT(chainid) as totals');
                 $return_array[$chainusertype1] = intval($sub_counter[0]['totals']);
-                $return_array[4341] += intval($sub_counter[0]['totals']);
                 continue;
             }
 
