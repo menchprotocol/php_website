86b838b86c70eaf5c4e4112ac7114a98d11e330f
shervinesta <shervin@mench.com>
2025-10-03 21:54:09 -0700

    Ideachain V1.0

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -2452,7 +2452,7 @@ function search_enabled()
 function update_algolia($focus__node = null, $s__id = 0)
 {
 
-    if (!search_enabled() || 1) {
+    if (!search_enabled()) {
         return array(
             'status' => 0,
             'message' => 'Search engine disabled',
