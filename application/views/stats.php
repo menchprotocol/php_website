2bd7bdcd77ba96d26214e1997ad28671cd79a2d2
shervinesta <shervin@mench.com>
2025-10-07 09:40:25 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -50,7 +50,7 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
         echo '<div>';
 
         if ($is_chain) {
-            echo '<div class="medium_cover card_header">' . $m2['m__cover'] . '</div>';
+            echo '<div class="medium_cover card_header" userid="' . $userid2 . '">' . $m2['m__cover'] . '</div>';
         } else {
             echo '<a href="'.$chain_link.'" class="large_cover" style="cursor:default !important;">' . $m2['m__cover'] . '</a>';
         }
