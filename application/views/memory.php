124548451c2f7945e5b5a8e87aa8095e8a10cd87
shervinesta <shervin@mench.com>
2025-10-06 22:25:20 -0700

    Ideachain V1.0

--- application/views/memory.php
+++ application/views/memory.php
@@ -87,7 +87,7 @@ foreach ($this->Chains->read(array(
         $memory_text .= '     ' . $follower['userid'] . ' => array(' . "\n";
         $memory_text .= '        \'m__user\' => \'' . $follower['userhandle'] . '\',' . "\n";
         $memory_text .= '        \'m__title\' => \'' . (str_replace('\'', '\\\'', str_replace($prefix_common_words, '', $follower['username']))) . '\',' . "\n";
-        $memory_text .= '        \'m__message\' => \'' . (str_replace('\'', '\\\'', $follower['chainvalue'])) . '\',' . "\n";
+        $memory_text .= '        \'m__message\' => \'' . (str_replace('\'', '\\\'', ( strlen($follower['chainvalue']) ? $follower['chainvalue'] : $follower['userbio'] ))) . '\',' . "\n";
         $memory_text .= '        \'m__cover\' => \'' . str_replace('\'', '\\\'', view_cover($follower['usercover'])) . '\',' . "\n";
         $memory_text .= '        \'m__following\' => array(' . join(',', $down_up_ids) . '),' . "\n";
         $memory_text .= '     ),' . "\n";
--- application/views/stats.php
+++ application/views/stats.php
@@ -56,7 +56,7 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
         }
 
         echo '<div class="main__title large_title"><a href="'.$chain_link.'" class="card_count_' . $userid2 . '"><i class="fas fa-yin-yang fa-spin"></i></a></div>';
-        echo '<div class="main__title large_title" title="@' . $userid2 . ' @' . $m2['m__user'] . '"><a href="'.view_memory(42903,42902).$m2['m__user'].'">' . $m2['m__title'] . '</a></div>';
+        echo '<div class="main__title large_title" title="@' . $userid2 . ' @' . $m2['m__user'] . '"><a href="'.view_memory(42903,42902).$m2['m__user'].'">' . $m2['m__title'] . '</a>'.( strlen($m2['m__message']) ? '<i class="fas fa-info-circle grey" data-toggle="tooltip" data-placement="top" title="' . $m2['m__message'] . '"></i>' : '' ).'</div>';
         echo '</div>';
 
         if ($is_chain) {
