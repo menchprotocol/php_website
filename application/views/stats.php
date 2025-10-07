0228e5f8bdaf36713c060c02e6ac4598d3bd2558
shervinesta <shervin@mench.com>
2025-10-06 22:37:45 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -56,14 +56,14 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
         }
 
         echo '<div class="main__title large_title"><a href="'.$chain_link.'" class="card_count_' . $userid2 . '"><i class="fas fa-yin-yang fa-spin"></i></a></div>';
-        echo '<div class="main__title large_title" title="@' . $userid2 . ' @' . $m2['m__user'] . '"><a href="'.view_memory(42903,42902).$m2['m__user'].'">' . $m2['m__title'] . '</a>'.( strlen($m2['m__message']) ? '<span class="info-box" title="' . $m2['m__message'] . '"><i class="far fa-info-circle grey"></i></span>' : '' ).'</div>';
+        echo '<div class="main__title large_title" title="@' . $userid2 . ' @' . $m2['m__user'] . '"><a href="'.view_memory(42903,42902).$m2['m__user'].'">' . $m2['m__title'] . '</a>'.( strlen($m2['m__message']) ? '<span class="info-box" data-toggle="tooltip" data-placement="top" title="' . $m2['m__message'] . '"><i class="far fa-info-circle grey"></i></span>' : '' ).'</div>';
         echo '</div>';
 
         if ($is_chain) {
             echo '<table class="table card_subcat card_subcat_' . $userid2 . ' hidden" style="width:100%; margin-top:13px;">';
             foreach ($this->config->item('users___' . $userid2) as $userid3 => $m3) {
-                echo '<tr class="main__title mobile-shrink" title="' . $m3['m__message'] . '" data-toggle="tooltip" data-placement="top">';
-                echo '<td style="text-align: left;" title="@' . $userid3 . ' @' . $m3['m__user'] . '"><a href="' . view_memory(42903, 42902) . $m3['m__user'] . '"><span class="icon-block-sm">' . $m3['m__cover'] . '</span>' . $m3['m__title'] . '</a>'.( strlen($m3['m__message']) ? '<span class="info-box" title="' . $m3['m__message'] . '"><i class="far fa-info-circle grey"></i></span>' : '' ).'<span class="last-right-col"><a href="'.view_app_chain(4341).'?chainusertype='.  $userid3 . '&chainvoid=0" class="card_count_' . $userid3 . '"><i class="fas fa-yin-yang fa-spin"></i></a></span></td>';
+                echo '<tr class="main__title mobile-shrink">';
+                echo '<td style="text-align: left;" title="@' . $userid3 . ' @' . $m3['m__user'] . '"><a href="' . view_memory(42903, 42902) . $m3['m__user'] . '"><span class="icon-block-sm">' . $m3['m__cover'] . '</span>' . $m3['m__title'] . '</a>'.( strlen($m3['m__message']) ? '<span class="info-box" data-toggle="tooltip" data-placement="right" title="' . $m3['m__message'] . '"><i class="far fa-info-circle grey"></i></span>' : '' ).'<span class="last-right-col"><a href="'.view_app_chain(4341).'?chainusertype='.  $userid3 . '&chainvoid=0" class="card_count_' . $userid3 . '"><i class="fas fa-yin-yang fa-spin"></i></a></span></td>';
                 echo '</tr>';
 
             }
