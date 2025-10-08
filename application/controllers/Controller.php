38b01f4e466dc1ff33926f98ee235bda645e0213
shervinesta <shervin@mench.com>
2025-10-08 14:31:18 -0700

    Ideachain V1.0

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -98,7 +98,9 @@ class Controller extends CI_Controller
         if(in_array($_POST['chainusertype'], $this->config->item('userids___6287'))){
 
             //yes load the app:
-            return load_app(intval($_POST['chainusertype']), 0, 0, intval($_POST['postid']));
+            echo 'hi';
+            echo load_app(intval($_POST['chainusertype']), 0, 0, intval($_POST['postid']));
+            return false;
 
         }
 
