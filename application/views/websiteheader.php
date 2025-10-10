8fd173b70648262feb7e00106d3c3202090c9085
shervinesta <shervin@mench.com>
2025-10-10 13:50:40 -0700

    Ideachain V1.0

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -741,8 +741,10 @@ if($user_session && ( !isset($basic_header_footer) || !$basic_header_footer )){
 
                         <!-- Post Hashtag -->
                         <div class="dynamic_editing_input single_line hash_group" >
-                            <h3 class="mini-font"><span class="icon-block"><?= $users___12273[32337]['m__cover']  ?></span></h3>
-                            <input type="text" class="form-control unsaved_warning save_posthashtag no-border" placeholder="<?= $users___12273[32337]['m__title'] ?>" maxlength="<?= view_memory(6404,41985) ?>" title="<?= $users___12273[32337]['m__message'] ?>" data-toggle="tooltip" data-placement="top">
+                            <div title="<?= $users___12273[32337]['m__message'] ?>" data-toggle="tooltip" data-placement="top">
+                                <h3 class="mini-font"><span class="icon-block"><?= $users___12273[32337]['m__cover']  ?></span></h3>
+                                <input type="text" class="form-control unsaved_warning save_posthashtag no-border" placeholder="<?= $users___12273[32337]['m__title'] ?>" maxlength="<?= view_memory(6404,41985) ?>" >
+                            </div>
                         </div>
 
                         <!-- Post Creator -->
