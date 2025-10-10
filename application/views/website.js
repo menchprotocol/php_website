c86d698cd16e69863102f23f6b080107b3586445
shervinesta <shervin@mench.com>
2025-10-10 14:17:14 -0700

    Ideachain V1.0

--- application/views/website.js
+++ application/views/website.js
@@ -1326,7 +1326,7 @@ $(document).ready(function () {
 
         $('.algolia__e').textcomplete([
             {
-                match: /(^|\s)?@(\w*(?:\s*\w*))$/,
+                match: /(^|\s)\?@(\w*(?:\s*\w*))$/,
                 search: function (q, callback) {
                     index_algolia.search(q, {
                         hitsPerPage: js_users___6404[31112]['m__message'],
