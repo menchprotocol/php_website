fc62bd97f5e0f83846e1a546fccac5afb21c9935
shervinesta <shervin@mench.com>
2025-10-13 16:58:00 -0700

    Ideachain V1.0

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -160,7 +160,7 @@ if(!$basic_header_footer){
         $('.text_adder').on('click', function (event) {
             //This prevents the emoji modal from closing when an emoji is selected
             var text_value = $(this).attr('text_value');
-            insertText($(".save_postmessage"), ( $(this).attr('text_value').length>2 ? "\n" : ' ' )+text_value);
+            insertText($(".save_postmessage"), ( text_value.length>2 ? "\n" : ' ' )+text_value);
         });
 
     </script>
