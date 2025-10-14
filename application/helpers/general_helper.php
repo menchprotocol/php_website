14da0dcc9e052c9ed93a50390bb8749b0df9ea63
shervinesta <shervin@mench.com>
2025-10-14 14:12:06 -0700

    Ideachain V1.0

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -5375,21 +5375,13 @@ function sendPaypalInvoice($accessToken, $invoiceId)
 function data_type_example($dataid)
 {
     $CI =& get_instance();
-    $dataid = intval($dataid);
     $users___4592 = $CI->config->item('users___4592'); //Data Types
-    if (isset($users___4592[$dataid]['m__message']) && strlen($users___4592[$dataid]['m__message'])) {
-        return ' ' . $users___4592[$dataid]['m__message'];
-    }
-
-    //See if we find in children:
     foreach ($users___4592 as $userid => $m) {
-        $users___this = $CI->config->item('users___'.$userid);
-        if (isset($users___this[$dataid]['m__message']) && strlen($users___this[$dataid]['m__message'])) {
-            return ' ' . $users___this[$dataid]['m__message'];
+        if (in_array($dataid, $CI->config->item('userids___' . $userid)) && strlen($users___4592[$userid]['m__message'])) {
+            return ' ' . $users___4592[$userid]['m__message'];
         }
     }
-
-    return ' ';
+    return '';
 }
 
 function post_index($postmessage, $save_postid = 0, $chainusercreator = 0, $current_term = null, $new_term = null)
