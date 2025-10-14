29320fd0bc12cb4bbba14c5ba72f31a55aa60770
shervinesta <shervin@mench.com>
2025-10-13 23:43:14 -0700

    Ideachain V1.0

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -5376,7 +5376,7 @@ function data_type_example($dataid)
 {
     $CI =& get_instance();
     $users___4592 = $CI->config->item('users___4592'); //Data Types
-    return ( isset($users___4592[$dataid]['m__message']) && strlen($users___4592[$dataid]['m__message']) ? ' '+$users___4592[$dataid]['m__message'] : false );
+    return ( isset($users___4592[$dataid]['m__message']) && strlen($users___4592[$dataid]['m__message']) ? ' '.$users___4592[$dataid]['m__message'] : '' );
 }
 
 function post_index($postmessage, $save_postid = 0, $chainusercreator = 0, $current_term = null, $new_term = null)
