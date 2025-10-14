7982b0387804942a7e5a41e7517f81a40daa742d
shervinesta <shervin@mench.com>
2025-10-13 23:14:43 -0700

    Ideachain V1.0

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -786,7 +786,7 @@ if ($user_session && (!isset($basic_header_footer) || !$basic_header_footer)) {
 
                                     //Suggested Posts
                                     echo '<div class="dynamic_editing_input no_padded compact_dropdown">';
-                                    echo '<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="' . $m['m__name'] . '">' . $m['m__cover'] . '</button>';
+                                    echo '<button class="btn btn-secondary dropdown-toggle icon-block" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="' . $m['m__name'] . '">' . $m['m__cover'] . '</button>';
 
                                     echo '<ul class="dropdown-menu">';
                                     foreach ($this->config->item('users___2125205') as $userid2 => $m2) {
@@ -817,7 +817,7 @@ if ($user_session && (!isset($basic_header_footer) || !$basic_header_footer)) {
 
                                     //Suggested Users
                                     echo '<div class="dynamic_editing_input no_padded compact_dropdown">';
-                                    echo '<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="' . $m['m__name'] . '" id="suggest_' . $userid . '">' . $m['m__cover'] . '</button>';
+                                    echo '<button class="btn btn-secondary dropdown-toggle icon-block" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="' . $m['m__name'] . '" id="suggest_' . $userid . '">' . $m['m__cover'] . '</button>';
 
                                     echo '<ul class="dropdown-menu" aria-labelledby="suggest_' . $userid . '">';
                                     foreach ($this->config->item('users___2125246') as $userid2 => $m2) {
