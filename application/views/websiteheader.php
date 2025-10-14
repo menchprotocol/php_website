19eca40063981fe3c8a45b19f9702d1f2759c91c
shervinesta <shervin@mench.com>
2025-10-14 14:12:50 -0700

    Ideachain V1.0

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -849,7 +849,7 @@ if ($user_session && (!isset($basic_header_footer) || !$basic_header_footer)) {
                                             foreach ($this->config->item('users___42179') as $userid3 => $m3) {
                                                 echo '<li class="inline-block"><a class="dropdown-item inline-block text_adder" href="javascript:void(0);" text_value="@'.$m3['m__handle'].data_type_example($userid3).'" title="' . $m3['m__name'].': '.$m3['m__message'] . '">@' . $m3['m__handle'] . '</a></li>';
                                                 $count++;
-                                                if($count>=13){
+                                                if($count>=55){
                                                     break;
                                                 }
                                             }
