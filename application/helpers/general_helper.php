e3c956b8d72f665d31c0788c469cd7802f6cda79
shervinesta <shervin@mench.com>
2025-10-15 13:22:21 -0700

    Ideachain V1.0

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -4791,11 +4791,7 @@ function user_view($chainusertype, $e, $extra_class = null, $extra_value = null)
     //User Handle
     $ui .= '<div class="center-block">';
 
-    if (isset($e['chainusertype']) && $users___4593[$e['chainusertype']]['m__cover'] != '@') {
-        $ui .= '<div class="creator_headline grey"><span title="' . $users___4593[$e['chainusertype']]['m__message'] . '">' . $users___4593[$e['chainusertype']]['m__name'] . '&nbsp;</span></div>';
-    }
-
-    $ui .= '<div class="creator_headline grey"><span class="ignore-click ui_userhandle_' . $e['userid'] . '" title="ID ' . $e['userid'] . '">@' . $e['userhandle'] . '</span></div>';
+    $ui .= '<div class="creator_headline grey"><span class="ignore-click ui_userhandle_' . $e['userid'] . '" title="ID ' . $e['userid'] . '">'.( substr_count($users___4593[$e['chainusertype']]['m__cover'], '@') ? $users___4593[$e['chainusertype']]['m__cover'] : '<span class="underdot" title="'.$users___4593[$e['chainusertype']]['m__name'].'">'.$users___4593[$e['chainusertype']]['m__cover'].'</span> @' ) . $e['userhandle'] . '</span></div>';
     $ui .= '<div class="' . ($focus__node ? '' : ' hidden ') . '"><div class="creator_headline hidden grey hideIfEmpty userbio_' . $e['userid'] . '" style="display:block !important;">' . $e['userbio'] . '</div></div>';
 
     //User Location?
