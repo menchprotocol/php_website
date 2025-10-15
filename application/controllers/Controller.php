5af1d9438185960a28293059c39f2c19a1a44aef
shervinesta <shervin@mench.com>
2025-10-15 10:19:16 -0700

    Ideachain V1.0

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -179,7 +179,7 @@ class Controller extends CI_Controller
         $user_http_request = (isset($_SERVER['SERVER_NAME']) ? 1 : 0);
 
 
-        if(!$user_http_request && in_array($app_userid, $this->config->item('userids___7247'))){
+        if(in_array($app_userid, $this->config->item('userids___7247'))){
             //Disable Cron Jobs?
             //TODO Remove later to enable cron jobs
             return false;
