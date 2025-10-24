a8f92dbb5b80b9e4bd57918ba2146e43c7973670
shervinesta <shervin@mench.com>
2025-10-23 22:28:26 -0700

    Ideachain V1.0

--- application/views/syncusers.php
+++ application/views/syncusers.php
@@ -89,7 +89,7 @@ if ($stats['sync_datatypes']) {
                     if (in_array($data_users_array[intval($chain['chainuserinput'])]['userhandle'], array('Instagram')) && !substr_count(trim($chain['chainvalue']), ' ') && strlen($chain['chainvalue']) < 30 and strlen($chain['chainvalue']) > 2) {
                         $new_val = 'https://instagram.com/' . str_replace('@', '', trim($chain['chainvalue']));
                         $stats['message'] .= "UPDATE TO [" . $new_val . "] ";
-                    } elseif (in_array($data_users_array[intval($chain['chainuserinput'])]['userhandle'], array('WhatsApp1')) && strlen(preg_replace('/[^0-9]+/', '', $chain['chainvalue'])) >= 10) {
+                    } elseif (in_array($data_users_array[intval($chain['chainuserinput'])]['userhandle'], array('Whatsapp1')) && strlen(preg_replace('/[^0-9]+/', '', $chain['chainvalue'])) >= 10) {
                         $new_val = 'https://wa.me/' . preg_replace('/[^0-9]+/', '', $chain['chainvalue']);
                         $stats['message'] .= "UPDATE TO [" . $new_val . "] "; //17788826962
                     }
