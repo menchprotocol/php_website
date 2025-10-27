0f60f6327a925dbc33f9bb13a6ec526def17675f
shervinesta <shervin@mench.com>
2025-10-26 20:18:09 -0700

    Ideachain V1.0

--- application/views/slide.php
+++ application/views/slide.php
@@ -1,5 +1,7 @@
 <?php
 
+//Requires a user $user_session to be logged in so their view history can be loaded
+
 //Load the entire JSON object of the input post tree to be able to build our 1-page JS app:
 $post_tree = object_to_array(json_decode(file_get_contents('https://mench.com/json/' . $focus_post['posthashtag'])));
 
@@ -13,12 +15,6 @@ $post_tree = object_to_array(json_decode(file_get_contents('https://mench.com/js
 <script>
     /* JS HERE */
 </script>
-
-
-<!-- HTML HERE -->
-<div>Hello Slide</div>
-<br/>
-<div><?= nl2br(print_r($user_session, true)); ?></div>
 
 
 <!-- Custom CSS -->
