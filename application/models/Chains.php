9324e03415e4a5d1ca4891d767c39f2a2789e987
shervinesta <shervin@mench.com>
2025-10-26 18:21:49 -0700

    Ideachain V1.0

--- application/models/Chains.php
+++ application/models/Chains.php
@@ -1512,11 +1512,15 @@ class Chains extends CIdea_cache
         foreach ($all_next as $next_post) {
             $next_post['current_level'] = ($current_level+1);
             $next_post['next_posts'] = array();
+
             if(!in_array(intval($next_post['postid']), $top_ids)){
                 array_push($top_ids, intval($next_post['postid']));
-                foreach($this->Chains->post_json($next_post, $top_ids, ($current_level+1)) as $tree_post){
-                    if(!in_array(intval($tree_post['postid']), $top_ids)){
-                        array_push($next_post['next_posts'], $tree_post);
+                $tree_results = $this->Chains->post_json($next_post, $top_ids, ($current_level+1));
+                if(isset($tree_results['next_posts']) && is_array($tree_results['next_posts'])){
+                    foreach($tree_results['next_posts'] as $tree_post){
+                        if(!in_array(intval($tree_post['postid']), $top_ids)){
+                            array_push($next_post['next_posts'], $tree_post);
+                        }
                     }
                 }
                 array_push($i['next_posts'], $next_post);
