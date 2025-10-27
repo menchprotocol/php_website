0a221b8d9a06f314b39955100ea0ef3e246302a5
shervinesta <shervin@mench.com>
2025-10-26 19:49:47 -0700

    Ideachain V1.0

--- application/views/slide.php
+++ application/views/slide.php
@@ -3,8 +3,6 @@
 
 $post_tree = json_decode(file_get_contents('https://mench.com/json/'.$focus_post['posthashtag']));
 
-print_r($post_tree['poststats']);
-
 ?>
 
 <style>
@@ -18,3 +16,4 @@ print_r($post_tree['poststats']);
 
 <!-- HTML HERE -->
 <div>Hello Slide</div>
+<div><?= print_r($post_tree['poststats']); ?></div>
