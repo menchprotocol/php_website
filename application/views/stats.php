142362282c52a6fd647580b20aebff4c43927924
shervinesta <shervin@mench.com>
2025-10-30 16:48:00 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -109,7 +109,7 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
 
                     //Print Menu:
                     echo '<tr class="mobile-shrink grey">';
-                    echo '<td style="text-align: center;"><a href="' . view_memory(42903, 42902) . $users___3470452[$last_group_displayed]['m__handle'] . '"><span class="icon-block-sm">' . $users___3470452[$last_group_displayed]['m__cover'] . '</span><span class="dotted_under" data-toggle="tooltip" data-placement="top" title="' . $users___3470452[$last_group_displayed]['m__message'] . '">' . $users___3470452[$last_group_displayed]['m__name'] . '</span></a> <a href="' . view_app_chain(3445693) . '?chainusertype=' . join(',', $this->config->item('userids___'.$last_group_displayed)) . '&chainvoid=0'  . '" class="card_count_' . $focus_chain_group . '"><i class="fas fa-yin-yang fa-spin"></i></a>:</td>';
+                    echo '<td style="text-align: center;"><a href="' . view_memory(42903, 42902) . $users___3470452[$last_group_displayed]['m__handle'] . '"><span class="icon-block-sm">' . $users___3470452[$last_group_displayed]['m__cover'] . '</span><a href="' . view_app_chain(3445693) . '?chainusertype=' . join(',', $this->config->item('userids___'.$last_group_displayed)) . '&chainvoid=0'  . '" class="card_count_' . $focus_chain_group . '"><i class="fas fa-yin-yang fa-spin"></i></a> <span class="dotted_under" data-toggle="tooltip" data-placement="top" title="' . $users___3470452[$last_group_displayed]['m__message'] . '">' . $users___3470452[$last_group_displayed]['m__name'] . '</span></a>:</td>';
                     echo '</tr>';
 
                 }
