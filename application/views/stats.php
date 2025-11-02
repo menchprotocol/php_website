6f53027f071fb5f82c747e6fe7a3144909fa281f
shervinesta <shervin@mench.com>
2025-11-01 20:55:11 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -29,14 +29,14 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
 
     if ($userid1 == 1309754) {
         //Voided
-        echo '<div class="mid-text-line compact-midline voidstats grey"><span><a href="' . view_app_chain(3445693) . '?chainvoid=1" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '<a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span></span></div>';
+        echo '<div class="mid-text-line compact-midline voidstats grey"><span><a href="' . view_app_chain(3445693) . '?chainvoid=1" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '</span><a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span></div>';
         continue;
     } elseif ($userid1 == 28956) {
         //Nodes
-        echo '<div class="mid-text-line compact-midline"><span class="grey"><a href="' . view_app_chain(3445693) . '?chainusertype=12273,12274&chainvoid=0" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span  data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '<a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span>:</span></div>';
+        echo '<div class="mid-text-line compact-midline"><span class="grey"><a href="' . view_app_chain(3445693) . '?chainusertype=12273,12274&chainvoid=0" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span  data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '</span> <a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a>:</span></div>';
     } elseif ($userid1 == 31770) {
         //Links
-        echo '<div class="mid-text-line compact-midline"><span class="grey"><a target="_blank" href="' . view_app_chain(3445693) . '?chainusertype=' . join(',', $this->config->item('userids___2123863')) . '&chainvoid=0" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span  data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '<a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span>:</span></div>';
+        echo '<div class="mid-text-line compact-midline"><span class="grey"><a target="_blank" href="' . view_app_chain(3445693) . '?chainusertype=' . join(',', $this->config->item('userids___2123863')) . '&chainvoid=0" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span  data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '</span> <a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a>:</span></div>';
     }
 
     echo '<div class="row justify-content list-covers">';
