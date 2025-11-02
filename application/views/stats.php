47dbe40c72e9642ce43c9c28a05c2cc5e1bebef1
shervinesta <shervin@mench.com>
2025-11-01 22:58:06 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -186,5 +186,6 @@ echo '<div class="center hidden" style="padding-top: 13px;"><a href="javascript:
 </script>
 <style>
     .main__title { font-size:1.1em; }
-    .icon-block { height: 28px !important; }
+    i { font-size:0.9em; }
+    .icon-block { height: 21px !important; }
 </style>
