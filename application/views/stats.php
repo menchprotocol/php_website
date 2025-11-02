b860fff9858d451309edf9e421a3338e5ba3b029
shervinesta <shervin@mench.com>
2025-11-01 21:44:41 -0700

    Ideachain V1.0

--- application/views/stats.php
+++ application/views/stats.php
@@ -28,7 +28,7 @@ foreach ($this->config->item('users___33292') as $userid1 => $m1) {
 
     if ($userid1 == 1309754) {
         //Voided
-        echo '<div class="mid-text-line compact-midline voidstats grey"><span><a href="' . view_app_chain(3445693) . '?chainvoid=1" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '</span><a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span></div>';
+        echo '<div class="mid-text-line compact-midline voidstats"><span><a href="' . view_app_chain(3445693) . '?chainvoid=1" class="card_count_' . $userid1 . '"><i class="fas fa-yin-yang fa-spin"></i></a><span data-toggle="tooltip" data-placement="top" title="' . $m1['m__message'] . '">' . $m1['m__cover'] . '</span><a href="/@' . $m1['m__handle'] . '">' . $m1['m__name'] . '</a></span></div>';
         continue;
     } elseif ($userid1 == 28956) {
         //Nodes
