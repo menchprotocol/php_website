1aa5e58cba55b44b29fd17518b0d827a85efcfdc
shervinesta <shervin@mench.com>
2026-01-04 21:47:46 -0800

    Ideachain V1.0

--- application/views/communities.php
+++ application/views/communities.php
@@ -34,7 +34,7 @@ foreach ((isset($_GET['userhandle']) && strlen($_GET['userhandle']) ? $this->Use
 
 if (strlen($community_pills)) {
 
-    //Community:
+    //Community of users:
     echo '<h2 class="center">' . $user_item['username'] . '</h2>';
     echo '<ul class="nav nav-tabs nav12274"></ul>';
     echo $community_pills;
