45f262687c3027bdd3a500971f9d52b19b9f5f77
shervinesta <shervin@mench.com>
2026-01-10 21:48:15 -0800

    prompts

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -582,9 +582,9 @@ if (!$basic_header_footer) {
     echo (strlen($domain_cover) ? '<a href="' . view_memory(42903, 14565) . '" class="sidebar-logo-link">' . view_cover($domain_logo) . '</a>' : '');
     echo '</div>';
     echo '<div class="sidebar-menu-items">';
-    echo '<a href="#" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="fas fa-home"></i></span><span class="sidebar-menu-text">For You</span></a>';
+    echo '<a href="/" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="fas fa-home"></i></span><span class="sidebar-menu-text">Home</span></a>';
     echo '<a href="javascript:void(0);" class="sidebar-menu-item" onclick="toggle_finder()"><span class="sidebar-menu-icon"><i class="fas fa-search"></i></span><span class="sidebar-menu-text">Search</span></a>';
-    echo '<a href="' . view_memory(42903, 6287) . '" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="fas fa-th-large"></i></span><span class="sidebar-menu-text">Apps</span></a>';
+    echo '<a href="/apps" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="fas fa-forward-slash"></i></span><span class="sidebar-menu-text">Apps</span></a>';
     echo '<a href="javascript:void(0);" class="sidebar-menu-item" onclick="post_edit_start()"><span class="sidebar-menu-icon"><i class="fas fa-plus"></i></span><span class="sidebar-menu-text">New Prompt</span></a>';
     echo '</div>';
     echo '</nav>';
