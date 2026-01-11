3de2b50ee99624d22197a9f843fe4fa9d62ad4a8
shervinesta <shervin@mench.com>
2026-01-11 03:43:59 -0800

    prompts

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -344,6 +344,8 @@ if(!$basic_header_footer){
         .fixed-top.container,
         .fixed-top.top-header-position.container {
             left: calc(250px + (100vw - 250px - 610px) / 2) !important;
+            width: 610px !important;
+            max-width: 610px !important;
             transform: translateX(0) !important;
         }
     }
