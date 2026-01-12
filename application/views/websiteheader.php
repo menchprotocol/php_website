e41436dc53cc95f99cd3d7c38f5b2a7e9afa6372
shervinesta <shervin@mench.com>
2026-01-11 19:54:26 -0800

    prompts

--- application/views/websiteheader.php
+++ application/views/websiteheader.php
@@ -512,6 +512,7 @@ if(!$basic_header_footer){
         background-color: #000000 !important;
         color: #ffffff !important;
         font-weight: 600;
+        border: 1px solid #ffffff !important;
     }
     
     .sidebar-menu-item-primary:hover {
@@ -653,9 +654,9 @@ if (!$basic_header_footer) {
     echo '<div class="sidebar-menu-items">';
     echo '<a href="/" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="fas fa-home"></i></span><span class="sidebar-menu-text">Home</span></a>';
     echo '<a href="javascript:void(0);" class="sidebar-menu-item" onclick="toggle_finder()"><span class="sidebar-menu-icon"><i class="fas fa-search"></i></span><span class="sidebar-menu-text">Search</span></a>';
-    echo '<a href="javascript:void(0);" class="sidebar-menu-item sidebar-menu-item-primary" onclick="post_edit_start()"><span class="sidebar-menu-icon"><i class="fas fa-plus"></i></span><span class="sidebar-menu-text">Prompt</span></a>';
     echo '<a href="/messages" class="sidebar-menu-item sidebar-menu-item-with-badge"><span class="sidebar-menu-icon"><i class="fas fa-paper-plane fa-sharp"></i><span class="sidebar-menu-badge">345</span></span><span class="sidebar-menu-text">Messages</span></a>';
     echo '<a href="/apps" class="sidebar-menu-item"><span class="sidebar-menu-icon"><i class="far fa-slash-forward fa-sharp"></i></span><span class="sidebar-menu-text">Apps</span></a>';
+    echo '<a href="javascript:void(0);" class="sidebar-menu-item sidebar-menu-item-primary" onclick="post_edit_start()"><span class="sidebar-menu-icon"><i class="fas fa-plus"></i></span><span class="sidebar-menu-text">Prompt</span></a>';
     echo '</div>';
     echo '</nav>';
 }
