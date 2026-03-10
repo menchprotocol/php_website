6c5bbcf3aaa034189e5bbfef6492b94008dc7f71
shervinesta <shervin@mench.com>
2026-03-10 14:12:44 -0700

    Mench V1.0

--- application/views/doc.php
+++ application/views/doc.php
@@ -4,5 +4,8 @@ $user_session = user_session();
 
 //Post Tree:
 echo '<div class="row justify-content">';
-view_tree($this->Chains->post_json($focus_post), true, $user_session);
+
+print_r($this->Chains->post_json($focus_post));
+
+//view_tree($this->Chains->post_json($focus_post), true, $user_session);
 echo '</div>';
