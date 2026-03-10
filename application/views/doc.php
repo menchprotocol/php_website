3720c56f8516337e08e238ab6a66703195c5ddf2
shervinesta <shervin@mench.com>
2026-03-10 12:38:52 -0700

    Ideachain V1.0

--- application/views/doc.php
+++ application/views/doc.php
@@ -4,5 +4,6 @@ $user_session = user_session();
 
 //Post Tree:
 echo '<div class="row justify-content">';
+echo 'Testing agent:';
 view_tree($this->Chains->post_json($focus_post), true, $user_session);
 echo '</div>';
