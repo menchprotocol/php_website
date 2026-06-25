6ceff211e435cd68e59c736bbf23e784c3383fa7
shervinesta <shervin@mench.com>
2026-06-25 05:12:34 +0100

    Discotique Mass Message

--- application/views/messenger.php
+++ application/views/messenger.php
@@ -7,13 +7,13 @@ foreach ($this->Posts->read(array(
     echo $i['postmessageraw']."<hr />";
 
     //Now let's see who will receive this:
-    $post_settings = post_settings($i['posthashtag']);
+    $post_settings = post_settings($i['posthashtag'], true);
 
     $total_sent = $this->Chains->broadcast($post_settings['query_string_filtered'], $i, 0, true, false);
 
     echo view_post_title($i) . ' Sent ' . $total_sent . ' Messages to ' . count($post_settings['query_string_filtered']) . ' Members<hr />';
 
-    echo print_r($post_settings, true);
+    echo print_r($post_settings['query_string_filtered'], true);
 
     //Ready to be done:
     $this->Chains->post_discovered(($total_sent > 0 ? 1309378 /* Post Trigerred */ : 31022 /* Post Skipped */), 26582, 0, $i);
