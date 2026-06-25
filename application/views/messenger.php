b38c7fab53ec8a71b422da6322fdfef61c6bde21
shervinesta <shervin@mench.com>
2026-06-25 05:15:54 +0100

    Discotique Mass Message

--- application/views/messenger.php
+++ application/views/messenger.php
@@ -13,7 +13,7 @@ foreach ($this->Posts->read(array(
 
     echo view_post_title($i) . ' Sent ' . $total_sent . ' Messages to ' . count($post_settings['query_string_filtered']) . ' Members<hr />';
 
-    echo str_replace('  ','    ',nl2br(print_r($post_settings['query_string_filtered'], true)));
+    echo str_replace("\t",'&nbsp;&nbsp;&nbsp;&nbsp;',nl2br(print_r($post_settings['query_string_filtered'], true)));
 
     //Ready to be done:
     $this->Chains->post_discovered(($total_sent > 0 ? 1309378 /* Post Trigerred */ : 31022 /* Post Skipped */), 26582, 0, $i);
