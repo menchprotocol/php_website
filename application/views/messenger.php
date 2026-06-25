6e68595b4f46d2b54539edaaa8d8a9a3f7890443
shervinesta <shervin@mench.com>
2026-06-25 05:07:03 +0100

    Discotique Mass Message

--- application/views/messenger.php
+++ application/views/messenger.php
@@ -16,54 +16,15 @@ foreach ($this->Posts->read(array(
        continue;
     }
 
-    $end_sending = true;
-    /*
-    //Determine if it's time to send this message:
-    $time_starts = 0;
-    foreach ($this->Chains->read(array(
-        'chainusertype IN (' . join(',', $this->config->item('userids___42991')) . ')' => null, //Active Writes
-        'chainpostinput' => $i['postid'],
-        'chainuserinput' => 43743, //Sending Starts
-    )) as $time) {
-        $time_starts = strtotime($time['chainvalue']);
-        break;
-    }
-
-    if ($time_starts > 0 && $time_starts > time()) {
-        //Still not time, go next:
-        continue;
-    }
-
-    //Does it have an end time?
-    $end_sending = 0;
-    foreach ($this->Chains->read(array(
-        'chainusertype IN (' . join(',', $this->config->item('userids___42991')) . ')' => null, //Active Writes
-        'chainpostinput' => $i['postid'],
-        'chainuserinput' => 43744, //Sending Ends
-    )) as $time) {
-        $end_sending = strtotime($time['chainvalue']);
-        break;
-    }
-    $end_sending = (!$end_sending || $end_sending < time());
-    */
-
     //Now let's see who will receive this:
-    $demo_only = false;
     $post_settings = post_settings($i['posthashtag']);
-    $total_sent = $this->Chains->broadcast($post_settings['query_string_filtered'], $i, $i['chainuserdomain'], true, $demo_only);
+
+    $total_sent = $this->Chains->broadcast($post_settings['query_string_filtered'], $i, 0, true, false);
 
     echo view_post_title($i) . ' Sent ' . $total_sent . ' Messages to ' . count($post_settings['query_string_filtered']) . ' Members<hr />';
 
-    //Mark this as complete?
-    if (!$demo_only && $end_sending) {
-
-        //Ready to be done:
-        $this->Chains->post_discovered(($total_sent > 0 ? 1309378 /* Post Trigerred */ : 31022 /* Post Skipped */), 26582, 0, $i);
-
-    } else {
-        echo 'Nothing to send';
-    }
-
+    //Ready to be done:
+    $this->Chains->post_discovered(($total_sent > 0 ? 1309378 /* Post Trigerred */ : 31022 /* Post Skipped */), 26582, 0, $i);
 
 }
 
